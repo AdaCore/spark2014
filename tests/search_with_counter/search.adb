c8d3af0779e9636d67bfc02dad1f4aa442c570d3
Yannick Moy <moy@adacore.com>
2010-02-25 17:46:10 +0100

    Add test with global variable reads/writes

---
+++ tests/search_with_counter/search.adb
@@ -0,0 +1,32 @@
+package body Search is
+
+  procedure Linear_Search
+    (Table : in IntArray;
+     Value : in Integer;
+     Found : out Boolean;
+     Index : out Integer) is
+  begin
+     Found := False;
+     Index := 0;
+
+     for I in Integer range Table'Range loop
+        pragma Assert (Found = False);
+        if Table(I) = Value then
+           Counter := Counter + 1;
+           Found := True;
+           Index := I;
+           pragma Assert (Found and then Index = I);
+           exit;
+        end if;
+     end loop;
+  end Linear_Search;
+
+  function Check_Index
+    (Table : IntArray;
+     Value : Integer;
+     Index : Integer) return Boolean is
+  begin
+     return Table(Index) = Value;
+  end Check_Index;
+
+end Search;
---
+++ tests/search_with_counter/search.ads
@@ -0,0 +1,24 @@
+package Search is
+
+  type IntArray is array (Integer range <>) of Integer;
+
+  Counter : Natural := 0;
+
+  procedure Linear_Search
+    (Table : in IntArray;
+     Value : in Integer;
+     Found : out Boolean;
+     Index : out Integer);
+  pragma Precondition (Counter < Integer'Last);
+  pragma Postcondition (not Found or else
+                          ((Table(Index) = Value) and then
+                           Counter = Counter'Old + 1));
+
+  function Check_Index
+    (Table : IntArray;
+     Value : Integer;
+     Index : Integer) return Boolean;
+  pragma Precondition (Table'First <= Index and then Index <= Table'Last);
+  pragma Postcondition (Check_Index'Result = (Table(Index) = Value));
+
+end Search;
