f58eed1532f5fc4905fbb7f264f0dd3bdcdde4dc
Jerome Guitton <guitton@adacore.com>
2010-03-01 18:38:20 +0100

    Add new testcase to check that implicit loop invariants are generated.

---
+++ tests/implicit_invariant/test.cmd
@@ -0,0 +1,10 @@
+sparkify -w -q s.ads s.adb
+cd sparkified
+echo "------------------------------------------------------------------------"
+cat s.ads s.adb
+echo "------------------------------------------------------------------------"
+sparkmake > dummy.log
+spark -noecho -flow=data -config=../../standard.ads -vcg @spark
+sparksimp >> dummy.log
+pogs -o=s.sum >> dummy.log
+tail -11 s.sum
