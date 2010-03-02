7951898cb16e3f313de121b62abc3c6eb9f904cf
Olivier Ramonat <olivier@ramonat.fr>
2010-03-02 10:17:05 +0100

    Use TEST_SUPPORT variable

--- tests/while_loop_invariant/test.cmd
+++ tests/while_loop_invariant/test.cmd
@@ -4,7 +4,7 @@ echo "------------------------------------------------------------------------"
 cat s.ads s.adb
 echo "------------------------------------------------------------------------"
 sparkmake > dummy.log
-spark -noecho -flow=data -config=../../standard.ads -vcg @spark
+spark -noecho -flow=data -config=$TEST_SUPPORT/standard.ads -vcg @spark
 sparksimp > dummy.log
 pogs -o=s.sum > dummy.log
 tail -11 s.sum
