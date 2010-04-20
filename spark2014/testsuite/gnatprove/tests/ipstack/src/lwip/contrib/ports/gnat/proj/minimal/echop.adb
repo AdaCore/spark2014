a2b196f755d0dbaae6c86470a9b3f39965e50751
gingold <gingold@adacore.com>
2010-04-20 15:19:05 +0000

    Remove Memory. Part of J215-016.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/ipstack@159220 936e1b1b-40f2-da11-902a-00137254ae57

--- spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/gnat/proj/minimal/echop.adb
+++ spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/gnat/proj/minimal/echop.adb
@@ -3,6 +3,6 @@
 --             Copyright (C) 2010, Free Software Foundation, Inc.           --
 ------------------------------------------------------------------------------
 
-with Assert, Mainloop, Memory, Memory_Compare, Memory_Copy, Memory_Set,
+with Assert, Mainloop, Memory_Compare, Memory_Copy, Memory_Set,
   Strings, PPC_Clock, Timers, Time_Types;
 procedure Echop is begin null; end Echop;
