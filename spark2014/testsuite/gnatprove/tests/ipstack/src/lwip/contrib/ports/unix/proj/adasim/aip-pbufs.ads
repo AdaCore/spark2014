7c1bf6a41fef82e85547c6baa29316294ffd2004
hainque <hainque@adacore.com>
2010-04-21 17:07:32 +0000

    A few extra comments on pbufs for J215-016 (ipstack, technical work).
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/ipstack@159261 936e1b1b-40f2-da11-902a-00137254ae57

--- spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/unix/proj/adasim/aip-pbufs.ads
+++ spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/unix/proj/adasim/aip-pbufs.ads
@@ -3,7 +3,7 @@
 --             Copyright (C) 2010, Free Software Foundation, Inc.           --
 ------------------------------------------------------------------------------
 
--- Packet Buffer (network data containers) services.
+-- Packet Buffer (network packet data containers) services.
 
 --# inherit AIP;
 
@@ -12,9 +12,21 @@ package AIP.Pbufs is
    subtype Pbuf_Id is AIP.IPTR_T;
    NOPBUF : constant Pbuf_Id := AIP.NULID;
 
+   --  Network packet data is held in buffers, chained as required when
+   --  several buffers are needed for a single packet. Chaining capabilities
+   --  are very useful to allow storage of large data blocks in possibly
+   --  smaller buffers allocated in static pools. They also offer convenient
+   --  incremental packet construction possibilities, such as chaining
+   --  initially separate buffers to make up a single packet, prepending info
+   --  ahead exsiting data, ...
+
    ---------------------------------
    -- Pbuf allocation and release --
    ---------------------------------
+
+   --  Pbufs may be declared as intended to hold data pertaining to some stack
+   --  layer, which causes allocation of extra room for the necessary protocol
+   --  headers.
 
    type Pbuf_Layer is
      (TRANSPORT_PBUF,
@@ -23,11 +35,24 @@ package AIP.Pbufs is
       RAW_PBUF);
    pragma Convention (C, Pbuf_Layer);
 
+   --  Pbufs always materialize as a least control structure and can be used
+   --  to hold or designate different kinds of data locations.
+
    type Pbuf_Kind is
      (RAM_PBUF,
+      --  Pbuf struct and data (protocol headers included) are allocated as
+      --  one single chunk of RAM.
+
       ROM_PBUF,
       REF_PBUF,
-      POOL_PBUF);
+      --  A Pbuf struct alone gets allocated. The data (payload) it "holds"
+      --  is a reference that needs to be attached explicitely before use.
+
+      POOL_PBUF
+      --  Pbuf struct and data are both allocated from a static pool. A chain
+      --  is constructed if a single pbuf is not big enough for the intended
+      --  packet size.
+     );
    pragma Convention (C, Pbuf_Kind);
 
    function Pbuf_Alloc
@@ -48,19 +73,27 @@ package AIP.Pbufs is
 
    function Pbuf_Tlen (Pb : Pbuf_Id) return AIP.U16_T;
    pragma Import (C, Pbuf_Tlen, "pbuf_tot_len_w");
+   --  Amount of packet data held from pbuf PB to the end of the chain it
+   --  participates in.
 
    function Pbuf_Len (Pb : Pbuf_Id) return AIP.U16_T;
    pragma Import (C, Pbuf_Len, "pbuf_len_w");
+   --  Amount of data held in pbuf PB. Len = Tlen means PB is the last
+   --  buffer in a chain.
 
    function Pbuf_Next (Pb : Pbuf_Id) return Pbuf_Id;
    pragma Import (C, Pbuf_Next, "pbuf_next_w");
+   --  Pbuf following PB in a chain. Note that full *packets*, not only
+   --  packet *buffers* may be chained together, so the len/tlen check is
+   --  the only proper way to detect the end of a packet.
 
    function Pbuf_Payload (Pb : Pbuf_Id) return AIP.IPTR_T;
    pragma Import (C, Pbuf_Payload, "pbuf_payload_w");
+   --  Pointer to Data held or referenced by pbuf PB.
 
-   -----------------------------------
-   -- Pbuf characteristic accessors --
-   -----------------------------------
+   ---------------------
+   -- Pbuf operations --
+   ---------------------
 
    procedure Pbuf_Cat (Head : Pbuf_Id; Tail : Pbuf_Id);
    pragma Import (C, Pbuf_Cat, "pbuf_cat");
