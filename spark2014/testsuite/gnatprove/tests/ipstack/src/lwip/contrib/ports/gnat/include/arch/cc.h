2b55f181818af6882171cbe2bee3698b0df2c06a
quinot <quinot@adacore.com>
2010-04-26 14:59:39 +0000

    Minor fixes for J215-016
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/ipstack@159406 936e1b1b-40f2-da11-902a-00137254ae57

--- spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/gnat/include/arch/cc.h
+++ spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/gnat/include/arch/cc.h
@@ -1,8 +1,9 @@
 /*
  * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
- * All rights reserved. 
- * 
- * Redistribution and use in source and binary forms, with or without modification, 
+ * Copyright (C) 2010, AdaCore
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice,
@@ -11,21 +12,21 @@
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission. 
+ *    derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
- * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
  * OF SUCH DAMAGE.
  *
  * This file is part of the lwIP TCP/IP stack.
- * 
+ *
  * Author: Adam Dunkels <adam@sics.se>
  *
  */
@@ -38,9 +39,9 @@
 #include <sys/time.h>
 #endif
 
-/* Define platform endianness */
+/* Define platform endianness (PPC: big endian) */
 #ifndef BYTE_ORDER
-#define BYTE_ORDER LITTLE_ENDIAN
+#define BYTE_ORDER BIG_ENDIAN
 #endif /* BYTE_ORDER */
 
 /* Define generic types used in lwIP */
@@ -67,15 +68,19 @@ typedef u32_t mem_ptr_t;
 #define PACK_STRUCT_BEGIN
 #define PACK_STRUCT_END
 
-#if 0
 /* prototypes for printf() and abort() */
+#if 0
 #include <stdio.h>
 #include <stdlib.h>
+#else
+int printf(const char *format, ...);
+void abort (void);
+#endif
+
 /* Plaform specific diagnostic output */
 #define LWIP_PLATFORM_DIAG(x)	do {printf x;} while(0)
 
 #define LWIP_PLATFORM_ASSERT(x) do {printf("Assertion \"%s\" failed at line %d in %s\n", \
-                                     x, __LINE__, __FILE__); fflush(NULL); abort();} while(0)
-#endif
-				     
+                                     x, __LINE__, __FILE__); abort();} while(0)
+				
 #endif /* __ARCH_CC_H__ */
--- spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/gnat/include/string.h
+++ spark2014/testsuite/gnatprove/tests/ipstack/src/lwip/contrib/ports/gnat/include/string.h
@@ -1,2 +1,18 @@
-#define memset __builtin_memset
+/*******************************
+ *            IPStack          *
+ *******************************
+ * Copyright (C) 2010, AdaCore *
+ *******************************/
+
+#ifndef __STRING_H__
+#define __STRING_H__
+
+typedef unsigned int size_t;
+extern void *memset(void *s, int c, size_t n);
+extern void *memcpy(void *dest, const void *src, size_t n);
+extern void *strncpy(void *dest, const void *src, size_t n);
+extern size_t strlen (char*);
+
 #define NULL ((void*)0)
+
+#endif
