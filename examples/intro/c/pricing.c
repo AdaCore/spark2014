4ce266586c8a894d4ee51f8c7bd0b1399dfa5298
Yannick Moy <moy@adacore.com>
2010-05-10 16:39:05 +0200

    Update ACSL annotations because one-line comments cannot be used

--- examples/intro/c/pricing.c
+++ examples/intro/c/pricing.c
@@ -7,8 +7,9 @@ int price_of_item (Item it) {
 int price_of_basket (Basket bk, int len) {
    int total = 0;
    int it;
-   //@ loop invariant \forall int k; 0 <= k < it ==> 
-   //@                   total >= price_of_item (&bk[it]);
+   /*@ loop invariant \forall int k; 0 <= k < it ==> 
+                         total >= price_of_item (&bk[it]);
+    */
    for (it = 0; it < len; it++) {
       total = add (total, price_of_item (bk[it]));
    }
--- examples/intro/c/pricing.h
+++ examples/intro/c/pricing.h
@@ -5,25 +5,31 @@ typedef struct _Item {
    int number;
 } Item;
 
-//@ predicate type_item{S} (Item* it) =
-//@    0 <= it->price <= 10000 && 0 <= it->number <= 10000;
+/*@ predicate type_item{S} (Item* it) =
+       0 <= it->price <= 10000 && 0 <= it->number <= 10000;
+ */
 
-//@ logic integer mult (integer x, integer y) = 
-//@    x * y <= 10000 ? x * y : 10000;
+/*@ logic integer mult (integer x, integer y) = 
+       x * y <= 10000 ? x * y : 10000;
+ */
 
-//@ requires type_item (&it);
-//@ ensures \result == mult (it.price, it.number);
+/*@ requires type_item (&it);
+    ensures \result == mult (it.price, it.number);
+ */
 int price_of_item (Item it);
 
-//@ logic integer price_of_item{S} (Item* it) = 
-//@    mult (it->price, it->number);
+/*@ logic integer price_of_item{S} (Item* it) = 
+       mult (it->price, it->number);
+ */
 
 typedef Item* Basket;
 
-//@ predicate type_basket{S} (Basket bk, integer len) =
-//@    \forall int it; 0 <= it < len ==> type_item (&bk[it]);
+/*@ predicate type_basket{S} (Basket bk, integer len) =
+       \forall int it; 0 <= it < len ==> type_item (&bk[it]);
+ */
 
-//@ requires \valid_range(bk, 0, len - 1) && type_basket (bk, len);
-//@ ensures \forall int it; 0 <= it < len ==> 
-//@                            \result >= price_of_item (&bk[it]);
+/*@ requires \valid_range(bk, 0, len - 1) && type_basket (bk, len);
+    ensures \forall int it; 0 <= it < len ==> 
+                               \result >= price_of_item (&bk[it]);
+ */
 int price_of_basket (Basket bk, int len);
