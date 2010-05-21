625f0b46a704a703449599d627adbaaab1c777bd
Marc Sango <sango@adacore.com>
2010-05-21 17:46:37 +0200

    Test of component declaration with anonymous type

--- tests/anonymous_type_2/anon_type.adb
+++ tests/anonymous_type_2/anon_type.adb
@@ -1,13 +1,18 @@
 package body Anon_Type
 is
-   procedure exchange(A1 : out Array1; A2 : out Array2)
+   procedure exchange(A1 : out Array1; A2 : out Array2;
+                     A3 : out Array3; A4 : out Array4)
    is
    begin
-        A1 := (0,1,2,3,4);
-        A2 := (5,6,7,8,9);
-          for I in Integer range 1 .. 9 loop
-             A2(I) := A1(I);
-          end loop;
-	null;
+        A1 := (0,0,0,0,0);
+        A2 := (1,1,1,1,1);
+        A3 := (0,1,2,3,4,5,6,7,8,9);
+        A4 := (9,8,7,6,5,4,3,2,1,0);
+      for I in 1 .. 9 loop
+         A2(I) := A1(I);
+      end loop;
+      for I in Array3'Range loop
+         A4(I) := A3(I);
+      end loop;
    end exchange;
 end Anon_Type;
--- tests/anonymous_type_2/anon_type.ads
+++ tests/anonymous_type_2/anon_type.ads
@@ -8,7 +8,11 @@ is
    subtype Value is Integer range 0 .. 9;
    type Array1 is array (Index) of Value;
    type Array2 is array (Integer range 5 .. 9) of Integer range 0 .. 9;
-   type Array3 is array (Integer range <>) of Integer;
-   procedure exchange(A1 : out Array1; A2 : out Array2);
+   Size : constant Integer :=10;
+   type Array3 is array(1 .. Size) of Value;
+   type Array4 is array (1 .. 10) of Value;
+
+   procedure exchange(A1 : out Array1; A2 : out Array2;
+                     A3 : out Array3; A4 : out Array4);
 end Anon_Type;
 
