5217f413ba49320e200ce76eaf3c424257737c8b
Marc Sango <sango@adacore.com>
2010-05-21 17:45:46 +0200

    Test of object declaration with anonymous type

--- tests/anonymous_type_1/anon_type.ads
+++ tests/anonymous_type_1/anon_type.ads
@@ -1,7 +1,8 @@
--------------------------------------------------------------
---  Test the anonymous type in definition of simple variables
+-----------------------------------------------------------------------------
+--  Examples for fix the anonymous type in object declarations or definitions
 
 package Anon_Type is
+   type Matrix     is array(Integer  range <>, Integer range <>) of Integer;
     Function Increment (Var_In : in Integer) return Integer;
 end Anon_Type;
 
