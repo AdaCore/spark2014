363b042d955fc3eb33eb8fe91df239c5b7cc8c01
Jerome Guitton <guitton@adacore.com>
2010-06-07 18:51:40 +0200

    Add modular types.
    Part of J526-010.

--- gnat_backends/design/why/ch3.5.4__integer_types/test.cmd
+++ gnat_backends/design/why/ch3.5.4__integer_types/test.cmd
@@ -1,3 +1,12 @@
+rm -rf why_out
+mkdir why_out
+
+# signed integer types
 gcc -c -gnatc signed.ads
 why --dir why_out --alt-ergo signed.why signed_test.why
 alt-ergo why_out/signed_test_why.why
+
+# modular types
+gcc -c -gnatc modular.ads
+why --dir why_out --alt-ergo modular.why modular_test.why
+alt-ergo why_out/modular_test_why.why
