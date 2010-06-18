fcde97c3fb942b509ffc8a7016cdfa66cafb10e7
Yannick Moy <moy@adacore.com>
2010-06-18 16:25:38 +0200

    Update test to remove flow error

--- tests/anonymous_type_5/anon_type.adb
+++ tests/anonymous_type_5/anon_type.adb
@@ -1,9 +1,10 @@
 package body Anon_Type is
-   procedure To_Fix
+   procedure To_Fix (Y : out Integer)
    is
       T : Tab (0 .. 9);
       X : array (Integer range 0 .. 10) of Integer := (others => 0);
    begin
       T  := (others => 0);
+      Y := T (1) + X (1);
    end To_Fix;
 end Anon_Type;
