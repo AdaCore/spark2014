1e75d86259b6e564bdb9294584bd9c4b88d65dbe
Yannick Moy <moy@adacore.com>
2010-06-22 16:11:24 +0200

    Reduced test case for regression with globals in package body

---
+++ tests/global_rec/globals.ads
@@ -0,0 +1,11 @@
+package Globals is
+
+   function Get return Integer;
+
+   function Get_It return Integer;
+
+   procedure Set (X : Integer; Y : out Integer);
+
+   procedure Set_It (X : Integer; Y : out Integer);
+
+end;
