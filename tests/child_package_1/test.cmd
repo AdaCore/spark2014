eb20b4a7c7c8018d9d8270f7bd9a6347895018de
Yannick Moy <moy@adacore.com>
2010-07-02 16:00:23 +0200

    Update test to work with new generated packages

--- tests/child_package_1/test.cmd
+++ tests/child_package_1/test.cmd
@@ -8,6 +8,9 @@ echo "------------------------------------------------------------------------"
 cat *.ads *.adb
 echo "------------------------------------------------------------------------"
 sparkmake > dummy.log
+# start diff wrt common test driver, cf J618-015
+cp ../spark.smf .
+# end diff wrt common test driver
 spark -noecho -fdl=_fdl_ -flow=data -config=$TEST_SUPPORT/standard.ads -vcg @spark
 if [ $? -ge 2 ]; then
   echo "Examiner terminates in error"
