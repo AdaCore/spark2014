0f791db6647fe90fcf4691d5352246cab2271083
Jerome Guitton <guitton@adacore.com>
2010-07-06 20:35:15 +0200

    Variant record types
    Add initial support for "variant" record types with a short description
    and an example.
    
    Part of J526-010.

--- gnat_backends/design/why/ch3.8__record_types/test.cmd
+++ gnat_backends/design/why/ch3.8__record_types/test.cmd
@@ -5,3 +5,6 @@ gcc -c -gnatc rec.ads
 why --dir why_out --alt-ergo ./rec_test.why
 alt-ergo why_out/rec_test_why.why
 
+gcc -c -gnatc var.ads
+why --dir why_out --alt-ergo ./var_test.why
+alt-ergo why_out/var_test_why.why
