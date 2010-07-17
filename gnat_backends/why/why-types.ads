f318f711db52c555eda71fbb503585c9d971869f
Jerome Guitton <guitton@adacore.com>
2010-07-17 04:45:56 +0200

    Basic types of the Why backend
    Part of J719-024.

---
+++ gnat_backends/why/why-types.ads
@@ -0,0 +1,40 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GNAT2WHY COMPONENTS                           --
+--                                                                          --
+--                            W H Y - T Y P E S                             --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                       Copyright (C) 2010, AdaCore                        --
+--                                                                          --
+-- gnat2why is  free  software;  you can redistribute it and/or modify it   --
+-- under terms of the  GNU General Public License as published  by the Free --
+-- Software Foundation;  either version  2,  or  (at your option) any later --
+-- version. gnat2why is distributed in the hope that it will  be  useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-  --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details. You  should  have  received a copy of the GNU --
+-- General Public License  distributed with GNAT; see file COPYING. If not, --
+-- write to the Free Software Foundation,  51 Franklin Street, Fifth Floor, --
+-- Boston,                                                                  --
+--                                                                          --
+-- gnat2why is maintained by AdaCore (http://www.adacore.com)               --
+--                                                                          --
+------------------------------------------------------------------------------
+
+package Why.Types is
+   pragma Pure;
+
+   --  This package hierarchy provides basic types for Why syntax trees
+
+   type Int is range -2 ** 31 .. +2 ** 31 - 1;
+
+   type Why_Node_Id is new Int range 0 .. Int'Last;
+
+   type Why_Node_List is new Int range 0 .. Int'Last;
+
+   Why_Empty      : constant := 0;
+   Why_Empty_List : constant := 0;
+
+end Why.Types;
