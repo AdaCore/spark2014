8012d45bc80aaf7f64d5ab48755c352bd1a08b60
Yannick Moy <moy@adacore.com>
2010-09-16 00:54:31 +0200

    Add user rules for demo_1

--- tests/demo_1/test.cmd
+++ tests/demo_1/test.cmd
@@ -13,6 +13,10 @@ if [ $? -ge 2 ]; then
   echo "Examiner terminates in error"
   exit
 fi
+
+# Special rules added
+cp ../internal_asm_stack.rlu internal_asm_stack
+
 sparksimp > dummy.log
 pogs > dummy.log
 tail -11 *.sum
