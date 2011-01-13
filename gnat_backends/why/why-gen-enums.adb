69c04997f4010ca807bb6962b2eb00b52adfe9f7
Johannes Kanig <kanig@adacore.com>
2011-01-13 10:56:33 +0100

    Dealing with Enums by generating Why algebraic data types
    We generate
    * The type definition
    * a conversion logic from int (without definition)
    * a conversion function to int, defined by pattern matching
    * a coercion axiom

--- gnat_backends/why/why-gen-enums.adb
+++ gnat_backends/why/why-gen-enums.adb
@@ -23,10 +23,22 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
+with Uintp;              use Uintp;
+with Types;              use Types;
 with Why.Atree.Builders; use Why.Atree.Builders;
 with Why.Atree.Mutators; use Why.Atree.Mutators;
+with Why.Gen.Axioms;     use Why.Gen.Axioms;
+with Why.Gen.Funcs;      use Why.Gen.Funcs;
+with Why.Gen.Names;      use Why.Gen.Names;
+with Why.Gen.Preds;      use Why.Gen.Preds;
+with Why.Unchecked_Ids;  use Why.Unchecked_Ids;
 
 package body Why.Gen.Enums is
+
+   procedure Define_Enum_To_Int_Function (
+      File         : W_File_Id;
+      Name         : String;
+      Constructors : String_Lists.List);
 
    -----------------------------------
    -- Declare_Abstract_Boolean_Type --
@@ -40,6 +52,56 @@ package body Why.Gen.Enums is
       File_Append_To_Declarations (File, New_Logic_Declaration (Decl => T));
    end Declare_Abstract_Boolean_Type;
 
+   ---------------------------------
+   -- Define_Enum_To_Int_Function --
+   ---------------------------------
+
+   procedure Define_Enum_To_Int_Function
+      (File         : W_File_Id;
+       Name         : String;
+       Constructors : String_Lists.List)
+      --  define conversion function from enum type to integer
+      --  using pattern matching
+      --  ??? Not fully implemented yet
+   is
+      use String_Lists;
+      Arg_Name : constant String := "x";
+      Func  : constant W_Function_Unchecked_Id := New_Unchecked_Function;
+      Match : constant W_Matching_Term_Unchecked_Id :=
+         New_Unchecked_Matching_Term;
+      Cur : Cursor := First (Constructors);
+      Cnt : Uint := Uint_1;
+   begin
+      Function_Set_Name (Func, New_Conversion_To_Int (Name));
+      Function_Set_Return_Type (Func, New_Type_Int);
+      Function_Append_To_Binders (Func,
+         New_Logic_Binder
+            (Name => New_Identifier (Arg_Name),
+             Param_Type => New_Abstract_Type (Name)));
+      Matching_Term_Set_Term (Match, New_Term (Arg_Name));
+      while Has_Element (Cur) loop
+         declare
+            Result : constant W_Term_Id := New_Integer_Constant (Value => Cnt);
+            Pat    : constant W_Pattern_Id :=
+               New_Pattern (Constr => New_Identifier (Element (Cur)));
+         begin
+            Matching_Term_Append_To_Branches
+               (Match,
+                New_Match_Case (Pattern => Pat, Term => Result));
+            Cur := Next (Cur);
+            Cnt := Cnt + Uint_1;
+         end;
+      end loop;
+      Function_Set_Def (Func, Match);
+      File_Append_To_Declarations
+         (File,
+          New_Logic_Declaration (Decl => Func));
+   end Define_Enum_To_Int_Function;
+
+   -----------------------
+   -- Declare_Enum_Type --
+   -----------------------
+
    procedure Declare_Enum_Type
      (File         : W_File_Id;
       Name         : String;
@@ -51,6 +113,22 @@ package body Why.Gen.Enums is
         (File,
          New_Logic_Declaration
          (Decl => Declare_Enum_Type (Name, Constructors)));
+      Declare_Logic (File,
+                     New_Conversion_From_Int (Name),
+                     (1 => New_Type_Int),
+                     New_Abstract_Type (Name));
+      Define_Range_Predicate
+         (File,
+          Name,
+          First => Uint_1,
+          Last => UI_From_Int (Int (String_Lists.Length (Constructors))));
+      Define_Enum_To_Int_Function (File, Name, Constructors);
+      Define_Coerce_Axiom
+         (File,
+          New_Identifier (Name),
+          New_Type_Int,
+          New_Conversion_From_Int (Name),
+          New_Conversion_To_Int (Name));
    end Declare_Enum_Type;
 
 end Why.Gen.Enums;
