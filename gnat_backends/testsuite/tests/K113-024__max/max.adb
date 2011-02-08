54fc587788b471adaf380dc0a69a2e4292b69127
Johannes Kanig <kanig@adacore.com>
2011-02-08 15:14:19 +0100

    Make test Max work

--- gnat_backends/testsuite/tests/K113-024__max/max.adb
+++ gnat_backends/testsuite/tests/K113-024__max/max.adb
@@ -3,6 +3,7 @@ package body Max is
       Imax : Integer := T (1);
    begin
       for I in Integer range 2 .. 10 loop
+         Pragma Assert (T (1) <= Imax);
          if T (I) > Imax then
             Imax := T (I);
          end if;
