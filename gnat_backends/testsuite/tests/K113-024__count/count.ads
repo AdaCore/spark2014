84d73b70660c9a70d53d8094c4f09dbb9afe9dd2
Johannes Kanig <kanig@adacore.com>
2011-02-09 10:19:46 +0100

    Adapt tests to overflow checks
    * a new patch for why for labels
    * everything goes through except divide
    
    [TN: K113-013]

--- gnat_backends/testsuite/tests/K113-024__count/count.ads
+++ gnat_backends/testsuite/tests/K113-024__count/count.ads
@@ -1,5 +1,6 @@
 package Count is
    function Count (Max : Integer; Step : Integer) return Integer
-   with Pre => Step > 0 and Max >= 0,
+   with Pre =>
+      (Step > 0 and Max >= 0 and Max + Step < Integer'Last),
      Post => Count'Result > Max and Count'Result <= Max + Step;
 end Count;
