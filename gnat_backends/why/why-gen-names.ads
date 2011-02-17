3200fe2344833447f9f569ec61a6d0d02a915473
Johannes Kanig <kanig@adacore.com>
2011-02-17 12:57:32 +0100

    Alphabetic order in why-gen-names.ads

--- gnat_backends/why/why-gen-names.ads
+++ gnat_backends/why/why-gen-names.ads
@@ -30,12 +30,42 @@ package Why.Gen.Names is
    --  This package provides ways to manipulate subprogram names and
    --  to create identifiers from their string representation
 
-   function New_Definition_Name (Name : String) return String;
-   --  Create a new identifier for the "definition only" version of a
-   --  subprogram, which is not meant to be called.
+   function Allocator_Name (Name : String) return W_Identifier_Id;
+   --  From the name of an abstract type, return the name of
+   --  its allocator.
 
-   function New_Identifier (Name : String) return W_Identifier_Id;
-   --  Create a new identifier for Name and return the result
+   function Array_Access_Name (Name : String) return W_Identifier_Id;
+   --  From the name of an array type, return the name of its access function
+   --
+   function Array_Update_Name (Name : String) return W_Identifier_Id;
+   --  From the name of an array type, return the name of its update function
+
+   function Array_Accupd_Eq_Axiom (Name : String) return W_Identifier_Id;
+   --  From the name of an array type, return the name of the axiom of
+   --  access/update equality
+
+   function Coerce_Axiom (Name : String) return  W_Identifier_Id;
+   function Coerce_Axiom (Name : W_Identifier_Id) return  W_Identifier_Id;
+   --  From the name of an abstract type, return the name of
+   --  its coerce axiom.
+
+   function Eq_Param_Name (Name : String) return W_Identifier_Id;
+   function Eq_Param_Name (Name : W_Identifier_Id) return W_Identifier_Id;
+   --  From the name of an abstract type, return the name of
+   --  its equality parameter.
+
+   function Eq_Pred_Name (Name : String) return W_Identifier_Id;
+   function Eq_Pred_Name (Name : W_Identifier_Id) return W_Identifier_Id;
+   --  From the name of an abstract type, return the name of
+   --  its equality predicate.
+
+   function New_Bool_Int_Axiom (Rel : W_Relation) return W_Identifier_Id;
+   --  Return the name of the boolean comparison axiom for Why ints that
+   --  corresponds to the comparison operator in argument.
+
+   function New_Bool_Int_Cmp (Rel : W_Relation) return W_Identifier_Id;
+   --  Return the name of the boolean comparison operator for Why ints that
+   --  corresponds to the comparison operator in argument.
 
    function New_Conversion_Axiom (From : String; To : String)
       return W_Identifier_Id;
@@ -49,43 +79,32 @@ package Why.Gen.Names is
    --  Create a new identifier for a conversion from int to an abstract type.
    --  The name of the abstract type is given in parameter.
 
+   function New_Definition_Name (Name : String) return String;
+   --  Create a new identifier for the "definition only" version of a
+   --  subprogram, which is not meant to be called.
+
+   function New_Exit_Identifier return W_Identifier_Id;
+   --  Return an new identifier for the exception "Exit".
+
+   function New_Identifier (Name : String) return W_Identifier_Id;
+   --  Create a new identifier for Name and return the result
+
+   function New_Result_Identifier return W_Term_Identifier_Id;
+   --  Return an new identifier for a function result as it
+   --  would be used into a postcondition.
+
+   function New_Term (Name : String) return W_Term_Identifier_Id;
+   --  Return a term identified by the given name
+
    function Range_Pred_Name (Name : String) return W_Identifier_Id;
    function Range_Pred_Name (Name : W_Identifier_Id) return W_Identifier_Id;
    --  From the name of an abstract type, return the name of
    --  its range predicate.
 
-   function Eq_Param_Name (Name : String) return W_Identifier_Id;
-   function Eq_Param_Name (Name : W_Identifier_Id) return W_Identifier_Id;
-   --  From the name of an abstract type, return the name of
-   --  its equality parameter.
-
-   function Eq_Pred_Name (Name : String) return W_Identifier_Id;
-   function Eq_Pred_Name (Name : W_Identifier_Id) return W_Identifier_Id;
-   --  From the name of an abstract type, return the name of
-   --  its equality predicate.
-
    function Range_Axiom (Name : String) return  W_Identifier_Id;
    function Range_Axiom (Name : W_Identifier_Id) return W_Identifier_Id;
    --  From the name of an abstract type, return the name of
    --  its range axiom.
-
-   function Coerce_Axiom (Name : String) return  W_Identifier_Id;
-   function Coerce_Axiom (Name : W_Identifier_Id) return  W_Identifier_Id;
-   --  From the name of an abstract type, return the name of
-   --  its coerce axiom.
-
-   function Unicity_Axiom (Name : String) return  W_Identifier_Id;
-   function Unicity_Axiom (Name : W_Identifier_Id) return  W_Identifier_Id;
-   --  From the name of an abstract type, return the name of
-   --  its unicity axiom.
-
-   function Allocator_Name (Name : String) return W_Identifier_Id;
-   --  From the name of an abstract type, return the name of
-   --  its allocator.
-
-   function To_Program_Space (Name : W_Identifier_Id) return W_Identifier_Id;
-   --  Create a new identifier for an entity in program space, given
-   --  the name of the corresponding entity in logic space.
 
    function Safe_Version (Name : W_Identifier_Id) return W_Identifier_Id;
    --  Create a new identifier for the "safe" version (no precondition
@@ -95,36 +114,18 @@ package Why.Gen.Names is
    procedure Set_Name (Id : W_Identifier_Id; Name : String);
    --  Change the name of the given identifier
 
-   function New_Result_Identifier return W_Term_Identifier_Id;
-   --  Return an new identifier for a function result as it
-   --  would be used into a postcondition.
-
-   function New_Exit_Identifier return W_Identifier_Id;
-   --  Return an new identifier for the exception "Exit".
+   function To_Program_Space (Name : W_Identifier_Id) return W_Identifier_Id;
+   --  Create a new identifier for an entity in program space, given
+   --  the name of the corresponding entity in logic space.
 
    function To_Term_Identifier
      (Name : W_Identifier_Id)
      return W_Term_Identifier_Id;
    --  Create a label identifier from Name. Name is duplicated.
 
-   function New_Term (Name : String) return W_Term_Identifier_Id;
-   --  Return a term identified by the given name
+   function Unicity_Axiom (Name : String) return  W_Identifier_Id;
+   function Unicity_Axiom (Name : W_Identifier_Id) return  W_Identifier_Id;
+   --  From the name of an abstract type, return the name of
+   --  its unicity axiom.
 
-   function Array_Access_Name (Name : String) return W_Identifier_Id;
-   --  From the name of an array type, return the name of its access function
-   --
-   function Array_Update_Name (Name : String) return W_Identifier_Id;
-   --  From the name of an array type, return the name of its update function
-
-   function Array_Accupd_Eq_Axiom (Name : String) return W_Identifier_Id;
-   --  From the name of an array type, return the name of the axiom of
-   --  access/update equality
-
-   function New_Bool_Int_Axiom (Rel : W_Relation) return W_Identifier_Id;
-   --  Return the name of the boolean comparison axiom for Why ints that
-   --  corresponds to the comparison operator in argument.
-
-   function New_Bool_Int_Cmp (Rel : W_Relation) return W_Identifier_Id;
-   --  Return the name of the boolean comparison operator for Why ints that
-   --  corresponds to the comparison operator in argument.
 end Why.Gen.Names;
