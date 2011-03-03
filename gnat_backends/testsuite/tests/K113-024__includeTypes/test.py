17f8fc7cc7f143bdfc12bf37066fbb710e95042d
Johannes Kanig <kanig@adacore.com>
2011-03-03 17:25:40 +0100

    Update test suite

--- gnat_backends/testsuite/tests/K113-024__includeTypes/test.py
+++ gnat_backends/testsuite/tests/K113-024__includeTypes/test.py
@@ -2,4 +2,5 @@ from test_support import *
 
 gnat2why("basictypes.ads")
 gnat2why("iandatypes.ads")
-why("iandatypes.why")
+why("iandatypes.why","--type-only")
+why("bascictypes.why","--type-only")
--- gnat_backends/testsuite/tests/K223-016__includeSubPackage/test.py
+++ gnat_backends/testsuite/tests/K223-016__includeSubPackage/test.py
@@ -1,4 +1,4 @@
 from test_support import *
 import glob
 
-prove(base.adb)
+prove("base.adb")
