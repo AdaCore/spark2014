954c841bdbb3bdbefcda4ae5ececf29a49a2a6b4
Johannes Kanig <kanig@adacore.com>
2011-03-03 17:46:51 +0100

    Fix test

--- gnat_backends/testsuite/tests/K113-024__includeTypes/test.py
+++ gnat_backends/testsuite/tests/K113-024__includeTypes/test.py
@@ -3,4 +3,4 @@ from test_support import *
 gnat2why("basictypes.ads")
 gnat2why("iandatypes.ads")
 why("iandatypes.why","--type-only")
-why("bascictypes.why","--type-only")
+why("basictypes.why","--type-only")
