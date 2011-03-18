2253f37b3acde8c598510e4246a590fc2bdd2342
Yannick Moy <moy@adacore.com>
2011-03-18 18:09:13 +0100

    Add more complete test for alfa output file

---
+++ gnat_backends/testsuite/tests/K302-029__more/pack.adb
@@ -0,0 +1,58 @@
+package body Pack is
+
+   procedure P0 is
+      procedure P3 is
+      begin
+         if Y.all = 0 then
+            return;
+         end if;
+      end;
+      procedure P0_Ok is
+      begin
+         null;
+      end;
+   begin
+      P3;
+   end;
+
+   procedure P1 is
+      procedure P4 is
+      begin
+         loop
+            exit;
+         end loop;
+      end;
+      procedure P1_Ok is
+      begin
+         null;
+      end;
+   begin
+      if X then
+         return;
+      end if;
+   end;
+
+   procedure P2 is
+   begin
+      declare
+         procedure P5 is
+            type R is record
+               X, Y, Z : Integer;
+            end record;
+            RR : R;
+         begin
+            RR := (0,1,2);
+         end;
+         Z : Boolean := X;
+         procedure P2_Ok is
+         begin
+            null;
+         end;
+      begin
+         if Z then
+            return;
+         end if;
+      end;
+   end;
+
+end;
---
+++ gnat_backends/testsuite/tests/K302-029__more/pack.ads
@@ -0,0 +1,19 @@
+package Pack is
+
+   X : Boolean;
+
+   type Acc is access Integer;
+   Y : Acc;
+
+   type R is record
+      X, Y, Z : Integer;
+   end record;
+   Z : R;
+
+   procedure P0 with Pre => Z.X = Z.Y;
+
+   procedure P1 with Post => Y.all = 0;
+
+   procedure P2 with Pre => (if X then True else False);
+
+end;
