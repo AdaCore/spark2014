6490786adf4cb5ce25078a521ec30bb43ba35ec8
Johannes Kanig <kanig@adacore.com>
2011-04-04 10:19:48 +0200

    Fix Header comment

--- gnat_backends/why/gnat2why_wrapper.adb
+++ gnat_backends/why/gnat2why_wrapper.adb
@@ -9,8 +9,14 @@
 --                       Copyright (C) 2010-2011, AdaCore                   --
 --                                                                          --
 -- gnat2why is  free  software;  you can redistribute  it and/or  modify it --
--- under terms of the  GNU General Public License as published  by the Free --  -- Software  Foundation;  either version  3,  or (at your option) any later --
--- version. gnat2why is distributed in the hope that it will be useful, but --  -- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --  -- LITY or FITNESS  FOR A PARTICULAR  PURPOSE.  See the GNU General  Public --  -- License for  more details.  You should have  received a copy of  the GNU --  -- General Public License distributed with gnat2why; see file COPYING3.  If --  -- not,  go to  http://www.gnu.org/licenses  for a  complete  copy  of  the --
+-- under terms of the  GNU General Public License as published  by the Free --
+-- Software  Foundation;  either version  3,  or (at your option) any later --
+-- version. gnat2why is distributed in the hope that it will be useful, but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or FITNESS  FOR A PARTICULAR  PURPOSE.  See the GNU General  Public --
+-- License for  more details.  You should have  received a copy of  the GNU --
+-- General Public License distributed with gnat2why; see file COPYING3.  If --
+-- not,  go to  http://www.gnu.org/licenses  for a  complete  copy  of  the --
 -- license.                                                                 --
 --                                                                          --
 -- gnat2why is maintained by AdaCore (http://www.adacore.com)               --
