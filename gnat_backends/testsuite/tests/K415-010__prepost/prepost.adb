fea1bb1cee0d343ee120719f4ceac977404f13f4
Yannick Moy <moy@adacore.com>
2011-04-19 17:27:45 +0200

    update test

--- gnat_backends/testsuite/tests/K415-010__prepost/prepost.adb
+++ gnat_backends/testsuite/tests/K415-010__prepost/prepost.adb
@@ -6,4 +6,15 @@ package body Prepost is
       return True;
    end F;
 
+   function Fun_Is_Zero (X : Integer) return Boolean is
+   begin
+      return Is_Zero (X);
+   end Fun_Is_Zero;
+
+   function G (Z : Integer) return Boolean is
+   begin
+      pragma Assert (Z = 0);
+      return True;
+   end G;
+
 end Prepost;
--- gnat_backends/testsuite/tests/K415-010__prepost/prepost.ads
+++ gnat_backends/testsuite/tests/K415-010__prepost/prepost.ads
@@ -6,4 +6,9 @@ package Prepost is
    function F (Z : Integer) return Boolean
       with Pre => (Z /= 1 and then Is_Zero (Z));
 
+   function Fun_Is_Zero (X : Integer) return Boolean;
+
+   function G (Z : Integer) return Boolean
+      with Pre => (Z /= 1 and then Fun_Is_Zero (Z));
+
 end Prepost;
