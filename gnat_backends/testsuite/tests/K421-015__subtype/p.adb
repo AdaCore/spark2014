90a4afce7d582347195e71b4148da6ecbfa25db3
Yannick Moy <moy@adacore.com>
2011-04-21 17:56:49 +0200

    update test outputs

---
+++ gnat_backends/testsuite/tests/K421-015__subtype/p.adb
@@ -0,0 +1,15 @@
+procedure P is
+   procedure Proc (W, X, Y, Z : Integer) is
+      subtype S1 is Integer range W .. X;
+      subtype S2 is S1 range Y .. Z;
+   begin
+      null;
+   end;
+begin
+   Proc (1, 4, 2, 3);  --  ok
+   Proc (1, 4, 3, 2);  --  ok S2 empty
+   Proc (2, 3, 4, 1);  --  ok S2 empty
+   Proc (4, 1, 3, 2);  --  ok S1 and S2 empty
+   Proc (3, 2, 4, 1);  --  ok S1 and S2 empty
+   Proc (1, 2, 3, 4);  --  bad
+end;
