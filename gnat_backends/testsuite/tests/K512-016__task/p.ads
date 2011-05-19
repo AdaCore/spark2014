adf42820725f936afde9a8311d9e064ec68cd0f5
Yannick Moy <moy@adacore.com>
2011-05-19 09:10:19 +0200

    missing files

---
+++ gnat_backends/testsuite/tests/K512-016__task/p.ads
@@ -0,0 +1,10 @@
+package P is
+   type Base is range 1 .. 10;
+   type Int is new Base;
+
+   CPU_SETSIZE : constant := 1_024;
+   type bit_field is array (1 .. CPU_SETSIZE) of Boolean;
+   for bit_field'Size use CPU_SETSIZE;
+   pragma Pack (bit_field);
+   pragma Convention (C, bit_field);
+end;
---
+++ gnat_backends/testsuite/tests/K512-016__task/t.adb
@@ -0,0 +1,10 @@
+procedure T is
+   task T1;
+
+   task body T1 is
+   begin
+      null;
+   end T1;
+begin
+   null;
+end T;
