ff5c4bf1421b97001942d5fcbb9d126b1e0b2453
Jerome Guitton <guitton@adacore.com>
2011-05-23 12:29:59 +0200

    Add testcase for K523-007

---
+++ gnat_backends/testsuite/tests/K523-007__generic/pack.ads
@@ -0,0 +1,11 @@
+with Ada.Text_IO;
+with System.Storage_Elements;
+
+package Pack is
+
+   package Address_Value_IO is
+     new Ada.Text_IO.Modular_IO (System.Storage_Elements.Integer_Address);
+
+   function Address_To_Hex (Adder: System.Address) return String;
+
+end Pack;
