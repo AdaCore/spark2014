f1cfd0fc8795e01f6cb0e532b66fb203c0ba1b43
Johannes Kanig <kanig@adacore.com>
2011-05-25 17:55:22 +0200

    Improve test case

--- gnat_backends/testsuite/tests/K519-031__subarray/subar.adb
+++ gnat_backends/testsuite/tests/K519-031__subarray/subar.adb
@@ -12,4 +12,21 @@ package body Subar is
       F (AB);
    end Try_B;
 
+   procedure G (X : C) is
+   begin
+      null;
+   end G;
+
+   procedure Try_D is
+      AD : D (1..10);
+   begin
+      G (AD);
+   end Try_D;
+
+   procedure Try_E is
+      AE : E;
+   begin
+      G (AE);
+   end Try_E;
+
 end Subar;
--- gnat_backends/testsuite/tests/K519-031__subarray/subar.ads
+++ gnat_backends/testsuite/tests/K519-031__subarray/subar.ads
@@ -4,14 +4,20 @@ package Subar is
 
    type A is array (One_Ten) of One_Twenty;
 
+   type C is array (Integer range <>) of One_Ten;
+
    -- simple subtype
    subtype B is A;
 
-   type C is array (One_Ten) of One_Ten;
-   type D is array (One_Twenty) of One_Twenty;
-   type E is array (One_Twenty) of One_Ten;
+   --  subtype of unconstrained type, without additional bounds
+   subtype D is C;
+   --  subtype of unconstrained type, with additional bounds
+   subtype E is C (Integer range 1..10);
 
    procedure F (X : A)
       with Pre => (X (1) = 1);
+
+   procedure G (X : C)
+      with Pre => (X (X'First) = 1);
 
 end Subar;
