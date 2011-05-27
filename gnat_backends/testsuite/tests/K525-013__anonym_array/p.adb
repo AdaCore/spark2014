68fd869c16af4e43e2d81dd47af4c22680b5a835
Yannick Moy <moy@adacore.com>
2011-05-27 15:01:20 +0200

    add test

---
+++ gnat_backends/testsuite/tests/K525-013__anonym_array/p.adb
@@ -0,0 +1,8 @@
+procedure P (A, B : Integer) is
+   pragma Precondition (A in 1 .. 20 and then B in 1 .. 20);
+   X : array (1..20) of Integer;
+begin
+   X (A) := 1;
+   X (B) := 2;
+   pragma Assert (if A /= B then X (A) = 1);
+end;
