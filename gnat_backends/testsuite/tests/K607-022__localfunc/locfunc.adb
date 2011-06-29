3e54b7ac5440922ad24a5235e26f2da6b2741ce3
Johannes Kanig <kanig@adacore.com>
2011-06-29 11:11:12 +0200

    a new test case
    TN: K607-022

---
+++ gnat_backends/testsuite/tests/K607-022__localfunc/locfunc.adb
@@ -0,0 +1,17 @@
+package body Locfunc is
+   procedure P (X : in out Integer) is
+      Y : Integer;
+
+      procedure Q
+         with Post => (X = 0 and then Y = 0);
+
+      procedure Q is
+      begin
+         X := 0;
+         Y := 0;
+      end;
+
+   begin
+      Q;
+   end P;
+end Locfunc;
