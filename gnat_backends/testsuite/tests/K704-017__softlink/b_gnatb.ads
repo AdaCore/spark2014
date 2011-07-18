f5d68732d7c42098a5aebbdd44d1cae29cf1b16d
Johannes Kanig <kanig@adacore.com>
2011-07-18 15:28:26 +0200

    Test case
    TN: K704-017

---
+++ gnat_backends/testsuite/tests/K704-017__softlink/b_gnatb.ads
@@ -0,0 +1,8 @@
+pragma Ada_95;
+with System;
+package ada_main is
+   pragma Warnings (Off);
+
+   procedure adainit;
+
+end ada_main;
---
+++ gnat_backends/testsuite/tests/K704-017__softlink/test.py
@@ -0,0 +1,3 @@
+from test_support import *
+
+gnatprove(opt=["-u", "b_gnatb.adb"])
