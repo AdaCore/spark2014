d8a838ae93af0afc9b3f00c323a08a99e52fd25d
Johannes Kanig <kanig@adacore.com>
2011-07-20 17:29:33 +0200

    Improve test to cover conditional expressions as well as If-statements
    TN: K719-022

--- gnat_backends/testsuite/tests/K719-022__elsif/minus.adb
+++ gnat_backends/testsuite/tests/K719-022__elsif/minus.adb
@@ -13,4 +13,10 @@ package body Minus is
       return X;
    end Minus;
 
+   function Minus2 (X : Natural) return Natural is
+   begin
+      return
+         (if X = 1 then 0 elsif X = 2 then 1 elsif X = 3 then 2 else X);
+   end Minus2;
+
 end Minus;
--- gnat_backends/testsuite/tests/K719-022__elsif/minus.ads
+++ gnat_backends/testsuite/tests/K719-022__elsif/minus.ads
@@ -3,4 +3,8 @@ package Minus is
    with Pre => 1 <= X and X <= 3,
      Post => Minus'Result = X - 1;
 
+   function Minus2 (X : Natural) return Natural
+   with Pre => 1 <= X and X <= 3,
+     Post => Minus2'Result = X - 1;
+
 end Minus;
