885e206fbcd3fc6fabbd5cc6b7265ca722861dce
Johannes Kanig <kanig@adacore.com>
2011-07-25 18:30:56 +0200

    Improve test
    TN: K722-017

--- gnat_backends/testsuite/tests/K722-017__renaming/renam.adb
+++ gnat_backends/testsuite/tests/K722-017__renaming/renam.adb
@@ -1,5 +1,19 @@
 package body Renam is
 
+   procedure O (M : out Integer)
+   is
+      PM : Integer renames M;
+
+      function F return Integer
+      is
+      begin
+         PM := 1;
+         return PM;
+      end F;
+   begin
+      null;
+   end O;
+
    procedure P (M : Integer)
    is
       PM : Integer renames M;
