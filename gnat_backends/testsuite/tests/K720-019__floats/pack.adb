393485c500c2e919717aed57fc7430d785c49f7f
Jerome Guitton <guitton@adacore.com>
2011-08-01 17:27:34 +0200

    Add test case for floating-point support
    Part of K720-019

---
+++ gnat_backends/testsuite/tests/K720-019__floats/pack.adb
@@ -0,0 +1,13 @@
+package body Pack is
+
+   function Add (R1, R2 : Float) return Float is
+   begin
+      return R1 + R2;
+   end Add;
+
+   function Associativity_Test return Boolean is
+   begin
+      return Add (Add (0.1, 0.2), 0.3) = Add (0.1, Add (0.2, 0.3));
+   end Associativity_Test;
+   
+end Pack;
