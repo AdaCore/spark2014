358b805fc5a07de71047d108f572ccaa2ce4b35b
Johannes Kanig <kanig@adacore.com>
2011-08-09 09:27:06 +0200

    File test case
    For K809-002

---
+++ gnat_backends/testsuite/tests/K809-002__double/p.adb
@@ -0,0 +1,14 @@
+procedure P is
+
+     generic
+          type priv is private;
+     package gen_p is
+        T : Integer;
+     end gen_p;
+
+     package P is new gen_p(Integer);
+
+  begin
+   null;
+
+  end P;
