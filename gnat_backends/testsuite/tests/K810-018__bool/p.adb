4aaee6f99606cba0aff6bfcd6200fe368a787f8a
Johannes Kanig <kanig@adacore.com>
2011-08-10 15:53:20 +0200

    File test case
    For K810-018

---
+++ gnat_backends/testsuite/tests/K810-018__bool/p.adb
@@ -0,0 +1,16 @@
+procedure P is
+   generic
+        type Bool is (<>);
+        F, T : Bool;
+   procedure Q;
+
+   procedure Q is
+   begin
+      null;
+   end Q;
+
+   procedure NP1 is new Q ( Bool => Boolean, F => False, T => True );
+begin
+   null;
+
+end P;
