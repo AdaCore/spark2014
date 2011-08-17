aa9b86958fea419bc7f23fdce05f749c68d2398c
Johannes Kanig <kanig@adacore.com>
2011-08-17 16:07:03 +0200

    Binary search as example

---
+++ examples/binary_search/binary_search.adb
@@ -0,0 +1,30 @@
+package body Binary_Search is
+
+   function Search (A : Ar ; I : Integer) return T is
+      Left  : U;
+      Right : U;
+      Med   : U;
+   begin
+      if A'Length = 0 then
+         return 0;
+      end if;
+      Left := Ar'First;
+      Right := Ar'Last;
+      if A (Left) > I or else A (Right) < I then
+         return 0;
+      end if;
+      while Left < Right loop
+         pragma Assert (A (Left) <= I and then I <= A (Right));
+         Med := Left + (Right - Left) / 2;
+         if A (Med) < I then
+            Left := Med;
+         elsif A (Med) > I then
+            Right := Med;
+         else
+            return Med;
+         end if;
+      end loop;
+      return 0;
+   end Search;
+
+end Binary_Search;
