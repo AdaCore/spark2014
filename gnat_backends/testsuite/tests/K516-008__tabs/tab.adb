22823169df2a8ec4fd024d250c56841b48de7374
Yannick Moy <moy@adacore.com>
2011-11-14 16:12:15 +0100

    Update test and output. Hosting of expressions from invariant is fine.
    For K516-008
    
    The recent disabling of the generation of checks in the frontend leads to no
    expressions being hosted of invariants, even with calls. So TN is fixed. If a
    more complex case leads to expressions being hosted, a new TN will be opened
    (and this is safe, as it just leads to an invariant being considered like an
    assertion).

--- gnat_backends/testsuite/tests/K516-008__tabs/tab.adb
+++ gnat_backends/testsuite/tests/K516-008__tabs/tab.adb
@@ -2,10 +2,12 @@ package body Tab is
    procedure Tab_Filter (B : in Arr; Threshold : in Integer; A : in out Arr)
    is
       Cur : Integer := 0;
+
+      function Prev (X:Index) return Index is (X-1);
    begin
       for I in Index'First .. Index'Last loop
          pragma Assert
-           (for all J in Index range Index'First .. I-1 =>
+           (for all J in Index range Index'First .. Prev(I) =>
               (if (B(J) > Threshold) then
                  (for some K in Index range 0 .. Cur-1 => (A(K)=B(J)))));
          if (B(I) > Threshold) then
