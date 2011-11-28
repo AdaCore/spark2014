0ee6e8cd683e684b6ea5de5fd3e95b570251682a
Yannick Moy <moy@adacore.com>
2011-11-28 12:49:47 +0100

    Correct loop invariant

--- dist-examples/painting/p.adb
+++ dist-examples/painting/p.adb
@@ -2,19 +2,20 @@ package body P is
    procedure Shadow_Effect (P : in out Painting; D : Dot) is
    begin
       for C in Color loop
-	 
-	 --  Loop invariant required to prove the postcondition
-	 
-	 pragma Assert 
-	   (for all J in Color'First .. Color'Pred(C) =>
-	      (if P.Plain'Old(J) = D then P.Shadow(J) = D));
-	 
-	 if P.Plain(C) = D then 
-	    P.Shadow(C) := D;
-	 end if;
+
+         --  Loop invariant required to prove the postcondition
+
+         pragma Assert
+           (P.Plain = P.Plain'Old and then
+             (for all J in Color'First .. Color'Pred (C) =>
+               (if P.Plain (J) = D then P.Shadow (J) = D)));
+
+         if P.Plain (C) = D then
+            P.Shadow (C) := D;
+         end if;
       end loop;
    end Shadow_Effect;
-   
+
    procedure Shadow_Effect_2 (P : in out Painting; D : Dot) is
    begin
       Shadow_Effect (P, D);
