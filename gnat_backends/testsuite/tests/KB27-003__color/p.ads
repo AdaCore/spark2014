564377a0fcc2b3cea2f147511fa0a0f6ee996f75
Johannes Kanig <kanig@adacore.com>
2011-11-28 11:43:45 +0100

    update base line

--- gnat_backends/testsuite/tests/KB27-003__color/p.ads
+++ gnat_backends/testsuite/tests/KB27-003__color/p.ads
@@ -6,13 +6,16 @@ package P is
    for Repr use (A => 1, B => 5, C => 8);
 
    procedure Shadow_Effect (D : in out Dots) with
+      --  should not be proved
      Post => (for all C in Color => False);
 
    function P (R : Repr) return Integer
       with Pre  => (R = C),
+      --  should not be proved
            Post => (P'Result = 8);
 
    function Q (R : Repr) return Integer
       with Pre  => (R = C),
+      --  should be proved
            Post => (Q'Result = 2);
 end P;
