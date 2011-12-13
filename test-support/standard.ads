86c5af258d64b0467b1253b87b3b40ec9fabdeaf
Johannes Kanig <kanig@adacore.com>
2011-12-13 17:32:00 +0100

    Remove unneeded file
    For KC13-017

--- test-support/standard.ads
+++
@@ -1,5 +0,0 @@
-package Standard is
-   type Integer is range -2**31 .. 2**31-1;
-   type Long_Integer is range -2**63 .. 2**63-1;
-   type Float is digits  6 range -3.40282E+38 ..  3.40282E+38;
-end Standard;
