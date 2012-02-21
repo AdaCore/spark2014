8447cca66615fff4bab5ef01b7b27e4cbc33d459
quinot <quinot@adacore.com>
2012-02-21 16:18:44 +0000

    Minor reformatting
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/ipstack@186053 936e1b1b-40f2-da11-902a-00137254ae57

--- spark2014/testsuite/gnatprove/tests/ipstack/src/support/aip-time_types.ads
+++ spark2014/testsuite/gnatprove/tests/ipstack/src/support/aip-time_types.ads
@@ -1,11 +1,12 @@
 ------------------------------------------------------------------------------
 --                            IPSTACK COMPONENTS                            --
---             Copyright (C) 2010, Free Software Foundation, Inc.           --
+--          Copyright (C) 2010-2012, Free Software Foundation, Inc.         --
 ------------------------------------------------------------------------------
 
 --# inherit AIP;
 
 package AIP.Time_Types is
+
    type Time is mod 2 ** 32;
    subtype Interval is Time;
 
@@ -13,7 +14,6 @@ package AIP.Time_Types is
    --  Number of Time units per second
 
    function Now return Time;
-   --  Elapsed time since unspecified epoch, in ms
-   --  Should this be specified as "in Hz units"???
+   --  Elapsed time since unspecified epoch, in 1/Hz units
 
 end AIP.Time_Types;
