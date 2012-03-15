c90ba8754af1a9d230b1e408ec9380673bf1ef9f
Johannes Kanig <kanig@adacore.com>
2012-03-15 17:24:07 +0100

    File test case
    For K513-011

---
+++ testsuite/gnatprove/tests/K513-011__closure/a.adb
@@ -0,0 +1,6 @@
+with B; use B;
+
+procedure A is
+begin
+   Simple_Call;
+end A;
---
+++ testsuite/gnatprove/tests/K513-011__closure/b.adb
@@ -0,0 +1,8 @@
+package body B is
+
+   procedure Simple_Call is
+   begin
+      null;
+   end Simple_Call;
+
+end B;
