3e602fb4409a868ca0d1d1e7b3215158c1dfdf1b
Yannick Moy <moy@adacore.com>
2012-03-20 15:19:36 +0100

    Add test cases for L320-006 and L320-018

---
+++ testsuite/gnatprove/tests/L320-006__deferred/p.adb
@@ -0,0 +1,14 @@
+procedure P is
+    package Pack is
+       type T1 is record
+          X : Boolean;
+       end record;
+       subtype T2 is T1;
+       X1 : constant T1;
+       X2 : T2;
+    private
+       X1 : constant T1 := X2;
+    end Pack;
+begin
+    null;
+end P;
