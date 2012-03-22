2f1730b5c400c6478ee7451271d21aaeac0d34be
Yannick Moy <moy@adacore.com>
2012-03-22 12:12:00 +0100

    fix test

--- testsuite/gnatprove/tests/L320-014__string/user.adb
+++ testsuite/gnatprove/tests/L320-014__string/user.adb
@@ -3,7 +3,7 @@ procedure User is
    package P1 is new Pack (1);
    package P2 is new Pack (2);
 begin
-   pragma Assert (P1.S (1 .. 5) = "image");
+   pragma Assert (P1.S = "image of N");
    pragma Assert (P2.S = "image of N");
 end User;
 
