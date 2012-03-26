d2013ee893f8d7259cebe3898749854a60c810e9
Johannes Kanig <kanig@adacore.com>
2012-03-26 14:39:14 +0200

    File test case
    For L316-015

---
+++ testsuite/gnatprove/tests/L316-015__main/b.adb
@@ -0,0 +1,10 @@
+with C;
+
+package body B is
+
+   procedure P (X : Integer) is
+   begin
+      C.P (X);
+   end P;
+
+end B;
---
+++ testsuite/gnatprove/tests/L316-015__main/b.ads
@@ -0,0 +1,5 @@
+package B is
+
+   procedure P (X : Integer) with
+      Pre => (X > 0);
+end B;
