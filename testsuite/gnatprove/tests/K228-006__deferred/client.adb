b0f19d458b3bbad918d5a4b5be85cd23a084f8e2
Yannick Moy <moy@adacore.com>
2012-04-06 11:56:19 +0200

    Add test for K228-006

---
+++ testsuite/gnatprove/tests/K228-006__deferred/client.adb
@@ -0,0 +1,12 @@
+with Const; use Const;
+
+procedure Client (B1, B2 : Boolean) is
+begin
+   if B1 then
+      pragma Assert (C = 10_000);  --  should be unprovable
+   elsif B2 then
+      pragma Assert (Get = 10_000);  --  should be unprovable
+   else
+      pragma Assert (Get2 = 10_000);
+   end if;
+end Client;
