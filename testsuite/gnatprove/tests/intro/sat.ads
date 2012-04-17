e1c19e28ae3db4e66917321109741f2d5c1b626d
Yannick Moy <moy@adacore.com>
2012-04-18 00:17:57 +0200

    Update test with contract cases. Call full proof.

--- testsuite/gnatprove/tests/intro/sat.ads
+++ testsuite/gnatprove/tests/intro/sat.ads
@@ -3,11 +3,23 @@ package Sat is
    subtype My_Int is Integer range 0 .. 10_000;
 
    function Add (X , Y : My_Int) return My_Int with
-     Post => (X + Y < 10_000 and Add'Result = X + Y) or
-             (X + Y >= 10_000 and Add'Result = 10_000);
+     Contract_Case => (Name     => "normal operation",
+                       Mode     => Nominal,
+                       Requires => X + Y < 10_000,
+                       Ensures  => Add'Result = X + Y),
+     Contract_Case => (Name     => "saturating",
+                       Mode     => Nominal,
+                       Requires => X + Y >= 10_000,
+                       Ensures  => Add'Result = 10_000);
 
    function Mult (X , Y : My_Int) return My_Int with
-     Post => (X * Y < 10_000 and Mult'Result = X * Y) or
-             (X * Y >= 10_000 and Mult'Result = 10_000);
+     Contract_Case => (Name     => "normal operation",
+                       Mode     => Nominal,
+                       Requires => X * Y < 10_000,
+                       Ensures  => Mult'Result = X * Y),
+     Contract_Case => (Name     => "saturating",
+                       Mode     => Nominal,
+                       Requires => X * Y >= 10_000,
+                       Ensures  => Mult'Result = 10_000);
 
 end Sat;
