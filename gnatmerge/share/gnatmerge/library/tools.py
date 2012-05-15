b9e45610f1207f21a18f7861301f0221e1d37397
Jerome Guitton <guitton@adacore.com>
2012-05-15 19:36:42 +0200

    Factorize out a few common entities (entities)
    [L222-014]

--- gnatmerge/share/gnatmerge/library/tools.py
+++ gnatmerge/share/gnatmerge/library/tools.py
@@ -2,25 +2,88 @@
 """
 
 from internal.attributes import lattices
+from internal.attributes import lattice_ops
+from internal import sets
 
-slocs_attribute = lattices.RangeAttribute(lattices.Sloc,
-                                          "SLOCS", "LOW", "HIGH")
-tristate_attribute = lattices.PartialOrderAttribute("STATUS", {"OK", "KO"})
-tristate_attribute.name_meet("PARTIAL OK", {"OK", "KO"})
+class Entity:
+    """Represents a source code entity on which merges should be done
 
-def slocs():
-    """Return the range attribute used to represent source lines of codes"""
-    global slocs_attribute
-    return slocs_attribute
+    This class allows to merge results of an entity of a lower level
+    (e.g. child) with entities in entities of a higher level. Typically,
+    to merge proof results on the subprogram level to the package level,
+    one which do:
 
-def tristate():
-    """Return the attribute used to represent a tristate result of a tool
+        m = Merge()
+        pck = m.new_entity("PACKAGE")
+        subp = pck.new_child("SUBPROGRAM", inherits=m.tristate)
 
-    The three possible status are:
-    * OK: all sub-entities are OK;
-    * KO: all sub-entities are KO;
-    * PARTIAL OK: some sub-entities are OK, some are KO"""
-    global tristate_attribute
-    return tristate_attribute
+    meaning that the status at the package level will be deduced from the
+    status at subprogram level.
+    """
 
+    def __init__(self, merge, name):
+        """Constructor.
 
+        PARAMETERS
+            merge: Merge object to which this entity is attached
+            name: name of the entity
+            object: Underlying object, of type sets.Object.
+        """
+        self.name = name
+        self.merge = merge
+        self.object = self.merge.repository.new_object(name)
+        self.object.new_attribute(self.merge.slocs)
+
+    def new_child(self, name, inherits):
+        """Create a new child entity
+
+        Create an entity whose instances are always included in
+        at most one instance of self, inheriting the specified
+        properties.
+
+        PARAMETERS
+            name: name of the newly created entity
+            inherits: attribute inherited by self from the new child
+        """
+        child = Entity(self.merge, name)
+        child.object.new_attribute(inherits)
+        child.object.new_arrow(self.name,
+                               lattice_ops.Inclusion(lattice=self.merge.slocs,
+                                                     object=self.object))
+        self.object.new_arrow(inherits.name,
+                              lattice_ops.Join(lattice=inherits,
+                                               subobject=child.object,
+                                               in_object_key=self.name))
+
+        return child
+
+class Merge:
+    """Represent a set of merge operations
+
+    ATTRIBUTES
+        repository: object repository, of type sets.Objects
+        tristate: Basic PartialOrderAttribute to represent a tool result
+            The three possible status are:
+            * OK: all sub-entities are OK;
+            * KO: all sub-entities are KO;
+            * PARTIAL OK: some sub-entities are OK, some are KO
+        slocs: RangeAttribute used to represent slocs
+    """
+
+    def __init__(self):
+        """Constructor."""
+        self.repository = sets.Objects()
+        self.slocs = lattices.RangeAttribute(lattices.Sloc,
+                                             "SLOCS", "LOW", "HIGH")
+        self.tristate = lattices.PartialOrderAttribute("STATUS",
+                                                       {"OK", "KO"})
+        self.tristate.name_meet("PARTIAL OK", {"OK", "KO"})
+
+    def new_entity(self, name):
+        """Create a new entity to be used for this merge"""
+        result = Entity(self, name)
+        return result
+
+    def loads(self, filename):
+        """Load a set of results to be used for this merge"""
+        self.repository.loads("program.json")
--- gnatmerge/tests/simple/driver.py
+++ gnatmerge/tests/simple/driver.py
@@ -1,48 +1,24 @@
 
-from internal import sets
 import readers
-from internal.attributes import lattices
-from internal.attributes import lattice_ops
-
 from tools import *
 
 # Build sketch of inputs
 
-m = sets.Objects()
+m = Merge()
 
-# subp = new_entity("SUBPROGRAM")
-subp = m.new_object("SUBPROGRAMS")
-subp.new_attribute(slocs())
+subp = m.new_entity("SUBPROGRAM")
+vcs = subp.new_child("VC", inherits=m.tristate)
 
-# subp = new_entity("VC")
-# ...then will disappear when new_child is implemented
-vcs = m.new_object("VCS")
-vcs.new_attribute(slocs())
-
-# Decorate sketch with the spec of results
-
-# vc = subp.new_child("VC", inherits=proved)
-vcs.new_arrow("SUBPROGRAM", lattice_ops.Inclusion(lattice=slocs(),
-                                                  object=subp))
-subp.new_arrow("STATUS", lattice_ops.Join(lattice=tristate(),
-                                          subobject=vcs,
-                                          in_object_key="SUBPROGRAM"))
-# ...move status attribute down...
-vcs.new_attribute(tristate())
-
-
-# Instanciate sketch from inputs
-# ...deduced from command line
-
-vcs.load(readers.ErrorListing("VC", "proofs.out"))
+vcs.object.load(readers.ErrorListing("VC", "proofs.out"))
 m.loads("program.json")
 
 # Output results
 
 # set_goal(ok)
 
-for i in subp.content():
-    print i + " - STATUS : " + str(subp.follow("STATUS", i))
+for i in vcs.object.content():
+    print i + " - SUBPROGRAM : " + str(vcs.object.follow("SUBPROGRAM", i))
 
-for i in vcs.content():
-    print i + " - SUBPROGRAM : " + str(vcs.follow("SUBPROGRAM", i))
+for i in subp.object.content():
+    print i + " - STATUS : " + str(subp.object.follow("STATUS", i))
+
