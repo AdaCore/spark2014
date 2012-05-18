48973249c640863647e0f0e4a5121beffd9f0e76
Johannes Kanig <kanig@adacore.com>
2012-05-18 11:44:31 +0200

    update test output
    For L503-007

--- testsuite/gnatprove/tests/L503-007__generic_sloc/main.adb
+++ testsuite/gnatprove/tests/L503-007__generic_sloc/main.adb
@@ -4,7 +4,6 @@ procedure Main is
    type Sub is new Integer range 1 .. 10;
    package A is new Gen (Integer);
    package B is new Gen (Sub);
-   pragma Unreferenced (A,B);
 begin
    null;
 end Main;
