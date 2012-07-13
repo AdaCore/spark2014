19a60898f1f2ca174e406e47dcae2af3177cd7ed
Yannick Moy <moy@adacore.com>
2012-07-13 16:16:13 +0200

    Add iterator in test

--- testsuite/gnatprove/tests/L413-031__fullview/int_list.adb
+++ testsuite/gnatprove/tests/L413-031__fullview/int_list.adb
@@ -2,21 +2,24 @@ pragma Ada_2012;
 with Ada.Containers.Formal_Doubly_Linked_Lists;
 
 procedure Int_List is
-   function my_eq (I1 : Integer; I2 : Integer) return Boolean
+   function My_Eq (I1 : Integer; I2 : Integer) return Boolean
    is (I1 = I2);
 
    package MyLists is new Ada.Containers.Formal_Doubly_Linked_Lists
-        (Integer, "=" => my_eq);
+     (Integer, "=" => My_Eq);
    use MyLists;
 
    procedure Add (L : in out List; I : Integer) with
-   Post => Element (L, First (L)) = I;
+     Post => Element (L, First (L)) = I
+       and then (for all C in L'Old => Element (L, C) = Element (L'Old, C));
 
    procedure Add (L : in out List; I : Integer) is
    begin
-            Prepend (L, I);
+      Prepend (L, I);
    end Add;
 
+   L : List (10);
 begin
-   null;
+   Add (L, 0);
+   Add (L, 1);
 end Int_List;
