d86f94b276e60e98eecde401fe19544f5b59416f
Johannes Kanig <kanig@adacore.com>
2012-07-25 11:14:25 +0200

    Simplify test to not contain static constraint errors
    For L531-021

--- testsuite/gnatprove/tests/L531-021__discr/special.adb
+++ testsuite/gnatprove/tests/L531-021__discr/special.adb
@@ -6,18 +6,13 @@ package body Special is
    begin
       pragma Assert (V.X = A);
       V.A_Field := 1;
-      --  discriminant check fails here
-      V.C_Field1 := True;
    end P;
 
-   -- specialize on assignment (in subprogram body)
    procedure T is
       V : R := (A, 1, 0);
    begin
       pragma Assert (V.X = A);
       V.A_Field := 1;
-      --  discriminant check fails here
-      V.C_Field1 := True;
    end T;
 
 end Special;
