65754212ab5eef186af3c03324bd8748817ad145
Johannes Kanig <kanig@adacore.com>
2012-10-03 14:48:53 +0200

    File test case
    For LA01-031

---
+++ testsuite/gnatprove/tests/LA01-031__deriv/super.ads
@@ -0,0 +1,11 @@
+package Super is
+
+   Max_Length : constant Positive := 10;
+   type Super_String is record
+      Current_Length : Natural := 0;
+      Data           : String (1 .. Max_Length);
+   end record;
+
+   function Super_Length (Source : Super_String) return Natural;
+
+end Super;
