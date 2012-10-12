6cffd9d6714b30b6a5b74939052329ec8112cefa
Yannick Moy <moy@adacore.com>
2012-10-12 11:19:12 +0200

    Add test written by Duc

---
+++ testsuite/gnatprove/tests/power_and_sum/power_and_sum.ads
@@ -0,0 +1,12 @@
+package Power_and_Sum is
+   procedure Power(X : in Integer; N : in Positive; Result: out Integer) with
+     Post => X ** N = Result;
+
+   procedure Sum(N : in Positive; Result: out Positive) with
+     Post => 2 * Result = N * (N+1)
+   ;
+   procedure Sum_Of_Sum(N : in Positive; Result: out Positive) with
+     Post => 6 * Result = N * (N+1) * (N+2)
+   ;
+
+end Power_and_Sum;
