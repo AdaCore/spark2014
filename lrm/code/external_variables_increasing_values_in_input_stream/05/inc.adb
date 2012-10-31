7201d6d45cdc8ab13c228af6c1b568717813decd
Jon Burton <jon.burton@altran-praxis.com>
2012-10-31 14:27:11 +0000

    Added 2005 code for mapping spec example on referencing multiple input stream elements in a post-condition.

---
+++ lrm/code/external_variables_increasing_values_in_input_stream/05/inc.adb
@@ -0,0 +1,43 @@
+package body Inc
+--# own Sensor is in S;
+is
+   S : Integer;
+   for S'Address use 16#DEADBEEF#;
+
+   procedure Read (V     : out Integer;
+                   Valid : out Boolean)
+   --# global in S;
+   --# post (Valid -> V = S~) and
+   --#      (S = S'Tail (S~));
+   is
+      Tmp : Integer;
+   begin
+      Tmp := S;
+      if Tmp'Valid then
+         V := Tmp;
+         Valid := True;
+         --# check S = S'Tail (S~);
+      else
+         V := 0;
+         Valid := False;
+      end if;
+   end Read;
+
+   procedure Increases (Result : out Boolean;
+                        Valid  : out Boolean)
+   --# global in S;
+   --# post Valid -> (Result <-> S'Tail (S~) > S~);
+   is
+      A, B : Integer;
+   begin
+      Result := False;
+      Read (A, Valid);
+      if Valid then
+         Read (B, Valid);
+         if Valid then
+            Result := B > A;
+         end if;
+      end if;
+   end Increases;
+
+end Inc;
