4856ee25baf7491782ab2f8cbccba6214be9915d
Jon Burton <jon.burton@altran-praxis.com>
2012-10-31 17:50:09 +0000

    Renamed directory.

--- lrm/code/proof_check_contract/05/input_port.ads
+++
@@ -1,8 +0,0 @@
-package Input_Port
-  --# own in Inputs;
-is
-   procedure Read_From_Port(Input_Value : out Integer);
-   --# global in Inputs;
-   --# derives Input_Value from Inputs;
-
-end Input_Port;
