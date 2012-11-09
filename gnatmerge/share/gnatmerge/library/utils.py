43c1a8832a60bad58488927925f68a6993f6e462
Jerome Guitton <guitton@adacore.com>
2012-11-09 17:47:53 +0100

    gnatmerge: forgotten module in previous commit [L831-035]

---
+++ gnatmerge/share/gnatmerge/library/utils.py
@@ -0,0 +1,36 @@
+"""Various utility functions
+"""
+
+import types
+
+def attr_str(object, attribute_name, format="%s"):
+    """Formatted string for object.attribute_name
+
+    ...object.attribute_name meaning object.__dict__[attribute_name] here.
+    If attribute is not found or is None, returns an empty string.
+
+    PARAMETERS
+      object: object to dereference
+      attribute_name: name of the attribute in object.
+      format: format string, replace %s by object.attribute_name if
+              attribute_name exists.
+    """
+    if attribute_name in dir(object):
+        value = object.__dict__[attribute_name]
+        if value is not None:
+            return format % full_str(value)
+    return ""
+
+def full_str(value):
+    """Recursive str"""
+    if isinstance(value, set):
+        return "{ %s }" % ', '.join([full_str(e) for e in value])
+    elif isinstance(value, list):
+        return "[%s]" % ', '.join([full_str(e) for e in value])
+    elif isinstance(value, tuple):
+        return "(%s)" % ', '.join([full_str(e) for e in value])
+    elif isinstance(value, dict):
+        return "{ %s }" % ', '.join(["%s : %s" % (k, full_str(value[k]))
+                                     for k in value])
+    else:
+        return str(value)
