ce24e8425bbc5faf0dd17bdce116feab92b3eeef
Duc Hoang <hoang@adacore.com>
2012-12-06 17:16:25 +0100

    Complete loop invariant for prefixsum (also add 1 version which avoids using modulo computation)

--- testsuite/gnatprove/tests/prefix_sum/prefixsum_expanded.adb
+++ testsuite/gnatprove/tests/prefix_sum/prefixsum_expanded.adb
@@ -1,5 +1,5 @@
 with Ada.Text_IO; use Ada.Text_IO;
-package body PrefixSum is
+package body PrefixSum_Expanded is
 
    procedure Upsweep (A : in out Input; Output_Space : out Positive) is
       Space : Positive := 1;
@@ -11,27 +11,20 @@ package body PrefixSum is
       Copy1 := A;
       while Space < A'Length loop
          pragma Loop_Invariant
-           (All_Elements_In (A, Space * Maximum)
+            (All_Elements_In (A, Space * Maximum)
               and then
-           (Space = 1 or Space = 2 or Space = 4)
+            (Space = 1 or Space = 2 or Space = 4)
               and then
-           (for all K in A'Range =>
-              (if (K + 1) mod 8 = 0
-                 and then Space = 8
-               then
-                 A (K) = Copy1 (0) + Copy1 (1) + Copy1 (2) + Copy1 (3) +
-                         Copy1 (4) + Copy1 (5) + Copy1 (6) + Copy1 (7)
-               elsif (K + 1) mod 4 = 0
-                 and then Space >= 4
-               then
-                 A (K) = Copy1 (K) + Copy1 (K-1) + Copy1 (K-2) + Copy1 (K-3)
-               elsif (K + 1) mod 2 = 0
-                 and then Space >= 2
-               then
-                 A (K) = Copy1 (K) + Copy1 (K-1)
-               else
-                 A (K) = Copy1 (K)))
-           );
+            (if Space = 1 then
+               (for all K in A'Range => A(K) = Copy1(K))
+             elsif Space = 2 then
+                (A(1) = Copy1(1) + Copy1(0) and A(3) = Copy1(3) + Copy1(2) and
+                 A(5) = Copy1(5) + Copy1(4) and A(7) = Copy1(7) + Copy1(6) and
+                 A(0) = Copy1(0) and A(2) = Copy1(2) and A(4) = Copy1(4) and A(6) = Copy1(6))
+            elsif Space = 4 then
+                (A(3) = Copy1(3) + Copy1(2) + Copy1(1) + Copy1(0) and A(7) = Copy1(7) + Copy1(6) + Copy1(5) + Copy1(4) and
+                 A(1) = Copy1(1) + Copy1(0) and A(5) = Copy1(5) + Copy1(4) and
+                 A(0) = Copy1(0) and A(2) = Copy1(2) and A(4) = Copy1(4) and A(6) = Copy1(6))));
          pragma Loop_Variant (Increases => Space);
 
          Left := Space - 1;
@@ -64,6 +57,11 @@ package body PrefixSum is
          end loop;
          Space := Space * 2;
       end loop;
+      pragma Assert
+       (A (7) = Copy1 (0) + Copy1 (1) + Copy1 (2) + Copy1 (3) + Copy1 (4) + Copy1 (5) + Copy1 (6) + Copy1 (7) and
+        A (3) = Copy1 (3) + Copy1 (2) + Copy1 (1) + Copy1 (0) and
+        A (5) = Copy1 (5) + Copy1(4) and A (1) = Copy1 (1) + Copy1(0) and
+        A (0) = Copy1 (0) and A (2) = Copy1 (2) and A (4) = Copy1 (4) and A (6) = Copy1 (6));
       Output_Space := Space;
    end Upsweep;
 
@@ -80,29 +78,35 @@ package body PrefixSum is
       A (A'Last) := 0;
       Space      := Space / 2;
       Copy1 := A;
+      pragma Assert
+         (Copy1(0) = Ghost(0)
+          and Copy1(1) = Ghost(0) + Ghost(1)
+          and Copy1(2) = Ghost(2)
+          and Copy1(3) = Ghost(0) + Ghost(1) + Ghost(2) + Ghost(3)
+          and Copy1(4) = Ghost(4)
+          and Copy1(5) = Ghost(4) + Ghost(5)
+          and Copy1(6) = Ghost(6)
+          and Copy1(7) = 0);
       while Space > 0 loop
          pragma Loop_Invariant
            ((Space = 4 or Space = 2 or Space = 1)
               and then
             All_Elements_In (A, (4 / Space) * 8 * Maximum)
               and then
-            (for all K in A'Range =>
-              	 (if Space = 4 then
-                    A(K) = Copy1(K)
-                 elsif Space = 2 and then (K+1) mod 8 = 0 then
-                    A(K) = Copy1(K) +Copy1(K - 2*Space)
-                 elsif Space = 2 and then (K+1) mod 4 = 0 then
-                    A(K) = Copy1(K + 2*Space)
-                 elsif Space = 2 then
-                    A(K) = Copy1(K)
-                 elsif Space = 1 and then (K+1) mod 2 = 0 then
-                    A(1) = Copy1(7) and
-                    A(3) = Copy1(1) + Copy1(7) and
-                    A(5) = Copy1(7) + Copy1(3) and
-                    A(7) = Copy1(5) + Copy1(7) + Copy1(3)
-                 else
-                    A(K) = Copy1(K)
-             )));
+           (for all K in A'Range =>
+              (if Space = 4 then
+                 A(K) = Copy1(K)
+               elsif Space = 2 and (K = 7 or K = 3)  then
+               	 A(7) = Copy1(7) +Copy1(3) and A(3) = Copy1(7)
+               elsif Space = 2 then
+		 A(K) = Copy1(K)
+               elsif (K = 1 or K = 3 or K = 5 or K = 7) then
+                 A(1) = Copy1(7) and
+                 A(3) = Copy1(1) + Copy1(7) and
+                 A(5) = Copy1(7) + Copy1(3) and
+                 A(7) = Copy1(5) + Copy1(7) + Copy1(3)
+               else
+                 A(K) = Copy1(K))));
          pragma Loop_Variant (Decreases => Space);
 
          Right := Space * 2 - 1;
@@ -134,6 +138,15 @@ package body PrefixSum is
          end loop;
          Space := Space / 2;
       end loop;
+      pragma Assert
+        ((A(0) = Copy1(7)
+          and A(1) = Copy1(0) + Copy1(7)
+          and A(2) = Copy1(1) + Copy1(7)
+          and A(3) = Copy1(2) + Copy1(1) + Copy1(7)
+          and A(4) = Copy1(3) + Copy1(7)
+          and A(5) = Copy1(4) + Copy1(3) + Copy1(7)
+          and A(6) = Copy1(3) + Copy1(5) + Copy1(7)
+          and A(7) = Copy1(6) + Copy1(3) + Copy1(5) + Copy1(7)));
    end Downsweep;
 
-end PrefixSum;
+end PrefixSum_Expanded;
