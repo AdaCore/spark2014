268cb8d55e81ee1450c4d8a72ec0541a8c8e0079
Jerome Guitton <guitton@adacore.com>
2013-01-07 14:36:26 +0100

    gnatmerge: minor addition to spec maps API [L830-022]

--- gnatmerge/share/gnatmerge/library/spec_maps.py
+++ gnatmerge/share/gnatmerge/library/spec_maps.py
@@ -19,15 +19,29 @@ class SpecMap:
     """
 
     @log_method
-    def __init__(self, map):
+    def __init__(self, map=None):
         """Constructor
 
         map contains pairs <name> : <expression> that records the
         named expressions that are to be handled in that object.
         """
-        self.map = { key : MergeSpec(map[key], MergeStatusFactory(),
-                                     name=key)
-                     for key in map }
+        self.map = {}
+
+        if map is not None:
+            self.add(map)
+
+    @log_method
+    def add(self, map):
+        """Add entries to map
+
+        PARAMETERS
+          map: Same as in constructor
+        """
+        for key in map:
+            assert(key not in self.map)
+            self.map[key] = MergeSpec(map[key],
+                                      MergeStatusFactory(),
+                                      name=key)
 
     @log_method
     def check(self, model):
@@ -68,28 +82,38 @@ def unit_testing():
                  message="assertion not proved")
 
     spec_map = SpecMap({"PROVED"           : "some OK and no   KO",
-                        "PARTIALLY PROVED" : "some OK and some KO",
+                        "PARTIALLY PROVED" : "some OK",
                         "NOT PROVED"       : "no   OK and some KO",
-                        "NO VCS"           : "no   OK and no   KO"})
+                        "NO VCS"           : "no   OK and no KO"})
+    spec_map.add({"OK SO FAR"        : "no KO",
+                  "STRICTLY PARTIAL" : "some OK and some KO"})
+
     model = {}
-    assert(spec_map.check(model) == {"NO VCS"})
-    assert(spec_map.proof(model, True)  == {"NO VCS"           : set([])})
+    assert(spec_map.check(model) == {"NO VCS", "OK SO FAR"})
+    assert(spec_map.proof(model, True)  == {"NO VCS"           : set([]),
+                                            "OK SO FAR"        : set([])})
     assert(spec_map.proof(model, False) == {"PROVED"           : set([]),
                                             "PARTIALLY PROVED" : set([]),
+                                            "STRICTLY PARTIAL" : set([]),
                                             "NOT PROVED"       : set([])})
 
     model["OK"] = {m1}
-    assert(spec_map.check(model) == {"PROVED"})
-    assert(spec_map.proof(model, True)  == {"PROVED"           : {m1}})
+    assert(spec_map.check(model) == {"PROVED", "PARTIALLY PROVED",
+                                     "OK SO FAR"})
+    assert(spec_map.proof(model, True)  == {"PROVED"           : {m1},
+                                            "OK SO FAR"        : set([]),
+                                            "PARTIALLY PROVED" : {m1}})
     assert(spec_map.proof(model, False) == {"NO VCS"           : {m1},
-                                            "PARTIALLY PROVED" : set([]),
+                                            "STRICTLY PARTIAL" : set([]),
                                             "NOT PROVED"       : {m1}})
 
     model["KO"] = {m2}
-    assert(spec_map.check(model) == {"PARTIALLY PROVED"})
-    assert(spec_map.proof(model, True)  == {"PARTIALLY PROVED" : {m1, m2}})
+    assert(spec_map.check(model) == {"PARTIALLY PROVED", "STRICTLY PARTIAL"})
+    assert(spec_map.proof(model, True)  == {"PARTIALLY PROVED" : {m1},
+                                            "STRICTLY PARTIAL" : {m1, m2}})
     assert(spec_map.proof(model, False) == {"NO VCS"           : {m1, m2},
                                             "PROVED"           : {m2},
+                                            "OK SO FAR"        : {m2},
                                             "NOT PROVED"       : {m1}})
 
     model = {"KO" : {m2, m4}}
@@ -97,4 +121,6 @@ def unit_testing():
     assert(spec_map.proof(model, True)  == {"NOT PROVED"       : {m2, m4}})
     assert(spec_map.proof(model, False) == {"NO VCS"           : {m2, m4},
                                             "PROVED"           : {m2, m4},
-                                            "PARTIALLY PROVED" : set([])})
+                                            "OK SO FAR"        : {m2, m4},
+                                            "PARTIALLY PROVED" : set([]),
+                                            "STRICTLY PARTIAL" : set([])})
