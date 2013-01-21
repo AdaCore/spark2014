02a25be977b05b19145233ad18f4e2d2f154187e
Yannick Moy <moy@adacore.com>
2013-01-21 18:05:19 +0100

    use option --proof=no_split on long tests

--- testsuite/gnatprove/tests/K303-009__aggregate/test.py
+++ testsuite/gnatprove/tests/K303-009__aggregate/test.py
@@ -1,3 +1,4 @@
 from test_support import *
 
-prove_all()
+# do not split unproved VCs to avoid reaching the timeout
+prove_all(opt=["--proof=no_split"])
--- testsuite/gnatprove/tests/power_and_sum/test.py
+++ testsuite/gnatprove/tests/power_and_sum/test.py
@@ -1,3 +1,4 @@
 from test_support import *
 
-prove_all(opt=["-u", "power_and_sum.adb"])
+# do not split unproved VCs to avoid reaching the timeout
+prove_all(opt=["-u", "power_and_sum.adb", "--proof=no_split"])
