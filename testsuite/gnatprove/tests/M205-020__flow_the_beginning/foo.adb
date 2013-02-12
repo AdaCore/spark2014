bc4e30f72ab6d7c012ef65da569614d12ce442ae
Florian Schanda <florian.schanda@altran.com>
2013-02-12 13:53:02 +0000

    [M205-020] flow analysis - first steps
    Further improve the testcase. Annotate with expected errors.

--- testsuite/gnatprove/tests/M205-020__flow_the_beginning/foo.adb
+++ testsuite/gnatprove/tests/M205-020__flow_the_beginning/foo.adb
@@ -1,10 +1,18 @@
-procedure Foo (A       : in     Boolean;
-	       X, Y, Z : in out Integer)
+procedure Foo (A : in     Boolean;
+	       X : in out Integer;
+               Y : in out Integer; --  Initial value of Y not used
+               Z :    out Integer) --  Initial value of Z used
 is
-   B : Boolean;
+   B : Boolean;  --  Initial value used
 begin
 
-   if A or B then
+   if A then   --  Pointless
+      Y := X;  --  Pointless
+   else
+      Y := Z;  --  Pointless
+   end if;
+   Y := 3;
+   if A or B then  --  Use of B
       return;
    else
       X := 0;
@@ -12,3 +20,8 @@ begin
    Y := 0;
 
 end Foo;
+
+--  Final derives should be:
+--  X from X, A, [B]
+--  Y from    A, [B]
+--  Z from            Z
