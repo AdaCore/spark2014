1f62357a8b847f5e73bd8d1a35868ec25377886f
Yannick Moy <moy@adacore.com>
2013-02-15 13:05:40 +0100

    Add test

---
+++ testsuite/gnatprove/tests/KC15-034__target_config/testtarg.adb
@@ -0,0 +1,9 @@
+with System; use System;
+with Interfaces.C; use Interfaces.C;
+procedure TestTarg is
+begin
+   pragma Assert (wchar_t'Size = 32);
+   pragma Assert (Long_Long_Integer'Size = 64);
+   pragma Assert (Float'Size = 64);
+   pragma Assert (Long_Float'Size = 64);
+end;
