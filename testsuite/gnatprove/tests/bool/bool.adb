715f8d2febca89fe29dacd00978b87485e447519
Yannick Moy <moy@adacore.com>
2013-02-27 11:37:29 +0100

    Modify test to make it more elegant

--- testsuite/gnatprove/tests/bool/bool.adb
+++ testsuite/gnatprove/tests/bool/bool.adb
@@ -1,17 +1,20 @@
 procedure Bool is
    --  in place swapping of Boolean values
    procedure Swap (X, Y : in out Boolean) with
-     Post => X = not Y'Old and Y = not X'Old
+     Post => X = Y'Old and Y = X'Old
    is
    begin
       X := X xor Y;
-      Y := X = Y;  --  Y is not X'Old
+      Y := X = Y;    --  Y is not X'Old
       X := X xor Y;  --  X is not Y'Old
+      X := not X;    --  X is Y'Old
+      Y := not Y;    --  Y is X'Old
    end Swap;
 
    X, Y : Boolean;
 begin
-   X := True; Y := False; Swap (X, Y); Swap (X, Y);
+   X := True;  Y := False; Swap (X, Y);
+   X := False; Y := True;  Swap (X, Y);
    X := False; Y := False; Swap (X, Y);
-   X := True; Y := True; Swap (X, Y);
+   X := True;  Y := True;  Swap (X, Y);
 end Bool;
