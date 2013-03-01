bfdd245fe158a1904fb0549d87b4f76786e99042
Yannick Moy <moy@adacore.com>
2013-03-01 10:35:21 +0100

    Add example from Robert's Dr Dobbs article

---
+++ testsuite/gnatprove/tests/duplicates/duplicates.ads
@@ -0,0 +1,21 @@
+package Duplicates is
+
+   type Int_Array is array (Natural range <>) of Integer;
+
+   function Has_Duplicates(Arr : Int_Array) return Boolean is
+      (for some I in Arr'First .. Arr'Last-1 =>
+         (for some J in I+1 .. Arr'Last => Arr(I)=Arr(J)));
+
+   procedure Dedupe (Arr: in out Int_Array; Last : out Natural) with
+     Pre => Has_Duplicates(Arr),
+     Post => not Has_Duplicates(Arr(Arr'First .. Last))
+       and then (for all Item of Arr'Old =>
+                   (for some J in Arr'First .. Last =>
+                      Item = Arr(J)))
+                -- Only duplicates removed
+       and then (for all J in Arr'First .. Last =>
+                   (for some Item of Arr'Old =>
+                      Item = Arr(J)));
+                -- Nothing new added
+
+end Duplicates;
