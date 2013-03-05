32310a5e6ad7f8bfe8e94c14b87d71c47a160447
Florian Schanda <florian.schanda@altran.com>
2013-03-05 13:36:15 +0000

    [M304-013] flow analysis - check contracts
    More testcases.

--- testsuite/gnatprove/tests/M304-013__flow_check_contracts/test.adb
+++ testsuite/gnatprove/tests/M304-013__flow_check_contracts/test.adb
@@ -1,15 +1,69 @@
 package body Test is
 
-   procedure Swap_01 (X, Y : in out Integer)
+   G : Integer;
+
+   --  procedure Swap_01_A (X, Y : in out Integer)
+   --    with Depends => (X => Y,
+   --                     Y => X)
+   --  is
+   --  begin
+   --     X := Y;
+   --     Y := X;
+   --     --  Well, of course this won't work.
+   --  end Swap_01_A;
+
+   procedure Swap_01_B (X, Y : in out Integer)
      with Depends => (X => Y,
                       Y => X);
 
-   procedure Swap_01 (X, Y : in out Integer)
+   procedure Swap_01_B (X, Y : in out Integer)
    is
    begin
       X := Y;
       Y := X;
       --  Well, of course this won't work.
-   end Swap_01;
+   end Swap_01_B;
+
+   procedure Test_01
+     with Global  => (Output => G),
+          Depends => (G => null);
+
+   procedure Test_01
+   is
+   begin
+      G := 10;
+   end Test_01;
+
+   procedure Test_02 (X : in out Integer)
+     with Global  => (Output => G),
+          Depends => (G => null,
+                      X => X);
+
+   procedure Test_02 (X : in out Integer)
+   is
+   begin
+      X := X + 1;
+   end Test_02;
+
+   procedure Test_03 (X : in out Integer)
+     with Global  => (Input => G),
+          Depends => (X => X,
+                      null => G);
+
+   procedure Test_03 (X : in out Integer)
+   is
+   begin
+      X := X + 1;
+   end Test_03;
+
+   procedure Test_04 (X : in out Integer)
+     with Depends => (X => X,
+                      null => G);
+
+   procedure Test_04 (X : in out Integer)
+   is
+   begin
+      X := X + 1;
+   end Test_04;
 
 end Test;
