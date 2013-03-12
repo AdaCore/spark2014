917ce57d5e22b4f10d0d529737d25cc9d79d0481
Yannick Moy <moy@adacore.com>
2013-03-12 10:15:57 +0100

    [K610-014] Use convention Ada for imported function
    Previous convention Intrinsic resulted in the function being known to
    the prover, which is not the intent in this test.

--- testsuite/gnatprove/tests/K610-014__import/p.ads
+++ testsuite/gnatprove/tests/K610-014__import/p.ads
@@ -1,7 +1,7 @@
 package P is
    type T is new Integer;
    function "<"  (Left, Right : T) return Boolean;
-   pragma Import (Intrinsic, "<");
+   pragma Import (Ada, "<");
 
    function Compare (Left, Right : T) return Boolean
      with Post => (if Compare'Result then not (Left = Right));
