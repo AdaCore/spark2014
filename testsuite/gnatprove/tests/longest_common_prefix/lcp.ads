eff2a73d2d6d36973436fd55e724b8b0e19c58ea
Yannick Moy <moy@adacore.com>
2013-03-21 12:10:33 +0100

    [LA19-005] Better postcondition for challenge 1

--- testsuite/gnatprove/tests/longest_common_prefix/lcp.ads
+++ testsuite/gnatprove/tests/longest_common_prefix/lcp.ads
@@ -4,6 +4,6 @@ function LCP (A : Text; X, Y : Integer) return Natural with
   Pre  => X in A'Range and then Y in A'Range,
   Post =>
     (for all K in 0 .. LCP'Result - 1 => A (X + K) = A (Y + K))
-      and then (X + LCP'Result > A'Last
-                  or else Y + LCP'Result > A'Last
+      and then (X + LCP'Result = A'Last + 1
+                  or else Y + LCP'Result = A'Last + 1
                   or else A (X + LCP'Result) /= A (Y + LCP'Result));
