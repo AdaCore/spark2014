17d8696195abfefdf1d30b2aae0fbadbfbe23278
Yannick Moy <moy@adacore.com>
2013-04-09 10:49:15 +0200

    [M329-006] Add contract cases to source and update output

--- testsuite/gnatprove/tests/longest_common_prefix/lcp.ads
+++ testsuite/gnatprove/tests/longest_common_prefix/lcp.ads
@@ -6,4 +6,8 @@ function LCP (A : Text; X, Y : Integer) return Natural with
     (for all K in 0 .. LCP'Result - 1 => A (X + K) = A (Y + K))
       and then (X + LCP'Result = A'Last + 1
                   or else Y + LCP'Result = A'Last + 1
-                  or else A (X + LCP'Result) /= A (Y + LCP'Result));
+                  or else A (X + LCP'Result) /= A (Y + LCP'Result)),
+  Contract_Cases =>
+     (A (X) /= A (Y) => LCP'Result = 0,
+      X = Y          => LCP'Result = A'Last - X + 1,
+      others         => LCP'Result > 0);
