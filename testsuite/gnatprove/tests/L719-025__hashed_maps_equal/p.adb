4f512e0d05dc6f9bbdbdfb1da65c63701770340b
dross <dross@adacore.com>
2013-04-17 10:37:34 +0200

    [L719-025] add theory files for formal containers and some tests
    only doubly_liked_lists, ordered_maps and hashed_maps are working for now

---
+++ testsuite/gnatprove/tests/L719-025__hashed_maps_equal/p.adb
@@ -0,0 +1,25 @@
+package body P is
+
+   procedure Identity (L : in out Map; K : Key_Type) is
+   begin
+      Insert (L, K, 1);
+      Delete (L, K);
+   end Identity;
+
+   procedure Nearly_Identity (L : in out Map; K : Key_Type) is
+      E : Element_Type := Element (L, K);
+   begin
+      Delete (L, K);
+      Insert (L, K, E);
+   end Nearly_Identity;
+
+   procedure My_Include (L : in out Map; K : Key_Type; E : Element_Type) is
+   begin
+      if Contains (L, K) then
+         Replace (L, K, E);
+      else
+         Insert (L, K, E);
+      end if;
+   end My_Include;
+
+end P;
---
+++ testsuite/gnatprove/tests/L719-025__hashed_sets_equal/p.ads
@@ -0,0 +1,29 @@
+with Ada.Containers.Formal_Hashed_Sets;
+with Ada.Containers; use Ada.Containers;
+package P is
+
+   type Element_Type is new Integer range 1 .. 100;
+
+   function My_Eq (I1 : Element_Type; I2 : Element_Type) return Boolean is
+     (I1 = I2);
+
+   function Hash (Id : Element_Type) return Hash_Type is (Hash_Type (Id));
+
+   package My_Sets is new Ada.Containers.Formal_Hashed_Sets
+     (Element_Type, Hash, My_Eq, My_Eq);
+   use My_Sets;
+
+   procedure My_Include (L : in out Set; E : Element_Type) with
+     Pre => Contains (L, E) or Length (L) < L.Capacity,
+     Post => Element (L, Find (L, E)) = E;
+
+   procedure Identity (L : in out Set; E : Element_Type) with
+     Pre => Length (L) < L.Capacity and not Contains (L, E),
+     Post => Strict_Equal (L, L'Old);
+
+   procedure Nearly_Identity (L : in out Set; Cu : in out Cursor) with
+     Pre => Has_Element (L, Cu),
+     Post => L = L'Old and
+     (if Cu = Cu'Old then Strict_Equal (L, L'Old));
+
+end P;
