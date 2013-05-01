4dbbf20954e3e1ba7ae145614141c98a729e8cc6
Yannick Moy <moy@adacore.com>
2013-05-01 12:25:20 +0200

    [M405-024] Add inermediate versions of example in Getting Started

---
+++ docs/ug/linear_search/search_ada/search.ads
@@ -0,0 +1,15 @@
+pragma SPARK_Mode;
+
+package Search is
+
+   type Index is range 1 .. 10;
+   type Element is new Integer;
+
+   type Arr is array (Index) of Element;
+
+   function Linear_Search 
+     (A        : Arr; 
+      Val      : Element; 
+      At_Index : out Index) return Boolean;
+
+end Search;
---
+++ docs/ug/linear_search/search_prove/search.adb
@@ -0,0 +1,24 @@
+package body Search is
+   
+   function Linear_Search 
+     (A   : Arr; 
+      Val : Element) return Search_Result
+   is
+      Pos : Index := A'First;
+      Res : Search_Result;
+   begin
+      while Pos < A'Last loop
+         if A(Pos) = Val then
+            Res := (Found    => True,
+                    At_Index => Pos);
+            return Res;
+         end if;
+
+         Pos := Pos + 1;
+      end loop;
+
+      Res := (Found => False);
+      return Res;
+   end Linear_Search;
+   
+end Search;
