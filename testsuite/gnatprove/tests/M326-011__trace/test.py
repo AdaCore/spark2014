5e296c463acdf6ea1e127f8ac2299c80bf62934c
Johannes Kanig <kanig@adacore.com>
2013-05-03 15:43:14 +0200

    M415-028 update test output

--- testsuite/gnatprove/tests/M326-011__trace/test.py
+++ testsuite/gnatprove/tests/M326-011__trace/test.py
@@ -3,7 +3,7 @@ import glob
 import os.path
 
 prove_all(opt=["--limit-line=test.ads:4", "--proof=then_split"], steps=10)
-cat (os.path.join("gnatprove","test.ads_4_30_postcondition.trace"), sort=True)
+cat (os.path.join("gnatprove","test.ads_4_18_postcondition.trace"), sort=True)
 prove_all(opt=["--limit-line=test2.ads:4", "--proof=then_split"], steps=10)
-cat (os.path.join("gnatprove","test2.ads_4_30_postcondition.trace"), sort=True)
+cat (os.path.join("gnatprove","test2.ads_4_18_postcondition.trace"), sort=True)
 
