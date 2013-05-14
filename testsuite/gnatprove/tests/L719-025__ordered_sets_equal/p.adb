d8d4da08bd2a401a0b6454df876ac5e916126239
dross <dross@adacore.com>
2013-05-14 14:10:40 +0200

    [L719-025] Why3 axiomatization for ordered sets

---
+++ testsuite/gnatprove/tests/L719-025__ordered_sets_equal/p.adb
@@ -0,0 +1,30 @@
+package body P is
+
+   procedure Identity (L : in out Set; E : Element_Type) is
+      Cu : Cursor;
+      I : Boolean;
+   begin
+      Insert (L, E, Cu, I);
+      pragma Assert (I);
+      Delete (L, Cu);
+   end Identity;
+
+   procedure Nearly_Identity (L : in out Set; Cu : in out Cursor) is
+      E : Element_Type := Element (L, Cu);
+      I : Boolean;
+   begin
+      Delete (L, Cu);
+      Insert (L, E, Cu, I);
+      pragma Assert (I);
+   end Nearly_Identity;
+
+   procedure My_Include (L : in out Set; E : Element_Type) is
+   begin
+      if Contains (L, E) then
+         Replace (L, E);
+      else
+         Insert (L, E);
+      end if;
+   end My_Include;
+
+end P;
