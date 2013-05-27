43366eb1af42572d2f718f2fa07d2f5be5dea6d9
Johannes Kanig <kanig@adacore.com>
2013-05-27 17:37:53 +0200

    M521-010 File test case

---
+++ testsuite/gnatprove/tests/M521-010__array/array_aggregates.ads
@@ -0,0 +1,7 @@
+----------------------------------------------------------------------------
+--  These are test VCs which exercise array aggregates.
+----------------------------------------------------------------------------
+package Array_Aggregates
+is
+   pragma Elaborate_Body (Array_Aggregates);
+end Array_Aggregates;
