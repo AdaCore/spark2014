31771bc0e7307b83a94780248da4a483953b35fa
Yannick Moy <moy@adacore.com>
2013-05-30 17:38:43 +0200

    Minor change to bump version number

--- hilitevsn.ads
+++ hilitevsn.ads
@@ -24,7 +24,7 @@
 
 package Hilitevsn is
 
-   Hilite_Static_Version_String : constant String := "0.2w";
+   Hilite_Static_Version_String : constant String := "0.9.0w";
    --  Static string identifying this version, that can be used as an argument
    --  to e.g. pragma Ident.
    --
