dc497d968dae3b950b448d9a717ec8fb09a39e5b
Johannes Kanig <kanig@adacore.com>
2013-05-31 12:32:19 +0200

    M529-013 file test case

---
+++ testsuite/gnatprove/tests/M529-013__modular/modular.ads
@@ -0,0 +1,10 @@
+------------------------------------------------------------------------------
+--  These are test VCs which exercise modular arithmetic.Guess which FDL
+--  operator will appear int the tests....
+------------------------------------------------------------------------------
+
+
+package Modular
+is
+   pragma Elaborate_Body (Modular);
+end Modular;
