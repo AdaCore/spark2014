0368ec787afed5284caf9431f7e894c3cb04282e
Johannes Kanig <kanig@adacore.com>
2013-06-07 16:04:53 +0200

    M604-004 improve test and file correct output

--- testsuite/gnatprove/tests/M604-044__overflow/misovattr.adb
+++ testsuite/gnatprove/tests/M604-044__overflow/misovattr.adb
@@ -5,6 +5,8 @@ is
    begin
       pragma Assert (Enum_S'Succ (A) > A);
       pragma Assert (Enum_T'Succ (A) > A);
+      pragma Assert (Enum_S'Pred (A) < A);
+      pragma Assert (Enum_T'Pred (A) < A);
       return A;
    end Range_Check_Error;
 end Misovattr;
