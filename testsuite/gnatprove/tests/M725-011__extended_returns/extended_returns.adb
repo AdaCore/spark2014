d7c04d2438d8edf5b1d0e1c0bb4da61aab5bb2a0
Pavlos Efstathopoulos <pavlos.efstathopoulos@altran-praxis.com>
2013-07-25 12:19:17 +0100

    [M725-011] mark extended returns in-SPARK
    A basic test has now been added in GNATprove's testsuite
    for extended returns.

---
+++ testsuite/gnatprove/tests/M725-011__extended_returns/extended_returns.adb
@@ -0,0 +1,17 @@
+package body Extended_Returns
+is
+   function Init (Discr    : Integer;
+                  Init_Val : Natural) return Record_T
+   is
+   begin
+      return Result : Record_T (Discr) do
+         Result.A := Init_Val;
+         if Discr = 0 then
+            Result.B := Init_Val;
+         elsif Discr = 1 then
+            Result.C := Init_Val;
+            Result.D := Init_Val;
+         end if;
+      end return;
+   end Init;
+end Extended_Returns;
---
+++ testsuite/gnatprove/tests/M725-011__extended_returns/extended_returns.ads
@@ -0,0 +1,19 @@
+package Extended_Returns
+is
+   type Record_T (Discr : Natural := 0) is
+      record
+         A : Integer;
+         case Discr is
+            when 0 =>
+               B : Integer;
+            when 1 =>
+               C, D : Integer;
+            when others =>
+               null;
+         end case;
+      end record;
+
+   function Init (Discr    : Integer;
+                  Init_Val : Natural) return Record_T
+      with Post => Init'Result.A = Init_Val;
+end Extended_Returns;
