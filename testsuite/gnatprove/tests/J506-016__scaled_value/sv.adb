779614e014e55356e775a6331c1f52aba5a92956
Arnaud Charlet <charlet@adacore.com>
2013-07-26 11:59:22 +0200

    File missing sources.

---
+++ testsuite/gnatprove/tests/J506-016__scaled_value/sv.adb
@@ -0,0 +1,12 @@
+package body SV is
+
+   function Scale
+     (Capacity           : Capacity_Type;
+      Requested_Capacity : Sum_Type;
+      Value              : Request_Type) return Request_Type
+   is
+   begin
+      return (Value * Capacity) / Requested_Capacity;
+   end Scale;
+
+end SV;
---
+++ testsuite/gnatprove/tests/J506-016__scaled_value/sv.ads
@@ -0,0 +1,17 @@
+package SV is
+
+   type Base_Type is mod 2 ** 64;
+   for Base_Type'Size use 64;
+
+   subtype Capacity_Type is Base_Type range 1 .. 2 ** 32 - 1;
+   subtype Request_Type is Base_Type range 0 .. 2 ** 32 - 1;
+   subtype Sum_Type is Base_Type range 0 .. Base_Type'Last / 1000;
+
+   function Scale
+     (Capacity           : Capacity_Type;
+      Requested_Capacity : Sum_Type;
+      Value              : Request_Type) return Request_Type with
+     Pre => Requested_Capacity > Capacity and Value <= Capacity,
+     Post => Scale'Result <= Capacity;
+
+end SV;
