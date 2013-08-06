b9fd3b2ef34eb000876a7047da1e6a2b689681d4
Johannes Kanig <kanig@adacore.com>
2013-08-06 17:36:44 +0200

    KB25-022 file test case

---
+++ testsuite/gnatprove/tests/KB25-022__shift/shift_main.adb
@@ -0,0 +1,37 @@
+with Interfaces; use Interfaces;
+
+procedure Shift_Main is
+
+   function P (A : Unsigned_64) return Unsigned_64 is
+   begin
+      return Shift_Right (A, 5);
+   end P;
+
+   function Q (A : Unsigned_64) return Unsigned_64 is
+   begin
+      return Shift_Left (A, 5);
+   end Q;
+
+   function R (A : Unsigned_64) return Unsigned_64 is
+   begin
+      return Rotate_Left (A, 5);
+   end R;
+
+   function S (A : Unsigned_64) return Unsigned_64 is
+   begin
+      return Rotate_Right (A, 5);
+   end S;
+
+   function T (A : Unsigned_64) return Unsigned_64 is
+   begin
+      return Shift_Right_Arithmetic (A, 5);
+   end T;
+
+   A : Unsigned_64 := 12;
+begin
+   A := P (A);
+   A := Q (A);
+   A := R (A);
+   A := S (A);
+   A := T (A);
+end;
