d84e83cd28262dc21889ad6604fecc330765819b
Yannick Moy <moy@adacore.com>
2013-09-17 15:55:48 +0200

    [M917-032] New test for SPARK_Mode

---
+++ testsuite/gnatprove/tests/M917-032__spark_mode/arith.adb
@@ -0,0 +1,32 @@
+package body Arith is
+
+   function Sum (X, Y : Integer) return Integer is
+   begin
+      return X + Y;
+   end Sum;
+
+   function Sum2 (X, Y : Integer) return Integer is
+   begin
+      return X + Y;
+   end Sum2;
+
+   function Sum_Array (X, Y : Int_Array) return Int_Array is
+      Z : Int_Array;
+   begin
+      for J in Index loop
+         Z(J) := X(J) + Y(J);
+      end loop;
+      return Z;
+   end Sum_Array;
+
+   function Sum_Array2 (X, Y : Int_Array) return Int_Array is
+      Z : Int_Array;
+   begin
+      for J in Index loop
+         Z(J) := X(J) + Y(J);
+         pragma Loop_Invariant (for all K in 1 .. J => Z(K) = X(K) + Y(K));
+      end loop;
+      return Z;
+   end Sum_Array2;
+
+end Arith;
