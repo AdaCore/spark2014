c8676eef5cd2dc13ddef4c0684099f6ba0ea0ca1
dross <dross@adacore.com>
2013-10-07 16:11:47 +0200

    [M614-033] Update stack example
    Not fully proved:
    Miss information about last element of an unconstrained array
    see MA01-027

--- testsuite/gnatprove/tests/M607-025__rev_stack/list.adb
+++ testsuite/gnatprove/tests/M607-025__rev_stack/list.adb
@@ -9,17 +9,34 @@ package body List is
      (L : List)
       return List
    is
-      S   : Stack.Stack;
-      Res : List;
+      S     : Stack.Stack;
+      Res   : List;
+      First : constant Integer := First_Index (L);
+      Last  : constant Integer := Last_Index (L);
    begin
-      for I in First_Index (L) .. Last_Index (L) loop
+      for I in First .. Last loop
          Push (S, Element (L, I));
+         pragma Loop_Invariant
+         -- The size of the model of the stack increases with each push
+           (Model.To (S)'Last = I - First + 1 and then
+            -- the model of the stack contains each element of the list in order
+              (for all J in First .. I => Model.To (S)(J) = Element (L, J)));
       end loop;
 
-      for I in  First_Index (L) .. Last_Index (L) loop
+      for I in  First .. Last loop
          Append (Res, Top (S));
          Pop (S);
-         pragma Loop_Invariant (for all J in 1..I => Element (Res, J) = Element (L, Last_Index (L) - J + 1));
+         pragma Loop_Invariant
+         -- The size of the List increases with each Append
+           (Natural (Length (Res)) = I - First + 1 and then
+            -- The size of the model of the stack decreases with each pop
+            Model.To (S)'Last = Last - I and then
+            -- The remaining of the model of the stack is unchanged
+              (for all J in First .. Last - I =>
+                   Model.To (S) (J) = Model.To (S'Loop_Entry)(J)) and then
+            -- the Post of the subprogram is partially valid at this stage
+              (for all J in First .. I =>
+                   Element (Res, J) = Element (L, Last_Index (L) - J + 1)));
       end loop;
 
       return Res;
--- testsuite/gnatprove/tests/M607-025__rev_stack/list.ads
+++ testsuite/gnatprove/tests/M607-025__rev_stack/list.ads
@@ -1,6 +1,7 @@
 with Ada.Containers.Formal_Vectors;
 
 package List is 
+   pragma Spark_Mode (On);
 
    Max : constant := 100;
    subtype T is integer range 1 .. Max;
@@ -10,6 +11,6 @@ package List is
 
    function Reverse_List (L : List) return List with
      Post => (for all I in First_Index (L) .. Last_Index (L) =>
-                Element (L, I) = Element (Reverse_List'Result, Last_Index (L) - I + 1));
+                  Element (L, I) = Element (Reverse_List'Result, Last_Index (L) - I + 1));
 
 end List;
