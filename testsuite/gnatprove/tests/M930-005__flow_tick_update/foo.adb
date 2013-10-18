df4f3be72e4656a0b87b612a25a8719100c17188
Florian Schanda <florian.schanda@altran.com>
2013-10-18 18:15:25 +0100

    [M930-005] flow - tick update
    Add another test.

--- testsuite/gnatprove/tests/M930-005__flow_tick_update/foo.adb
+++ testsuite/gnatprove/tests/M930-005__flow_tick_update/foo.adb
@@ -9,21 +9,28 @@ is
 
    type Arr is array (1 .. 10) of Integer;
 
-   --  procedure Test_01 (A, B, C : Integer;
-   --                     D, E, F : out Integer)
-   --  with Global => null,
-   --       Depends => (D => A,
-   --                   E => B,
-   --                   F => C)
-   --  is
-   --     Tmp : Coordinate_T := (A, A, A);
-   --  begin
-   --     Tmp := Tmp'Update (Y => B,
-   --                        Z => C);
-   --     D := Tmp.X;
-   --     E := Tmp.Y;
-   --     F := Tmp.Z;
-   --  end Test_01;
+   type CArr is array (1 .. 5) of Coordinate_T;
+
+   type Var_CArray is record
+      N : Integer;
+      A : CArr;
+   end record;
+
+   procedure Test_01 (A, B, C : Integer;
+                      D, E, F : out Integer)
+   with Global => null,
+        Depends => (D => A,
+                    E => B,
+                    F => C)
+   is
+      Tmp : Coordinate_T := (A, A, A);
+   begin
+      Tmp := Tmp'Update (Y => B,
+                         Z => C);
+      D := Tmp.X;
+      E := Tmp.Y;
+      F := Tmp.Z;
+   end Test_01;
 
    procedure Test_02 (A          : in out Arr;
                       X, Y, Z, N : Integer;
@@ -37,5 +44,15 @@ is
                     Y => Z);
       W := A'Update(N => 3) (9);
    end Test_02;
+
+   procedure Test_03 (A    : in out Var_CArray;
+                      N, M : Integer)
+   with
+     Global  => null,
+     Depends => (A => (A, N, M))
+   is
+   begin
+      A := A'Update(A => A.A'Update (N => A.A (M)));
+   end Test_03;
 
 end Foo;
