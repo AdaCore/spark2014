6c73ad1d03425971fa85781520225705de7f35a7
Angela Wallenburg <angelaw@sparkdev09.praxis.local>
2013-10-21 14:36:57 +0100

    [LC17-035] added tests for 'Update

---
+++ testsuite/gnatprove/tests/LC17-035_update_attribute/update.adb
@@ -0,0 +1,76 @@
+package body Update is
+
+   function F1 (Init_Val : Integer) return Array_1D is
+      Arr : Array_1D := Array_1D'(others => Init_Val + 1);
+   begin
+      Arr(1) := X;
+      return Arr;
+   end F1;
+
+   procedure Basic_Array_Update
+     (A: in out Array_1D; I: in Index; New_Val: Integer) is
+   begin
+     A(I) := New_Val;
+   end Basic_Array_Update;
+
+   procedure Basic_Array_Update2
+     (A: in out Array_1D; I: in Index; New_Val: Integer) is
+   begin
+     A(I) := New_Val;
+   end Basic_Array_Update2;
+
+   function F2 (Arr_In : Array_1D; I : Index; J : Index) return Array_1D is
+   begin
+      return Arr_In'Update(1..I => 7, I..J => X, 3 => 5);
+   end F2;
+
+   function F3 (Arr_In: Array_1D) return Array_1D is
+      Arr : Array_1D := Arr_In;
+   begin
+      Arr(1) := X;
+      Arr(2) := 10;
+      return Arr;
+   end F3;
+
+   function F4 (Arr_In : Array_1D; I : Index; J : Index) return Array_1D is
+   begin
+      return Arr_In'Update(1..I => 7, I..J => X, 3 => 5);
+   end F4;
+
+   function F5 (Arr_In: Array_1D) return Array_1D is
+      Arr : Array_1D := Arr_In;
+   begin
+      Arr(1) := X;
+      Arr(2) := 10;
+      return Arr;
+   end F5;
+
+   procedure My_Init_Array (A: out Array_1D) is
+   begin
+      A := Array_1D'(others => 1);
+      A := A'Update(3..5 => 3, 3 => 2);
+   end My_Init_Array;
+
+   procedure My_Init_Array2 (A: out Array_1D; I : in Index) is
+   begin
+      A := Array_1D'(others => 1);
+      A := A'Update(I => 2, 4..5 => X);
+   end My_Init_Array2;
+
+   procedure Swap_Elements (I,J: in Index; A: in out Array_1D) is
+      Tmp: Integer;
+   begin
+      Tmp  := A(I);
+      A(I) := A(J);
+      A(J) := Tmp;
+   end Swap_Elements;
+
+   function Swap_Fun (Arr_In : Array_1D; I, J : Index) return Array_1D is
+      Arr : Array_1D := Arr_In;
+   begin
+      Arr(I) := Arr_In(J);
+      Arr(J) := Arr_In(I);
+      return Arr;
+   end Swap_Fun;
+
+end Update;
---
+++ testsuite/gnatprove/tests/LC17-035_update_attribute/update.ads
@@ -0,0 +1,78 @@
+package Update is
+
+   pragma SPARK_Mode (On);
+
+   type Index is range 1 .. 8;
+
+   type Array_1D is array (Index) of Integer;
+   type Array_2D is array (Index, Index) of Integer;
+
+   type Array_3D is array (Natural range 1 .. 5,
+                           Natural range 1 .. 5,
+                           Natural range 1 .. 5) of Integer;
+
+   X : Integer;
+   An_Arr : Array_1D := Array_1D'(others => 7);
+
+   ----------------------------------------------------------------------------
+   -- 'Update Array tests
+   ----------------------------------------------------------------------------
+
+   --  aggregate reference test, compare WHY VC models
+   function F1 (Init_Val : Integer) return Array_1D
+     with Post => (F1'Result =
+                     Array_1D'(1..2   => X,
+                               3      => Init_Val + 1,
+                               Others => An_Arr(5)));
+
+   --  basic array update, loop reference test
+   procedure Basic_Array_Update (A: in out Array_1D;
+                                 I: in Index;
+                                 New_Val: in Integer)
+     with Post => (for all J in Index =>
+                     (if I /= J then A(J) = A'Old(J) else A(J) = New_Val));
+
+   --  same basic array test using 'Update, one dynamic choice,
+   --  prefix is in/out parameter
+   procedure Basic_Array_Update2 (A: in out Array_1D;
+                                  I: in Index;
+                                  New_Val: in Integer)
+     with Post => A = A'Old'Update(I => New_Val);
+
+   --  dynamic choice interval
+   function F2 (Arr_In : Array_1D; I : Index; J : Index) return Array_1D
+     with Post => (F2'Result = Arr_In'Update(1..I => 7));
+
+   --  overlapping choice intervals
+   function F3 (Arr_In : Array_1D) return Array_1D
+     with Post => (F3'Result = Arr_In'Update(1..5 => X,
+                                             2    => 1,
+                                             3    => An_Arr(4)));
+
+   --  several dynamic choices, overlapping intervals
+   function F4 (Arr_In : Array_1D; I : Index; J : Index) return Array_1D
+     with Post => (F4'Result = Arr_In'Update(1..I => 10, I..J => 20));
+
+   --  single association (special case in gnat2why code)
+   function F5 (Arr_In : Array_1D) return Array_1D
+     with Post => (F5'Result = Arr_In'Update(2 => 1));
+
+   --  prefix is aggregate
+   procedure My_Init_Array (A: out Array_1D)
+     with Post => A = Array_1D'(others => 1)'Update(3 => 2, 4..5 => 3);
+
+   --  prefix is aggregate, dynamic choice
+   procedure My_Init_Array2 (A: out Array_1D; I : in Index)
+     with Post => A = Array_1D'(others => 1)'Update(I => 2, 4..5 => X);
+
+   --  prefix is 'Old attribute
+   procedure Swap_Elements (I, J: in Index; A: in out Array_1D)
+     --  with Post => (A = A'Old'Update(I => A'Old(J), J => A'Old(I)));
+     with Post => (A = A'Old'Update(3 => 5, 2 => 6));
+
+   --  array update, swap inversion check, prefix is 'Result
+   function Swap_Fun (Arr_In : Array_1D; I, J : Index) return Array_1D
+     with Post => (Swap_Fun'Result'Update(I => Swap_Fun'Result(J),
+                                          J => Swap_Fun'Result(I)) = Arr_In);
+
+end Update;
