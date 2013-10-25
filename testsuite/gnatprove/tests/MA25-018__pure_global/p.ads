bfb931e632b81f25fae133aeab2776a38482368d
Arnaud Charlet <charlet@adacore.com>
2013-10-25 15:28:33 +0200

    MA25-018 - file test case

---
+++ testsuite/gnatprove/tests/MA25-018__pure_global/p.ads
@@ -0,0 +1,10 @@
+package P
+  with SPARK_Mode => On
+is
+   procedure Op1 (X, Y : in Integer; Z : out Integer)
+     with Depends => (Z => (X, Y));
+
+   procedure Op2 (X, Y : in Integer; Z : out Integer)
+     with Depends => (Z => (X, Y));
+   
+end P;
