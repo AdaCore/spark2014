ad4fa457a4fafd6c1d0fbab405337626fdb03d21
Arnaud Charlet <charlet@adacore.com>
2013-10-27 12:42:20 +0100

    Minor editing, remove redundant parens.

--- docs/lrm/code/assert_loop_contract/14/assert_loop_14.adb
+++ docs/lrm/code/assert_loop_contract/14/assert_loop_14.adb
@@ -1,13 +1,14 @@
 package body Assert_Loop_14 is
-   function Value_Present (A: A_Type; X : Integer) return Boolean is
+   function Value_Present (A : A_Type; X : Integer) return Boolean is
       I : Index := Index'First;
    begin
       while A (I) /= X and I < Index'Last loop
          pragma Loop_Invariant
            (I < Index'Last
-            and (for all M in Index'First .. I => (A(M) /= X)));
+            and (for all M in Index'First .. I => A (M) /= X));
          I := I + 1;
       end loop;
+
       return A (I) = X;
    end Value_Present;
 end Assert_Loop_14;
