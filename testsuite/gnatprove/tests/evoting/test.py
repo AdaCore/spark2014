e765319de7bd450061537c5d7ed27da68d229003
Johannes Kanig <kanig@adacore.com>
2013-11-04 17:57:56 +0100

    MA23-007 update test output
    also put test evoting back to "then_split"

--- testsuite/gnatprove/tests/evoting/test.py
+++ testsuite/gnatprove/tests/evoting/test.py
@@ -1,10 +1,6 @@
 from test_support import *
 
-prove(steps=400)
-
-# Do not use splitting of VCs because it causes differences between
-# machines in nightly tests (MA23-007)
-# prove(steps=400, opt=["--proof=then_split"])
+prove(steps=400, opt=["--proof=then_split"])
 
 # Cannot activate flow analysis. There is a legit issue with
 # Ada.Text_IO [MA04-032]
