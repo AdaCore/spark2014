f127a447192b29c97e6f3481c18967f052c44d77
Yannick Moy <moy@adacore.com>
2013-11-13 22:06:45 +0100

    [MB13-065] Add test

---
+++ testsuite/gnatprove/tests/MB13-065__refined_global/pack.adb
@@ -0,0 +1,13 @@
+pragma SPARK_Mode;
+
+package body Pack with
+  Refined_State => (Content => (A, B))
+is
+   A, B : Integer := 0;
+   
+   procedure Update_Content with
+     Refined_Global => (In_Out => A) is
+   begin
+      A := A + B;
+   end Update_Content;
+end Pack;
