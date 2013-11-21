a96330a3a6235b5a980d696f9637fdaf0a85b30f
Rod Chapman <chapman@adacore.com>
2013-11-21 17:05:24 +0000

    [MB21-044] Add test case - static record type membership test

---
+++ testsuite/gnatprove/tests/MB21-044__static_record_membership/p.adb
@@ -0,0 +1,21 @@
+package body P
+  with SPARK_Mode => On
+is
+   procedure Op1 (S : in R;
+                  Z : out Boolean)
+   is
+   begin
+      Z := S in R;       
+   end Op1;
+
+   procedure Op2 (S : in AofR;
+                  I : in T;
+                  Z : out Boolean)
+   is
+   begin
+      Z := (S (I) in R);
+   end Op2;
+
+end P;
+
+   
---
+++ testsuite/gnatprove/tests/MB21-044__static_record_membership/p.ads
@@ -0,0 +1,24 @@
+package P
+  with SPARK_Mode => On
+is
+   type R is record
+      F1 : Integer;
+      F2 : Boolean;
+   end record;
+
+   type T is range 1 .. 10;
+
+   type AofR is array (T) of R;
+
+   procedure Op1 (S : in R;
+                  Z : out Boolean)
+     with Post => Z = (S in R);
+
+   procedure Op2 (S : in AofR;
+                  I : in T;
+                  Z : out Boolean)
+     with Post => Z = (S (I) in R);
+
+end P;
+
+   
