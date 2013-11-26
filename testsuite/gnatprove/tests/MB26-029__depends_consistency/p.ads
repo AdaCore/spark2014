54a78da406caae6adc045a45153f05639ca0872a
Rod Chapman <chapman@adacore.com>
2013-11-26 16:08:45 +0000

    [MB26-029] Add test case for this issue
    Shows unhelpful and possibly wrong
    error messages for checking of mode and Depends consistency.

---
+++ testsuite/gnatprove/tests/MB26-029__depends_consistency/p.ads
@@ -0,0 +1,63 @@
+package P
+  with SPARK_Mode => On
+is
+   A : Integer := 1;
+   B : Integer := 1;
+
+   --  Formal Mode and Depends inconsistent
+   procedure P1a (X : in Integer)
+     with Depends => (X => X);
+
+   --  Formal Mode and Depends inconsistent - same as P1a,
+   --  but using +null on the right
+   procedure P1b (X : in Integer)
+     with Depends => (X => +null);
+   
+
+
+   --  Global Mode and Depends inconsistent
+   procedure P2a
+     with Global => (Input => A),
+          Depends => (A => A);
+
+   --  Global Mode and Depends inconsistent - same as P2a,
+   --  but using +null on the right
+   procedure P2b
+     with Global => (Input => A),
+          Depends => (A => +null);
+   
+   
+   --  Formal Mode and Depends inconsistent.
+   procedure P3a (X : out Integer)
+     with Depends => (X => X);
+
+   --  Formal Mode and Depends inconsistent, same as P3a,
+   --  but using +null on the right
+   procedure P3b (X : out Integer)
+     with Depends => (X => +null);
+   
+
+
+   --  Global mode and Depends inconsistent.
+   procedure P4a
+     with Global => (Output => A),
+          Depends => (A => A);
+
+   --  Global mode and Depends inconsistent, same as P4a
+   --  but using +null on the right
+   procedure P4b
+     with Global => (Output => A),
+          Depends => (A => +null);
+   
+   
+
+   --  Formal mode and Depends inconsistent
+   procedure P5 (X : in out Integer)
+     with Depends => (X => null);
+   
+   --  Global mode and Depends inconsistent
+   procedure P6
+     with Global => (In_Out => A),
+          Depends => (A => null);
+
+end P;
