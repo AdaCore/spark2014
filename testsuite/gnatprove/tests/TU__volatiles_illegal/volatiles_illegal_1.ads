7c171348e5bcb2985f89750b6f77b29f291fa3db
Rod Chapman <chapman@adacore.com>
2013-11-29 13:15:22 +0000

    [MB28-009] Update TU in test case to match that in LRM.

--- testsuite/gnatprove/tests/TU__volatiles_illegal/volatiles_illegal_1.ads
+++ testsuite/gnatprove/tests/TU__volatiles_illegal/volatiles_illegal_1.ads
@@ -5,7 +5,7 @@ package Volatiles_Illegal_1
        Abstract_State => (State with External => (Async_Readers,
                                                   Async_Writers => True))
        --  TU: 2. If just the name of the property is given then its value
-       --  defaults to True [; for instance Async_Readers defaults to
+       --  defaults to True [for instance Async_Readers defaults to
        --  Async_Readers => True].
 
        --  TU: 4. If any one property is explicitly defined, all undefined
