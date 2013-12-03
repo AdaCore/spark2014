6bfca86aa22ec6e55d1b3880a15d4bcd8ed84696
Angela Wallenburg <angelaw@sparkdev09.praxis.local>
2013-12-03 14:46:43 +0000

    [LC17-035] added record tests

---
+++ testsuite/gnatprove/tests/LC17-035__update_attribute/records.adb
@@ -0,0 +1,50 @@
+package body Records is
+
+   procedure P1 (R: in out Rec; New_Data: in Integer) is
+   begin
+      R.S1 := 3;
+      R.S2 := New_Data;
+      R.S3 := 4;
+   end P1;
+
+   procedure P2 (R: in out Rec; New_Data: in Integer) is
+   begin
+      R.S2 := New_Data;
+   end P2;
+
+   procedure P3 (R: in out Rec; New_Data: in Integer; New_Data_2: in My_Range)
+   is
+   begin
+      R := R'Update(S2 => New_Data, S3 => New_Data_2);
+   end P3;
+
+   procedure P4 (R: in out Rec; New_Data: in Integer) is
+   begin
+      R.S1 := 10;
+      R.S2 := New_Data;
+      R.S3 := 10;
+   end P4;
+
+   procedure P5(R: in out Rec) is
+   begin
+      R.S1 := R.S1 + 1;
+      R.S3 := R.S3 + 1;
+   end P5;
+
+   procedure P6(R: in out Rec) is
+   begin
+      R.S1 := R.S1 + 1;
+      R.S3 := R.S3 + 1;
+   end P6;
+
+   procedure P7 (R: in out Rec) is
+   begin
+      R.S2 := 5;
+   end P7;
+
+   procedure P8 (R: in out Rec) is
+   begin
+      R.S2 := 5;
+   end P8;
+
+end Records;
---
+++ testsuite/gnatprove/tests/LC17-035__update_attribute/records.ads
@@ -0,0 +1,47 @@
+with Types;       use Types;
+
+package Records is
+
+   pragma SPARK_Mode (On);
+
+   ----------------------------------------------------------------------------
+   -- 'Update Record tests
+   ----------------------------------------------------------------------------
+
+   --  aggregate reference test, to compare Why3 models
+   procedure P1 (R: in out Rec; New_Data: in Integer)
+     with Post => R = Rec'(S1 => 3, S2 => New_Data, S3 => 4);
+
+   --  single record component update
+   procedure P2(R: in out Rec; New_Data: in Integer)
+     with Post => R = R'Old'Update(S2 => New_Data);
+
+   --  several but not all components update
+   procedure P3(R: in out Rec; New_Data: in Integer; New_Data_2: in My_Range)
+     with Post => R = R'Old'Update(S2 => New_Data, S3 => New_Data_2);
+
+   --  all components update
+   procedure P4 (R: in out Rec; New_Data: in Integer)
+     with Post => R = R'Old'Update(S1 => 10, S2 => New_Data, S3 => 10);
+
+   --  inc/decrement components, w/o precondition, overflow checks should fail
+   procedure P5(R: in out Rec)
+     with Post => R = R'Old'Update(S1 => R'Old.S1 + 1,
+                                   S3 => R'Old.S3 - 1);
+
+   --  inc/decrement components, w precondition
+   procedure P6(R: in out Rec)
+     with Pre  => R.S1 < Integer'Last and
+                  R.S3 > My_Range'First,
+          Post => R = R'Old'Update(S1 => R'Old.S1 + 1,
+                                   S3 => R'Old.S3 - 1);
+
+   --  aggregate, multiple choices, one association
+   procedure P7(R: in out Rec)
+   with Post => R = Rec'(S1 | S3 => 2, S2 => 3);
+
+   --  update attribute, multiple choices, one association
+   procedure P8(R: in out Rec);
+   --  with Post => R = R'Old'Update(S1 | S3 => 2);
+
+end Records;
