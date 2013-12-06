81737d8b2cac83c2ed86f7bd86b49406b68ef7f8
Pavlos Efstathopoulos <pavlos.efstathopoulos@altran-praxis.com>
2013-12-06 09:11:23 +0000

    [MB26-023] flow - nested packages effects
    * Made the test more complex and updated expected output.

--- testsuite/gnatprove/tests/MB26-023__flow_nested_packages_effects/outer.adb
+++ testsuite/gnatprove/tests/MB26-023__flow_nested_packages_effects/outer.adb
@@ -6,7 +6,8 @@ package body Outer
                                    Inner_2.Y,
                                    Inner_2.Z,
                                    Inner_3.W,
-                                   Foo))
+                                   Foo,
+                                   Inner.Nested_In_Spec.Nested_In_Spec_Var))
 is
    Foo : Integer := 0;
 
@@ -20,15 +21,43 @@ is
    is
       X : Integer := 0;
 
+      package Nested_In_Spec
+        with Initializes => (Nested_In_Spec_Var => Init.Var)
+      is
+         Nested_In_Spec_Var : Integer := Init.Var;
+      end Nested_In_Spec;
+
       function Get_Inner_State return Integer
         with Global => Inner_State;
    end Inner;
 
    package body Inner
-     with Refined_State => (Inner_State   => Y,
+     with Refined_State => (Inner_State   => (Y,
+                                              Nested_In_Body.Deep_State,
+                                              Nested_In_Body.Deep_Var),
                             Inner_State_2 => Z)
    is
-      Y : Integer := Init.Var + Foo;
+      package Nested_In_Body
+        with Abstract_State => Deep_State,
+             Initializes    => (Deep_State => Init.Var,
+                                Deep_Var   => Foo)
+      is
+         Deep_Var : Integer := Foo;
+
+         function Get_Deep_State return Integer
+           with Global => Deep_State;
+      end Nested_In_Body;
+
+      package body Nested_In_Body
+        with Refined_State => (Deep_State => Deep_Ref)
+      is
+         Deep_Ref : Integer := Init.Var;
+
+         function Get_Deep_State return Integer is (Deep_Ref)
+           with Refined_Global => Deep_Ref;
+      end Nested_In_Body;
+
+      Y : Integer := Nested_In_Body.Deep_Var + Nested_In_Body.Get_Deep_State;
       Z : Integer;
 
       function Get_Inner_State return Integer is (Y)
