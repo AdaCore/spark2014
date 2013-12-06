f1d2a51a0575d17ae78ff41662fc28202fd17183
Pavlos Efstathopoulos <pavlos.efstathopoulos@altran-praxis.com>
2013-12-06 09:52:21 +0000

    Minor: Updated some trace units to match recent LRM changes

--- testsuite/gnatprove/tests/TU__assume_and_assert_and_cut_illegal/assume_and_assert_and_cut_illegal.adb
+++ testsuite/gnatprove/tests/TU__assume_and_assert_and_cut_illegal/assume_and_assert_and_cut_illegal.adb
@@ -1,10 +1,11 @@
 package body Assume_And_Assert_And_Cut_Illegal is
    procedure Prover_Forgets_After_Assert_And_Cut is
-   --  TU: 1. Pragma Assert_And_Cut is the same as a pragma Assert except it
-   --  also acts as a cut point in formal verification. The cut point means
-   --  that a prover is free to forget all information about modified variables
-   --  that has been established from the statement list before the cut point.
-   --  Only the given Boolean expression is carried forward.
+      --  TU: 1. Pragma Assert_And_Cut is an assertion the same as a pragma
+      --  Assert except it also acts as a cut point in formal verification. The
+      --  cut point means that a prover is free to forget all information about
+      --  modified variables that has been established from the statement
+      --  list before the cut point. Only the given Boolean expression is
+      --  carried forward.
       X, Y : Integer;
    begin
       X := 0;
