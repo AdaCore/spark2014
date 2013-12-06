0e7095967d85b2778163ac0d8b35074bf2585447
Yannick Moy <moy@adacore.com>
2013-12-06 15:03:45 +0100

    Minor update tests which should not be XFAIL

--- testsuite/gnatprove/tests/M612-007__local_uc/test.adb
+++ testsuite/gnatprove/tests/M612-007__local_uc/test.adb
@@ -1,15 +1,13 @@
 with Ada.Unchecked_Conversion;
 
-package body Test is 
+package body Test is
    type A is new Integer range 0 .. 20;
    type B is new Natural range 0 .. 20;
 
    function A_To_B is new Ada.Unchecked_Conversion (A, B);
-   --  This one is in SPARK
 
    procedure Test is
       function B_To_A is new Ada.Unchecked_Conversion (B, A);
-        This one is not in SPARK for some reason
    begin
       null;
    end Test;
