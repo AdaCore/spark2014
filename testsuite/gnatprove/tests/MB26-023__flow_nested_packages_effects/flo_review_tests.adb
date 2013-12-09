dda394dcd4cb0270effb7dd2343b18985fdb0c6a
Florian Schanda <florian.schanda@altran.com>
2013-12-09 09:47:29 +0000

    [MB26-023] flow - nested packages
    Add tests which crashes flow analysis and/or produce incorrect errors.

---
+++ testsuite/gnatprove/tests/MB26-023__flow_nested_packages_effects/flo_review_tests.adb
@@ -0,0 +1,39 @@
+package body Flo_Review_Tests
+is
+
+   --  This is correct and should raise no errors or warnings.
+   procedure Test_01 (X :     Integer;
+                      Y : out Integer)
+   with
+      Global => null,
+      Depends => (Y => X)
+   is
+      N : Integer := X;
+
+      package P
+      with Initializes => (M => N)
+      is
+         M : Integer := N;
+      end P;
+   begin
+      Y := P.M;
+   end Test_01;
+
+   --  This is correct and should raise no errors or warnings.
+   procedure Test_02 (X :     Integer;
+                      Y : out Integer)
+   with
+      Depends => (Y => X)
+   is
+      N : Integer := X;
+
+      package P
+      with Initializes => (M => N)
+      is
+         M : Integer := N;
+      end P;
+   begin
+      Y := P.M;
+   end Test_02;
+
+end Flo_Review_Tests;
---
+++ testsuite/gnatprove/tests/MB26-023__flow_nested_packages_effects/flo_review_tests.ads
@@ -0,0 +1,6 @@
+package Flo_Review_Tests
+is
+
+   pragma Elaborate_Body;
+
+end Flo_Review_Tests;
