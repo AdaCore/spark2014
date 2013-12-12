29e07e2599a03d5a557f1ce865442ce4bc77965b
Rod Chapman <chapman@adacore.com>
2013-12-12 13:15:48 +0000

    [MC12-026] Add test case.
    Illustrates the way that R-R AEC use a hidden subprogram body to
    "overlay" objects.
    
    p.adb - call to hidden subprogram is rejected by GNAT2Why.
     I think this is wrong.
    
    p2.adb - hidden subprogram has a distinct specification and contract.
     Should also work, but currently crashes GNAT2prove - will be fixed
     under MC12-024, then re-visit results.

---
+++ testsuite/gnatprove/tests/MC12-026__overlayed_objects/p.adb
@@ -0,0 +1,36 @@
+package body P
+  with SPARK_Mode => On
+is
+   procedure Sync_Raw_S_With_Full_S
+     with Global => (Output => Raw_S,
+                     Input  => Full_S),
+          Depends => (Raw_S => Full_S),
+          SPARK_Mode => Off
+   is
+   begin
+      --  Full_S and Raw_S are actually aliased in memory
+      --  so no action required here.  SPARK_Mode is Off so
+      --  I expect the mis-match between contract and body
+      --  to be ignored.
+      null;      
+   end Sync_Raw_S_With_Full_S;
+
+                     
+   procedure Initialize
+   is
+   begin
+      Full_S := Null_R;
+      Sync_Raw_S_With_Full_S;
+   end Initialize;
+   
+   procedure Update_R (X : in Integer;
+                       Y : in Float)
+   is   
+   begin
+      Full_S.A := Full_S.A + X;
+      Full_S.B := Full_S.B + Y;
+      Sync_Raw_S_With_Full_S;
+   end Update_R;
+
+end P;
+
---
+++ testsuite/gnatprove/tests/MC12-026__overlayed_objects/p2.adb
@@ -0,0 +1,45 @@
+package body P2
+  with SPARK_Mode => On
+is
+   --  As package P, but this subprogram has as distinct
+   --  specification, and the SPARK_Mode => Off appears
+   --  on the body only.
+   procedure Sync_Raw_S_With_Full_S
+     with Global => (Output => Raw_S,
+                     Input  => Full_S),
+          Depends => (Raw_S => Full_S);
+
+
+
+   procedure Sync_Raw_S_With_Full_S
+     with SPARK_Mode => Off
+   is
+   begin
+      --  Full_S and Raw_S are actually aliased in memory
+      --  so no action required here.  SPARK_Mode is Off so
+      --  I expect the mis-match between contract and body
+      --  to be ignored.
+      null;      
+   end Sync_Raw_S_With_Full_S;
+
+                     
+
+   procedure Initialize
+   is
+   begin
+      Full_S := Null_R;
+      Sync_Raw_S_With_Full_S;
+   end Initialize;
+   
+
+   procedure Update_R (X : in Integer;
+                       Y : in Float)
+   is   
+   begin
+      Full_S.A := Full_S.A + X;
+      Full_S.B := Full_S.B + Y;
+      Sync_Raw_S_With_Full_S;
+   end Update_R;
+
+end P2;
+
