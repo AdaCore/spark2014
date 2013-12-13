bb6c1a533ac9d6af74bcc36d09ba6af9d458ce68
Yannick Moy <moy@adacore.com>
2013-12-13 16:25:17 +0100

    [MC13-026] Add test

---
+++ testsuite/gnatprove/tests/MC13-026__float_div/protectedfloat.adb
@@ -0,0 +1,22 @@
+package body protectedFloat is pragma SPARK_Mode (On);
+
+   function protectedRealDivide( left, right : in Real) return Real
+   is
+   begin
+      -- Check for fractional denominator
+      if right < 1.0 then
+         -- Guard against divide by zero
+         if right = 0.0 then
+            return Real'Last;
+         -- Guard against overflow where left/right > Real'Last
+         elsif right < (left/Real'Last ) then
+            return Real'Last;
+         else
+            return left/right;
+         end if;
+      else
+         return left/right;
+      end if;
+   end protectedRealDivide;
+
+end protectedFloat;
