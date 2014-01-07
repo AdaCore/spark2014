826ca43c342b0abb930354f0fb33a0850c68c96e
Rod Chapman <chapman@adacore.com>
2014-01-07 15:25:03 +0000

    [MB27-015] Make this test case legal again following this TN.

--- testsuite/gnatprove/tests/KA17-027__obj_renaming/main.adb
+++ testsuite/gnatprove/tests/KA17-027__obj_renaming/main.adb
@@ -1,4 +1,4 @@
-procedure Main is 
+procedure Main is
    type Rec is record
       Comp : Natural := 1234;
    end record;
@@ -8,12 +8,12 @@ procedure Main is
 
    Cheat : Boolean := True;
 
-   function Get_Rec return Rec with 
+   function Get_Rec return Rec with
      Post => Get_Rec'Result = Rec'(Comp => 5678);
-   
+
    function Get_Arr return Arr with
      Post => Get_Arr'Result = Arr'(others => Rec'(Comp => 9012));
-     
+
    function Get_Index return Index with
      Post => (if Cheat then Get_Index'Result = 3 else Get_Index'Result = 5);
 
@@ -41,45 +41,47 @@ procedure Main is
    Arr_Obj : Arr := Get_Arr;
    Rec_Obj : Rec := Get_Rec;
 
+   The_Index : constant Index := Get_Index;
+
    Obj_1 : Rec     renames Arr_Obj (1);
-   Obj_2 : Rec     renames Arr_Obj (Get_Index);
-   Obj_3 : Rec     renames Get_Arr (Get_Index);
+   Obj_2 : Rec     renames Arr_Obj (The_Index);
+   Obj_3 : Rec     renames Get_Arr (The_Index);
    Obj_4 : Natural renames Rec_Obj.Comp;
    Obj_5 : Natural renames Get_Rec.Comp;
-   Obj_6 : Natural renames Get_Arr (Get_Index).Comp;
-       
+   Obj_6 : Natural renames Get_Arr (The_Index).Comp;
+
    procedure Set_Obj_1 is
    begin
       Obj_1 := (Comp => 0);
    end Set_Obj_1;
-   
+
    procedure Set_Obj_2 is
    begin
       Obj_2.Comp := Obj_2.Comp + 1;
    end Set_Obj_2;
-   
+
    procedure Set_Obj_4 is
    begin
       Obj_4 := 0;
    end Set_Obj_4;
-   
+
 begin
    Cheat := False;
-   
+
    pragma Assert (Obj_1.Comp = 9012);
    pragma Assert (Obj_2.Comp = 9012);
    pragma Assert (Obj_3.Comp = 9012);
    pragma Assert (Obj_4 = 5678);
    pragma Assert (Obj_5 = 5678);
    pragma Assert (Obj_6 = 9012);
-   
+
    Set_Obj_1;
    Set_Obj_2;
    Set_Obj_4;
-   
+
    --  It should not be possible to prove the assertions below, after the
    --  calls to Set_Obj_?
-   
+
    pragma Assert (Obj_1.Comp = 9012);
    pragma Assert (Obj_2.Comp = 9012);
    pragma Assert (Obj_4 = 5678);
