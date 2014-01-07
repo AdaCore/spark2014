8219f81bc3cc045b5c5994cf2363d5795e96ab93
Rod Chapman <chapman@adacore.com>
2014-01-07 13:17:42 +0000

    [MB27-015] Add expected test results.
    Following explanation of the rules from Steve B.

--- testsuite/gnatprove/tests/MB27-015__renamings_var_free/p.ads
+++ testsuite/gnatprove/tests/MB27-015__renamings_var_free/p.ads
@@ -15,30 +15,30 @@ is
     Named_Constant : constant Integer := X;
 
     -- renaming of an indexed_component of an array object
-    Illegal_Index_Rename : Character renames Y (X);
-    Legal_Index_Rename   : Character renames Y (Named_Constant);
+    Illegal_Index_Rename : Character renames Y (X); -- illegal
+    Legal_Index_Rename   : Character renames Y (Named_Constant); -- legal
 
     -- renaming of a slice of an array object
-    Illegal_Slice_Rename : String renames Y (1 .. X);
-    Legal_Slice_Rename   : String renames Y (1 .. Named_Constant);
+    Illegal_Slice_Rename : String renames Y (1 .. X); -- illegal
+    Legal_Slice_Rename   : String renames Y (1 .. Named_Constant); -- legal
 
 
     -- Renamings involving attribute_reference
 
     -- Attribute with 1 param, literal
-    AR2 : Integer renames Integer'Succ (1);
+    AR2 : Integer renames Integer'Succ (1); -- legal
 
     -- Attribute with 1 param, variable
-    AR3 : Integer renames Integer'Succ (X);
+    AR3 : Integer renames Integer'Succ (X); -- legal
 
     -- Attribute with 2 params, literals and constants
-    AR4 : Integer renames Integer'Max (1, 2);
-    AR5 : Integer renames Integer'Max (1, C);
-    AR6 : Integer renames Integer'Max (C, 1);
+    AR4 : Integer renames Integer'Max (1, 2); -- legal
+    AR5 : Integer renames Integer'Max (1, C); -- legal
+    AR6 : Integer renames Integer'Max (C, 1); -- legal
 
     -- Attribute with 2 params, literals and variables
-    AR7 : Integer renames Integer'Max (X, 1);
-    AR8 : Integer renames Integer'Max (1, X);
-    AR9 : Integer renames Integer'Max (X, Z);
+    AR7 : Integer renames Integer'Max (X, 1); -- legal
+    AR8 : Integer renames Integer'Max (1, X); -- legal
+    AR9 : Integer renames Integer'Max (X, Z); -- legal
 
 end P;
--- testsuite/gnatprove/tests/MB27-015__renamings_var_free/q.adb
+++ testsuite/gnatprove/tests/MB27-015__renamings_var_free/q.adb
@@ -30,69 +30,80 @@ package body Q is
       return X (I);
    end Get_Arr_P2;
 
+   -- Below, for each test case
+   --   I = "Illegal"
+   --   L = "Legal"
+
    -- Direct ref to Arr_Obj, but indexing expression refs a variable
-   S1 : Boolean renames Arr_Obj (Get_V);
+   S1 : Boolean renames Arr_Obj (Get_V); -- I
 
    -- Ref to Arr_Obj via function call
-   S2 : Boolean renames Get_Arr (1);
+   S2 : Boolean renames Get_Arr (1); -- L
 
    -- Ref to Arr_Obj via function call AND variable index
-   S3 : Boolean renames Get_Arr (Get_V);
+   S3 : Boolean renames Get_Arr (Get_V); -- I
 
 
    -- Function call with 1 param
-   S4 : T renames Get_P (1); -- function call with static params
+   S4 : T renames Get_P (1); -- function call with static params - L
 
-   S5 : T renames Get_P (V); -- function call with variable param
+   S5 : T renames Get_P (V); -- function call with variable param - L
 
-   S6 : T renames Get_P (Get_V); -- function call with variable param
+   S6 : T renames Get_P (Get_V); -- function call with variable param - L
 
 
    -- Function calls with 1 param, followed by a indexed component
-   S7 : Boolean renames Get_Arr_P1 (Arr_Obj) (1); -- function call, var param
 
-   S8 : Boolean renames Get_Arr_P1 (Get_Arr) (1); -- function call, var param
+   -- function call, var param, static index - L
+   S7 : Boolean renames Get_Arr_P1 (Arr_Obj) (1);
 
-   S9 : Boolean renames Get_Arr_P1 (Arr_Obj) (V); -- function call, var params
+   -- function call, var param, static index - L
+   S8 : Boolean renames Get_Arr_P1 (Get_Arr) (1);
 
-   SA : Boolean renames Get_Arr_P1 (Get_Arr) (V); -- function call, var params
+   -- function call, var params, var index - I
+   S9 : Boolean renames Get_Arr_P1 (Arr_Obj) (V);
 
-   SB : Boolean renames Get_Arr_P1 (Arr_Obj) (Get_V); -- function call, var params
+   -- function call, var params, var index - I
+   SA : Boolean renames Get_Arr_P1 (Get_Arr) (V);
 
-   SC : Boolean renames Get_Arr_P1 (Get_Arr) (Get_V); -- function call, var params
+   -- function call, var params, var index - I
+   SB : Boolean renames Get_Arr_P1 (Arr_Obj) (Get_V);
+
+   -- function call, var params, var index - I
+   SC : Boolean renames Get_Arr_P1 (Get_Arr) (Get_V);
 
 
    -- Cases where the outermost object is a constant array
-   SD  : Boolean renames Null_Arr (1); -- all static - legal
+   SD  : Boolean renames Null_Arr (1); -- all static - L
 
-   SE  : Boolean renames Null_Arr (V); -- variable index
+   SE  : Boolean renames Null_Arr (V); -- var index - I
 
-   SF  : Boolean renames Null_Arr (Get_V); -- variable index
+   SF  : Boolean renames Null_Arr (Get_V); -- var index - I
 
-   S10 : Boolean renames Null_Arr (Get_P (1)); -- all static - legal
+   S10 : Boolean renames Null_Arr (Get_P (1)); -- all static - L
 
-   S11 : Boolean renames Null_Arr (Get_P (V)); -- variable index
+   S11 : Boolean renames Null_Arr (Get_P (V)); -- var index - I
 
-   S12 : Boolean renames Null_Arr (Get_P (Get_V)); -- variable index
+   S12 : Boolean renames Null_Arr (Get_P (Get_V)); -- var index - I
 
 
    -- Cases with a function call with 2 args
-   S13 : Boolean renames Get_Arr_P2 (Null_Arr, 1); -- static args
+   S13 : Boolean renames Get_Arr_P2 (Null_Arr, 1); -- static args - L
 
-   S14 : Boolean renames Get_Arr_P2 (Null_Arr, Get_P (1)); -- static args
+   S14 : Boolean renames Get_Arr_P2 (Null_Arr, Get_P (1)); -- static args - L
 
-   S15 : Boolean renames Get_Arr_P2 (Arr_Obj, 1); -- 1 variable, 1 literal
+   S15 : Boolean renames Get_Arr_P2 (Arr_Obj, 1); -- 1 variable, 1 literal - L
 
-   S16 : Boolean renames Get_Arr_P2 (Get_Arr, 1); -- 1 variable, 1 literal
+   S16 : Boolean renames Get_Arr_P2 (Get_Arr, 1); -- 1 variable, 1 literal - L
 
-   S17 : Boolean renames Get_Arr_P2 (Get_Arr, Get_P (1)); -- 1 variable, 1 static
+   S17 : Boolean renames Get_Arr_P2 (Get_Arr, Get_P (1)); -- 1 variable, 1 static - L
 
-   S18 : Boolean renames Get_Arr_P2 (Arr_Obj, V); -- 2 variables
+   S18 : Boolean renames Get_Arr_P2 (Arr_Obj, V); -- 2 variables - L
 
-   S19 : Boolean renames Get_Arr_P2 (Arr_Obj, Get_V); -- 2 variables
+   S19 : Boolean renames Get_Arr_P2 (Arr_Obj, Get_V); -- 2 variables - L
 
-   S20 : Boolean renames Get_Arr_P2 (Get_Arr, V); -- 2 variables
+   S20 : Boolean renames Get_Arr_P2 (Get_Arr, V); -- 2 variables - L
 
-   S21 : Boolean renames Get_Arr_P2 (Get_Arr, Get_V); -- 2 variables
+   S21 : Boolean renames Get_Arr_P2 (Get_Arr, Get_V); -- 2 variables - L
 
 end Q;
