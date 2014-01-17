56890bb68df58f1326f164e2381bcdcd0001bf7b
Yannick Moy <moy@adacore.com>
2014-01-17 12:16:37 +0100

    M823-017 Refactor theory for fixed-points for better readability
    * share/spark/theories/ada__model.mlw
    (Fixed_Point_Base): Use common helper predicate for all axioms. Fix
      previous case of "false" in axioms on division, for divisor = 0,
      which could lead to inconsistent axiom. Instead state "true" in that
      case (hence nothing is known).
    
    * testsuite/gnatprove/tests/M823-017__fixed_points/test.out
    * testsuite/gnatprove/tests/M823-017__fixed_points/test.py
    Update test output

--- testsuite/gnatprove/tests/M823-017__fixed_points/test.py
+++ testsuite/gnatprove/tests/M823-017__fixed_points/test.py
@@ -1,5 +1,5 @@
 from test_support import *
-prove_all(opt=["-u","binary_fixed.adb"], steps=1000)
-prove_all(opt=["-u","decimal_fixed.adb"], steps=1000)
+prove_all(opt=["-u","binary_fixed.adb"], steps=2000)
+prove_all(opt=["-u","decimal_fixed.adb"], steps=2000)
 prove_all(opt=["-u","dynamic_fixed.adb"])
 prove_all(opt=["-u","unsupported.adb"])
