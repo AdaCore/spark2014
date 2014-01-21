79f42302ff1406120be4f567f8de3cead15fb6d8
Yannick Moy <moy@adacore.com>
2014-01-21 14:42:28 +0100

    N121-024 Add call that fails postcondition

--- testsuite/gnatprove/tests/N121-024__binary_search/test_search.adb
+++ testsuite/gnatprove/tests/N121-024__binary_search/test_search.adb
@@ -3,6 +3,7 @@ with Ada.Text_IO;   use Ada.Text_IO;
 
 procedure Test_Search is
    A   : constant Ar := (0, 0, 1, 1, 3, 4, 5, 8, 8);
+   B   : constant Ar := (1, 2);
    Res : Opt_Index;
    
 begin
@@ -22,5 +23,16 @@ begin
       Put_Line ("OK: Did not find non-existing value");
    else
       Put_Line ("not OK: Found non-existing value");
+   end if;
+   
+   Res := Search (B, 2);
+   if Res /= No_Index then
+      if Res = 2 then
+         Put_Line ("OK: Found existing value at first index");
+      else
+         Put_Line ("not OK: Found existing value at other index");
+      end if;
+   else
+      Put_Line ("not OK: Did not find existing value");
    end if;
 end Test_Search;
