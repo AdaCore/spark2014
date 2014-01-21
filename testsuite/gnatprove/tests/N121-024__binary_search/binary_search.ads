2cbec94d63f54f7868dc638aff4518c2d8dbf29b
Yannick Moy <moy@adacore.com>
2014-01-21 14:11:02 +0100

    N121-024 Add test

---
+++ testsuite/gnatprove/tests/N121-024__binary_search/binary_search.ads
@@ -0,0 +1,19 @@
+pragma SPARK_Mode (On);
+
+package Binary_Search is
+
+   type Opt_Index is new Natural;
+   subtype Index is Opt_Index range 1 .. Opt_Index'Last;
+   No_Index : constant Opt_Index := 0;
+   type Ar is array (Index range <>) of Integer;
+   
+   function Empty (A : Ar) return Boolean is (A'First > A'Last);
+   function Sorted (A : Ar) return Boolean is
+     (for all I1 in A'Range => (for all I2 in I1 .. A'Last => A (I1) <= A (I2)));
+
+   function Search (A : Ar; I : Integer) return Opt_Index with
+     Pre  => Sorted (A),
+     Post => (if Search'Result in A'Range then A (Search'Result) = I
+              else (for all Index in A'Range => A (Index) /= I));
+
+end Binary_Search;
---
+++ testsuite/gnatprove/tests/N121-024__binary_search/test.py
@@ -0,0 +1,2 @@
+from test_support import *
+prove_all(opt=["-u", "binary_search.adb"], steps=10000)
