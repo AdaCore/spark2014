6b6a74a1908fe8d00bacf58ca705a9e19307f41f
Yannick Moy <moy@adacore.com>
2014-01-22 12:44:27 +0100

    N121-026 Add test

---
+++ testsuite/gnatprove/tests/N121-026__nonlinear/nonlinear.adb
@@ -0,0 +1,12 @@
+procedure Nonlinear (X, Y, Z : Float)
+  with SPARK_Mode 
+is
+   Result : Float;
+begin
+   pragma Assume(X >= 0.0 and then X <= 180.0);
+   pragma Assume(Y >= -180.0 and then Y <= 0.0);
+   pragma Assume(Z >= 0.0 and then Z <= 1.0);
+   pragma Assume(X + Y >= 0.0);
+   Result := X + Y * Z;
+   pragma Assert (Result >= 0.0 and then Result <= 360.0);
+end Nonlinear;
