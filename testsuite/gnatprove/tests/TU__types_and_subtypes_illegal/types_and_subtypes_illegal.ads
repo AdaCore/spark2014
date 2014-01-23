806b66f6775cb7c4c1de06d2b18d76ae17c3c993
Rod Chapman <chapman@adacore.com>
2014-01-23 15:34:53 +0000

    MB28-009 Add traceunits and further test cases here
    For RM legality rules 3.2.4(1), 3.9(1) and 3.9(2)

--- testsuite/gnatprove/tests/TU__types_and_subtypes_illegal/types_and_subtypes_illegal.ads
+++ testsuite/gnatprove/tests/TU__types_and_subtypes_illegal/types_and_subtypes_illegal.ads
@@ -1,6 +1,25 @@
 package Types_And_Subtypes_Illegal
   with SPARK_Mode
 is
+   -- TU: 1. A Dynamic_Predicate aspect shall not occur in an aspect specification.
+   subtype S1 is Integer
+     with Dynamic_Predicate => S1 mod 2 = 0;
+
+
+   type T0 is private;
+
+   -- TU: 1. A record or private type declaration shall not contain the reserved
+   --     word **tagged**.
+   type T1 is tagged private;
+
+   type T2 is tagged record
+      F1 : Integer;
+   end record;
+
+   -- TU: 2. The attribute 'Class shall not be denoted.
+   procedure Op (X : in T1'Class);
+
+
    function Foo return Integer;
 
    --  TU: 2. For a type or subtype to be in |SPARK|, all predicate
@@ -9,4 +28,9 @@ is
    --  |SPARK| if its applicable predicate is not in |SPARK|.
    subtype One_To_Ten is Integer range 1 .. 10
      with Dynamic_Predicate => Foo < 100;
+private
+   type T0 is new Integer;
+
+   type T1 is tagged null record;
+
 end Types_And_Subtypes_Illegal;
