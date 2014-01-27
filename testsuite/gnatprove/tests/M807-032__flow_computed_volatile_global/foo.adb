00c9609b980bc4146636411e70d28fafbc794b80
Florian Schanda <florian.schanda@altran.com>
2014-01-27 14:06:07 +0000

    M807-032 flow - computed globals for volatiles
    Add testcase.

---
+++ testsuite/gnatprove/tests/M807-032__flow_computed_volatile_global/foo.adb
@@ -0,0 +1,35 @@
+package body Foo
+is
+
+   X : Integer with Volatile, Async_Writers, Effective_Reads;
+
+   procedure P_G (N : out Integer)
+   with Depends => (N => X)
+   is
+   begin
+      N := X;
+   end P_G;
+
+   procedure P_C (N : out Integer)
+   with Global => (In_Out => X),
+        Depends => ((N, X) => X)
+   is
+   begin
+      N := X;
+   end P_C;
+
+   procedure Verify_P_G (N : out Integer)
+   with Depends => ((N, X) => X)
+   is
+   begin
+      P_G (N);
+   end Verify_P_G;
+
+   procedure Verify_P_C (N : out Integer)
+   with Depends => ((N, X) => X)
+   is
+   begin
+      P_C (N);
+   end Verify_P_C;
+
+end Foo;
