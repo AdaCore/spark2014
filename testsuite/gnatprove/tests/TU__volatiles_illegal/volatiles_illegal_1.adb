1be16f42b0da9212ecea9a1fd789c02039886a4f
Pavlos Efstathopoulos <pavlos.efstathopoulos@altran-praxis.com>
2014-01-28 13:45:00 +0000

    Minor: Enabled a test and updated its expected output.

--- testsuite/gnatprove/tests/TU__volatiles_illegal/volatiles_illegal_1.adb
+++ testsuite/gnatprove/tests/TU__volatiles_illegal/volatiles_illegal_1.adb
@@ -2,13 +2,21 @@ with System.Storage_Elements;
 
 package body Volatiles_Illegal_1
   with SPARK_Mode,
-       Refined_State => (State => Vol)  --  Aspect Async_Readers is not set
-                                        --  for Vol while it is set
-                                        --  for State1.
+       Refined_State => (State  => Vol,  --  Aspect Async_Readers is NOT set
+                                         --  for Vol while it is set for State.
 
+                         State2 => Vol2) --  Aspect Async_Writers is set for
+                                         --  Vol2 while it is NOT set for
+                                         --  State2.
 is
    Vol : Natural
      with Volatile,
           Async_Writers,
           Address => System.Storage_Elements.To_Address (16#DEAD0#);
+
+   Vol2 : Natural
+     with Volatile,
+          Async_Readers,
+          Async_Writers,
+          Address => System.Storage_Elements.To_Address (16#BEEF0#);
 end Volatiles_Illegal_1;
--- testsuite/gnatprove/tests/TU__volatiles_illegal/volatiles_illegal_1.ads
+++ testsuite/gnatprove/tests/TU__volatiles_illegal/volatiles_illegal_1.ads
@@ -2,12 +2,13 @@ with System.Storage_Elements;
 
 package Volatiles_Illegal_1
   with SPARK_Mode,
-       Abstract_State => (State with External => (Async_Readers,
-                                                  Async_Writers => True))
+       Abstract_State => ((State with External => (Async_Readers,
+                                                   Async_Writers => True)),
        --  TU: 2. If just the name of the property is given then its value
        --  defaults to True [for instance Async_Readers defaults to
        --  Async_Readers => True].
 
+                          (State2 with External => Async_Readers))
        --  TU: 4. If any one property is explicitly defined, all undefined
        --  properties default to a value of False.
 is
