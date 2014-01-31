e74c4803ad089fb8e272af6a46172cf5d19630aa
Rod Chapman <chapman@adacore.com>
2014-01-31 13:19:11 +0000

    N128-041 Add example of how warning can be suppressed.

--- testsuite/gnatprove/tests/N128-041__filter_pragma_volatile_components/foo.ads
+++ testsuite/gnatprove/tests/N128-041__filter_pragma_volatile_components/foo.ads
@@ -7,6 +7,18 @@ is
 
    type Array_T3 is array (1 .. 10) of Integer;
 
+
+   -- Test that the warning can be suppressed
+   pragma Warnings (Off, "pragma*not yet supported*");
+
+   type Array_T4 is array (1 .. 10) of Integer;
+   pragma Volatile_Components (Array_T4);
+
+   type Array_T5 is array (1 .. 10) of Integer
+     with Volatile_Components;
+
+   pragma Warnings (On, "pragma*not yet supported*");
+
    -- Some body using Array_T to demonsrate flow-analysis
    -- and proof involving such a type.
    function Sum (A : in Array_T) return Integer
