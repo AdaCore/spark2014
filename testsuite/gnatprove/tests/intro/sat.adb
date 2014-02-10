5a49d755b253373b2babe418308b861b4378ffb1
Rod Chapman <chapman@adacore.com>
2014-02-10 14:54:13 +0000

    N207-018 Update intro example for SPARK 2014
    1. make -gnaty friendly.
    2. Add comments.

--- testsuite/gnatprove/tests/intro/sat.adb
+++ testsuite/gnatprove/tests/intro/sat.adb
@@ -1,14 +1,21 @@
-pragma SPARK_Mode (On);
+package body Sat
+  with SPARK_Mode
+is
 
-package body Sat is
+   ------------------------------------------------------
+   -- Sat package - body                               --
+   --                                                  --
+   -- Illustrates use of conditional expressions and   --
+   -- bodies completed by an expression function.      --
+   ------------------------------------------------------
 
-   function Add (X , Y : My_Int) return My_Int is
+   function Add (X, Y : My_Int) return My_Int is
      (if X + Y < 10_000 then
          X + Y
        else
          10_000);
 
-   function Mult (X , Y : My_Int) return My_Int is
+   function Mult (X, Y : My_Int) return My_Int is
      (if X * Y < 10_000 then
          X * Y
        else
--- testsuite/gnatprove/tests/intro/sat.ads
+++ testsuite/gnatprove/tests/intro/sat.ads
@@ -1,14 +1,21 @@
-pragma SPARK_Mode (On);
+package Sat
+  with SPARK_Mode
+is
 
-package Sat is
+   ------------------------------------------------------
+   -- A simple abstract data type (ADT) for saturating --
+   -- mathematics on integers.                         --
+   --                                                  --
+   -- Illustrates use of Contract_Cases in SPARK 2014  --
+   ------------------------------------------------------
 
    subtype My_Int is Integer range 0 .. 10_000;
 
-   function Add (X , Y : My_Int) return My_Int with
+   function Add (X, Y : My_Int) return My_Int with
      Contract_Cases => (X + Y < 10_000  => Add'Result = X + Y,
                         X + Y >= 10_000 => Add'Result = 10_000);
 
-   function Mult (X , Y : My_Int) return My_Int with
+   function Mult (X, Y : My_Int) return My_Int with
      Contract_Cases => (X * Y < 10_000  => Mult'Result = X * Y,
                         X * Y >= 10_000 => Mult'Result = 10_000);
 
