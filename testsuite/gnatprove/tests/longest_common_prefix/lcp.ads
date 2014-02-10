e16a8a46e42de109d18652f8d78d7acff43ba744
Rod Chapman <chapman@adacore.com>
2014-02-10 16:03:56 +0000

    N207-018 Update longest_common_prefix example for SPARK 2014
    1) make -gnaty compatible
    2) Use SPARK_Mode aspect not pragma
    3) Add comments
    4) Regenerate test results

--- testsuite/gnatprove/tests/longest_common_prefix/lcp.ads
+++ testsuite/gnatprove/tests/longest_common_prefix/lcp.ads
@@ -1,6 +1,14 @@
 with Types; use Types;
 
+----------------------------------------------------
+-- SPARK 2014 - Longest_Common_Prefix Example     --
+--                                                --
+-- This example illustrates the use of Pre, Post  --
+-- and Contract_Cases aspects in SPARK 2014.      --
+----------------------------------------------------
+
 function LCP (A : Text; X, Y : Integer) return Natural with
+  SPARK_Mode,
   Pre  => X in A'Range and then Y in A'Range,
   Post =>
     (for all K in 0 .. LCP'Result - 1 => A (X + K) = A (Y + K))
@@ -11,4 +19,3 @@ function LCP (A : Text; X, Y : Integer) return Natural with
      (A (X) /= A (Y) => LCP'Result = 0,
       X = Y          => LCP'Result = A'Last - X + 1,
       others         => LCP'Result > 0);
-pragma SPARK_Mode (On);
--- testsuite/gnatprove/tests/longest_common_prefix/main.adb
+++ testsuite/gnatprove/tests/longest_common_prefix/main.adb
@@ -1,8 +1,8 @@
 with Types; use Types;
 with LCP;
 with Ada.Text_IO; use Ada.Text_IO;
-procedure Main is pragma SPARK_Mode (On); 
-   A : Text := (1, 2, 3, 4, 5, 1, 2, 3, 4, 5);
+procedure Main is
+   A : constant Text := (1, 2, 3, 4, 5, 1, 2, 3, 4, 5);
 begin
    Put_Line ("LCP returns non-null");
    pragma Assert (LCP (A, 1, 6) = 5);
