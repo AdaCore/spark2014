5b8a1a5dabf103272b40e5a583b692cc82eeafa9
Rod Chapman <chapman@adacore.com>
2014-02-11 12:47:20 +0000

    N211-013 Update for possible inclusion in distro examples
    1) Set steps=40000 for both user (in test.gpr) and for internal
    testing (in test.py)
    2) Make compatible with -gnaty mode
    3) Add comments as to basic intent of this example
    4) Regenerate and update expected results.

--- testsuite/gnatprove/tests/L719-025__queue/queue.ads
+++ testsuite/gnatprove/tests/L719-025__queue/queue.ads
@@ -1,6 +1,18 @@
 with Ada.Containers.Formal_Doubly_Linked_Lists;
 with Ada.Containers; use Ada.Containers;
-package Queue is pragma SPARK_Mode (On);
+
+package Queue
+  with SPARK_Mode
+is
+   ----------------------------------------------------
+   --         SPARK 2014 - Queue Example             --
+   --                                                --
+   -- This example illustrates the use of the formal --
+   -- container libraries in SPARK 2014.  In this    --
+   -- example the Formal_Doubly_Linked_Lists package --
+   -- is used to implement a container for a queue   --
+   -- of Integers.                                   --
+   ----------------------------------------------------
 
    subtype Val is Integer range -2 ** 31 .. 2 ** 31 - 1;
 
@@ -11,19 +23,19 @@ package Queue is pragma SPARK_Mode (On);
         (Val, "=" => Eq);
    use MyLists;
 
-   function Front (Q : in List) return Val with
-     Pre  => Length (Q) > 0,
-     Post => Last_Element (Q) = Front'Result;
+   function Front (Q : in List) return Val
+     with Pre  => Length (Q) > 0,
+          Post => Last_Element (Q) = Front'Result;
 
-   function Tail (Q : in List) return List with
-     Pre  => Length (Q) > 0,
-     Post => Strict_Equal (Tail'Result, First_To_Previous (Q, Last (Q)));
+   function Tail (Q : in List) return List
+     with Pre  => Length (Q) > 0,
+          Post => Strict_Equal (Tail'Result, First_To_Previous (Q, Last (Q)));
 
-   function Enqueue (Q : in List; V : in Val) return List with
-     Pre  => Length (Q) < Q.Capacity,
-     Post => Length (Enqueue'Result) > 0 and then
-     First_Element (Enqueue'Result) = V and then
-      Strict_Equal (Current_To_Last (Enqueue'Result,
+   function Enqueue (Q : in List; V : in Val) return List
+     with Pre  => Length (Q) < Q.Capacity,
+          Post => Length (Enqueue'Result) > 0 and then
+                  First_Element (Enqueue'Result) = V and then
+                  Strict_Equal (Current_To_Last (Enqueue'Result,
                     Next (Enqueue'Result, First (Enqueue'Result))), Q);
 
 end Queue;
