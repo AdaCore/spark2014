caa4f9cbc29af79fcf422bb0d7bc2076eeffcc06
Rod Chapman <chapman@adacore.com>
2014-02-13 12:52:57 +0000

    N207-018 Minor updates for rel 1
    1) comply with -gnaty
    2) Adjust so all VCs prove OK.
    3) Add comments

--- testsuite/gnatprove/tests/natural/natural_set.ads
+++ testsuite/gnatprove/tests/natural/natural_set.ads
@@ -1,19 +1,33 @@
-package Natural_Set is 
+package Natural_Set
+  with SPARK_Mode
+is
+   ----------------------------------------------------
+   --       SPARK 2014 - Natural_Set Example         --
+   --                                                --
+   -- This example illustrates a simple abstract     --
+   -- data type for a bounded set of Naturals in     --
+   -- SPARK 2014.                                    --
+   --                                                --
+   -- Note the use of expression functions in the    --
+   -- private part of this package.                  --
+   ----------------------------------------------------
+
    Maximum_Set_Size : constant Positive := 10;
 
    type T is private;
 
    function Valid (S : in T) return Boolean;
 
+   function Members (S : in T) return Natural
+     with Post => Members'Result >= 0 and
+                  Members'Result <= Maximum_Set_Size;
 
-   function Members (S: in T) return Natural
-   with Post =>
-     Members'Result >= 0 and then Members'Result <= Maximum_Set_Size;
-
-   function Full (S: in T) return Boolean is (Members (S) = Maximum_Set_Size);
+   function Full (S : in T) return Boolean is (Members (S) = Maximum_Set_Size);
 
    procedure Create (S : out T)
-     with Post => Valid (S) and then Members (S) = 0 and then not Full (S);
+     with Post => Valid (S) and
+                  Members (S) = 0 and
+                  not Full (S);
 
    function Contains (S     : in T;
                       Value : in Natural)
@@ -21,8 +35,8 @@ package Natural_Set is
 
    procedure Insert (S     : in out T;
                      Value : in     Natural)
-   with Pre  => (Valid (S) and then not Full (S)),
-     Post => (Contains (S, Value));
+     with Pre  => (Valid (S) and not Full (S)),
+          Post => (Contains (S, Value));
 
 private
    subtype Element_T is Integer range -1 .. Integer'Last;
@@ -39,7 +53,7 @@ private
       M   : Set_Array_T;
    end record;
 
-   function Members (S: in T) return Natural is (S.Len);
+   function Members (S : in T) return Natural is (S.Len);
 
    function Valid (S : in T) return Boolean is
      ((for all I in Set_Index range 1 .. S.Len => (S.M (I) in Natural)) and
@@ -49,6 +63,6 @@ private
    function Contains (S     : in T;
                       Value : in Natural)
                       return Boolean is
-      (for some I in 1 .. Members (S) => S.M (I) = Value);
+      (for some I in 1 .. S.Len => S.M (I) = Value);
 
 end Natural_Set;
