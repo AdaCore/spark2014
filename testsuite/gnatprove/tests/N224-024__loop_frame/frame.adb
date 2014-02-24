34199f20ac80788d781dd96f9e3a33f8e3bcfe36
Yannick Moy <moy@adacore.com>
2014-02-24 14:38:59 +0100

    N224-024 Add test

---
+++ testsuite/gnatprove/tests/N224-024__loop_frame/frame.adb
@@ -0,0 +1,24 @@
+package body Frame is
+   
+   procedure Threshold_Ar (A : in out Ar) is
+   begin
+      for J in Index loop
+         if A(J) > 10 then
+            A(J) := 10;
+         end if;
+         pragma Loop_Invariant
+           (for all K in 1 .. J => A(K) =
+              (if A'Loop_Entry(K) > 10 then 10 else A'Loop_Entry(K)));
+      end loop;
+   end Threshold_Ar;
+   
+   procedure Copy_Rec (A : in out Arrec) is
+   begin
+      for J in Index loop
+         A(J).C1 := A(J).C2;
+         pragma Loop_Invariant 
+           (for all K in 1 .. J => A(K).C1 = A'Loop_Entry(K).C2);
+      end loop;
+   end Copy_Rec;
+   
+end Frame;
---
+++ testsuite/gnatprove/tests/N224-024__loop_frame/frame.ads
@@ -0,0 +1,17 @@
+package Frame is
+   type Index is new Integer range 1 .. 10;
+   type Ar is array (Index) of Natural;
+   type Rec is record
+      C1 : Natural;
+      C2 : Natural;
+   end record;
+   type Arrec is array (Index) of Rec;
+   
+   procedure Threshold_Ar (A : in out Ar) with
+     Post => (for all J in Index => A(J) = 
+                (if A'Old(J) > 10 then 10 else A'Old(J)));
+   
+   procedure Copy_Rec (A : in out Arrec) with
+     Post => (for all J in Index => A(J).C1 = A'Old(J).C2);
+   
+end Frame;
