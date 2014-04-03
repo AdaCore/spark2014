fb15bd9c633b6bd493feac41d16b54ea0b12c9b7
Yannick Moy <moy@adacore.com>
2014-04-03 10:37:56 +0200

    N317-015 Update test with subprograms with contract on body

--- testsuite/gnatprove/tests/N317-015__no_separate_spec/no_separate_spec.adb
+++ testsuite/gnatprove/tests/N317-015__no_separate_spec/no_separate_spec.adb
@@ -1,13 +1,25 @@
 package body No_Separate_Spec with
   SPARK_Mode => On
 is
-   procedure Should_Inline (B : out Boolean) is
+   Maybe : Boolean := True;
+
+   procedure Should_Inline (B : in out Boolean) is
    begin
-      B := True;
+      if not B then
+         B := True;
+      end if;
    end Should_Inline;
+
+   procedure Should_Not_Inline (B : out Boolean) with
+     Post => B or not B
+   is
+   begin
+      B := Maybe;
+   end Should_Not_Inline;
 
    procedure Should_Prove (B : out Boolean) is
    begin
+      Should_Not_Inline (B);
       Should_Inline (B);
    end Should_Prove;
 
