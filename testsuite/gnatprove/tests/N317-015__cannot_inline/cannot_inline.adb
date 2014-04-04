e2dd8f77df292c11aa889f3dc4fa8e1aaa2976b5
Yannick Moy <moy@adacore.com>
2014-04-04 12:46:19 +0200

    N317-015, N328-028 Add test with subprograms that cannot be inlined
    Frontend attempts inlining all local subprograms that have no contract,
    but in some cases these cannot be inlined. In that case, we expect a
    warning to let the user know that the subprogram is not inlined (and why).

---
+++ testsuite/gnatprove/tests/N317-015__cannot_inline/cannot_inline.adb
@@ -0,0 +1,73 @@
+package body Cannot_Inline with
+  SPARK_Mode
+is
+
+   function Return_String return String;
+   procedure Has_Local_Subprogram;
+   procedure Has_Local_Subprogram_In_Block;
+
+   function Return_String return String is
+   begin
+      return "bla";
+   end Return_String;
+
+   procedure Has_Local_Subprogram is
+      procedure Local is
+      begin
+         null;
+      end Local;
+   begin
+      Local;
+   end Has_Local_Subprogram;
+
+   procedure Has_Local_Subprogram_In_Block is
+   begin
+      declare
+         procedure Local is
+         begin
+            null;
+         end Local;
+      begin
+         Local;
+      end;
+   end Has_Local_Subprogram_In_Block;
+
+   package Has_Pending_Instantiations is
+      generic
+         V : Integer;
+      package P is
+         function Get return Integer;
+      end P;
+
+      procedure Proc;
+   end Has_Pending_Instantiations;
+
+   package body Has_Pending_Instantiations is
+      procedure Other is
+         package One is new P (1);
+      begin
+         null;
+      end Other;
+
+      package body P is
+         function Get return Integer is
+         begin
+            return V;
+         end Get;
+      end P;
+
+      procedure Proc is
+      begin
+         null;
+      end Proc;
+   end Has_Pending_Instantiations;
+
+   procedure Test is
+      S : String := Return_String;
+   begin
+      Has_Local_Subprogram;
+      Has_Local_Subprogram_In_Block;
+      Has_Pending_Instantiations.Proc;
+   end Test;
+
+end Cannot_Inline;
---
+++ testsuite/gnatprove/tests/N317-015__cannot_inline/cannot_inline.ads
@@ -0,0 +1,7 @@
+package Cannot_Inline with
+  SPARK_Mode
+is
+
+   procedure Test;
+
+end Cannot_Inline;
