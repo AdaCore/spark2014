e2f5c07c7f3f2ab6bba898d353826bcc5985a1eb
Yannick Moy <moy@adacore.com>
2014-04-07 14:40:03 +0200

    N317-015 Update tests outputs after partial revert in GNAT

--- testsuite/gnatprove/tests/JC17-017__aggr/p.ads
+++ testsuite/gnatprove/tests/JC17-017__aggr/p.ads
@@ -1,3 +1,10 @@
-package P is  
+package P is
    procedure Proc;
+   procedure Proc2;
+   procedure Proc3;
+   procedure Proc4;
+   procedure Proc5;
+   procedure Proc6;
+   procedure Proc7;
+   procedure Proc8;
 end;
