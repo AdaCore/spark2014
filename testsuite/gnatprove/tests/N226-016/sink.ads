2f6b1fe63cbe8d1151a2e0b635ddb0bc597d7084
dross <dross@adacore.com>
2014-04-08 14:35:29 +0200

    N226-016 test passes now

--- testsuite/gnatprove/tests/N226-016/sink.ads
+++ testsuite/gnatprove/tests/N226-016/sink.ads
@@ -3,5 +3,5 @@ with System;
 package Sink
 is
 	The_Sink : Integer
-		with Async_Readers, Effective_Writes, Volatile, Address => System'To_Address (16#1234567800#);
+		with Async_Readers, Effective_Writes, Volatile, Address => System'To_Address (16#1234#);
 end Sink;
