4056d14738bb1f4e67ea5fe9d44c282591fc42c3
Florian Schanda <florian.schanda@altran.com>
2014-04-09 09:20:36 +0100

    N131-004 test - review riposte tests
    Fixed up smaller issues (style, etc.)
    
    * Deleted one very riposte/fdl specific test in basic.
    * Rewrote many tests that the front-end completely constant-folded
      away.

--- testsuite/gnatprove/tests/riposte__logic/logic.adb
+++ testsuite/gnatprove/tests/riposte__logic/logic.adb
@@ -3,74 +3,71 @@ is
 
    function Ok (V: in Integer) return Boolean
      with Convention => Ghost,
-          Import;
+          Import,
+          Global => null;
 
-   pragma Warnings (Off, "* has no effect");
-   procedure Implies_Test
-     with Depends => null
+   pragma Warnings (Off, "unused initial value of *");
+   pragma Warnings (Off, "subprogram * has no effect");
+
+   procedure Implies_Test (T : Boolean;
+                           F : Boolean)
+     with Pre => T and not F
    is
    begin
-      pragma Assert ((if False then False));
-      pragma Assert ((if False then True));
-      pragma Assert (not (if True then False));
-      pragma Assert ((if True then True));
+      pragma Assert ((if F then F));      --  @ASSERT:PASS
+      pragma Assert ((if F then T));      --  @ASSERT:PASS
+      pragma Assert (not (if T then F));  --  @ASSERT:PASS
+      pragma Assert ((if T then T));      --  @ASSERT:PASS
 
       --  We will need the extra asserts to avoid introducing false facts.
 
-      pragma Assert (not (if False then False));  --  @ASSERT:FAIL
+      pragma Assert (not (if F then F));  --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert (not (if False then True));  --  @ASSERT:FAIL
+      pragma Assert (not (if F then T));  --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert ((if True then False));  --  @ASSERT:FAIL
+      pragma Assert ((if T then F));      --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert (not (if True then True));  --  @ASSERT:FAIL
+      pragma Assert (not (if T then T));  --  @ASSERT:FAIL
 
       null;
    end Implies_Test;
 
-   pragma Warnings (Off, "* has no effect");
-   procedure Equivalence_Test
-     with Depends => null
+   procedure Equivalence_Test (T : Boolean;
+                               F : Boolean)
+     with Pre => T and not F
    is
    begin
-      pragma Assert (False = False);  --  @ASSERT:PASS
-      pragma Assert (not (False = True));  --  @ASSERT:PASS
-      pragma Assert (not (True = False));  --  @ASSERT:PASS
-      pragma Assert (True = True);  --  @ASSERT:PASS
+      pragma Assert (F = F);        --  @ASSERT:PASS
+      pragma Assert (not (F = T));  --  @ASSERT:PASS
+      pragma Assert (not (T = F));  --  @ASSERT:PASS
+      pragma Assert (T = T);        --  @ASSERT:PASS
 
       --  We will need the extra asserts to avoid introducing false facts.
 
-      pragma Assert (not (False = False));  --  @ASSERT:FAIL
+      pragma Assert (not (F = F));  --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert (False = True);  --  @ASSERT:FAIL
+      pragma Assert (F = T);        --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert (True = False);  --  @ASSERT:FAIL
+      pragma Assert (T = F);        --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert (not (True = True));  --  @ASSERT:FAIL
+      pragma Assert (not (T = T));  --  @ASSERT:FAIL
       null;
    end Equivalence_Test;
-   pragma Warnings (On, "* has no effect");
 
-   pragma Warnings (Off, "* has no effect");
-   procedure De_Morgan
-      with Depends => null
+   procedure De_Morgan (A, B : Boolean)
    is
-      A, B : Boolean;
    begin
-
-      A := False;
-      B := False;
       pragma Assert_And_Cut (True);
-      pragma Assert (not (A or B) = (not A and not B));  --  @ASSERT:PASS
+      pragma Assert (not (A or B) = (not A and not B));     --  @ASSERT:PASS
 
       pragma Assert_And_Cut (True);
-      pragma Assert ((not (A and B)) = (not A or not B));  --  @ASSERT:PASS
+      pragma Assert ((not (A and B)) = (not A or not B));   --  @ASSERT:PASS
 
       --  And now for some false ones...
 
@@ -78,26 +75,21 @@ is
       pragma Assert ((not (A and B)) = (not A and not B));  --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert ((A or B) = (not A and not B));  --  @ASSERT:FAIL
+      pragma Assert ((A or B) = (not A and not B));         --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert (not (A or B) = (A and not B));  --  @ASSERT:FAIL
+      pragma Assert (not (A or B) = (A and not B));         --  @ASSERT:FAIL
 
       pragma Assert_And_Cut (True);
-      pragma Assert (not (A or B) = (not A and B));  --  @ASSERT:FAIL
+      pragma Assert (not (A or B) = (not A and B));         --  @ASSERT:FAIL
    end De_Morgan;
-   pragma Warnings (On, "* has no effect");
 
-   procedure Contraposition
-     with Depends => null
+   procedure Contraposition (A, B : Boolean)
    is
-      A, B : Boolean;
    begin
-      A := False;
-      B := False;
-
       pragma Assert_And_Cut (True);
-      pragma Assert (if (if not A then not B) then (if B then A));  --  @ASSERT:PASS
+      pragma Assert (if (if not A then not B)  --  @ASSERT:PASS
+                     then (if B then A));
    end Contraposition;
 
    function XOR_Test_A (A, B: Boolean) return Boolean
@@ -117,14 +109,9 @@ is
       return R;
    end XOR_Test_A;
 
-   procedure XOR_Test_B
-     with Depends => null
+   procedure XOR_Test_B (A, B: Boolean)
    is
-      A, B : Boolean;
    begin
-      A := False;
-      B := False;
-
       pragma Assert_And_Cut (True);
       pragma Assert ((A xor B) = ((A or B) and (not (A and B))));  --  @ASSERT:PASS
 
@@ -132,33 +119,28 @@ is
       pragma Assert ((A xor B) = ((A and not B) or (not A and B)));  --  @ASSERT:PASS
    end XOR_Test_B;
 
-   pragma Warnings (Off, "* has no effect");
    procedure Equivalence_Test_2 (A: in Integer)
      with Depends => (null => A),
           Pre     => (A > 0) = Ok (A),
-          Post    => Ok (A) = (A > 0)
+          Post    => Ok (A) = (A > 0) --  @POSTCONDITION:PASS
    is
    begin
       null;
    end Equivalence_Test_2;
-   pragma Warnings (On, "* has no effect");
 
    function Equivalence_Test_3 (A, B, C: in Boolean) return Boolean
-     with Post => Equivalence_Test_3'Result = ((A = B) = C)
+     with Post => Equivalence_Test_3'Result = ((A = B) = C)  --  @POSTCONDITION:PASS
    is
    begin
       return A = (B = C);
    end Equivalence_Test_3;
 
    --  Implication is not associative.
-   function Implies_Test_1 (A, B, C: in Boolean) return Boolean
-     with Depends => (Implies_Test_1'Result => null,
-                      null => (A, B, C)),
-          Post    => Implies_Test_1'Result =  --  @POSTCONDITION:FAIL
-            ((if (if A then B) then C) = (if A then (if B then C)))
+   procedure Implies_Test_1 (A, B, C: in Boolean)
    is
    begin
-      return True;
+      pragma Assert ((if (if A then B) then C) =  --  @ASSERT:FAIL
+                     (if A then (if B then C)));
    end Implies_Test_1;
 
 end Logic;
