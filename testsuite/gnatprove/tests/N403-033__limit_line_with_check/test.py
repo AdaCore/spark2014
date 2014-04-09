bb5449c472d18ae5b96f496503c1432e6ddfaf48
Julien Thierry <thierry@adacore.com>
2014-04-09 12:12:21 +0200

    N403-033 Test for extention of --limit-line
    * testsuite/gnatprove/tests/N403-033__limit_line_with_check
    Folder containing test files for --limitline option with parameter
    filename:line:column:checkkind.

---
+++ testsuite/gnatprove/tests/N403-033__limit_line_with_check/test.py
@@ -0,0 +1,3 @@
+from test_support import *
+
+prove_all(opt=["--limit-line=test_if.ads:5:37:VC_OVERFLOW_CHECK"])
---
+++ testsuite/gnatprove/tests/N403-033__limit_line_with_check/test_if.adb
@@ -0,0 +1,20 @@
+package body Test_If is
+
+   function Test (A : integer) return Integer is
+      B : Integer;
+      C : integer := 3;
+   begin
+      if (a = 0) then
+         B := 500;
+      else
+         B := 1000;
+      end if;
+
+      C := C + B;
+
+      pragma assert (C > 0);
+
+      return C;
+   end;
+
+end Test_If;
