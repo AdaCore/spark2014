6d74b5f397f7bdbeb1be093fdfda237cd7c77e28
Florian Schanda <florian.schanda@altran.com>
2014-04-16 13:40:30 +0100

    N409-030 make sure we still test the right thing given inlining

--- testsuite/gnatprove/tests/M807-032__async_writers_globals/async_globals.adb
+++ testsuite/gnatprove/tests/M807-032__async_writers_globals/async_globals.adb
@@ -2,18 +2,36 @@ package body Async_Globals is
 
    G : Integer with Volatile, Async_Writers;
 
-   procedure Get (X : out Integer) is
+   procedure Get_1 (X : out Integer) is
    begin
       X := G;
-   end Get;
+   end Get_1;
 
-   procedure Test (X : out Integer;
-                   Y : out Integer)
+   procedure Get_2 (X : out Integer)
+   with Global => G
    is
    begin
-      Get (X);
-      Get (Y);
+      X := G;
+   end Get_2;
+
+   procedure Test_1 (X : out Integer;
+                     Y : out Integer)
+   with Pre => True
+   is
+   begin
+      Get_1 (X);
+      Get_1 (Y);
       pragma Assert (X = Y);  --  should not be provable
-   end Test;
+   end Test_1;
+
+   procedure Test_2 (X : out Integer;
+                     Y : out Integer)
+   with Pre => True
+   is
+   begin
+      Get_2 (X);
+      Get_2 (Y);
+      pragma Assert (X = Y);  --  should not be provable
+   end Test_2;
 
 end Async_Globals;
