7fa46ca8c117a633febddb04966db319555cd767
Yannick Moy <moy@adacore.com>
2014-04-23 15:55:57 +0200

    N423-024 Add test

---
+++ testsuite/gnatprove/tests/N423-024__generic_inst/context.adb
@@ -0,0 +1,21 @@
+procedure Context (X : Integer) with
+  SPARK_Mode
+is
+   generic
+      V : Integer;
+   procedure Gen;
+   
+   procedure Gen is
+   begin
+      pragma Assert (V > 0);
+   end Gen;
+   
+   procedure Inst is new Gen (X);
+   
+   procedure Local is
+   begin
+      Inst;
+   end Local;
+begin
+   Local;
+end Context;
