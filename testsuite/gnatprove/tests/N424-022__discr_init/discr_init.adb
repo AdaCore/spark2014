817fe684063a3cc43e35b3709e1c9146d793d0c7
Yannick Moy <moy@adacore.com>
2014-04-24 15:40:30 +0200

    N424-022 Add test

---
+++ testsuite/gnatprove/tests/N424-022__discr_init/discr_init.adb
@@ -0,0 +1,19 @@
+procedure Discr_Init with
+  SPARK_Mode 
+is
+   type A1 is array (Natural range <>) of Boolean;
+   type R1 (J : Integer) is record
+      case J is
+         when Positive =>
+            Arr : A1(1 .. J) := (others => True);
+         when others =>
+            null;
+      end case;
+   end record;
+   subtype R2 is R1(10);
+
+   X : R2;
+   Y : R2;
+begin
+   X.Arr(1) := Y.Arr(10);
+end;
