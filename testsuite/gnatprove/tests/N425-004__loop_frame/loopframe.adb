c38843a9cc80ab0d8615b78280d424b053d03135
Yannick Moy <moy@adacore.com>
2014-04-25 11:08:03 +0200

    N425-004 Add test

---
+++ testsuite/gnatprove/tests/N425-004__loop_frame/loopframe.adb
@@ -0,0 +1,18 @@
+package body Loopframe with
+  SPARK_Mode
+is
+   
+   function Get return Idx is
+      V : Idx := 0;
+   begin
+      for X in Valid_Idx loop
+         if A (X) then
+            V := X;
+            exit;
+         end if;
+--         pragma Loop_Invariant (V = 0);
+      end loop;
+      return V;
+   end Get;
+   
+end Loopframe;
