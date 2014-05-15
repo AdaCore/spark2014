744669c94c174fb6750add484de032460e1e8067
Yannick Moy <moy@adacore.com>
2014-05-15 19:35:44 +0200

    N515-042 Add test

---
+++ testsuite/gnatprove/tests/N515-042__ipstack_aggregate/assign_aggr.adb
@@ -0,0 +1,21 @@
+package body Assign_Aggr is
+   
+   procedure Consume (U : T; Err : out Boolean) is
+   begin
+      if U.X > U.Y then
+         Err := True;
+      else
+         Err := False;
+      end if;
+   end Consume;
+   
+   procedure Compute (V : out Integer) is
+      Err : Boolean;
+      W : T;
+   begin
+      W := (0, 0, 0);
+      Consume (W, Err);
+      V := 10;
+   end Compute;
+   
+end Assign_Aggr;
---
+++ testsuite/gnatprove/tests/N515-042__ipstack_aggregate/test.py
@@ -0,0 +1,3 @@
+from test_support import *
+
+prove_all(opt=["--ide-progress-bar"])
