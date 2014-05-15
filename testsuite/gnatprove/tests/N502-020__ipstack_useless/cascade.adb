fc34491c1815d07fbc0af372b8a690acf7289548
Yannick Moy <moy@adacore.com>
2014-05-15 11:42:32 +0200

    N502-020 Add test

---
+++ testsuite/gnatprove/tests/N502-020__ipstack_useless/cascade.adb
@@ -0,0 +1,26 @@
+procedure Cascade (X : in out Integer) is
+   
+   procedure Link_Output (Nid : Integer; Buf : Integer; Err : out Integer) 
+     with Global => null
+   is
+   begin
+      Err := Nid + Buf;
+   end Link_Output;
+   
+   T1, T2, T3, T4, T5 : Integer;
+   Err : Integer;
+begin
+   T1 := X + X;
+   if T1 > X then
+      T2 := T1;
+   else
+      T2 := X;
+   end if;
+   T3 := T2 - T1;
+   T4 := 2 * T3;
+   T5 := (if T1 > T2 then T3 else T4);
+   
+   Link_Output (X, T5, Err);
+   
+   X := 10;
+end Cascade;
