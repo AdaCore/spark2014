fa1bc9febbc4c66ce51b4923db2395de0c99b9b4
dross <dross@adacore.com>
2014-05-20 10:26:36 +0200

    M614-033 fully prove stack example
    * ada__containers__formal_vectors.mlw
    (Append) Add a redundant info in the postcondition of Append for the
    special case of appending only one element.
    
    * M607-025__rev_stack
    (stack.ads) Use universal quantification instead of slice/concatenation
    in postconditions of push and pop.

--- testsuite/gnatprove/tests/M607-025__rev_stack/list.adb
+++ testsuite/gnatprove/tests/M607-025__rev_stack/list.adb
@@ -22,10 +22,10 @@ package body List is
          Push (S, Element (L, I));
          pragma Loop_Invariant
          -- The size of the model of the stack increases with each push
-           (Model.To (S)'Last = I - First + Model.To (S)'First and then
+           (Model.To (S)'Last = I - First + 1 and then
             -- the model of the stack contains each element of the list in order
               (for all J in First .. I =>
-                   Model.To (S)(J - First + Model.To (S)'First) = Element (L, J)));
+                   Model.To (S)(J - First + 1) = Element (L, J)));
       end loop;
 
       for I in  First .. Last loop
@@ -33,11 +33,10 @@ package body List is
          -- The size of the List increases with each Append
            (Natural (Length (Res)) = I - First and then
             -- The size of the model of the stack decreases with each pop
-            Model.To (S)'Last = Last - I + Model.To (S)'First and then
+            Model.To (S)'Last = Last - I + 1 and then
             -- The remaining of the model of the stack is unchanged
-              (for all J in Model.To (S)'First .. Last - I + Model.To (S)'First =>
-                   Model.To (S) (J) = Model.To (S'Loop_Entry)
-               (J - Model.To (S)'First + Model.To (S'Loop_Entry)'First)) and then
+              (for all J in 1 .. Last - I + 1 =>
+                   Model.To (S) (J) = Model.To (S'Loop_Entry) (J)) and then
             -- the Post of the subprogram is partially valid at this stage
               (for all J in First .. I - 1 =>
                    Element (Res, J) = Element (L, Last - J + First)));
--- testsuite/gnatprove/tests/M607-025__rev_stack/stack.ads
+++ testsuite/gnatprove/tests/M607-025__rev_stack/stack.ads
@@ -8,28 +8,38 @@ package Stack is
    package Model is
       type M is array (Positive range <>) of Integer;
       
-      function To (S : Stack) return M;
+      function To (S : Stack) return M with
+        Post => To'Result'First = 1 and then To'Result'Last >= 0;
    end Model;
+   
    use Model;
 
    function Is_Full  (S : Stack) return Boolean is
-     (To (S)'Last - To (S)'First + 1 >= Max);
+     (To (S)'Last >= Max);
    function Is_Empty (S : Stack) return Boolean is
-     (To (S)'Last < To (S)'First);
+     (To (S)'Last < 1);
    
    function Empty_Stack return Stack with
      Post => Is_Empty (Empty_Stack'Result);
 
    function Top (S : Stack) return Integer with
-     Pre => not Is_Empty (S);
+     Pre  => not Is_Empty (S),
+     Post => Top'Result = To (S) (To (S)'Last);
 
    procedure Push(S : in out Stack; X : in Integer) with
      Pre  => not Is_Full (S),
-     Post => To (S) = To (S'Old) & X;
+--       Post => To (S) = To (S)'Old & X;
+     Post => To (S)'Last = To (S)'Old'Last + 1 and then
+     To (S) (To (S)'Last) = X and then
+     (for all I in To (S)'Old'Range =>
+                  To (S) (I) = To (S)'Old (I));
 
    procedure Pop (S : in out Stack) with
      Pre  => not Is_Empty (S),
-     Post => To (S) = To (S'Old) (To (S'Old)'First .. To (S'Old)'Last - 1);
+--       Post => To (S) = To (S)'Old (1 .. To (S)'Old'Last - 1);
+     Post => To (S)'Last = To (S)'Old'Last - 1 and then
+     (for all I in To (S)'Range =>
+                  To (S) (I) = To (S)'Old (I));
 
 private
    type Intarray is array (positive range <>) of integer;
