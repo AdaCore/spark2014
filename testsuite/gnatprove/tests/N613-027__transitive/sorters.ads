d605fca83119e73ce882a935038627c66332f58c
Johannes Kanig <kanig@adacore.com>
2014-06-16 11:05:56 +0200

    N613-027 file test case

---
+++ testsuite/gnatprove/tests/N613-027__transitive/sorters.ads
@@ -0,0 +1,25 @@
+package Sorters
+   with SPARK_Mode
+is
+   type Array_Type is array (Positive range <>) of Integer;
+
+   function Perm (A : in Array_Type;
+                  B : in Array_Type) return Boolean
+   -- Returns True is A is a permuation of B
+     with
+       Global => null,
+       Convention => Ghost, Import;
+
+
+   -- Sorts the elements in the array Values in ascending order
+   procedure Selection_Sort (Values : in out Array_Type)
+     with
+        Depends => (Values => Values),
+        Pre     => Values'Length >= 1 and then
+                   Values'Last   <= Positive'Last,
+        Post    => Perm (Values, Values'Old);
+
+
+--                   (for all J in Values'First .. Values'Last - 1 =>
+--                    Values (J) <= Values (J + 1));
+end Sorters;
