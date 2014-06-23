c245925e452a1ff54f8e865846a08d1cccd0552e
Zhi Zhang <zhangzhi@ksu.edu>
2014-06-23 18:14:26 +0200

    proof for copy_in and copy_out

--- language_formal/spark2014_semantics/spark2014_core_subset(non-nested)/checks_completeness.v
+++ language_formal/spark2014_semantics/spark2014_core_subset(non-nested)/checks_completeness.v
@@ -200,7 +200,95 @@ Proof.
 Qed.
 *)
 
+Lemma copy_in_checks_completeness: forall params s f args f' params' args' f'' ,
+  copy_in s f params args f' ->
+    copy_in_x s f params' args' f'' ->
+      compile2_flagged_parameter_specifications params params' ->
+        compile2_flagged_exps args args' ->
+          f' = f''.
+Proof.
+  induction params; smack.
+- inversion H1; clear H1; subst.
+  destruct args. 
+  + inversion H2; clear H2; subst.
+    inversion H; inversion H0; smack.
+  + inversion H.
+- destruct args, args', params';
+  match goal with 
+  | [H: copy_in _ _ (?a :: ?al) nil _ |- _] => inversion H
+  | [H: copy_in _ _ nil (?a :: ?al) _ |- _] => inversion H
+  | [H: copy_in_x _ _ nil (?a :: ?al) _ |- _] => inversion H
+  | [H: compile2_flagged_exps (?a :: ?al) nil |- _] => inversion H
+  | _ => idtac
+  end.
+  inversion H1; clear H1; subst.
+  inversion H2; clear H2; subst.
+  assert(HZ1: a.(parameter_name) = p.(parameter_name_x)).
+    inversion H6; smack.
+  assert(HZ2: a.(parameter_mode) = p.(parameter_mode_x)).
+    inversion H6; smack.
 
+  inversion H; clear H; subst;
+  inversion H0; clear H0; smack;
+  match goal with
+  | [H1: eval_expr ?s ?e ?v,
+     H2: eval_expr_x ?s ?e' ?v',
+     H3: compile2_flagged_exp ?e ?e' |- _] => specialize (expression_checks_completeness _ _ _ _ _ H1 H2 H3); smack
+  | _ => idtac
+  end;
+  match goal with
+  | [H1: copy_in ?s ?f ?params ?args _, 
+     H2: copy_in_x ?s ?f ?params' ?args' _, 
+     H3: compile2_flagged_parameter_specifications ?params ?params', 
+     H4: compile2_flagged_exps ?args ?args' |- _] => specialize (IHparams _ _ _ _ _ _ _ H1 H2 H3 H4); smack
+  end.
+Qed.
+
+Lemma copy_out_checks_completeness: forall params s f args f' params' args' f'' ,
+  copy_out s f params args f' ->
+    copy_out_x s f params' args' f'' ->
+      compile2_flagged_parameter_specifications params params' ->
+        compile2_flagged_exps args args' ->
+          f' = f''.
+Proof.
+  induction params; smack.
+- inversion H1; clear H1; subst.
+  destruct args. 
+  + inversion H2; clear H2; subst.
+    inversion H; inversion H0; smack.
+  + inversion H.
+- destruct args, args', params';
+  match goal with 
+  | [H: copy_out _ _ (?a :: ?al) nil _ |- _] => inversion H
+  | [H: copy_out _ _ nil (?a :: ?al) _ |- _] => inversion H
+  | [H: copy_out_x _ _ nil (?a :: ?al) _ |- _] => inversion H
+  | [H: compile2_flagged_exps (?a :: ?al) nil |- _] => inversion H
+  | _ => idtac
+  end.
+  inversion H1; clear H1; subst.
+  inversion H2; clear H2; subst.
+  assert(HZ1: a.(parameter_name) = p.(parameter_name_x)).
+    inversion H6; smack.
+  assert(HZ2: a.(parameter_mode) = p.(parameter_mode_x)).
+    inversion H6; smack.
+
+  inversion H; clear H; subst;
+  inversion H0; clear H0; smack.
+  rewrite HZ1 in *.
+  inversion H5; inversion H7; smack. 
+  repeat progress match goal with
+  | [H1: STACK.fetch ?x ?f = _, H2: STACK.fetch ?x ?f = _ |- _] => rewrite H1 in H2; smack
+  | [H1: STACK.updateG ?s ?x ?v = _ , H2: STACK.updateG ?s ?x ?v = _ |- _] => rewrite H1 in H2; smack
+  end.
+Qed.
+
+
+
+Lemma statement_checks_completeness: forall e e' s v v',
+  eval_stmt st s c s' ->
+    eval_stmt_x st s c' s'' ->
+      compile2_flagged_stmt c c' ->
+        s' = s''.
 
 
 
