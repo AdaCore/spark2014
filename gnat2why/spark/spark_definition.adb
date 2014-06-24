de1326e204b751235f00d1cbdc56388813acac14
Florian Schanda <florian.schanda@altran.com>
2014-06-24 08:49:38 +0100

    Minor compile fix.

--- gnat2why/spark/spark_definition.adb
+++ gnat2why/spark/spark_definition.adb
@@ -3120,6 +3120,7 @@ package body SPARK_Definition is
               Pragma_Check_Policy                 |
               Pragma_Inline_Always                |
               Pragma_Linker_Section               |
+              Pragma_No_Elaboration_Code_All      |
               Pragma_Pure_Function                |
               Pragma_Restriction_Warnings         |
               Pragma_Style_Checks                 |
