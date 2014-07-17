eb1c4899f2c57e2b761de0f267d41b21b9dcac4a
Johannes Kanig <kanig@adacore.com>
2014-07-17 10:24:21 +0200

    N715-032 remove gnatmake -P from SPARK testsuite

--- testsuite/gnatprove/tests/L503-003__clean/test.py
+++ testsuite/gnatprove/tests/L503-003__clean/test.py
@@ -3,7 +3,7 @@ from test_support import *
 
 call(["mkdir", "-p", "obj"])
 prove_all()
-call(["gnatmake", "-q", "-P", "test_obj.gpr"])
+call(["gprbuild", "-q", "-P", "test_obj.gpr"])
 prove_all(opt=["-P", "test_obj.gpr"])
 gnatprove(opt=["-P", "test.gpr", "--clean"])
 gnatprove(opt=["-P", "test_obj.gpr", "--clean"])
