bbb5c227b69e22f5921f08288861928e6c658adf
Florian Schanda <florian.schanda@altran.com>
2014-08-12 10:55:02 +0100

    N428-012 expand testcase

--- testsuite/gnatprove/tests/N428-012__flow_conversions_of_records/conversions_of_records.adb
+++ testsuite/gnatprove/tests/N428-012__flow_conversions_of_records/conversions_of_records.adb
@@ -1,17 +1,62 @@
 with Ada.Unchecked_Conversion;
 
 package body Conversions_Of_Records
-  with SPARK_Mode
 is
+   type Rec_A is record
+      X : Integer;
+      F : Boolean;
+   end record;
+
+   type Rec_B is record
+      A : Short_Integer;
+      B : Short_Integer;
+      G : Boolean;
+   end record;
+
+   type Rec_C is new Rec_A;
+
+   subtype Rec_D is Rec_A;
+
    function To_Rec_B is new Ada.Unchecked_Conversion (Source => Rec_A,
                                                       Target => Rec_B);
 
-   procedure Conv (R  : in     Rec_A;
-                   X1 :    out Integer;
-                   X2 :    out Integer)
+   procedure Test_01_OK (X1 : in     Integer;
+                         X2 : in     Boolean;
+                         Y1 :    out Short_Integer;
+                         Y2 :    out Short_Integer)
+   with Depends => (Y1 => (X1, X2),
+                    Y2 => (X1, X2))
    is
+      R : Rec_A := (X1, X2);
    begin
-      X1 := Rec_B (R).X;
-      X2 := To_Rec_B (R).X;
-   end Conv;
+      Y1 := To_Rec_B (R).A;
+      Y2 := To_Rec_B (R).B;
+   end Test_01_Ok;
+
+   procedure Test_02_OK (X1 : in     Integer;
+                         X2 : in     Boolean;
+                         Y1 :    out Integer;
+                         Y2 :    out Boolean)
+   with Depends => (Y1 => (X1, X2),
+                    Y2 => (X1, X2))
+   is
+      R : Rec_A := (X1, X2);
+   begin
+      Y1 := Rec_C (R).X;
+      Y2 := Rec_C (R).F;
+   end Test_02_Ok;
+
+   procedure Test_03_OK (X1 : in     Integer;
+                         X2 : in     Boolean;
+                         Y1 :    out Integer;
+                         Y2 :    out Boolean)
+   with Depends => (Y1 => X1,
+                    Y2 => X2)
+   is
+      R : Rec_A := (X1, X2);
+   begin
+      Y1 := Rec_D (R).X;
+      Y2 := Rec_D (R).F;
+   end Test_03_Ok;
+
 end Conversions_Of_Records;
--- testsuite/gnatprove/tests/N428-012__flow_conversions_of_records/conversions_of_records.ads
+++ testsuite/gnatprove/tests/N428-012__flow_conversions_of_records/conversions_of_records.ads
@@ -1,13 +1,4 @@
 package Conversions_Of_Records
-  with SPARK_Mode
 is
-   type Rec_A is record
-      X : Integer;
-   end record;
-
-   subtype Rec_B is Rec_A;
-
-   procedure Conv (R  : in     Rec_A;
-                   X1 :    out Integer;
-                   X2 :    out Integer);
+   pragma Elaborate_Body;
 end Conversions_Of_Records;
