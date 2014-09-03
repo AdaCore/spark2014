3db04d3a5b8362afa09a55ab3da300e89693275d
Johannes Kanig <kanig@adacore.com>
2014-09-03 11:50:34 +0200

    MC02-014 improved test

--- testsuite/gnatprove/tests/N306-009__operators/inst_ext_ax.ads
+++ testsuite/gnatprove/tests/N306-009__operators/inst_ext_ax.ads
@@ -8,6 +8,7 @@ package Inst_Ext_Ax with SPARK_Mode is
      (Element_Type => Natural, "=" => "=");
 
    subtype Int100 is Integer range 0 .. 100;
+   subtype Int200 is Integer range 0 .. 200;
 
    generic
       with function Add (I1, I2 : Int100) return Int100;
@@ -17,5 +18,18 @@ package Inst_Ext_Ax with SPARK_Mode is
          (Add (I1, I2));
    end;
 
+   generic
+      with function Add (I1, I2 : Int100) return Int100;
+      with function Add2 (I1, I2, I3 : Int200) return Int200;
+   package My_Add2 is
+      pragma Annotate (GNATprove, External_Axiomatization);
+      function Add_Wrap (I1, I2 : Int100) return Int200 is
+         (Add (I1, I2) + Add2(I1, I2, I1));
+   end;
+
+   function AddThree(A, B, C : Integer) return Integer is
+      (A + B + C);
+
    package Bad_Add is new My_Add ("+"); --@RANGE_CHECK:FAIL
+   package Bad_Add2 is new My_Add2 ("+", AddThree); --@RANGE_CHECK:FAIL
 end;
