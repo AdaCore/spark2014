9b820f98e651924fb21952295a0df915def209e5
Johannes Kanig <kanig@adacore.com>
2014-09-04 12:08:51 +0200

    N814-007 file test

---
+++ testsuite/gnatprove/tests/N814-007__refined_post/list_handler.ads
@@ -0,0 +1,14 @@
+pragma SPARK_Mode(On);
+
+package List_Handler
+   with
+      Abstract_State => List,
+      Initializes => List
+is
+
+   procedure Append_Range (Lower, Upper : in Integer)
+      with
+         Global => (In_Out => List),
+         Depends => (List =>+ (Lower, Upper));
+
+end List_Handler;
