db0b73f8f9dc721d65f9ef604017e1dd568c21c0
Johannes Kanig <kanig@adacore.com>
2014-09-26 15:42:11 +0200

    N716-015 steps factor for cvc4

--- gnatprove/gnatprove.adb
+++ gnatprove/gnatprove.adb
@@ -562,7 +562,8 @@ procedure Gnatprove is
          Start_Section ("prover");
          if Steps /= 0 then
             Put_Keyval ("command",
-                        Command & " --rlimit=" & Int_Image (Steps) & " %f");
+                        Command & " --rlimit=" & Int_Image (Steps * 1000) &
+                        " %f");
          else
             Put_Keyval ("command", Command & " %f");
          end if;
