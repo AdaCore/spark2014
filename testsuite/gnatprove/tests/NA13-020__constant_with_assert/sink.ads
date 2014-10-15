bcc886946458f1f74d031938c14faac3d6d5b97e
Yannick Moy <moy@adacore.com>
2014-10-15 15:08:04 +0200

    NA13-020 Fix test for 32 bits machines

--- testsuite/gnatprove/tests/NA13-020__constant_with_assert/sink.ads
+++ testsuite/gnatprove/tests/NA13-020__constant_with_assert/sink.ads
@@ -4,5 +4,5 @@ with SK;
 package Sink
 is
 	The_Sink : SK.Word64
-		with Async_Readers, Effective_Writes, Volatile, Address => System'To_Address (16#1234567800#);
+		with Async_Readers, Effective_Writes, Volatile, Address => System'To_Address (16#00000000#);
 end Sink;
