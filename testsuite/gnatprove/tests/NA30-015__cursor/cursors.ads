db5632b481e38f445cab47e14db3fde3451a3fa7
Yannick Moy <moy@adacore.com>
2014-10-30 12:17:10 +0100

    NA30-015 Add dummy value for cursors of formal containers
    Like other types, cursors of formal containers need to provide a dummy
    value for use in axioms involving other types/functions.

---
+++ testsuite/gnatprove/tests/NA30-015__cursor/cursors.ads
@@ -0,0 +1,21 @@
+with Ada.Containers.Formal_Doubly_Linked_Lists;
+
+package Cursors is
+
+   package L is new Ada.Containers.Formal_Doubly_Linked_Lists (Integer);
+   use L;
+
+   type R (B : Boolean) is record
+      case B is
+         when True =>
+            C : Cursor;
+         when False =>
+            null;
+      end case;
+   end record;
+
+   X : R(False);
+
+   function Get return R is (X);
+
+end Cursors;
