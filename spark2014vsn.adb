978b122ec565d96c82c59b4ba832cc1c380725cf
Arnaud Charlet <charlet@adacore.com>
2014-11-03 10:28:57 +0100

    Minor: update header.

--- spark2014vsn.adb
+++ spark2014vsn.adb
@@ -6,9 +6,9 @@
 --                                                                          --
 --                                   B o d y                                --
 --                                                                          --
---                         Copyright (C) 2011-2013, AdaCore                 --
+--                         Copyright (C) 2011-2014, AdaCore                 --
 --                                                                          --
--- Hi-Lite is free software; you can redistribute it and/or modify it under --
+-- SPARK is free  software; you can redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
 -- ware  Foundation;  either version 3,  or (at your option) any later ver- --
 -- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
@@ -17,8 +17,6 @@
 -- for  more details.  You should have  received  a copy of the GNU General --
 -- Public License  distributed with GNAT; see file COPYING3.  If not, go to --
 -- http://www.gnu.org/licenses for a complete copy of the license.          --
---                                                                          --
--- Hi-Lite is maintained by AdaCore (http://www.adacore.com)                --
 --                                                                          --
 ------------------------------------------------------------------------------
 
