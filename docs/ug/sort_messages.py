ff7b10634f72758d238d0459ad849ecc7fde8f32
Yannick Moy <moy@adacore.com>
2014-11-06 14:58:52 +0100

    Minor addition of sorting to produce gnatprove_by_example results
    This ensures that the output is always the same between two night's runs.
    The sorting function is taken from the spark2014 testsuite scripts.
    Update outputs with the new order.

---
+++ docs/ug/sort_messages.py
@@ -0,0 +1,22 @@
+"""
+This module implement a sorting of messages issued by GNATprove, suitable for
+inclusion in the User's Guide.
+
+It should be called with the absolute path for the command.
+"""
+
+import os
+import sys
+import glob
+
+# Add directory for finding test_support
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), "../../testsuite/gnatprove/lib/python")))
+from test_support import sort_key_for_errors
+
+def print_sorted(strlist):
+    strlist.sort(key=sort_key_for_errors)
+    for line in strlist:
+        print line, # do not add newline here, as already presentin lines read from input
+
+if __name__ == '__main__':
+    print_sorted (sys.stdin.readlines())
