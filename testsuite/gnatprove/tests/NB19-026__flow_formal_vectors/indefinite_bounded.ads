31bbb55657670a607bbed2df343427759b8ee62d
Yannick Moy <moy@adacore.com>
2014-11-19 15:26:31 +0100

    NB19-026 Add test

---
+++ testsuite/gnatprove/tests/NB19-026__flow_formal_vectors/indefinite_bounded.ads
@@ -0,0 +1,16 @@
+with Ada.Containers.Formal_Indefinite_Vectors; use Ada.Containers;
+
+package Indefinite_Bounded with
+  SPARK_Mode
+is
+   package Vect is new Ada.Containers.Formal_Indefinite_Vectors
+     (Index_Type   => Positive,
+      Element_Type => Integer,
+      Max_Size_In_Storage_Elements => Integer'Size,
+      "="          => "=",
+      Bounded      => True);
+   use Vect;
+
+   procedure Test;
+
+end Indefinite_Bounded;
