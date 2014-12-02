6d2ce4beba487a2ea7c26f361a04b8a835a8b3a5
Yannick Moy <moy@adacore.com>
2014-12-02 10:25:49 +0100

    N317-021 Update test and output

--- testsuite/gnatprove/tests/N317-021__vectors/test.py
+++ testsuite/gnatprove/tests/N317-021__vectors/test.py
@@ -1,20 +1,14 @@
 from subprocess import call
 from test_support import *
 
-prove_all(steps=1000, opt=["-u", "bounded.adb", "unbounded.adb"])
-# ADD AFTER NB19-026 AND NB20-048 ARE FIXED, AND REPLACE
+# Remove analysis on selected files only after NC02-004 has been fixed
+# AND REPLACE
 #   @ ASSERT:FAIL
 # BY
 #   @ASSERT:FAIL
 # IN THOSE TESTS AT THIS POINT
-# "indefinite_bounded.adb", "indefinite_unbounded.adb"
-
-# ADD ALSO THE TESTS BELOW AFTER THE ABOVE AND NB19-034 ARE FIXED, AND REPLACE
-#   @ ASSERT:FAIL
-# BY
-#   @ASSERT:FAIL
-# IN THOSE TESTS AT THIS POINT
-# "indefinite_bounded_tagged.adb", "indefinite_unbounded_tagged.adb"
+prove_all(steps=1000, opt = ["bounded.adb", "unbounded.adb",
+                             "indefinite_bounded.adb", "indefinite_unbounded.adb"])
 
 call(["gnatmake", "-gnata", "-q", "test_vectors.adb"])
 call(["./test_vectors"])
