af9d6c2387ae535d789b5e302bae186587f8ce88
Yannick Moy <moy@adacore.com>
2014-12-03 11:05:31 +0100

    NB17-028 Add missing files

---
+++ docs/ug/gnatprove_by_example/examples/use_logging_classwide.adb
@@ -0,0 +1,10 @@
+with Logging; use Logging;
+with Use_Logging;
+
+procedure Use_Logging_Classwide (Log : out Log_Type'Class) with
+  SPARK_Mode
+is
+begin
+   Log_Type (Log).Init_Log;
+   Log.Append_To_Log (2);
+end Use_Logging_Classwide;
