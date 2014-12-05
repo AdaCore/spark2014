8e9659c513deab8f2c0d3240225aabb740b8be61
Yannick Moy <moy@adacore.com>
2014-12-05 11:17:11 +0100

    NB17-028 Add bitwise swap to the challenges

--- testsuite/gnatprove/tests/proofinuse/bitwise.ads
+++ testsuite/gnatprove/tests/proofinuse/bitwise.ads
@@ -12,4 +12,7 @@ is
    --  from N613-008 (internal test)
    procedure Shift_Is_Div (X : Unsigned_32; Res : out Unsigned_32);
 
+   --  from NB17-028 (internal test)
+   procedure Swap (X, Y : in out Unsigned_32);
+
 end Bitwise;
