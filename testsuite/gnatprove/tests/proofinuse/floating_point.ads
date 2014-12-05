827c36a0e324daa35e40448bb23e7714187255f4
Johannes Kanig <kanig@adacore.com>
2014-12-05 11:23:15 +0100

    NC04-023 add to proofinuse challenges

--- testsuite/gnatprove/tests/proofinuse/floating_point.ads
+++ testsuite/gnatprove/tests/proofinuse/floating_point.ads
@@ -24,4 +24,12 @@ is
    --  from NC03-013 (industrial user)
    procedure Int_To_Float_Simple (X : Unsigned_16; Res : out Float_32);
 
+   --  from NC04-023 (industrial user)
+   function Float_to_Long_Float (X : Float) return Long_Float
+   is (Long_Float (X))
+      with Pre  => (X >= Float'First and
+                    X <= Float'Last),
+           Post => (Float_to_Long_Float'Result >= Long_Float (Float'First) and
+                    Float_to_Long_Float'Result <= Long_Float (Float'Last));
+
 end Floating_Point;
