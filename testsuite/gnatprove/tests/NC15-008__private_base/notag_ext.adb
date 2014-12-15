3ee3726d13247fb3dad5f939a98c79c8560a2661
Yannick Moy <moy@adacore.com>
2014-12-15 13:02:17 +0100

    NC15-008 Add test

---
+++ testsuite/gnatprove/tests/NC15-008__private_base/notag_ext.adb
@@ -0,0 +1,10 @@
+package body Notag_Ext with
+  SPARK_Mode
+is
+   procedure Test is
+      A : constant U := Create (1);
+      B : constant U := Create (2);
+   begin
+      pragma Assert (Sum(T(A)) /= Sum(T(B)));
+   end Test;
+end Notag_Ext;
