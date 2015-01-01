ec196c599196c346cdd0de0bb484bf1cf203ebed
Johannes Kanig <kanig@adacore.com>
2015-01-01 08:54:18 +0100

    minor: copyright update

--- gnat2why/why/gnat2why-nodes.ads
+++ gnat2why/why/gnat2why-nodes.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                  S p e c                                 --
 --                                                                          --
---                        Copyright (C) 2012-2014, AdaCore                  --
+--                        Copyright (C) 2012-2015, AdaCore                  --
 --                                                                          --
 -- gnat2why is  free  software;  you can redistribute  it and/or  modify it --
 -- under terms of the  GNU General Public License as published  by the Free --
