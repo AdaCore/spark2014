fa18768dc4da7b7c846fc7e9057180487832b196
Yannick Moy <moy@adacore.com>
2015-01-13 16:49:06 +0100

    O113-031 Update test script

--- testsuite/gnatprove/tests/M611-014__flow_no_return/test.py
+++ testsuite/gnatprove/tests/M611-014__flow_no_return/test.py
@@ -1,2 +1,5 @@
 from test_support import *
-prove_all()
+# run GNATprove separately on failing input foo.adb ...
+prove_all(opt=["-u", "foo.adb"])
+# ... and correct code in other units.
+prove_all(opt=["-u", "main1.adb", "main2.adb", "main3.adb", "main4.adb"])
