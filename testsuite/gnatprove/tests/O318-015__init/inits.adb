5d44b9fad5a715bd47373571bdaa7d7ea1fec94e
Yannick Moy <moy@adacore.com>
2015-03-18 11:42:31 +0100

    O318-015 Add test

---
+++ testsuite/gnatprove/tests/O318-015__init/inits.adb
@@ -0,0 +1,6 @@
+package body Inits with
+  SPARK_Mode,
+  Refined_State => (S => X)
+is
+   X : Integer := 10;
+end Inits;
---
+++ testsuite/gnatprove/tests/O318-015__init/inits.ads
@@ -0,0 +1,9 @@
+package Inits with
+  SPARK_Mode,
+  Abstract_State => S,
+  Initializes => S
+is
+   pragma Elaborate_Body;
+
+   Y : Integer := 0;
+end Inits;
