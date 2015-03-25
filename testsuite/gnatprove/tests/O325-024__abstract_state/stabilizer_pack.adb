b5bbe9882127e7724e5629a990105fc82d9db9e1
Yannick Moy <moy@adacore.com>
2015-03-25 15:36:24 +0100

    O325-024 Add test

---
+++ testsuite/gnatprove/tests/O325-024__abstract_state/stabilizer_pack.adb
@@ -0,0 +1,30 @@
+with Interfaces.C; use Interfaces.C;
+
+package body Stabilizer_Pack
+with SPARK_Mode,
+  Refined_State => (Asl_Parameters => (Asl_Err_Deadband,
+                                       Asl_Alpha,
+                                       Asl_Alpha_Long),
+                    Asl_Variables => (Temperature,
+                                      Pressure,
+                                      Asl,
+                                      Asl_Raw,
+                                      Asl_Long))
+is
+
+   --  Private procedures and functions
+
+   procedure Stabilizer_Alt_Hold_Update is
+      Asl_Tmp             : Float;
+      Asl_Long_Tmp        : Float;
+   begin
+      Asl_Tmp := Asl * Asl_Alpha + Asl_Raw * (1.0 - Asl_Alpha);
+      Asl_Long_Tmp := Asl_Long * Asl_Alpha_Long
+        + Asl_Raw * (1.0 - Asl_Alpha_Long);
+      if Pressure = 1.0 and Temperature = 25.0 and Asl_Err_Deadband = 0.0 then
+         null;
+      end if;
+
+   end Stabilizer_Alt_Hold_Update;
+
+end Stabilizer_Pack;
---
+++ testsuite/gnatprove/tests/O325-024__abstract_state/stabilizer_pack.ads
@@ -0,0 +1,42 @@
+with Interfaces.C.Extensions; use Interfaces.C.Extensions;
+
+package Stabilizer_Pack
+with SPARK_Mode,
+  Abstract_State => (Asl_Parameters,
+                     Asl_Variables),
+  Initializes => (Asl_Parameters,
+                  Asl_Variables)
+
+is
+private
+
+   --  Global variables and constants
+
+   --  Altitude variables
+   Temperature  : Float := 0.0
+     with Part_Of => Asl_Variables; --  Temperature from barometer
+   Pressure     : Float    := 1000.0
+     with Part_Of => Asl_Variables; --  Pressure from barometer
+   Asl          : Float    := 0.0
+     with Part_Of => Asl_Variables; --  Smoothed asl
+   Asl_Raw      : Float    := 0.0
+     with Part_Of => Asl_Variables; --  Raw asl
+   Asl_Long     : Float    := 0.0
+     with Part_Of => Asl_Variables; --  Long term asl
+
+   --  Altitude parameters
+   Asl_Err_Deadband     : Float := 0.00
+     with Part_Of => Asl_Parameters; --  error (target - altitude) deadband
+   Asl_Alpha            : Float := 0.92
+     with Part_Of => Asl_Parameters; --  Short term smoothing
+   Asl_Alpha_Long       : Float := 0.93
+     with Part_Of => Asl_Parameters; --  Long term smoothing
+
+   --  Function called when Alt_Hold mode is activated. Holds the drone
+   --  at a target altitude.
+   procedure Stabilizer_Alt_Hold_Update
+     with
+       Global => (Input   => (Asl_Parameters,
+                              Asl_Variables));
+
+end Stabilizer_Pack;
