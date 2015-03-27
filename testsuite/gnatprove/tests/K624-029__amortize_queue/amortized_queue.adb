782028110b2f9c5c29c3839a5aea4724949f659f
dross <dross@adacore.com>
2015-03-27 11:04:36 +0100

    minor update amortize_queue after change in formal vectors

--- testsuite/gnatprove/tests/K624-029__amortize_queue/amortized_queue.adb
+++ testsuite/gnatprove/tests/K624-029__amortize_queue/amortized_queue.adb
@@ -2,41 +2,81 @@ package body Amortized_Queue is pragma SPARK_Mode (On);
 
    function "&" (Left, Right : Vector) return Vector is
    begin
-      return Result : Vector (Capacity => Left.Capacity) do
-        for Idx in First_Index (Left) .. Last_Index (Left) loop
-           Append (Result, Element (Left, Idx));
-        end loop;
-        for Idx in First_Index (Right) .. Last_Index (Right) loop
-           Append (Result, Element (Right, Idx));
-        end loop;
+      return Result : Vector (Capacity => Length (Left) + Length (Right)) do
+         for Idx in 1 .. Last_Index (Left) loop
+            Append (Result, Element (Left, Idx));
+            pragma Loop_Invariant(Integer (Length (Result)) = Idx);
+            pragma Loop_Invariant
+              (for all I in 1 .. Idx =>
+                   Element (Result, I) = Element (Left, I));
+         end loop;
+         for Idx in 1 .. Last_Index (Right) loop
+            Append (Result, Element (Right, Idx));
+            pragma Loop_Invariant(Integer (Length (Result)) = Last_Index (Left) + Idx);
+            pragma Loop_Invariant
+              (for all I in 1 .. Last_Index (Left) =>
+                   Element (Result, I) = Element (Left, I));
+            pragma Loop_Invariant
+              (for all I in 1 .. Idx =>
+                   Element (Result, Last_Index (Left) + I) = Element (Right, I));
+         end loop;
       end return;
    end "&";
 
    function "&" (Left : Integer; Right : Vector) return Vector is
    begin
-      return Result : Vector (Capacity => Right.Capacity) do
-        Append (Result, Left);
-        for Idx in First_Index (Right) .. Last_Index (Right) loop
-           Append (Result, Element (Right, Idx));
-        end loop;
+      return Result : Vector (Capacity => 1 + Length (Right)) do
+         Append (Result, Left);
+         for Idx in First_Index (Right) .. Last_Index (Right) loop
+            Append (Result, Element (Right, Idx));
+            pragma Loop_Invariant(Integer (Length (Result)) = 1 + Idx);
+            pragma Loop_Invariant (Element (Result, 1) = Left);
+            pragma Loop_Invariant
+              (for all I in 1 .. Idx =>
+                 Element (Result, 1 + I) = Element (Right, I));
+         end loop;
       end return;
    end "&";
 
    function "&" (Left : Vector; Right : Integer) return Vector is
    begin
-      return Result : Vector (Capacity => Left.Capacity) do
-        for Idx in First_Index (Left) .. Last_Index (Left) loop
-           Append (Result, Element (Left, Idx));
+      return Result : Vector (Capacity => Length (Left) + 1) do
+         for Idx in First_Index (Left) .. Last_Index (Left) loop
+            Append (Result, Element (Left, Idx));
+            pragma Loop_Invariant(Integer (Length (Result)) = Idx);
+            pragma Loop_Invariant
+              (for all I in 1 .. Idx =>
+                 Element (Result, I) = Element (Left, I));
         end loop;
         Append (Result, Right);
       end return;
    end "&";
 
-   procedure Reverse_Insert (Front : out Vector; Rear : in out Vector) is
+   procedure Reverse_Insert (Front : in out Constrained_Vector;
+                             Rear : in out Constrained_Vector;
+                             OFront : in Constrained_Vector;
+                             ORear : in Constrained_Vector) with
+     Pre => Capacity - Length (Front) > Length (Rear)
+     and then Front = OFront and then Rear = Orear,
+     Post => Length (Front) = Length (OFront) + Length (ORear)
+     and then Length (Rear) = 0
+     and then (for all I in 1 .. Last_Index (OFront) =>
+                   Element (Front, I) = Element (OFront, I))
+     and then (for all I in 1 .. Last_Index (ORear) =>
+                 Element (Front, Last_Index (Front) + 1 - I) = Element (ORear, I))
+   is
    begin
-      for Idx in reverse First_Index (Rear) .. Last_Index (Rear) loop
+      for Idx in reverse 1 .. Last_Index (Rear) loop
          Append (Front, Element (Rear, Idx));
+         pragma Loop_Invariant(Integer (Length (Front)) = Integer (Length (OFront)) + Last_Index (Rear) - Idx + 1);
+         pragma Loop_Invariant
+           (for all I in 1 .. Last_Index (OFront) =>
+              Element (Front, I) = Element (OFront, I));
+         pragma Loop_Invariant
+           (for all I in Idx .. Last_Index (Rear) =>
+              Element (Front, Last_Index (Front) + Idx - I) = Element (Rear, I));
       end loop;
+      Clear (Rear);
    end Reverse_Insert;
 
    function Model (Q : in Queue) return Vector is
@@ -48,22 +88,22 @@ package body Amortized_Queue is pragma SPARK_Mode (On);
 
    function Tail (Q : Queue) return Queue is
    begin
-      return Result : Queue := (Front => Copy (Q.Front, 1000),
-                                Rear => Copy (Q.Rear, 1000)) do
+      return Result : Queue := (Front => Copy (Q.Front, Capacity),
+                                Rear => Copy (Q.Rear, Capacity)) do
         Delete_Last (Result.Front);
         if Length (Result.Front) < Length (Result.Rear) then
-           Reverse_Insert (Result.Front, Result.Rear);
+           Reverse_Insert (Result.Front, Result.Rear, Copy (Result.Front, Capacity), Copy (Result.Rear, Capacity));
         end if;
       end return;
    end Tail;
 
    function Enqueue (Q : in Queue; V : in Val) return Queue is
    begin
-      return Result : Queue := (Front => Copy (Q.Front, 1000),
-                                Rear => Copy (Q.Rear, 1000)) do
+      return Result : Queue := (Front => Copy (Q.Front, Capacity),
+                                Rear => Copy (Q.Rear, Capacity)) do
         Append (Result.Rear, V);
         if Length (Result.Front) < Length (Result.Rear) then
-           Reverse_Insert (Result.Front, Result.Rear);
+           Reverse_Insert (Result.Front, Result.Rear, Copy (Result.Front, Capacity), Copy (Result.Rear, Capacity));
         end if;
       end return;
    end Enqueue;
--- testsuite/gnatprove/tests/K624-029__amortize_queue/amortized_queue.ads
+++ testsuite/gnatprove/tests/K624-029__amortize_queue/amortized_queue.ads
@@ -14,17 +14,36 @@ package Amortized_Queue is pragma SPARK_Mode (On);
      (Index, Val, Eq, True);
    use My_Vectors;
 
-   function "&" (Left, Right : Vector) return Vector;
-   function "&" (Left : Integer; Right : Vector) return Vector;
-   function "&" (Left : Vector; Right : Integer) return Vector;
+   function "&" (Left, Right : Vector) return Vector with
+     Pre  => Integer (Length (Left) + Length (Right)) <= Index'Last,
+     Post => Length ("&"'Result) = Length (Left) + Length (Right)
+     and then (for all I in 1 .. Last_Index (Left) =>
+                   Element ("&"'Result, I) = Element (Left, I))
+     and then (for all I in 1 .. Last_Index (Right) =>
+                   Element ("&"'Result, Last_Index (Left) + I) = Element (Right, I));
+   function "&" (Left : Val; Right : Vector) return Vector with
+     Pre  => Integer (1 + Length (Right)) <= Index'Last,
+     Post => Length ("&"'Result) = 1 + Length (Right)
+     and then Element ("&"'Result, 1) = Left
+     and then (for all I in 1 .. Last_Index (Right) =>
+                   Element ("&"'Result, 1 + I) = Element (Right, I));
+   function "&" (Left : Vector; Right : Val) return Vector with
+     Pre  => Integer (Length (Left) + 1) <= Index'Last,
+     Post => Length ("&"'Result) = Length (Left) + 1
+     and then (for all I in 1 .. Last_Index (Left) =>
+                   Element ("&"'Result, I) = Element (Left, I))
+     and then Element ("&"'Result, Last_Index (Left) + 1) = Right;
+
    --  Implement local versions of concatenation for formal vectors, which do
    --  not define concatenation anymore.
 
    Capacity : constant Count_Type := Count_Type (Index'Last);
 
+   subtype Constrained_Vector is Vector (Capacity);
+
    type Queue is record
-      Front : Vector (Capacity);
-      Rear  : Vector (Capacity);
+      Front : Constrained_Vector;
+      Rear  : Constrained_Vector;
    end record;
 
    function Inv (Q : in Queue) return Boolean is
@@ -34,6 +53,7 @@ package Amortized_Queue is pragma SPARK_Mode (On);
 
    function Is_Model (Q : in Queue; M : in Vector) return Boolean is
      (Length (Q.Front) + Length (Q.Rear) = Length (M) and then
+      Length (M) <= Capacity and then
           (for all I in Index range 1 .. Integer (Length (Q.Rear)) =>
                 Element (M, I) =
                  Element (Q.Rear, Integer (Length (Q.Rear)) - I + 1)) and then
@@ -47,20 +67,17 @@ package Amortized_Queue is pragma SPARK_Mode (On);
 
    function Front (Q : in Queue) return Val with
      Pre  => Inv (Q) and then Length (Q.Front) > 0,
-     Post => (if is_Model (Q, Model (Q)) then
-                Last_Element (Model (Q)) = Front'Result);
+     Post => Last_Element (Model (Q)) = Front'Result;
 
    function Tail (Q : in Queue) return Queue with
      Pre  => Inv (Q) and then Length (Q.Front) > 0,
      Post => Inv (Tail'Result) and then
-     (if is_Model (Q, Model (Q)) and is_Model (Tail'Result, Model (Tail'Result)) then
-        Model (Q) = Model (Tail'Result) & Last_Element (Model (Q)));
+     Model (Q) = Model (Tail'Result) & Last_Element (Model (Q));
 
    function Enqueue (Q : in Queue; V : in Val) return Queue with
      Pre  => Inv (Q) and then
      Q.Front.Capacity - Length (Q.Front) > Length (Q.Rear),
      Post => Inv (Enqueue'Result) and then
-     (if is_Model (Q, Model (Q)) and is_Model (Enqueue'Result, Model (Enqueue'Result)) then
-        V & Model (Q) = Model (Enqueue'Result));
+     V & Model (Q) = Model (Enqueue'Result);
 
 end Amortized_Queue;
