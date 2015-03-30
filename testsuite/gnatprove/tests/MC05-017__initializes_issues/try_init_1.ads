768acf0f83e1d959973cfc8d0e31517fe0207617
Yannick Moy <moy@adacore.com>
2015-03-30 14:51:12 +0200

    MC05-017, O325-022 Constants now allowed in Initializes aspect

--- testsuite/gnatprove/tests/MC05-017__initializes_issues/try_init_1.ads
+++ testsuite/gnatprove/tests/MC05-017__initializes_issues/try_init_1.ads
@@ -3,12 +3,5 @@ with P; pragma Elaborate_All (P);
 package Try_Init_1
    with Initializes => (C => P.V)
 is
-   -- Attempting to initialize this constant with a variable
-   -- is currently (but erroneously) not allowed by the RM.
-   -- It gives a misleading error message because C cannot be
-   -- an initialized item in an Initializes aspect.
-   -- If we try and add an Initializes aspect with C as an
-   -- initialization item we correctly get the message that
-   -- "initialization item must denote variable or state"
    C : constant Integer := P.V;
 end Try_Init_1;
