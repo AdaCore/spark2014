642a388f74d1dff58f418d92787db4f25016bd8d
Yannick Moy <moy@adacore.com>
2015-03-30 15:21:58 +0200

    O325-022 Update tests now that constants are allowed in contracts
    * testsuite/gnatprove/tests/TU__initializes_illegal/test.out
    Update output
    
    * testsuite/gnatprove/tests/TU__types_and_subtypes_illegal/test.opt
    * testsuite/gnatprove/tests/TU__types_and_subtypes_illegal/test.out
    * testsuite/gnatprove/tests/TU__types_and_subtypes_illegal/test.py
    Flow analysis currently crashes on this test. To be fixed under O121-031.

--- testsuite/gnatprove/tests/TU__types_and_subtypes_illegal/test.py
+++ testsuite/gnatprove/tests/TU__types_and_subtypes_illegal/test.py
@@ -1,4 +1,4 @@
 from test_support import *
 
-prove_all(opt=["-u", "types_and_subtypes_illegal.ads"])
-gcc("types_and_subtypes_illegal_2.ads")
+prove_all(opt=["-u", "types_and_subtypes_illegal.adb"])
+gcc("types_and_subtypes_illegal_2.adb")
