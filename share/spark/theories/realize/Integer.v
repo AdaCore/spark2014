98fe3b7822c5f52d3446ac4045ba4d870a7a3973
dross <dross@adacore.com>
2015-05-11 13:52:12 +0200

    N311-015 add Coq realizations for Int theories
    Note that Floating.v is not finished yet.

--- share/spark/theories/realize/Integer.v
+++ share/spark/theories/realize/Integer.v
@@ -48,14 +48,14 @@ exact Z.geb.
 Defined.
 
 (* Why3 goal *)
-Lemma bool_eq_axiom : forall (x:Z), forall (y:Z), ((bool_eq x y) = true) <->
-  (x = y).
+Lemma bool_eq_axiom :
+forall (x:Z), forall (y:Z), ((bool_eq x y) = true) <-> (x = y).
 exact Z.eqb_eq.
 Qed.
 
 (* Why3 goal *)
-Lemma bool_ne_axiom : forall (x:Z), forall (y:Z), ((bool_ne x y) = true) <->
-  ~ (x = y).
+Lemma bool_ne_axiom :
+forall (x:Z), forall (y:Z), ((bool_ne x y) = true) <-> ~ (x = y).
 intros x y.
 rewrite <- bool_eq_axiom.
 unfold bool_ne, bool_eq, negb. case ((x =? y)%Z); split; intro H; auto.
@@ -63,28 +63,28 @@ contradict H; auto.
 Qed. 
 
 (* Why3 goal *)
-Lemma bool_lt_axiom : forall (x:Z), forall (y:Z), ((bool_lt x y) = true) <->
-  (x < y)%Z.
+Lemma bool_lt_axiom :
+forall (x:Z), forall (y:Z), ((bool_lt x y) = true) <-> (x < y)%Z.
 unfold bool_lt; exact Z.ltb_lt.
 Qed.
 
 (* Why3 goal *)
-Lemma bool_int__le_axiom : forall (x:Z), forall (y:Z), ((bool_le x
-  y) = true) <-> (x <= y)%Z.
+Lemma bool_int__le_axiom :
+forall (x:Z), forall (y:Z), ((bool_le x y) = true) <-> (x <= y)%Z.
 unfold bool_le; exact Z.leb_le.
 
 Qed.
 
 (* Why3 goal *)
-Lemma bool_gt_axiom : forall (x:Z), forall (y:Z), ((bool_gt x y) = true) <->
-  (y < x)%Z.
+Lemma bool_gt_axiom :
+forall (x:Z), forall (y:Z), ((bool_gt x y) = true) <-> (y < x)%Z.
 unfold bool_gt; exact Z.gtb_lt.
 
 Qed.
 
 (* Why3 goal *)
-Lemma bool_ge_axiom : forall (x:Z), forall (y:Z), ((bool_ge x y) = true) <->
-  (y <= x)%Z.
+Lemma bool_ge_axiom :
+forall (x:Z), forall (y:Z), ((bool_ge x y) = true) <-> (y <= x)%Z.
 unfold bool_ge; exact Z.geb_le.
 
 Qed.
