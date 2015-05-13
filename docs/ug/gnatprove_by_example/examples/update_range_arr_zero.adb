e9f264fad8f21c8761dbbf9624f52a3a2c62e3a2
Yannick Moy <moy@adacore.com>
2015-05-13 08:30:45 +0200

    O511-027 Update example

--- docs/ug/gnatprove_by_example/examples/update_range_arr_zero.adb
+++ docs/ug/gnatprove_by_example/examples/update_range_arr_zero.adb
@@ -4,10 +4,9 @@ procedure Update_Range_Arr_Zero (A : in out Arr_T; First, Last : Index_T) with
   SPARK_Mode,
   Post => A = A'Old'Update (First .. Last => 0)
 is
-   A_Old : constant Arr_T := A; --  HACK should use A'Loop_Entry'Update
 begin
    for J in First .. Last loop
       A(J) := 0;
-      pragma Loop_Invariant (A = A_Old'Update (First .. J => 0));
+      pragma Loop_Invariant (A = A'Loop_Entry'Update (First .. J => 0));
    end loop;
 end Update_Range_Arr_Zero;
