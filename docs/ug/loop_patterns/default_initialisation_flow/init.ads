d5c629593a2ef59d90ac304716046778297a4584
Yannick Moy <moy@adacore.com>
2015-05-15 14:49:33 +0200

    N318-007 Remove obsolete code, since all moved to 'examples' dir

--- docs/ug/loop_patterns/default_initialisation_flow/init.ads
+++
@@ -1,9 +0,0 @@
-with Loop_Types; use Loop_Types;
-package Init
-  with SPARK_Mode
-is
-   procedure Default_Initialize (A : out Arr_T; Init_Val : in Component_T) with
-     --  every element of the output array has been initialized:
-     Post => (for all J in A'Range => A (J) = Init_Val);
-
-end Init;
