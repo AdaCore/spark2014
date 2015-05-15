9c43ca03a0095724c7d9abb24d1ae7a0669c48d3
Yannick Moy <moy@adacore.com>
2015-05-15 12:40:21 +0200

    O512-022 Delete duplicate test

--- testsuite/gnatprove/tests/openETCS_array/example.ads
+++
@@ -1,7 +0,0 @@
-package Example is pragma SPARK_Mode (On);
-   function Saturated_Sum(X1, X2, Maximum : Natural) return Natural
-   with
-     Pre => ((X1 <= Natural'Last / 2) and (X2 <= Natural'Last / 2)),
-     Post => (if X1 + X2 <= Maximum then saturated_sum'Result = X1 + X2
-              else saturated_sum'Result = Maximum);
-end;
--- testsuite/gnatprove/tests/openETCS_array/sec_3_13_test.adb
+++
@@ -1,8 +0,0 @@
-with sec_3_13_2_monitoring_inputs; use sec_3_13_2_monitoring_inputs;
-with sec_3_13_6_deceleration; use sec_3_13_6_deceleration;
-with sec_3_13_8_targets_decel_curves; use sec_3_13_8_targets_decel_curves;
-
-procedure sec_3_13_test is pragma SPARK_Mode (On);
-begin
-   null;
-end;
