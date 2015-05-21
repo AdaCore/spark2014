2a45ff717eea664e9ad04506e8ec643bdcc8b9fa
Yannick Moy <moy@adacore.com>
2015-05-21 07:43:26 +0200

    O521-002 Add test

---
+++ testsuite/gnatprove/tests/O521-002__no_return/p.adb
@@ -0,0 +1,27 @@
+pragma SPARK_Mode;
+
+package body P is
+
+  procedure Help_Doing_Nothing with No_Return;
+
+  procedure Help_Doing_Nothing is
+  begin
+     loop
+        null;
+     end loop;
+  end Help_Doing_Nothing;
+
+  Dummy : Integer;
+  procedure Do_Nothing is
+  begin
+     Dummy := 0;
+     Help_Doing_Nothing;
+  end Do_Nothing;
+
+  procedure Do_Something is
+  begin
+     Dummy := 0;
+     Help_Doing_Nothing;
+  end Do_Something;
+
+end P;
---
+++ testsuite/gnatprove/tests/O521-002__no_return/p.ads
@@ -0,0 +1,9 @@
+pragma SPARK_Mode;
+
+package P is
+
+  procedure Do_Nothing with No_Return;
+
+  procedure Do_Something;
+
+end P;
