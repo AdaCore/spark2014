af665ff3c588001c7f8ae4d6ef4c6895fa9fa47e
Florian Schanda <florian.schanda@altran.com>
2015-05-27 13:53:52 +0100

    O526-005 proof - overuse of bv2int
    Remove duplicate testcase. I have chosen this one to remove because
    the other one:
    - has a longer description
    - uses 64 bit index so will make it more obvious when the solution is
      correct

--- testsuite/gnatprove/tests/O526-005__length_check/length_others.adb
+++
@@ -1,8 +0,0 @@
-package body Length_Others
-  with SPARK_Mode
-is
-   procedure Init (S : out Bytes) is
-   begin
-      S := (others => 0);
-   end Init;
-end Length_Others;
--- testsuite/gnatprove/tests/O526-005__length_check/length_others.ads
+++
@@ -1,7 +0,0 @@
-package Length_Others
-  with SPARK_Mode
-is
-   type U8 is mod 2 ** 8;
-   type Bytes is array (U8 range <>) of Integer;
-   procedure Init (S : out Bytes);
-end Length_Others;
