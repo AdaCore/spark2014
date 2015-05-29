02d3d67126508cd3e40e660caf7754a65110c7e4
Yannick Moy <moy@adacore.com>
2015-05-29 09:35:28 +0200

    O521-014 Update test as restrictions on calls to No_Return are gone

--- testsuite/gnatprove/tests/M611-010__no_return_illegal/no_return_illegal.adb
+++ testsuite/gnatprove/tests/M611-010__no_return_illegal/no_return_illegal.adb
@@ -3,28 +3,18 @@ is
 
    X : Integer := 0;
 
-   --  Placement of calls to non-returning procedures
-   --
-   --  Illegal cases
-
-   -- TU: 1. For a call to a nonreturning procedure to be in |SPARK|, it must
-   -- be either
-   --  * immediately enclosed by an if statement which encloses no other
-   --    statement; or
-   --  * the last statement of a subprogram.
-
    procedure Aborting is
    begin
       if X /= 0 then
          X := 1;
       end if;
-      raise Program_Error; -- legal
+      raise Program_Error;
    end Aborting;
 
    procedure P2 is
    begin
       if X = 1 then
-         Aborting; -- illegal
+         Aborting;
          X := X + 2;
       end if;
    end P2;
@@ -32,7 +22,7 @@ is
    procedure P3 is
    begin
       if X = 1 then
-         Aborting; -- illegal
+         Aborting;
       elsif X = 2 then
          X := X + 1;
       end if;
@@ -41,7 +31,7 @@ is
    procedure P4 is
    begin
       if X = 1 then
-         Aborting; -- illegal
+         Aborting;
       else
          X := X + 1;
       end if;
@@ -50,7 +40,7 @@ is
    procedure P5 is
    begin
       X := X + 1;
-      Aborting; -- illegal
+      Aborting;
    end P5;
 
 end No_Return_Illegal;
