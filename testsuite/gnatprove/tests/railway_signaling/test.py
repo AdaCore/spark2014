86ffdc083b223c52c743936f03c078044678959b
Yannick Moy <moy@adacore.com>
2015-06-12 16:18:35 +0200

    O424-002 Update comment, after checking results with Z3 added

--- testsuite/gnatprove/tests/railway_signaling/test.py
+++ testsuite/gnatprove/tests/railway_signaling/test.py
@@ -1,6 +1,9 @@
 from test_support import *
 
-# test is fully proved with 20s timeout, --proof=per_path and a combination
-# of provers
+# Test was fully proved with 20s timeout, --proof=per_path and a combination
+# of provers. Currently, 4 out of 5 unproved assertions are still not proved
+# with 10.000 steps and 120s timeout, with --prover=cvc4,altergo,z3 and
+# --proof=per_path. This is likely related to the changes in how arrays are
+# handled in provers now, to be investigated.
 
 prove_all(steps=570)
