d5cdb9cda9e655cd704d23f6dad86c0327e74b87
Yannick Moy <moy@adacore.com>
2015-06-12 14:42:44 +0200

    O424-002 Now all checks proved with CVC4,Alt-Ergo,Z3
    Only one check requires a large number of steps from CVC4, and is not
    repeatable on nightly runs. To be investigated under O612-012.

--- testsuite/gnatprove/tests/sparkskein/test.py
+++ testsuite/gnatprove/tests/sparkskein/test.py
@@ -1,10 +1,13 @@
 from test_support import *
 
-# Some checks currently require as much as 100.000 steps with CVC4
-# currently. This is notoriously high, and might be related to the issue with
-# CVC4 reported in O525-023. To be investigated.
-prove_all(opt=["--prover=cvc4"],steps=100000,procs=4)
+# Only unproved check is index check at line 266, proved by CVC4 with
+# 100.000 steps currently. This is notoriously high, and might be related
+# to the issue with CVC4 reported in O525-023. Moreover, this is proved on
+# developer setup, but not in nightly testing. To be investigated under
+# O612-012.
 
-# With --prover=cvc4,z3,altergo --timeout=120 everything is proved:
-# . CVC4 proves all checks expect 2
-# . Alt-Ergo and Z3 proves each one of the remaining check
+# All other checks are proved:
+# . CVC4 proves all other checks expect 2
+# . Alt-Ergo and Z3 each prove one remaining check
+
+prove_all(opt=["--prover=cvc4,altergo,z3"],procs=4)
