721c59887b67370c148c21e86fc592adac4eb359
Clément Fumex <clement.fumex@inria.fr>
2015-06-15 17:15:15 +0200

    minor add regression test for modulars with static predicates

---
+++ testsuite/gnatprove/tests/N811-037__static_pred_modular/t.adb
@@ -0,0 +1,14 @@
+procedure T with
+  SPARK_Mode
+is
+   type S is mod 2**8 with Static_Predicate => S > 0 and S /= 4;
+
+   procedure Decr (X : in out S);
+
+   procedure Decr (X : in out S) is
+   begin
+      X := S'Pred (X); -- @RANGE_CHECK:FAIL
+   end Decr;
+begin
+   null;
+end T;
