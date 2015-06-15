56b42e572c2a0cefbbb77b403a9067bed200d6c2
Johannes Kanig <kanig@adacore.com>
2015-06-15 16:13:35 +0900

    minor fix breaking test
    was breaking because of a change of identifiers in the testdriver
    also inserted a comment which explains what the test is there for

--- testsuite/gnatprove/tests/N804-036__bad_prover/test.py
+++ testsuite/gnatprove/tests/N804-036__bad_prover/test.py
@@ -1,3 +1,7 @@
 from test_support import *
 
+# this test calls a prover which is correctly configured but whose execution
+# gives an error (here: the prover executable doesn't exist). The intent is to
+# test the output of gnatprove in this specific case
+
 prove_all(opt=["--prover=plop", "--why3-conf=test.conf"])
