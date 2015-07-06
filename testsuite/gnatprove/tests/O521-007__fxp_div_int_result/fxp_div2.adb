80cc01b0f11e098f1af4379ec50df2d038410176
Piotr Trojanek <piotr.trojanek@altran.com>
2015-07-06 14:40:16 +0100

    O521-007 fxp division with int result axioms
    Fixed-point division with integer result is now axiomatized without
    referring to real-number truncation; this keeps proofs entirely within
    the logic of integer numbers and result in more proved VCs.
    
    * share/spark/theories/_gnatprove_standard.mlw
    (Floating): rounding axioms are no longer shared with fixed-point theory
    * share/spark/theories/ada__model.mlw
    (Fixed_Point_Base): axiom for division with integer result
    * testsuite/gnatprove/tests/O521-007__fxp_div_int_result/
    Test and test output updated.
    * testsuite/gnatprove/tests/tests/O617-006__fxp_div_int_res/test.out
    Test output removed; now everything is proved.

--- testsuite/gnatprove/tests/O521-007__fxp_div_int_result/fxp_div2.adb
+++ testsuite/gnatprove/tests/O521-007__fxp_div_int_result/fxp_div2.adb
@@ -3,7 +3,7 @@ procedure Fxp_div2 is
    type Duration_Rep is range -(2 ** 63) .. +((2 ** 63 - 1));
    for Duration_Rep'Size use 64;
 
-   X : Duration;
+   X : Duration := 3.0;
 
    Y : constant Duration_Rep := Duration_Rep (X / X);
    Z : constant Duration_Rep := Duration_Rep (X / Duration (Duration'Small + Duration'Small));
