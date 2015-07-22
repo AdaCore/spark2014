bd0d37c2b1314cfe8a38b570e542bd6e2c8dda04
Piotr Trojanek <piotr.trojanek@altran.com>
2015-07-22 11:58:18 +0100

    O722-011 test for Standard'XXX attributes

---
+++ testsuite/gnatprove/tests/O722-011__standard_attributes/objects.ads
@@ -0,0 +1,9 @@
+package Objects is
+
+   Address_Size               : Natural := Standard'Address_Size;
+   Maximum_Alignment          : Natural := Standard'Maximum_Alignment;
+   Storage_Unit               : Natural := Standard'Storage_Unit;
+   System_Allocator_Alignment : Natural := Standard'System_Allocator_Alignment;
+   Wchar_T_Size               : Natural := Standard'Wchar_T_Size;
+   Word_Size                  : Natural := Standard'Word_Size;
+end;
---
+++ testsuite/gnatprove/tests/O722-011__standard_attributes/params.ads
@@ -0,0 +1,9 @@
+package Params is
+
+   procedure Address_Size (X : Natural := Standard'Address_Size);
+   procedure Maximum_Alignment (X : Natural := Standard'Maximum_Alignment);
+   procedure Storage_Unit (X : Natural := Standard'Storage_Unit);
+   procedure System_Allocator_Alignment (X : Natural := Standard'System_Allocator_Alignment);
+   procedure Wchar_T_Size (X : Natural := Standard'Wchar_T_Size);
+   procedure Word_Size (X : Natural := Standard'Word_Size);
+end;
