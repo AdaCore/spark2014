2546af93119cfbfce2ee1a77a79762b034cec00b
Piotr Trojanek <piotr.trojanek@altran.com>
2015-07-22 13:32:58 +0100

    O722-011 Test extended
    Test with a record with default discriminant not "in" or "out" SPARK,
    but which have assignments for all of its components.

--- testsuite/gnatprove/tests/O722-011__standard_attributes/discr_rec.ads
+++ testsuite/gnatprove/tests/O722-011__standard_attributes/discr_rec.ads
@@ -4,4 +4,9 @@ package Discr_Rec is
      record
         A : String (1 .. Size);
      end record;
+
+   type Empty_Rec (Size : Natural := Standard'Word_Size) is
+     record
+        null;
+     end record;
 end;
