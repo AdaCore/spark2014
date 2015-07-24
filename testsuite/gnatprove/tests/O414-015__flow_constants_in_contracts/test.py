2660cfa98996050f4b84b5a4877b9c704fedde99
Pavlos Efstathopoulos <efstatho@adacore.com>
2015-07-24 10:25:16 +0100

    Minor test update to accommodate recent front end change

--- testsuite/gnatprove/tests/O414-015__flow_constants_in_contracts/test.py
+++ testsuite/gnatprove/tests/O414-015__flow_constants_in_contracts/test.py
@@ -1,2 +1,7 @@
 from test_support import *
-prove_all()
+
+prove_all(opt=["-u", "const.adb"])
+prove_all(opt=["-u", "const2.adb"])
+prove_all(opt=["-u", "const3.adb"])
+prove_all(opt=["-u", "const4.adb"])
+prove_all(opt=["-u", "other.adb"])
