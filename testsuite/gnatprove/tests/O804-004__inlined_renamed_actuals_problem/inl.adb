9ba7a7ee1948d8bc9f74cf7b618bc67909c4cc56
Florian Schanda <florian.schanda@altran.com>
2015-08-06 15:08:26 +0100

    O804-004 minor addition to test.

--- testsuite/gnatprove/tests/O804-004__inlined_renamed_actuals_problem/inl.adb
+++ testsuite/gnatprove/tests/O804-004__inlined_renamed_actuals_problem/inl.adb
@@ -9,6 +9,11 @@ package body Inl is
    type Record_Table_T is array (Range_T) of Record_T;
    RT : Record_Table_T;
 
+   type Numerals is range -256 .. 255;
+   Min : Numerals  := Numerals'First;
+   Foo : constant Numerals := Min;
+   subtype Numerals_Nonstatic is Numerals range Min .. Foo;  --  min not OK
+
    procedure Clear_X (R : in out Record_T) is
    begin
       R.X := 0;
@@ -17,6 +22,8 @@ package body Inl is
    procedure Call_Clear (I : Range_T) is
       Tmp : Range_T := I;
    begin
-      Clear_X (RT (Tmp));
+      Clear_X (RT (Tmp));  --  OK
    end Call_Clear;
+
+   --  var loop bound also ok
 end Inl;
