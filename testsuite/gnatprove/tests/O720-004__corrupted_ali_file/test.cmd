058ee6c64a1d4f93edf9e30fb77937483c17b269
Pavlos Efstathopoulos <efstatho@adacore.com>
2015-08-06 12:04:21 +0100

    O720-004 fixed test
    It works locally but then again so did yesterday's test. I will check GAIA
    again tomorrow.

--- testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.cmd
+++ testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.cmd
@@ -1,1 +1,7 @@
-gnatprove -P test.gpr
+gnatprove -P test.gpr -f -q
+
+head -n -4 gnatprove/test.ali > gnatprove/tmp.ali
+rm gnatprove/test.ali
+mv gnatprove/tmp.ali gnatprove/test.ali
+
+gnatprove -P test.gpr -q
