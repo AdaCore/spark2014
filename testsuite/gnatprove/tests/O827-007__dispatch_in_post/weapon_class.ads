cbf56a8fb8890d9c9b7ebf07a6a4cbae178672ce
dross <dross@adacore.com>
2015-08-27 15:25:57 +0200

    O827-007 Filing testcase

---
+++ testsuite/gnatprove/tests/O827-007__dispatch_in_post/weapon_class.ads
@@ -0,0 +1,33 @@
+package Weapon_Class
+with SPARK_Mode
+is
+
+   subtype Cycle_Time is Integer range 0 .. 30;
+
+   ------------------
+   -- Bullet class --
+   ------------------
+
+   type Bullet is abstract tagged record
+      null;
+   end record;
+
+   function Hit (Self : Bullet) return Integer is abstract
+   with Post'Class => Hit'Result < 12;
+
+   type Gun_Bullet is new Bullet with record
+      null;
+   end record;
+
+   function Hit (Self : Gun_Bullet) return Integer
+     with Post'Class => Hit'Result in 0 .. 10;
+
+   ------------------
+   -- Ennemy class --
+   ------------------
+
+
+   function Select_Bullet return Bullet'Class
+     with Post => Hit (Select_Bullet'Result) < 12;
+
+end Weapon_Class;
