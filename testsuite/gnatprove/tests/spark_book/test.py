c0428ee66c2446197f5aace9e90b36067de896e9
Yannick Moy <moy@adacore.com>
2015-09-22 16:41:32 +0200

    O615-009 Adapt to Z3 steps

--- testsuite/gnatprove/tests/spark_book/test.py
+++ testsuite/gnatprove/tests/spark_book/test.py
@@ -1,3 +1,5 @@
 from test_support import *
 
-prove_all(opt=["-U"], procs=4)
+# Do not use Z3, as the step limit fluctuates too much between platforms, thus
+# making it impossible to share a common expected output when Z3 is used.
+prove_all(opt=["--prover=cvc4,altergo", "-U"], procs=4)
