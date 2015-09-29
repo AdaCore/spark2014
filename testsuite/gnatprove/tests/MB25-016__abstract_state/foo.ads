ededda090fe930d3aba20766ffd1d3e1f98a689f
Yannick Moy <moy@adacore.com>
2015-09-29 16:51:48 +0200

    MB25-016 Add test

---
+++ testsuite/gnatprove/tests/MB25-016__abstract_state/foo.ads
@@ -0,0 +1,15 @@
+package Foo with
+   Abstract_State    => State,
+   Initializes       => State,
+   Initial_Condition => F2
+is
+
+   function F2 return Boolean with Global => State;
+
+private
+
+   C : Integer with Part_Of => State;
+
+   function F2 return Boolean is (C > 0) with Refined_Global => C;
+
+end Foo;
