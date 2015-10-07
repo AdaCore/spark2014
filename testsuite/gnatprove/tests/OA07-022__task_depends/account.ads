0c1bf8cf0aa4add7894e515cf537f11671f4e5b2
Yannick Moy <moy@adacore.com>
2015-10-07 15:47:00 +0200

    OA07-022 Add test

---
+++ testsuite/gnatprove/tests/OA07-022__task_depends/account.ads
@@ -0,0 +1,13 @@
+
+   pragma Profile (Ravenscar);
+   pragma Partition_Elaboration_Policy (Sequential);
+   package Account with SPARK_Mode is
+      Num_Accounts : Natural := 0;
+
+      type Account_Number is new Integer;
+      procedure Get_Next_Account_Created;
+
+      task type Account_Management with
+        Global => (In_Out => Account.Num_Accounts),
+        Depends => (Account.Num_Accounts => Account.Num_Accounts);
+   end Account;
