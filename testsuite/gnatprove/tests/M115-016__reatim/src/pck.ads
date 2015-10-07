9275035582476623c40e3d2faa16932fb60733cc
Piotr Trojanek <piotr.trojanek@altran.com>
2015-10-07 10:46:08 +0100

    M115-016 fix precondition to accept actual input
    The precondition for Mul_Div routine (originally from a-reatim-xi.adb) was
    incomplete; it would fail when called with V = Max_Neg_Time_Span, which is
    (2 ** 63).
    
    Now the test relies on a workaround for OA06-021.
    
    * testsuite/gnatprove/tests/M115-016__reatim/src/
    Test fixed to reflect actual use-case; output updated.

--- testsuite/gnatprove/tests/M115-016__reatim/src/pck.ads
+++ testsuite/gnatprove/tests/M115-016__reatim/src/pck.ads
@@ -2,6 +2,9 @@ package Pck is pragma SPARK_Mode (On);
    type Uint_64 is range 0 .. 2 ** 63 - 1;
    type Int64 is range 0 .. 2 ** 63 - 1;
    function Mul_Div (V : Uint_64; M : Natural; D : Natural) return Uint_64 with
-     Pre  => V <= 2 ** 63 - 1 and then D /= 0,
+     Pre  => V - 1 <= 2 ** 63 - 1 and then D /= 0,
+     --      ^^^^^
+     --  We subtract 1 from both sides because 2 ** 63 would not fit into any
+     --  signed integer type accepted by GNAT. See OA06-021 for details.
      Post => Int64 (Mul_Div'Result) = Int64 (V) * Int64 (M) / Int64 (D);
 end Pck;
