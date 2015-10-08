12afb4d1c437910f2d3818e0c318ed14215933d9
Piotr Trojanek <piotr.trojanek@altran.com>
2015-10-08 09:11:09 +0100

    minor typo in test.py comment

--- testsuite/gnatprove/tests/OA06-004__mul_div/test.py
+++ testsuite/gnatprove/tests/OA06-004__mul_div/test.py
@@ -1,6 +1,6 @@
 from test_support import *
 
-# Z3 and Alt-ergo proof all except two VCs in several seconds, but only
+# Z3 and Alt-ergo prove all except two VCs in several seconds, but only
 # with --steps limit higher than the default. CVC4 is excluded from the
 # --prover list, becasue is does not prove these two VCs, yet needs about
 # 2 minutes before it gives up.
