3cad8dfd9fccaff83804b131caf6914caed9910a
Piotr Trojanek <piotr.trojanek@altran.com>
2015-10-12 08:32:11 +0100

    M115-016 disable proof of invalid postcondition
    Division operations in contract and body had different roundings, thus the
    contract was unprovable.
    
    * testsuite/gnatprove/tests/M115-016__reatim/
    Postcondition disabled (but kept for reference); test output updated;
    -gnat12 removed from test.gpr (Ada 2012 mode is not the default).

--- testsuite/gnatprove/tests/M115-016__reatim/src/pck.ads
+++ testsuite/gnatprove/tests/M115-016__reatim/src/pck.ads
@@ -2,6 +2,10 @@ package Pck is pragma SPARK_Mode (On);
    type Uint_64 is range 0 .. 2 ** 63 - 1;
    type Int64 is range 0 .. 2 ** 63 - 1;
    function Mul_Div (V : Uint_64; M : Natural; D : Natural) return Uint_64 with
-     Pre  => D /= 0,
-     Post => Int64 (Mul_Div'Result) = Int64 (V) * Int64 (M) / Int64 (D);
+     Pre  => D /= 0;
+   --  Post => Int64 (Mul_Div'Result) = Int64 (V) * Int64 (M) / Int64 (D);
+
+   --  This function implements RM D.8 (24/2), which rounds away from zero if
+   --  exactly halfway between two values, but "/" in the above postcondition
+   --  was computer division, which rounds towards zero.
 end Pck;
