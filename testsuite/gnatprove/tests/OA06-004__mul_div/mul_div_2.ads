5157f98501a7582355c26150a9924cfebe3ec015
Piotr Trojanek <piotr.trojanek@altran.com>
2015-10-14 12:31:58 +0100

    OA06-004 test updated with fixed Mul_Div
    Replace test with fixed version of Mul_Div that will be sent for review and
    (if approved) checked in to GNAT.
    
    * testsuite/gnatprove/tests/OA06-004__mul_div/
    Test replaced; output updated.

---
+++ testsuite/gnatprove/tests/OA06-004__mul_div/mul_div_2.ads
@@ -0,0 +1,18 @@
+package Mul_Div_2
+  with SPARK_Mode => On
+is
+
+   subtype LLI is Long_Long_Integer;
+
+   pragma Overflow_Mode (General => Strict, Assertions => ELIMINATED);
+
+   function Mul_Div (V : LLI; M : Natural; D : Positive) return LLI with
+     Pre => (if V >= 0
+             then (V * LLI (M) + LLI (D) / 2) / LLI (D) <= LLI'Last
+             else -(((-V) * LLI (M) + LLI (D) / 2) / LLI (D)) >= LLI'First),
+     Post => Mul_Div'Result =
+             (if V >= 0
+              then (V * LLI (M) + LLI (D) / 2) / LLI (D)
+              else -(((-V) * LLI (M) + LLI (D) / 2) / LLI (D)));
+
+end;
--- testsuite/gnatprove/tests/OA06-004__mul_div/test.py
+++ testsuite/gnatprove/tests/OA06-004__mul_div/test.py
@@ -1,7 +1,3 @@
 from test_support import *
 
-# Z3 and Alt-ergo prove all except two VCs in several seconds, but only
-# with --steps limit higher than the default. CVC4 is excluded from the
-# --prover list, becasue is does not prove these two VCs, yet needs about
-# 2 minutes before it gives up.
-prove_all(opt=["--prover=z3,alt-ergo"], steps=8000)
+prove_all()
