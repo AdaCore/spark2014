d0b66b98a686ae8c3a7f11c3c16c66200bad228f
Yannick Moy <moy@adacore.com>
2015-10-18 00:16:25 +0200

    N318-011 Fix bug causing error when -cargs passed as opt arg

--- testsuite/gnatprove/lib/python/test_support.py
+++ testsuite/gnatprove/lib/python/test_support.py
@@ -492,10 +492,11 @@ def do_flow(opt=None, procs=parallel_procs):
 def prove_all(opt=None, steps=max_steps, procs=parallel_procs,
               vc_timeout=vc_timeout()):
     """Call gnatprove with standard options to prove all VCs"""
-    if opt is None:
-        opt = []
-    opt += ["--no-counterexample"]
-    prove(opt, steps, procs, vc_timeout, mode="all")
+    fullopt = ["--no-counterexample"]
+    # Add opt last, so that it may include switch -cargs
+    if opt is not None:
+        fullopt += opt
+    prove(fullopt, steps, procs, vc_timeout, mode="all")
 
 
 def prove_all_counterexample(opt=None, steps=max_steps,
