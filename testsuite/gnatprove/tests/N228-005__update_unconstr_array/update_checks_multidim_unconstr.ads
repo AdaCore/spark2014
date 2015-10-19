8546051a3380608b025610628db0d69aa6792667
Angela Wallenburg <angela.wallenburg@altran.com>
2015-10-19 11:47:07 +0100

    N228-005 separate multidimensional unconstrained array 'Update tests
    To prepare for introduction of error on 'Update of multidimensional
    unconstrained arrays, separating these tests not to break all the
    other tests.

---
+++ testsuite/gnatprove/tests/N228-005__update_unconstr_array/update_checks_multidim_unconstr.ads
@@ -0,0 +1,11 @@
+with Types; use Types;
+
+package Update_Checks_Multidim_Unconstr is
+
+   procedure Test1  (A       : in out AM;
+                     X       : in IT1;
+                     Y       : in IT2;
+                     Z       : in IT3;
+                     New_Val : in ET1);
+
+end Update_Checks_Multidim_Unconstr;
