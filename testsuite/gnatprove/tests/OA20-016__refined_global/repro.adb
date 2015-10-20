68c0cf5f54ee716638ed722a123591790d2e7fc5
Yannick Moy <moy@adacore.com>
2015-10-20 12:55:36 +0200

    OA20-016 Add test

---
+++ testsuite/gnatprove/tests/OA20-016__refined_global/repro.adb
@@ -0,0 +1,19 @@
+
+package body Repro with
+  SPARK_Mode    => On,
+  Refined_State => (Game_State => (Cur_Board))
+is
+  Cur_Board : Board;
+
+  procedure Game_Reset
+--    with Refined_Global => (Output => (Cur_Board))
+  is
+  begin
+      Cur_Board := (others => (others => Empty));
+  end Game_Reset;
+
+  function Get_Board return Board is (Cur_Board);
+
+begin
+  Game_Reset;
+end Repro;
---
+++ testsuite/gnatprove/tests/OA20-016__refined_global/repro.ads
@@ -0,0 +1,25 @@
+package Repro with
+  SPARK_Mode    => On,
+  Abstract_State => (Game_State),
+  Initializes    => (Game_State)
+is
+  type Cell is (Empty, I, O, J, L, S, T, Z);
+
+  subtype X_Coord is Integer range 1 .. 10;
+  subtype Y_Coord is Integer range 1 .. 20;
+
+  type Line is array (X_Coord) of Cell with Pack;
+  type Board is array (Y_Coord) of Line;
+
+  function Is_Empty (B : Board) return Boolean is
+      (for all Y in Y_Coord => (for all X in X_Coord => B (y)(X) = Empty))
+  with Ghost;
+
+  function Get_Board return Board
+    with Global => (Input => Game_State);
+
+  procedure Game_Reset
+    with
+      Global => (Output => (Game_State)),
+      Post  => Is_Empty (Get_Board);
+end Repro;
