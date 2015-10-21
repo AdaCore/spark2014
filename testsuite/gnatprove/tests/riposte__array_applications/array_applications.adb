7bd7973c9aeddffbc6d374bd38ef7d532105b683
Florian Schanda <florian.schanda@altran.com>
2015-10-21 12:53:47 +0100

    Minor update of riposte test
    Restore original meaning of test that got lost in translation from the
    original Riposte test suite to SPARK 2014.
    
    I am very pleased to notice that we actually can do some inductive
    proofs with CVC4!

--- testsuite/gnatprove/tests/riposte__array_applications/array_applications.adb
+++ testsuite/gnatprove/tests/riposte__array_applications/array_applications.adb
@@ -1,54 +1,101 @@
+--  The original purpose of this test was to highlight inductive reasoning.
+--  There are two versions of this, implicit and explicit; the latter
+--  adding some bounds on the values on the array that do not require
+--  inductive reasoning (but does require support for **).
+
 package body Array_Applications
 is
 
-   type Fibonacci_Index is range 0 .. 46;
-   type Fibonacci_Array is array (Fibonacci_Index) of Integer;
+   type Fibonacci_Index is range 0 .. 30;
+   type Fibonacci_Array is array (Fibonacci_Index) of Natural;
 
+   --  A(30) is actually 832040
    procedure Fibonacci_Implicit ( A : in out Fibonacci_Array)
-     with Depends => (A =>+ null),
-          Post    => (for all I in Fibonacci_Index  --  @POSTCONDITION:FAIL
-                        range 2..Fibonacci_Index'Last =>
-                          A(I) = A(I - 1) + A(I - 2))
-                     and A(46) /= 1836311903
+   with Global  => null,
+        Depends => (A =>+ null),
+        Post    => A(30) /= 832040 -- @POSTCONDITION:FAIL
    is
    begin
       A(0) := 0;
       A(1) := 1;
 
       for I in Fibonacci_Index range 2 .. Fibonacci_Index'Last loop
-         pragma Loop_Invariant
-           ((for all J in Fibonacci_Index range 2..I - 1 =>
-               A(J) = A(J - 1) + A(J - 2))
-            and A(0)= 0
-            and A(1) = 1);
-
          A(I) := A(I - 1) + A(I - 2);
-      end loop;
 
+         pragma Loop_Invariant
+           ((for all J in Fibonacci_Index range 2 .. I =>
+               A(J) = A(J - 1) + A(J - 2))
+            and A(0) = 0
+            and A(1) = 1);
+      end loop;
    end Fibonacci_Implicit;
 
-   procedure Fibonacci_Explicit ( A : in out Fibonacci_Array)
-     with Depends => (A =>+ null),
-          Post    => (for all I in Fibonacci_Index  --  @POSTCONDITION:FAIL
-                        range 2..Fibonacci_Index'Last =>
-                          A(I) = A(I - 1) + A(I - 2))
-                     and A(46) /= 1836311903
+   procedure Fibonacci_Implicit_OK ( A : in out Fibonacci_Array)
+   with Global  => null,
+        Depends => (A =>+ null),
+        Post    => A(30) = 832040 -- POSTCONDITION:PASS
    is
    begin
       A(0) := 0;
       A(1) := 1;
 
       for I in Fibonacci_Index range 2 .. Fibonacci_Index'Last loop
-         pragma Loop_Invariant
-           ((for all J in Fibonacci_Index range 2..I - 1 =>
-               A(J) = A(J - 1) + A(J - 2))
-            and (for all J in Fibonacci_Index
-                   range Fibonacci_Index'First..I - 1 =>
-                     A(J) >= 0 and A(J) < 2**Integer(J)));
-
          A(I) := A(I - 1) + A(I - 2);
-      end loop;
 
+         pragma Loop_Invariant
+           ((for all J in Fibonacci_Index range 2 .. I =>
+               A(J) = A(J - 1) + A(J - 2))
+            and A(0) = 0
+            and A(1) = 1);
+      end loop;
+   end Fibonacci_Implicit_OK;
+
+   --  To make overflow checking for the addition easier we can put
+   --  explicit bounds on the array that depend on the index only.
+   --  Fibonacci (n) grows slower than 2^n, and that is sufficient to show
+   --  we would not overflow.
+
+   --  A(30) is actually 832040
+   procedure Fibonacci_Explicit ( A : in out Fibonacci_Array)
+   with Global => null,
+        Depends => (A =>+ null),
+        Post    => A(30) /= 832040 -- @POSTCONDITION:FAIL
+   is
+   begin
+      A(0) := 0;
+      A(1) := 1;
+
+      for I in Fibonacci_Index range 2 .. Fibonacci_Index'Last loop
+         A(I) := A(I - 1) + A(I - 2);
+         pragma Loop_Invariant
+           (A(0) = 0 and
+            A(1) = 1 and
+            (for all J in Fibonacci_Index range 2 .. I =>
+               A(J) = A(J - 1) + A(J - 2)) and
+            (for all J in Fibonacci_Index range Fibonacci_Index'First .. I =>
+               A(J) >= 0 and A(J) < 2**Natural(J)));
+      end loop;
    end Fibonacci_Explicit;
+
+   procedure Fibonacci_Explicit_OK ( A : in out Fibonacci_Array)
+   with Global  => null,
+        Depends => (A =>+ null),
+        Post    => A(30) = 832040 -- POSTCONDITION:PASS
+   is
+   begin
+      A(0) := 0;
+      A(1) := 1;
+
+      for I in Fibonacci_Index range 2 .. Fibonacci_Index'Last loop
+         A(I) := A(I - 1) + A(I - 2);
+         pragma Loop_Invariant
+           (A(0) = 0 and
+            A(1) = 1 and
+            (for all J in Fibonacci_Index range 2 .. I =>
+               A(J) = A(J - 1) + A(J - 2)) and
+            (for all J in Fibonacci_Index range Fibonacci_Index'First .. I =>
+               A(J) >= 0 and A(J) < 2**Natural(J)));
+      end loop;
+   end Fibonacci_Explicit_OK;
 
 end Array_Applications;
