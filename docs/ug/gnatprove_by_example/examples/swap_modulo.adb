221c3d3ca56cb8cbf6ef84a82b23a93ad6730138
Yannick Moy <moy@adacore.com>
2015-10-23 17:14:46 +0200

    (minor) Fix layout

--- docs/ug/gnatprove_by_example/examples/swap_modulo.adb
+++ docs/ug/gnatprove_by_example/examples/swap_modulo.adb
@@ -2,7 +2,7 @@ with Interfaces; use Interfaces;
 
 procedure Swap_Modulo (X, Y : in out Unsigned_32) with
   SPARK_Mode,
-  Post    => X = Y'Old and Y = X'Old
+  Post => X = Y'Old and Y = X'Old
 is
 begin
    X := X xor Y;
