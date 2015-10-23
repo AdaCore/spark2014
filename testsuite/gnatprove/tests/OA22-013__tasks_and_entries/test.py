cce1341905232cf92ded461ee759faf12f7738bc
Johannes Kanig <kanig@adacore.com>
2015-10-23 15:34:43 +0900

    minor improve test, but still in diff

--- testsuite/gnatprove/tests/OA22-013__tasks_and_entries/test.py
+++ testsuite/gnatprove/tests/OA22-013__tasks_and_entries/test.py
@@ -3,7 +3,7 @@ import re, os
 
 do_flow()
 
-hand = open(os.path.join("gnatprove", "main.ali"))
+hand = open(os.path.join("gnatprove", "p.ali"))
 for line in hand :
     line = line.rstrip()
     if re.search('^F ', line) :
