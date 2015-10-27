5c394d85d566dd44dc15bd3cc1127015cf615b8e
Florian Schanda <florian.schanda@altran.com>
2015-10-27 12:51:25 +0000

    OA26-022 proof - add testcase
    Add test which crashes why3.

---
+++ testsuite/gnatprove/tests/OA26-022__gnatprove_crash/eg.adb
@@ -0,0 +1,7 @@
+package body Eg
+is
+   function Foo (A, B : Float) return Float is
+   begin
+      return A * B;
+   end;
+end Eg;
---
+++ testsuite/gnatprove/tests/OA26-022__gnatprove_crash/eg.ads
@@ -0,0 +1,4 @@
+package Eg
+is
+   pragma Elaborate_Body;
+end Eg;
