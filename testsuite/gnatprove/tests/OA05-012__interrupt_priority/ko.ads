a8ff1483a206754b8f7f05172997bc9f900a6f67
Yannick Moy <moy@adacore.com>
2015-10-29 12:44:02 +0100

    OA05-012 Fix test to use static values in range on all platforms

--- testsuite/gnatprove/tests/OA05-012__interrupt_priority/ko.ads
+++ testsuite/gnatprove/tests/OA05-012__interrupt_priority/ko.ads
@@ -1,5 +1,6 @@
+with System;
 with Interrupt_Priority;
 use Interrupt_Priority;
 package KO with SPARK_Mode is
-  P :  Interrupt_Needed_1 (1);
+  P :  Interrupt_Needed_1 (System.Any_Priority'First);
 end KO;
--- testsuite/gnatprove/tests/OA05-012__interrupt_priority/ok.ads
+++ testsuite/gnatprove/tests/OA05-012__interrupt_priority/ok.ads
@@ -1,9 +1,10 @@
+with System;
 with Interrupt_Priority;
 use Interrupt_Priority;
 package Ok with SPARK_Mode is
-   P1 : No_Interrupt_Needed_1 (1);
-   P2 : No_Interrupt_Needed_1 (98);
-   P3 : No_Interrupt_Needed_2 (1);
-   P4 : No_Interrupt_Needed_2 (98);
-   P5 : Interrupt_Needed_1 (98);
+   P1 : No_Interrupt_Needed_1 (System.Any_Priority'First);
+   P2 : No_Interrupt_Needed_1 (System.Interrupt_Priority'First);
+   P3 : No_Interrupt_Needed_2 (System.Any_Priority'First);
+   P4 : No_Interrupt_Needed_2 (System.Interrupt_Priority'First);
+   P5 : Interrupt_Needed_1 (System.Interrupt_Priority'First);
 end OK;
