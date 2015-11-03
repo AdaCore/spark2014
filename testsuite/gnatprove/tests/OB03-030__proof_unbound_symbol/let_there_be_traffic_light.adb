400866cd24cd192f4c8138cb9ebc10df39506418
Pavlos Efstathopoulos <efstatho@adacore.com>
2015-11-03 14:59:25 +0000

    OB03-030 added example

---
+++ testsuite/gnatprove/tests/OB03-030__proof_unbound_symbol/let_there_be_traffic_light.adb
@@ -0,0 +1,9 @@
+with Traffic_Lights; use Traffic_Lights;
+
+procedure Let_There_Be_Traffic_Light is
+begin
+   --  Dummy main that just runs forever...
+   loop
+      null;
+   end loop;
+end Let_There_Be_Traffic_Light;
---
+++ testsuite/gnatprove/tests/OB03-030__proof_unbound_symbol/traffic_lights.ads
@@ -0,0 +1,41 @@
+--  For the sake of this example the lights go as follows:
+--
+--  Vehicles                 Pedestrians
+--
+--  Green                    Red
+--  Yellow                   Red
+--  Red                      Green
+--  Red and Yellow           Red
+--
+--  and over and over they go..
+
+with Ada.Real_Time; use Ada.Real_Time;
+
+package Traffic_Lights is
+   protected Traffic_Light is
+      function Valid_Combination return Boolean;
+
+      entry Change_Lights
+        with Pre  => Valid_Combination,
+             Post => Valid_Combination;
+
+      procedure Check_Time;
+   private
+      Last_State_Change : Time    := Time_First;
+      Vehicles_Green    : Boolean := False;
+      Vehicles_Yellow   : Boolean := False;
+      Vehicles_Red      : Boolean := True;
+      Pedestrians_Green : Boolean := True;
+      Pedestrians_Red   : Boolean := False;
+      Change_State      : Boolean := False;
+   end Traffic_Light;
+
+   task Check_The_Time;
+   --  This task keeps checking the time in order to determine if the state of
+   --  the traffic light needs to change.
+
+   task Change_The_Lights;
+   --  This tasks keeps checking if the Lights need to be changed, and if
+   --  needed, it does so.
+
+end Traffic_Lights;
