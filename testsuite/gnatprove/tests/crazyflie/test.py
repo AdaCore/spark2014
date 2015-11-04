7e4aff7980da5a92015c887807ddb21d119574d0
Yannick Moy <moy@adacore.com>
2015-11-04 10:15:13 +0100

    O806-003 Add comment

--- testsuite/gnatprove/tests/crazyflie/test.py
+++ testsuite/gnatprove/tests/crazyflie/test.py
@@ -1,3 +1,5 @@
 from test_support import *
 
+# Do not include Z3 in list of provers due to runtime differences with steps
+# on different platforms.
 prove_all(opt=["-XMODE=Analyze","--prover=cvc4,altergo"],steps=5000)
