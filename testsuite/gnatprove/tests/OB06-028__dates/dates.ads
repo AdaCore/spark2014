7889e9faf69be18f15a33bb24dbe5ff098b8b6e0
Yannick Moy <moy@adacore.com>
2015-11-06 17:13:00 +0100

    OB06-028 Add test

---
+++ testsuite/gnatprove/tests/OB06-028__dates/dates.ads
@@ -0,0 +1,16 @@
+package Dates with SPARK_Mode is
+
+  pragma Elaborate_Body;
+
+  subtype Year_Number is Integer range 1 .. 2399;
+
+  function Is_Leap_Year (Year : Year_Number) return Boolean is
+    (Year mod 4 = 0 and (Year mod 400 not in 100 | 200 | 300));
+
+  subtype Date_Range is Integer range 1 .. 876216;
+
+  type Date_Type is record
+     Day_Of_Common_Era : Date_Range;
+  end record;
+
+end Dates;
