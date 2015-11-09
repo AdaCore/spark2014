cab6f32358673d680f36535bcc4402996cc8f2e6
Yannick Moy <moy@adacore.com>
2015-11-09 17:18:44 +0100

    O311-024 Update test script and output

--- testsuite/gnatprove/tests/O512-022__nose_gear/test.py
+++ testsuite/gnatprove/tests/O512-022__nose_gear/test.py
@@ -1,3 +1,5 @@
 from test_support import *
-
-prove_all(steps=1200)
+# Do not use Z3, as the step limit fluctuates too much between platforms, thus
+# making it impossible to share a common expected output when Z3 is used,
+# especially since it makes the test outrun the timeout limit on Darwin.
+prove_all(prover=["cvc4", "altergo"], steps=1200)
