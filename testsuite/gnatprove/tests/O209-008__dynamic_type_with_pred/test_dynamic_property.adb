9d67ced42a8eebed8ab25059437640be7613f758
Yannick Moy <moy@adacore.com>
2015-11-20 15:36:58 +0100

    O209-008 Test passes now

--- testsuite/gnatprove/tests/O209-008__dynamic_type_with_pred/test_dynamic_property.adb
+++ testsuite/gnatprove/tests/O209-008__dynamic_type_with_pred/test_dynamic_property.adb
@@ -2,8 +2,8 @@ procedure Test_Dynamic_Property with SPARK_Mode is
    procedure P (C : Positive) is
       subtype Pos_Dynamic_Discrete is Integer range -C .. C with
         Static_Predicate => Pos_Dynamic_Discrete /= 0;
-      S : Pos_Dynamic_Discrete := 1;
-      T : Pos_Dynamic_Discrete := C - 1;
+      S : Pos_Dynamic_Discrete := 1;      --  @PREDICATE_CHECK:PASS
+      T : Pos_Dynamic_Discrete := C - 1;  --  @PREDICATE_CHECK:FAIL
    begin
       null;
    end P;
