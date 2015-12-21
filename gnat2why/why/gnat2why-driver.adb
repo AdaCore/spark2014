aee855e36551d3205fb3dd39639261b5daa449f0
Yannick Moy <moy@adacore.com>
2015-12-21 11:38:42 +0100

    OC11-016 Fix bug in generation of VCs for classwide contracts
    The generation of VCs for classwide contracts of primitives of
    a private type not declared as a tagged type but completed as such
    may lead to crashes of GNATprove.
    
    * gnat2why/why/gnat2why-driver.adb
    (Do_Generate_VCs): Fix wrong matching between opening and closing of
      symbol scopes.

--- gnat2why/why/gnat2why-driver.adb
+++ gnat2why/why/gnat2why-driver.adb
@@ -312,7 +312,9 @@ package body Gnat2Why.Driver is
               --  Ignore predicate functions and invariant procedures
               and then not Subprogram_Is_Ignored_For_Proof (E)
             then
-               if Is_Dispatching_Operation (E) then
+               if Is_Dispatching_Operation (E)
+                 and then not Is_Invisible_Dispatching_Operation (E)
+               then
                   Ada_Ent_To_Why.Push_Scope (Symbol_Table);
                   Update_Symbol_Table_For_Inherited_Contracts (E);
                end if;
