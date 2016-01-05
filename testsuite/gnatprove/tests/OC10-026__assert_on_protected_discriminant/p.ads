358c42350fb1391127f154ccc7167dcd1055dd1b
Piotr Trojanek <piotr.trojanek@altran.com>
2016-01-05 15:39:54 +0000

    OC10-026 test extended

--- testsuite/gnatprove/tests/OC10-026__assert_on_protected_discriminant/p.ads
+++ testsuite/gnatprove/tests/OC10-026__assert_on_protected_discriminant/p.ads
@@ -1,7 +1,11 @@
 package P is
 
+    function Zero return Integer is (0);
+
     protected type PT (J : Integer) is
        pragma Assert (J > 0);
+    private
+       X : Positive := Zero;
     end;
 
     PO : PT (-1);
