251eceb8ab710b86afb013e24e91384bbddccfed
Yannick Moy <moy@adacore.com>
2016-01-07 18:03:42 +0100

    OB17-003, O909-010 Update test on branch
    As illegal call to protected function is not detected in SPARK
    precondition, in branch spark-16.0, update test to not trigger that
    issue.

--- testsuite/gnatprove/tests/O909-010__prot_effects/eff.ads
+++ testsuite/gnatprove/tests/O909-010__prot_effects/eff.ads
@@ -7,7 +7,7 @@ package Eff is
       procedure Set_B_To_Sum_Plus (Add : Integer)
          with Pre => Add > 0;
       entry Set_B_To_Sum_Plus_E (Add : Integer)
-         with Pre => Get_A = 10;
+         ;
    private
       A : Integer := 0;
       B : Integer := 1;
