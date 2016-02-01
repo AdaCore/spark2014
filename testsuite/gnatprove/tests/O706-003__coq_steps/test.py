910fa5ed9d1c913f3a676cdc20896bdad2682c97
Johannes Kanig <kanig@adacore.com>
2016-02-01 09:48:09 +0900

    minor: fix test script

--- testsuite/gnatprove/tests/O706-003__coq_steps/test.py
+++ testsuite/gnatprove/tests/O706-003__coq_steps/test.py
@@ -1,4 +1,4 @@
 from test_support import *
 
-prove_all(opt=["-P", "test.gpr", "--steps=1", "--prover=coq"])
-prove_all(opt=["-P", "test.gpr", "--steps=1", "--prover=coq"])
+prove_all(prover=["coq"],opt=["-P", "test.gpr", "--steps=1"])
+prove_all(prover=["coq"],opt=["-P", "test.gpr", "--steps=1"])
