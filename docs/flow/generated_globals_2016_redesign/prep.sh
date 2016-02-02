69d0a16cab220cb23e3d8ceed1e249ee670604cc
Florian Schanda <florian.schanda@altran.com>
2016-02-02 09:31:06 +0000

    P118-034 flow - redesign gg
    Minor renaming of supporting files.

--- docs/flow/generated_globals_2016_redesign/prep.sh
+++ docs/flow/generated_globals_2016_redesign/prep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-dot phase1_p.dot -Tpdf > phase1_p.pdf
-dot phase1_q.dot -Tpdf > phase1_q.pdf
-dot phase2.dot   -Tpdf > phase2.pdf
+for DOT in *.dot; do
+    PDF=$(echo $DOT | sed -e 's/.dot/.pdf/')
+    dot $DOT -Tpdf > $PDF
+done
