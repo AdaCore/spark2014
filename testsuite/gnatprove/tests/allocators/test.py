40acf53de34fe17b4ac60eff3f2bdb5ff245ec92
Yannick Moy <moy@adacore.com>
2016-02-02 07:36:46 +0100

    (minor) Double VC timeout for Linux

--- testsuite/gnatprove/tests/allocators/test.py
+++ testsuite/gnatprove/tests/allocators/test.py
@@ -1,5 +1,5 @@
 from test_support import *
 
 # We should remove --dbg-proof-only once OC14-044 is fixed
-prove_all(vc_timeout=60, steps=0, \
+prove_all(vc_timeout=120, steps=0, \
           opt=["--dbg-proof-only", "-u", "simple_allocator.adb", "list_allocator.adb"])
