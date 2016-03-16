fa00276fc39ce17c46c6df5677c61f88d60404b2
Yannick Moy <moy@adacore.com>
2016-03-16 15:12:45 +0100

    P226-043 Update test driver and output

--- testsuite/gnatprove/tests/text_io_get_line/test.py
+++ testsuite/gnatprove/tests/text_io_get_line/test.py
@@ -1,8 +1,6 @@
 from test_support import *
 
-# Run without flow analysis due to bug in P226-043
 # Do not run with Z3 (P212-018) until deterministic on all platforms
-prove_all(opt=["--dbg-proof-only",
-               "--proof=progressive",
+prove_all(opt=["--proof=progressive",
                "--prover=cvc4,altergo"],
           steps=500)
