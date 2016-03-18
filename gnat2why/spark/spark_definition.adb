3b6ba59bf03770f378ac905f61eab7b2b0146a1b
Piotr Trojanek <piotr.trojanek@altran.com>
2016-03-18 09:57:52 +0000

    P316-024 no warning on default expressions
    Ignore wrappers generated by the front end to process default expressions
    of subprogram parameters.
    
    * gnat2why/spark/spark_definition.adb
    (Mark_Subprogram_Body): detect wrappers generated by the front end and
     ignore them.
    
    * testsuite/gnatprove/tests/P316-024__body_on_default_params/test.out
    Primary test output updated.
    
    * testsuite/gnatprove/tests/N915-008__flow_acats_oo/test.out
    Other test output updated.

--- gnat2why/spark/spark_definition.adb
+++ gnat2why/spark/spark_definition.adb
@@ -5010,6 +5010,15 @@ package body SPARK_Definition is
       elsif Subprogram_Is_Ignored_For_Proof (E) then
          return;
 
+      --  Ignore subprograms that front-end generates to analyze default
+      --  expressions. They have no spec, only body and whose Is_Eliminated
+      --  flag is set. Unlike user's subprograms with pragma Eliminated, they
+      --  do come not from source. See Freeze.Process_Default_Expressions for
+      --  details.
+
+      elsif Is_Eliminated (E) and then not Comes_From_Source (E) then
+         return;
+
       else
          if In_Pred_Function_Body then
             Current_Delayed_Aspect_Type := Etype (First_Formal (E));
