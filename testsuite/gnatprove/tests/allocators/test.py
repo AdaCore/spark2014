57de05e3109b7eaac5790c05489468164b47c287
Johannes Kanig <kanig@adacore.com>
2016-03-31 11:33:09 +0900

    minor disable z3 on test to obtain more stability

--- testsuite/gnatprove/tests/allocators/test.py
+++ testsuite/gnatprove/tests/allocators/test.py
@@ -1,5 +1,5 @@
 from test_support import *
 
 # We should remove --dbg-proof-only once OC14-044 is fixed
-prove_all(vc_timeout=120, steps=0, \
+prove_all(vc_timeout=120, steps=0, prover=["cvc4"], \
           opt=["--dbg-proof-only", "-u", "simple_allocator.adb", "list_allocator.adb"])
