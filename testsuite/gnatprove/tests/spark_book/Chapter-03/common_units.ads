4a95b5617c050b157da4cbd1d05adc78028f7498
Clément Fumex <clement.fumex@inria.fr>
2016-04-20 17:35:00 +0200

    remove violation from spark_book test

--- testsuite/gnatprove/tests/spark_book/Chapter-03/common_units.ads
+++ testsuite/gnatprove/tests/spark_book/Chapter-03/common_units.ads
@@ -1,8 +1,8 @@
 with Ada.Numerics;
 package Common_Units is
 
-   type Degrees is digits 18 range 0.0 .. 360.0;
-   type Radians is digits 18 range 0.0 .. 2.0 * Ada.Numerics.Pi;
+   type Degrees is digits 15 range 0.0 .. 360.0;
+   type Radians is digits 15 range 0.0 .. 2.0 * Ada.Numerics.Pi;
 
    type Volts is delta 1.0 / 2.0**12 range -45_000.0 .. 45_000.0;
    type Amps  is delta 1.0 / 2.0**16 range -1_000.0 .. 1_000.0;
