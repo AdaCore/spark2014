0bd37c3533837fdfcddd401bffb27cee64b85d75
Yannick Moy <moy@adacore.com>
2016-04-29 10:16:13 +0200

    J506-016, P405-006 Test passes now with appropriate lemma

--- testsuite/gnatprove/tests/J506-016__scaled_value/sv.adb
+++ testsuite/gnatprove/tests/J506-016__scaled_value/sv.adb
@@ -1,12 +1,22 @@
-package body SV is  
+with SPARK.Mod64_Arithmetic_Lemmas; use SPARK.Mod64_Arithmetic_Lemmas;
+with Interfaces; use Interfaces;
+
+package body SV
+  with SPARK_Mode
+is
 
    function Scale
      (Capacity           : Capacity_Type;
       Requested_Capacity : Sum_Type;
       Value              : Request_Type) return Request_Type
    is
+      Res : Request_Type := (Value * Capacity) / Requested_Capacity;
    begin
-      return (Value * Capacity) / Requested_Capacity;
+      Lemma_Mult_Scale (Val         => Unsigned_64 (Value),
+                        Scale_Num   => Unsigned_64 (Capacity),
+                        Scale_Denom => Unsigned_64 (Requested_Capacity),
+                        Res         => Unsigned_64 (Res));
+      return Res;
    end Scale;
 
 end SV;
--- testsuite/gnatprove/tests/J506-016__scaled_value/test.py
+++ testsuite/gnatprove/tests/J506-016__scaled_value/test.py
@@ -1,3 +1,5 @@
 from test_support import *
+from gnatpython.env import putenv
 
+putenv("SPARK_LEMMAS_OBJECT_DIR", ".")
 prove_all(steps=450)
