b4166a9cd65370c2d84d15c81b8caa043b3bcb2f
Johannes Kanig <kanig@adacore.com>
2016-05-13 11:34:34 +0900

    P429-031 modify test and push why3 commit

--- testsuite/gnatprove/tests/P429-031__coq_change/test.py
+++ testsuite/gnatprove/tests/P429-031__coq_change/test.py
@@ -1,3 +1,4 @@
+from shutil import copyfile
 from test_support import *
 from time import sleep
 import glob
@@ -9,6 +10,9 @@ proof = """admit.
 Qed.
 """
 
+ads_file = "lemmas.ads"
+new_file = "lemmas.ads.new"
+
 def edit_proof():
     proof_file = glob.glob("proof/Coq/lemmas/*.v")[0]
     with open(proof_file, 'r') as file:
@@ -17,15 +21,19 @@ def edit_proof():
     with open(proof_file, 'w') as file:
         file.write(content)
 
+def edit_file():
+    copyfile(new_file, ads_file)
+
+
 write_why3_config_file_with_coq(conf_file)
-prove_all(opt=["-XRUN=1"], counterexample=False)
+prove_all(prover=["cvc4"], counterexample=False)
 print "======================================="
-prove_all(opt=["-XRUN=1", "--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:16"], steps=None, counterexample=False)
+prove_all(opt=["--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:15"], steps=None, counterexample=False)
 edit_proof()
-prove_all(opt=["-XRUN=1"], counterexample=False)
 print "======================================="
-prove_all(opt=["-XRUN=2", "--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:18"], steps=None, counterexample=False)
-prove_all(opt=["-XRUN=2"], counterexample=False)
+prove_all(opt=["--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:15"], steps=None, counterexample=False)
 print "======================================="
-prove_all(opt=["-XRUN=1", "--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:16"], steps=None, counterexample=False)
-prove_all(opt=["-XRUN=1"], counterexample=False)
+edit_file()
+sleep(4)
+prove_all(opt=["--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:15"], steps=None, counterexample=False)
+prove_all(counterexample=False)
