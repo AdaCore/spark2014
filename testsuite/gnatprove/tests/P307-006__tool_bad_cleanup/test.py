f389f351921a8a3c8a4587067c5e6e6ef88d229c
Florian Schanda <florian.schanda@altran.com>
2016-05-17 09:32:12 +0100

    Fix test for P307-006 for other platforms.

--- testsuite/gnatprove/tests/P307-006__tool_bad_cleanup/test.py
+++ testsuite/gnatprove/tests/P307-006__tool_bad_cleanup/test.py
@@ -1,9 +1,25 @@
 from test_support import *
 
+# First we generate an up-to-date ali file for foo
 prove_all(opt=["-u", "foo.adb"])
-with open("foo.spark_15_0_2", "r") as src:
-    content = src.read()
-with open(os.path.join("gnatprove", "foo.ali"), "w") as dst:
-    dst.write(content)
 
+# Then we transform it into something that looks like 15.0.2
+with open(os.path.join("gnatprove", "foo.ali"), "r") as src:
+    lines = src.read().splitlines()
+with open(os.path.join("gnatprove", "foo.ali"), "w") as dst:
+    for l in lines:
+        if l.startswith("QQ SPARKVERSION"):
+            break
+        dst.write(l + "\n")
+    dst.write("GG S foo__p\n")
+    dst.write("GG VP\n")
+    dst.write("GG VI\n")
+    dst.write("GG VO foo__g\n")
+    dst.write("GG CP\n")
+    dst.write("GG CD\n")
+    dst.write("GG CC\n")
+    dst.write("GG LV\n")
+
+# Then we attempt to prove everything (i.e. bar) but using the broken
+# foo.ali
 prove_all()
