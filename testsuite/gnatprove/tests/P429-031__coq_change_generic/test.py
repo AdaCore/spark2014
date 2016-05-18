5fc405f1b0756920fc1e8e92eb5ef337512f37ae
Yannick Moy <moy@adacore.com>
2016-05-18 11:56:44 +0200

    P429-031 Add test for Coq proof on generic

---
+++ testsuite/gnatprove/tests/P429-031__coq_change_generic/test.py
@@ -0,0 +1,34 @@
+from shutil import copyfile
+from test_support import *
+from time import sleep
+import glob
+
+conf_file = "test.whyconf"
+
+proof = """admit.
+
+Qed.
+"""
+
+def edit_proof(num):
+    proof_file = glob.glob("proof/Coq/lemmas_v1/*monotonic" + str(num) + "*.v")[0]
+    with open(proof_file, 'r') as file:
+        content = file.read()
+    content = str.replace(content, "Qed.", proof)
+    with open(proof_file, 'w') as file:
+        file.write(content)
+
+write_why3_config_file_with_coq(conf_file)
+prove_all(prover=["cvc4"], counterexample=False)
+print "======================================="
+prove_all(opt=["-U", "--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:17"], steps=None, counterexample=False)
+edit_proof(1)
+print "======================================="
+prove_all(opt=["-U", "--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:17"], steps=None, counterexample=False)
+print "======================================="
+prove_all(opt=["-U", "--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:26"], steps=None, counterexample=False)
+edit_proof(2)
+print "======================================="
+prove_all(opt=["-U", "--prover=coq", "--why3-conf=" + conf_file, "--limit-line=lemmas.ads:26"], steps=None, counterexample=False)
+print "======================================="
+prove_all(counterexample=False)
