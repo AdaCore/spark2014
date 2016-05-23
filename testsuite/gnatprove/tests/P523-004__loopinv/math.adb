27d3cae57e1c370e50d576413925037b3203b9ac
Yannick Moy <moy@adacore.com>
2016-05-23 10:19:53 +0200

    P523-004 Update test

--- testsuite/gnatprove/tests/P523-004__loopinv/math.adb
+++ testsuite/gnatprove/tests/P523-004__loopinv/math.adb
@@ -2,10 +2,15 @@ package body Math with
   SPARK_Mode
 is
    procedure Shift_Right (V : in out Sorted_Values) is
+      Prev_Value : Value := V(Index'First);
+      Tmp_Value  : Value;
    begin
       for J in Index loop
-         V(J) := V(Index'Max(Index'First, J - 1));
+         Tmp_Value := Prev_Value;
+         Prev_Value := V(J);
+         V(J) := Tmp_Value;
 
+         pragma Loop_Invariant (Prev_Value = V'Loop_Entry(J));
          pragma Loop_Invariant
            (for all K in Index'First .. J => V(K) = V'Loop_Entry(Index'Max(Index'First, K - 1)));
          pragma Loop_Invariant
