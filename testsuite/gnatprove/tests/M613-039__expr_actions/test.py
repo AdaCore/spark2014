a7b31e57cfe714e42dfbdaa022891b197939e1a7
Johannes Kanig <kanig@adacore.com>
2016-05-26 19:24:18 +0900

    minor remove useless steps limit from test

--- testsuite/gnatprove/tests/M613-039__expr_actions/test.py
+++ testsuite/gnatprove/tests/M613-039__expr_actions/test.py
@@ -1,4 +1,4 @@
 # disable z3 for cross-platform stability
 from test_support import *
 
-prove_all(steps=2100, opt=["--report=provers"], prover=["cvc4"])
+prove_all(opt=["--report=provers"], prover=["cvc4"])
