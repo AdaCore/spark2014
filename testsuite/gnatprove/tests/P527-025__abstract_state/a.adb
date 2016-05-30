18455be90bbee3ac152a14dce337a729a4753987
Yannick Moy <moy@adacore.com>
2016-05-30 09:52:47 +0200

    P527-025 Add test

---
+++ testsuite/gnatprove/tests/P527-025__abstract_state/a.adb
@@ -0,0 +1,7 @@
+package body A with
+  SPARK_Mode,
+  Refined_State => (aDATA1 => DATA1)
+is
+begin
+   null;
+end A;
---
+++ testsuite/gnatprove/tests/P527-025__abstract_state/a~b.adb
@@ -0,0 +1,8 @@
+package body A.B with
+  SPARK_Mode
+is
+   procedure example is
+   begin
+      DATA1 := 0;
+   end example;
+end A.B;
