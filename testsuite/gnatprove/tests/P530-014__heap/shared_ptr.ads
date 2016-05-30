2c115600c301eaad7e31097073b33c94b2926c5e
Yannick Moy <moy@adacore.com>
2016-05-30 15:12:09 +0200

    P530-014 Add test

---
+++ testsuite/gnatprove/tests/P530-014__heap/shared_ptr.ads
@@ -0,0 +1,24 @@
+package Shared_Ptr with
+  SPARK_Mode
+is
+   type T is private;
+
+   function Length (This : T) return Integer;
+
+   function To_String (This : T) return String with
+     Pre => Length (This) > 0,
+     Global => null;
+
+   function Test (This : T) return Boolean is
+      (Length (This) = To_String (This)'Length);
+
+private
+   pragma SPARK_Mode (Off);
+
+   type T is access String;
+
+   function Length (This : T) return Integer is (This.all'Length);
+
+   function To_String (This : T) return String is (This.all);
+
+end Shared_Ptr;
