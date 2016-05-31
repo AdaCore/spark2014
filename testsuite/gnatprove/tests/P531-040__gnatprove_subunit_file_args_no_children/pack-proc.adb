a8245bc6ea19a405073331951085c8551b9fad32
Angela Wallenburg <angela.wallenburg@altran.com>
2016-05-31 16:31:19 +0100

    P531-040 added test, no child structure yields correct output

---
+++ testsuite/gnatprove/tests/P531-040__gnatprove_subunit_file_args_no_children/pack-proc.adb
@@ -0,0 +1,8 @@
+separate (Pack)
+
+procedure Proc (R : in Exp_Seq_T; I: in Idx_T; Nf: out Opt_P_T) is
+begin
+   Nf := Opt_P_T' (Flag  => True,
+                   Idx   => I + 1,
+                   The_P => R.Points (I));
+end Proc;
---
+++ testsuite/gnatprove/tests/P531-040__gnatprove_subunit_file_args_no_children/test.py
@@ -0,0 +1,3 @@
+from test_support import *
+
+gnatprove (opt=["-P", "test.gpr", "-u", "pack-proc.adb"])
