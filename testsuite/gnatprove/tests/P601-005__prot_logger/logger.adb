6eb1da4a2c1b437b7ece14dd69498208dae8c699
Piotr Trojanek <piotr.trojanek@altran.com>
2016-06-01 09:07:47 +0100

    P601-005 test minimized

--- testsuite/gnatprove/tests/P601-005__prot_logger/logger.adb
+++ testsuite/gnatprove/tests/P601-005__prot_logger/logger.adb
@@ -1,25 +1,20 @@
 package body Logger
 is
-   type Log_Msg is null record;
-
    -- protected type to implement the buffer
    protected type Msg_Queue_T is
-      procedure New_Msg (msg : in Log_Msg);
+      procedure New_Msg;
    end Msg_Queue_T;
 
    queue : Msg_Queue_T;
 
    -- implementation of the message queue
    protected body Msg_Queue_T is
-      procedure New_Msg (msg : in Log_Msg) is null;
+      procedure New_Msg is null;
    end Msg_Queue_T;
 
-   procedure log
-   is
-      msg : Log_Msg;
+   procedure log is
    begin
-      queue.New_Msg(msg);
-  null;
+      queue.New_Msg;
    end log;
 
 end Logger;
