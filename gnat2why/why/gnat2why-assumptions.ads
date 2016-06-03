2623eafa3dce3f2cc92f00cd6b9ced07eb30ed0a
Piotr Trojanek <piotr.trojanek@altran.com>
2016-06-03 11:04:17 +0100

    minor style fix in comment

--- gnat2why/why/gnat2why-assumptions.ads
+++ gnat2why/why/gnat2why-assumptions.ads
@@ -49,7 +49,7 @@ package Gnat2Why.Assumptions is
       "="                 => "=");
 
    procedure Register_Claim (C : Claim);
-   --  This registers that the claim [C] has been established.
+   --  This registers that the claim [C] has been established
 
    procedure Assume_For_Claim
      (C      : Claim;
