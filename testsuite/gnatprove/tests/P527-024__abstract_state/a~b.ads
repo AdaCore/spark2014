57d3d63d855e5b7716f106fbc3c2f17a91c06e94
Piotr Trojanek <piotr.trojanek@altran.com>
2016-06-10 09:50:13 +0100

    P527-024 test file cleanups
    Whitespace alignments, Unix-style line endings

--- testsuite/gnatprove/tests/P527-024__abstract_state/a~b.ads
+++ testsuite/gnatprove/tests/P527-024__abstract_state/a~b.ads
@@ -2,6 +2,6 @@ package A.B with
   SPARK_Mode
 is
    procedure example with
-     Global => ( Input => aDATA2,
-                 In_Out => aDATA1);
+     Global => (Input  => aDATA2,
+                In_Out => aDATA1);
 end A.B;
