590475244803f6c9536e1c3acbcbb9c392d26336
Yannick Moy <moy@adacore.com>
2016-06-13 17:24:25 +0200

    P613-021 Add test

---
+++ testsuite/gnatprove/tests/P613-021__protected/core.adb
@@ -0,0 +1,36 @@
+pragma Profile(Ravenscar);
+pragma Partition_Elaboration_Policy(Sequential);
+pragma SPARK_Mode(On);
+
+package body Core is
+
+   protected body Mailbox is
+
+      function Item_Count return Message_Count is (Count);
+
+
+      procedure Install_Message(N : in Integer) is
+      begin
+         -- Ignore messages if there is no space for them!
+         if Count < Maximum_Message_Count then
+            Messages(Next_In).Value := N;
+            Messages(Next_In).Size := 0;
+            Next_In := Next_In + 1;
+            Count := Count + 1;
+            Message_Waiting := True;
+         end if;
+      end Install_Message;
+
+
+      entry Get_Message(N : out Integer) when Message_Waiting is
+      begin
+         pragma Assert (Message_Waiting);
+         N := Messages(Next_Out).Value;
+         Next_Out := Next_Out + 1;
+         Count := Count - 1;
+         Message_Waiting := (Count > 0);
+      end Get_Message;
+
+   end Mailbox;
+
+end Core;
---
+++ testsuite/gnatprove/tests/P613-021__protected/core.ads
@@ -0,0 +1,47 @@
+pragma Profile(Ravenscar);
+pragma Partition_Elaboration_Policy(Sequential);
+pragma SPARK_Mode(On);
+
+package Core is
+
+   ---------------------------------------------------------------------------------------
+   -- The declarations below are only needed to support the private section of the mailbox
+
+   Maximum_Message_Count : constant := 16;
+   type Message_Index is mod Maximum_Message_Count;
+   subtype Message_Count is Integer range 0 .. Maximum_Message_Count;
+   type Message_Record is
+      record
+         Value : Integer;
+         Size  : Natural;
+      end record;
+   type Message_Array is array(Message_Index) of Message_Record;
+
+   -- The declarations above are only needed to support the private section of the mailbox
+   ---------------------------------------------------------------------------------------
+
+   protected Mailbox is
+
+      -- Used only in contracts, but can't be made Ghost because of the protected type.
+      function Item_Count return Message_Count
+        with Global => null;
+
+      procedure Install_Message(N : in Integer)
+        with Post => Item_Count > 0;
+
+      -- Retrieve a message from this mailbox.
+      entry Get_Message(N : out Integer);
+        -- with Pre => Item_Count > 0;
+
+   private
+      Messages : Message_Array :=
+        (others => (Value => 0, Size => 0));
+      Next_In  : Message_Index := Message_Index'First;
+      Next_Out : Message_Index := Message_Index'First;
+      Count    : Message_Count := 0;
+      Message_Waiting : Boolean := False;
+   end Mailbox;
+
+
+end Core;
+
