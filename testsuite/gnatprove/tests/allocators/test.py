c0b69e986335b187b4d17b214c2ed4ad7885e771
Claire Dross <dross@adacore.com>
2016-06-15 12:52:39 +0200

    OA21-028 allocators test is fully proved

--- testsuite/gnatprove/tests/allocators/test.py
+++ testsuite/gnatprove/tests/allocators/test.py
@@ -3,5 +3,5 @@ from test_support import *
 # the test should be run with no step limit but it is too long
 # this test is a candidate to be a "large" test
 
-prove_all(vc_timeout=120, prover=["z3", "cvc4"], counterexample=False, \
-          opt=["--dbg-proof-only", "-u", "simple_allocator.adb", "list_allocator.adb", "list_mod_allocator.adb"])
+prove_all(vc_timeout=60, prover=["z3", "cvc4"], counterexample=False, \
+          opt=["--dbg-proof-only", "--steps=0", "-u", "simple_allocator.adb", "list_allocator.adb", "list_mod_allocator.adb"])
