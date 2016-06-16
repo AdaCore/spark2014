4bf727a2309a9383bdc814f7e50b915caa55bb7d
Johannes Kanig <kanig@adacore.com>
2016-06-16 18:16:28 +0900

    minor disable CE on test and move to large

--- testsuite/gnatprove/tests/proofinuse__floating_point/test.py
+++ testsuite/gnatprove/tests/proofinuse__floating_point/test.py
@@ -1,4 +1,4 @@
 from test_support import *
 # Do not use Z3, as the step limit fluctuates too much between platforms, thus
 # making it impossible to share a common expected output when Z3 is used.
-prove_all(prover=["cvc4", "altergo"], steps=800)
+prove_all(prover=["cvc4", "altergo"], steps=800, counterexample=False)
