f7ef4a807a5245916aeeb4b1a5bc2d1892d49376
Florian Schanda <florian.schanda@altran.com>
2016-07-05 08:33:18 +0100

    P704-003 proof - regressions after cvc4 update
    Change testscript so overall the test is faster (we use a level 2 for all
    but one of the files).
    
    Accept the 2 regressions (one of them I have reported under CVC4 bug 743).

--- testsuite/gnatprove/tests/openETCS/test.py
+++ testsuite/gnatprove/tests/openETCS/test.py
@@ -1,4 +1,19 @@
 from test_support import *
+from glob import glob
 # Do not use Z3, as the step limit fluctuates too much between platforms, thus
 # making it impossible to share a common expected output when Z3 is used.
-prove_all(prover=["cvc4", "altergo"], steps=1200, opt=["--report=provers"], counterexample=False)
+
+# List of all ada units (picking .adb in most cases, except when we only
+# have a spec file).
+files = [f + ".adb" if os.path.isfile(f + ".adb") else f + ".ads"
+         for f in set(os.path.splitext(f)[0] for f in glob("*.ad?"))]
+
+default_level = 2
+proof_level = {"step_function_test.adb" : 3}
+
+for f in sorted(files):
+    prove_all(prover=["cvc4", "altergo"],
+              steps=None,
+              level=proof_level.get(f, default_level),
+              counterexample=False,
+              opt=["--report=provers", "-u", f])
