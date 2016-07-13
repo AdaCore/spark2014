4641a3e91fe32eba8213961fba95a962f4831bfc
Yannick Moy <moy@adacore.com>
2016-07-13 16:05:21 +0200

    P620-017 Trivial support for new pragma Secondary_Stack_Size
    Pragma has no effect on SPARK analysis.

--- gnat2why/spark/spark_definition.adb
+++ gnat2why/spark/spark_definition.adb
@@ -4822,6 +4822,7 @@ package body SPARK_Definition is
               Pragma_No_Tagged_Streams            |
               Pragma_Pure_Function                |
               Pragma_Restriction_Warnings         |
+              Pragma_Secondary_Stack_Size         |
               Pragma_Style_Checks                 |
               Pragma_Test_Case                    |
               Pragma_Unmodified                   |
