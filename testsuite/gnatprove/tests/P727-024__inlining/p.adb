84294294b6dec489e27c1cc67d0d4ea878be9224
Yannick Moy <moy@adacore.com>
2016-07-27 15:13:28 +0200

    P727-024 Add test

---
+++ testsuite/gnatprove/tests/P727-024__inlining/p.adb
@@ -0,0 +1,18 @@
+procedure P is
+   type Vec is array (Integer range <>) of Integer;
+   subtype Vec_10_Sub is Vec (0 .. 10);
+
+   procedure Nothing  (X : Vec_10_Sub) is
+   begin
+      null;
+   end Nothing;
+
+   function F return Integer with Global => null is
+   begin
+      return 1;
+   end F;
+
+   A : Vec (F .. 10) := (others => 0);
+begin
+   Nothing (A);  --  @LENGTH_CHECK:FAIL
+end P;
