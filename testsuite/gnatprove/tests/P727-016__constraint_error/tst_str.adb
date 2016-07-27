745e1146b78a7d8140c3a89e668e30d592d540e7
Yannick Moy <moy@adacore.com>
2016-07-27 14:02:45 +0200

    P727-016 Add test

---
+++ testsuite/gnatprove/tests/P727-016__constraint_error/tst_str.adb
@@ -0,0 +1,13 @@
+procedure Tst_str is 
+
+  -- Create a badly defined string.
+  --  A valid String requires an index in range of Positive (1..)
+  subtype My_Str is String(0..9);
+
+  s : My_Str;
+  C : Character;
+
+begin
+  s := "1234567890";
+  c := s(0);
+end Tst_Str;
