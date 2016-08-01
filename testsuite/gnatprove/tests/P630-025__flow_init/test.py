9fd7418fed7071886b20148bc8754fc91769e0bd
Yannick Moy <moy@adacore.com>
2016-08-01 17:16:57 +0200

    P712-008, P801-019 Test passes now

--- testsuite/gnatprove/tests/P630-025__flow_init/test.py
+++ testsuite/gnatprove/tests/P630-025__flow_init/test.py
@@ -1,5 +1,3 @@
 from test_support import *
 
-# Justification for flow_only: this test would otherwise take 1.5 minutes
-# (instead of 8 seconds).
-do_flow_only()
+prove_all(opt=["-u", "decadriver.adb"])
