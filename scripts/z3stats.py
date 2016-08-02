f771347a9b04ea616633746bce81649d243276ec
Johannes Kanig <kanig@adacore.com>
2016-08-02 16:43:04 +0900

    minor improve prover stats script

--- scripts/z3stats.py
+++ scripts/z3stats.py
@@ -16,6 +16,73 @@ descr = """Run provers on files and return results in JSON format. Currently
 only z3 supported (hence the name)"""
 
 
+class Prover:
+    status_reg = re.compile("unsat|sat|unknown|timeout")
+
+    @staticmethod
+    def regex_get(regex, text, group=None):
+        m = regex.search(text)
+        if m is None:
+            print regex.pattern
+            print "regex not found, output was:"
+            print text
+            return "0"
+        if group is None:
+            return m.group()
+        else:
+            return m.group(group)
+
+
+class Z3(Prover):
+
+    limit_reg = re.compile("rlimit-count *(\d*)")
+    time_reg = re.compile(":time *(\d*.\d*)")
+
+    def command(self):
+        result = ["z3", "-st"]
+        if args.timeout:
+            result.append("-T:" + str(args.timeout))
+        return result
+
+    def parse_output(self, output, fn):
+        """ run on single file and extract statistics"""
+        try:
+            return\
+                {"filename": fn,
+                 "status": Prover.regex_get(Prover.status_reg, output),
+                 "steps": int(Prover.regex_get(self.limit_reg, output, 1)),
+                 "time": float(Prover.regex_get(self.time_reg, output, 1))}
+        except ValueError:
+            return {"filename": fn, "status": "error", "output": output}
+
+
+class CVC4(Prover):
+    limit_reg = re.compile("smt::SmtEngine::resourceUnitsUsed, (\d+.?\d*)")
+    time_reg = re.compile("driver::totalTime, *(\d*.\d*)")
+
+    def command(self):
+        result = ["cvc4",
+                  "--stats",
+                  "--quiet"]
+        if args.timeout:
+            result.append("--tlimit=" + str(args.timeout * 1000))
+        return result
+
+    def parse_output(self, output, fn):
+        """ run on single file and extract statistics"""
+        try:
+            status = Prover.regex_get(Prover.status_reg, output)
+            steps = int(Prover.regex_get(self.limit_reg, output, 1))
+            time = float(Prover.regex_get(self.time_reg, output, 1))
+            return\
+                {"filename": fn,
+                 "status": status,
+                 "steps": steps,
+                 "time": time}
+        except ValueError:
+            return {"filename": fn, "status": "error", "output": output}
+
+
 def parse_arguments():
     global args
     parser = argparse.ArgumentParser(description=descr)
@@ -35,14 +102,13 @@ def parse_arguments():
     parser.add_argument('--limit', dest='limit', type=int, action='store',
                         default=None, metavar='N',
                         help='randomly select N files from all files')
+    parser.add_argument('--prover', dest='prover', action='store',
+                        default="z3",
+                        help='prover to be used [z3]')
     args = parser.parse_args()
-
-
-def z3_cmd():
-    result = ["z3", "-st"]
-    if args.timeout:
-        result.append("-T:" + str(args.timeout))
-    return result
+    if args.prover != "z3" and args.prover != "cvc4":
+        print "prover " + args.prover + " not supported, exiting."
+        exit(1)
 
 
 def compute_file_list():
@@ -55,35 +121,6 @@ def compute_file_list():
     if args.limit and args.limit < len(result):
         result = random.sample(result, args.limit)
     return result
-
-
-status_reg = re.compile("unsat|sat|unknown|timeout")
-limit_reg = re.compile("rlimit-count *(\d*)")
-time_reg = re.compile(":time *(\d*.\d*)")
-
-
-def regex_get(regex, text, group=None):
-    m = regex.search(text)
-    if m is None:
-        print "regex not found, output was:"
-        print text
-        return "0"
-    if group is None:
-        return m.group()
-    else:
-        return m.group(group)
-
-
-def parse_z3_output(output, fn):
-    """ run on single file and extract statistics"""
-    try:
-        return\
-            {"filename": fn,
-             "status": regex_get(status_reg, output),
-             "steps": int(regex_get(limit_reg, output, 1)),
-             "time": float(regex_get(time_reg, output, 1))}
-    except ValueError:
-        return {"filename": fn, "status": "error", "output": output}
 
 
 def start_server(fname):
@@ -117,14 +154,15 @@ def read_request(fd):
     results = []
     for s in l:
         s = s.strip('\n')
-        l = s.split(';')
-        my_id = l[0]
-        fn = l[-1]
-        results.append((int(my_id), fn))
+        if s.startswith("F"):
+            l = s.split(';')
+            my_id = l[1]
+            fn = l[-1]
+            results.append((int(my_id), fn))
     return results
 
 
-def get_all_results(cmd, output_parser, fnlist, max_running_processes=1):
+def get_all_results(prover, fnlist, max_running_processes=1):
     """This function will, for each element fn of the string list [fnlist],
        append fn to the string list [cmd], run the result as a command,
        extract its output to stdout/stderr, and run the output_parser on the
@@ -136,7 +174,7 @@ def get_all_results(cmd, output_parser, fnlist, max_running_processes=1):
     file_map = {}
     fd = start_server("sock.sock")
     for fn in fnlist:
-        my_id = send_request(fd, cmd + [fn])
+        my_id = send_request(fd, prover.command() + [fn])
         file_map[my_id] = fn
     while len(file_map) > 0:
         if args.verbose:
@@ -148,7 +186,7 @@ def get_all_results(cmd, output_parser, fnlist, max_running_processes=1):
             os.remove(out)
             cur_file = file_map[my_id]
             del file_map[my_id]
-            results.append(parse_z3_output(output, cur_file))
+            results.append(prover.parse_output(output, cur_file))
 
     return results
 
@@ -156,10 +194,15 @@ def get_all_results(cmd, output_parser, fnlist, max_running_processes=1):
 def main():
     parse_arguments()
     file_list = compute_file_list()
-    results = get_all_results(cmd=z3_cmd(),
-                              output_parser=parse_z3_output,
+    if args.prover == "z3":
+        prover = Z3()
+    else:
+        prover = CVC4()
+
+    results = get_all_results(prover=prover,
                               fnlist=file_list,
                               max_running_processes=args.parallel)
+
     s = json.dumps({"results": results}, indent=2)
     if args.output:
         with open(args.output, "w+") as f:
