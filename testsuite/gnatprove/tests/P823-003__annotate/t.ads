e28c4f95367cb5321f07438b39d9d2acd51e11e8
Johannes Kanig <kanig@adacore.com>
2016-08-29 11:24:21 +0900

    P823-003 file test
    Change-Id: I85332b55f7453ab0590535b032846b4ee27dfc42

---
+++ testsuite/gnatprove/tests/P823-003__annotate/t.ads
@@ -0,0 +1,17 @@
+package T is
+
+   type Stupid_Record is record
+      A : Integer;
+      B : Boolean;
+      C : Float;
+   end record;
+
+   type Stupid_Array is array (Integer range <>) of Stupid_Record;
+
+   type Foo is new Stupid_Array (0 .. Stupid_Record'Size / 8)
+      with Annotate => (GNATProve, False_Positive, "o<", "ow");
+   --pragma Annotate (GNATProve, False_Positive, "o<", "ow");
+
+   Dummy : constant Foo := (others => (0, True, 1.0));
+
+end T;
