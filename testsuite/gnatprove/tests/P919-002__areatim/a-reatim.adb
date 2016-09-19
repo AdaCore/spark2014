dd6e2813165ec0f71d2428c3499ef5aeb6271f37
Piotr Trojanek <piotr.trojanek@altran.com>
2016-09-19 10:47:26 +0100

    P919-002 minimize reproducer
    Change-Id: Ia983dd1c5a8efa5c2d69196d24b8f9ca9d7b0dad

--- testsuite/gnatprove/tests/P919-002__areatim/a-reatim.adb
+++
@@ -1,7 +0,0 @@
-package body Ada.Real_Time with
-  SPARK_Mode => Off
-is
-
-   procedure P is null;
-
-end Ada.Real_Time;
--- testsuite/gnatprove/tests/P919-002__areatim/a-reatim.ads
+++ testsuite/gnatprove/tests/P919-002__areatim/a-reatim.ads
@@ -1,21 +1,9 @@
 with System.OS_Interface;
-pragma Elaborate_All (System.OS_Interface);
 
 package Ada.Real_Time with
-  SPARK_Mode,
-  Abstract_State => (Clock_Time with Synchronous,
-                                     External => (Async_Readers,
-                                                  Async_Writers)),
-  Initializes    => Clock_Time
+  SPARK_Mode
 is
-   pragma Assert
-     (System.OS_Interface.Ticks_Per_Second >= 50_000,
-      "Ada RM D.8 (30) requires " &
-      "that Time_Unit shall be less than or equal to 20 microseconds");
 
-   procedure P;
-
-private
-   pragma SPARK_Mode (Off);
+   pragma Assert (System.OS_Interface.Ticks_Per_Second >= 1);
 
 end Ada.Real_Time;
