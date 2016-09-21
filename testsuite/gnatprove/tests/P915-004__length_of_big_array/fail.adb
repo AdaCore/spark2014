b5339f0280ac0863fec3f002abec4dd2ffaedbb8
Claire Dross <dross@adacore.com>
2016-09-21 17:47:16 +0200

    P921-026 update test output
    Change-Id: I83767ea12cfa6b04b7919be1eb9b8e635bae362e

--- testsuite/gnatprove/tests/P915-004__length_of_big_array/fail.adb
+++ testsuite/gnatprove/tests/P915-004__length_of_big_array/fail.adb
@@ -10,21 +10,43 @@ procedure Fail with SPARK_Mode is
    end Create;
 
    C : constant A := Create (My_Index'Last);
---        CC : constant A (0 .. My_Index'Last) := (others => False);
-begin
-   if C'Length > My_Index'Last then
-      Ada.Text_IO.Put_Line ("max");
-   elsif C'Length = My_Index'First then
-      Ada.Text_IO.Put_Line ("zero");
-   end if;
-   if C (My_Index'Last) and C (0) then
-      Ada.Text_IO.Put_Line ("???");
-   end if;
 
-   if C'Length > Long_Long_Integer'Last then
-      Ada.Text_IO.Put_Line ("max");
-   elsif C'Length = Long_Long_Integer (0) then
-      Ada.Text_IO.Put_Line ("zero");
-   end if;
-   pragma Assert (C'Length /= Long_Long_Integer (0)); --@ASSERT:FAIL
+   procedure P1 with Pre => C'First = 0 and C'Last = My_Index'Last
+   is
+   begin
+      if C'Length > My_Index'Last then --@RANGE_CHECK:FAIL
+         Ada.Text_IO.Put_Line ("max");
+      elsif C'Length = My_Index'First then
+         Ada.Text_IO.Put_Line ("zero");
+      end if;
+   end P1;
+
+   procedure P2 with Pre => C'First = 0 and C'Last = My_Index'Last
+   is
+   begin
+      if C (My_Index'Last) and C (0) then
+         Ada.Text_IO.Put_Line ("???");
+      end if;
+   end P2;
+
+   procedure P3 with Pre => C'First = 0 and C'Last = My_Index'Last
+   is
+   begin
+      if C'Length > Long_Long_Integer'Last then --@RANGE_CHECK:FAIL
+         Ada.Text_IO.Put_Line ("max");
+      elsif C'Length = Long_Long_Integer (0) then
+         Ada.Text_IO.Put_Line ("zero");
+      end if;
+   end P3;
+
+   procedure P4 with Pre => C'First = 0 and C'Last = My_Index'Last
+   is
+   begin
+      pragma Assert (C'Length /= Long_Long_Integer (0)); --@RANGE_CHECK:FAIL
+   end P4;
+begin
+   P1;
+   P2;
+   P3;
+   P4;
 end Fail;
--- testsuite/gnatprove/tests/P915-004__length_of_big_array/mod_array.ads
+++ testsuite/gnatprove/tests/P915-004__length_of_big_array/mod_array.ads
@@ -3,5 +3,5 @@ package Mod_Array with SPARK_mode is
 
    type A is array (My_Index range <>) of Boolean with Pack;
 
-   function My_Length (X : A) return My_Index is (X'Length);
+   function My_Length (X : A) return My_Index is (X'Length); --@RANGE_CHECK:FAIL
 end Mod_Array;
