6f3250654be1d343701b46feaae3f688476fcb68
Clément Fumex <clement.fumex@inria.fr>
2016-09-22 17:25:19 +0200

    P922-026 move test to xfail and add an expected failing range check
    Change-Id: Ia2520fdd1a410c0e5f6853df07588f1ab2c0fb36

--- testsuite/gnatprove/tests/KB04-017__float/p.adb
+++ testsuite/gnatprove/tests/KB04-017__float/p.adb
@@ -1,7 +1,7 @@
-procedure P is 
+procedure P is
    function Id (X : Float) return Float is (X);
    type T is new Float range Id(0.0) .. Id(1.0);
-   Y : T := 1.5;
+   Y : T := 1.5;  --  @RANGE_CHECK:FAIL
 begin
    null;
 end P;
