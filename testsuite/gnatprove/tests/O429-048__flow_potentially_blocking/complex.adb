3bd96bd1b6f00962bc3608f84f0650075f1691ec
Clément Fumex <clement.fumex@inria.fr>
2016-10-03 17:40:32 +0200

    PA03-024 modify testsuite
    + move O713-024__dimensions to XFAIL
    + comment out the use of the type Complex in
      O429-048__flow_potentially_blocking
    
    Change-Id: Ia3b88109c8bc2efc11f722c86864d9cba343db4b

--- testsuite/gnatprove/tests/O429-048__flow_potentially_blocking/complex.adb
+++ testsuite/gnatprove/tests/O429-048__flow_potentially_blocking/complex.adb
@@ -2,12 +2,15 @@ with Ada.Numerics.Complex_Types;
 with Ada.Text_IO.Complex_IO;
 
 procedure Complex is
-   X : Ada.Numerics.Complex_Types.Complex;
+   --  the type Complex, since it uses long_long_float, is not supported yet
 
-   package CIO is new Ada.Text_IO.Complex_IO (Ada.Numerics.Complex_Types);
+   --  X : Ada.Numerics.Complex_Types.Complex;
+
+   --  package CIO is new Ada.Text_IO.Complex_IO (Ada.Numerics.Complex_Types);
 
 begin
-   X.Re := 0.0;
-   X.Im := 0.0;
-   CIO.Put (X);
+   --  X.Re := 0.0;
+   --  X.Im := 0.0;
+   --  CIO.Put (X);
+   null;
 end;
