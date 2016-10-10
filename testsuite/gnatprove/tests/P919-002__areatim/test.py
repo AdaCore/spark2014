dc168bf1df0f9e3939a508fc199029a3635842ad
Johannes Kanig <kanig@adacore.com>
2016-10-10 09:48:48 +0900

    P919-002 fix test
    the only thing missing from this test was the ALI file of system.ads,
    now compiled
    
    Change-Id: I0b1ae67df0c1ab45b11d13fa6c31e7e368c8daad

--- testsuite/gnatprove/tests/P919-002__areatim/test.py
+++ testsuite/gnatprove/tests/P919-002__areatim/test.py
@@ -1,3 +1,7 @@
 from test_support import *
+import os.path
+
+gcc(os.path.join('adainclude', 'system.ads'),
+    opt=['-c', '-gnatg', '-o' + os.path.join('adalib', 'system.o')])
 
 prove_all(opt=["--RTS=."])
