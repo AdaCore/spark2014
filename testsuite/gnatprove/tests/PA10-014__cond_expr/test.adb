9025c10f537da0ca8b2a2a6e3a0f4f2a3af00ce1
Claire Dross <dross@adacore.com>
2016-10-12 09:44:46 +0200

    PA10-014 update expected output
    Change-Id: I6c1a00a301f1856ee9a555d5a38e5fa7e13dc3ff
    (cherry picked from commit 638f1a16b4e7b9268922575cc9ed14d3d906d06c)
    
    Former-commit-id: 9b21232ec02d456a1154ed61a90b404304ccfe09

--- testsuite/gnatprove/tests/PA10-014__cond_expr/test.adb
+++ testsuite/gnatprove/tests/PA10-014__cond_expr/test.adb
@@ -2,7 +2,7 @@ package body Test with SPARK_Mode is
 
    procedure Do_Smth (I : Boolean; Oha : out T) is
    begin
-      Oha := (if I then 0 else 16); --@RANGE_CHECK:FAIL
+      Oha := (if I then 0 else 16);
    end Do_Smth;
 
 end Test;
