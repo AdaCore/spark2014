b3b0d4e239ba2ef470746e9cd855580845fcd749
Yannick Moy <moy@adacore.com>
2016-10-21 17:48:30 +0200

    P201-069 Use CodePeer to prove more overflow checks on generated code
    Change-Id: I283fade35afbf2ba48daba99ba25efbf50e04d28

--- testsuite/gnatprove/tests/P201-069__simulink/test.py
+++ testsuite/gnatprove/tests/P201-069__simulink/test.py
@@ -1,4 +1,4 @@
 from test_support import *
 
 # No counter-examples as there are platform issues in biasdivide
-prove_all(opt=["--no-counterexample"],prover=["cvc4"])
+prove_all(opt=["--no-counterexample"], codepeer=True)
