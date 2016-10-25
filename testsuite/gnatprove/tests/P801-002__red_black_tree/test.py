fa54f419d8f0656f396db627c209eff1b23d6cd1
Claire Dross <dross@adacore.com>
2016-10-25 17:52:50 +0200

    P801-002 document how to reproduce red black tree example
    Change-Id: Ib3b651860a006ce0aab25e852d06c77493547ce9
    
    Former-commit-id: 52a7b3f65ea3eae227b2f9349e79043ba3f7d05f

--- testsuite/gnatprove/tests/P801-002__red_black_tree/test.py
+++ testsuite/gnatprove/tests/P801-002__red_black_tree/test.py
@@ -1,7 +1,12 @@
 from test_support import *
 
-# binary_trees is all proved except for the two not implemented subprograms
-# In search_trees, insert is proved
-# read_balck_trees is not proved yet
+# The example should be all proved now. To reproduce:
+#  - remove session files to avoid getting obsolete goals
+#  - launch GNATprove with --level=2 on everything (~15min with -j4)
+#  - launch GNATprove with --level=3 --timeout=30 on everything (~8min with -j4)
+#
+#  rm -r proof/sessions/
+#  gnatprove -P test.gpr --level=2 -j0
+#  gnatprove -P test.gpr --level=3 -j0 --timeout=30
 
 prove_all(opt=["--replay"])
