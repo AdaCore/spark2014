f00020180caf5bebf1a50c11b9fed82bb339e593
Piotr Trojanek <piotr.trojanek@altran.com>
2016-11-02 08:07:54 +0000

    PB02-004 add test
    This test is a bigger than necessary, i.e. p.adb and main.adb could be
    removed, but they might be useful to validate that the runtime checks indeed
    fail.
    
    * testsuite/gnatprove/tests/PB02-004__protected_component/
    Added.
    
    Change-Id: I71e8bac324e3415981f7add65627b2733a246e2b

---
+++ testsuite/gnatprove/tests/PB02-004__protected_component/p.ads
@@ -0,0 +1,19 @@
+package P is
+
+   function Zero return Natural is (0);
+
+   protected type PT (X : Positive) is
+   end;
+
+   type Rec is record
+      PO : PT (Zero);
+   end record with Volatile;
+
+   type Arr is array (Boolean) of PT (Zero) with Volatile;
+
+   --  Range checks already fail at type definitions,
+   --  even without object declarations below.
+   --R : Rec;
+   --A : Arr;
+
+end;
