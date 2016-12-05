224ec62860b2d4ed4cc1e0b2c1a7e131e7c2cda6
Claire Dross <dross@adacore.com>
2016-12-05 16:16:54 +0100

    PB02-004 improve test case
    Change-Id: I2742c2f3a15f5ca6afa6853fe7989528cc837124

--- testsuite/gnatprove/tests/PB02-004__inherited_sub/main.adb
+++ testsuite/gnatprove/tests/PB02-004__inherited_sub/main.adb
@@ -1,6 +1,6 @@
 with Rec;
 
-procedure Main is
+procedure Main with SPARK_Mode is
 
    function Build_Rec (A : Rec.Enum) return Rec.Sub_Rec
       with Pre => True;
@@ -11,7 +11,27 @@ procedure Main is
       return Z;
    end Build_Rec;
 
+   function Build_Rec_OK (A : Rec.Enum) return Rec.Sub_Rec_OK
+      with Pre => True;
+
+   function Build_Rec_OK (A : Rec.Enum) return Rec.Sub_Rec_OK is
+      Z : Rec.Sub_Rec_OK (A);
+   begin
+      return Z;
+   end Build_Rec_OK;
+
+   function Build_Arr (F, L : Natural) return Rec.Sub_Arr
+      with Pre => True;
+
+   function Build_Arr (F, L : Natural) return Rec.Sub_Arr is
+      Z : Rec.Sub_Arr (F, L);
+   begin
+      return Z;
+   end Build_Arr;
+
    X : Rec.Sub_Rec := Build_Rec (Rec.C);
+   Y : Rec.Sub_Rec_OK := Build_Rec_OK (Rec.C);
+   F : Rec.Sub_Arr := Build_Arr (1, 2);
 begin
    null;
 end Main;
