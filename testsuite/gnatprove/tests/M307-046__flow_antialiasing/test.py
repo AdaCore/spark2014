41a0258c9cb0a5315e77c83ca86792df837b803f
Yannick Moy <moy@adacore.com>
2016-12-05 15:25:18 +0100

    O303-015 Run aliasing tests in flow only mode
    A high check is issued currently by flow analysis. Proof may crash on such
    code, to be fixed under O303-15, but no need to keep tests crashing.
    
    Change-Id: I1f6d66edc4d3e8866d9292c323a90d6d91e32573

--- testsuite/gnatprove/tests/M307-046__flow_antialiasing/test.py
+++ testsuite/gnatprove/tests/M307-046__flow_antialiasing/test.py
@@ -1,3 +1,3 @@
 from test_support import *
-
-do_flow()
+# proof crashes on possible aliased parameters (O303-015)
+do_flow_only()
--- testsuite/gnatprove/tests/O219-034__flow_aa_graph_consistency/test.py
+++ testsuite/gnatprove/tests/O219-034__flow_aa_graph_consistency/test.py
@@ -1,3 +1,3 @@
 from test_support import *
 # proof crashes on possible aliased parameters (O303-015)
-do_flow()
+do_flow_only()
