a78286ed1197e8e8dbf808e186ecbfbae2e52837
Johannes Kanig <kanig@adacore.com>
2017-01-04 10:00:51 +0900

    (no-tn-check) fix spark codepeer wrapper copyright notice
    Change-Id: I4a9d352e1c1028990b75335cf0ba26d1d7eb1378

--- gnatprove/spark_codepeer_wrapper.adb
+++ gnatprove/spark_codepeer_wrapper.adb
@@ -1,20 +1,26 @@
 ------------------------------------------------------------------------------
---                              C O D E P E E R                             --
 --                                                                          --
---                     Copyright (C) 2008-2017, AdaCore                     --
+--                            GNATPROVE COMPONENTS                          --
 --                                                                          --
--- This is free software;  you can redistribute it  and/or modify it  under --
--- terms of the  GNU General Public License as published  by the Free Soft- --
--- ware  Foundation;  either version 3,  or (at your option) any later ver- --
--- sion.  This software is distributed in the hope  that it will be useful, --
--- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+--               S P A R K _ C O D E P E E R _ W R A P P E R                --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                       Copyright (C) 2008-2017, AdaCore                   --
+--                                                                          --
+-- gnatprove is  free  software;  you can redistribute it and/or  modify it --
+-- under terms of the  GNU General Public License as published  by the Free --
+-- Software  Foundation;  either version 3,  or (at your option)  any later --
+-- version.  gnatprove is distributed  in the hope that  it will be useful, --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHAN- --
 -- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
 -- License for  more details.  You should have  received  a copy of the GNU --
--- General  Public  License  distributed  with  this  software;   see  file --
--- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
--- of the license.                                                          --
+-- General Public License  distributed with  gnatprove;  see file COPYING3. --
+-- If not,  go to  http://www.gnu.org/licenses  for a complete  copy of the --
+-- license.                                                                 --
 --                                                                          --
--- The CodePeer technology was originally developed by SofCheck, Inc.       --
+-- gnatprove is maintained by AdaCore (http://www.adacore.com)              --
+--                                                                          --
 ------------------------------------------------------------------------------
 
 with Ada.Characters;          use Ada.Characters;
