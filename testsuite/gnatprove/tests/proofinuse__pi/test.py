2aa6a3f4abf7c112d2d82b3e9423eae147344cf0
Johannes Kanig <kanig@adacore.com>
2017-01-10 18:08:34 +0900

    minor accelerate test
    test was really too slow, so enabling just one version (the fast one
    with codepeer)
    Change-Id: If12e33f92183c70d972b872f5cf71f18b4b6c5b9

--- testsuite/gnatprove/tests/proofinuse__pi/test.py
+++ testsuite/gnatprove/tests/proofinuse__pi/test.py
@@ -1,15 +1,2 @@
 from test_support import *
-
-# prove with CodePeer
-print "------------"
-print "CodePeer run"
-print "------------"
-prove_all(codepeer=True, prover=["altergo"])
-
-# prove with provers
-print ""
-print "----------"
-print "Prover run"
-print "----------"
-clean()
-prove_all(prover=["z3"], steps=50000)
+prove_all(codepeer=True)
