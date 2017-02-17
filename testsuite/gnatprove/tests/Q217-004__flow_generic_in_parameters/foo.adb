7793b8debad59750cb690017949abad76837b3c1
Florian Schanda <florian.schanda@altran.com>
2017-02-17 10:25:39 +0000

    Q217-004 flow - generic in parameters
    File testcase. We currently reject the code (which is good), but at the
    wrong place for the wrong reason.
    
    Change-Id: Ib56746006caf4d9a89a1c259f2e1ce98b4c6213e

---
+++ testsuite/gnatprove/tests/Q217-004__flow_generic_in_parameters/foo.adb
@@ -0,0 +1,27 @@
+package body Foo is
+
+   generic
+      V : in Integer;
+   package G with Initializes => State is
+      State : Integer;
+      procedure Init (X : out Integer) with Global => null;
+   end G;
+
+   package body G is
+      procedure Init (X : out Integer) is
+      begin
+         X := V;
+      end Init;
+   begin
+      State := V;
+   end G;
+
+   O_Var     : Integer := 666;
+   O_Const   : constant Integer := 42;
+   O_Const_V : constant Integer := O_Var;
+
+   package Inst_1 is new G (V => O_Var);     --  Illegal
+   package Inst_2 is new G (V => O_Const);   --  Legal
+   package Inst_3 is new G (V => O_Const_V); --  Illegal
+
+end Foo;
