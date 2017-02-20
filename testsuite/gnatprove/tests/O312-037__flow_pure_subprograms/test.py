15d20d168746ab3b5efabef0767eb0fc9e779277
Piotr Trojanek <piotr.trojanek@altran.com>
2017-02-20 08:44:26 +0000

    remove trailing whitespace from test support and output files
    Trailing whitespace and Windows-style line endings are annoying when using
    non-Windows editors with configuration that automatically chops such a
    whitespace.
    
    This commit merely cleans the test support and output files up with:
    
      sed -i 's/[[:space:]]*$//' `find . -type f -name "test.out"
                                             -or -name "test.py"
                                             -or -name "test.adc"`
    
    (no-tn-check)
    
    Change-Id: I1f7644da1dc3d0433516b02494ad47e497f4927a

--- testsuite/gnatprove/tests/O312-037__flow_pure_subprograms/test.py
+++ testsuite/gnatprove/tests/O312-037__flow_pure_subprograms/test.py
@@ -1,8 +1,8 @@
 from test_support import *
 
 # "purity" specs are violated by these bodies
-do_flow(opt=["-u", "impure_package.adb"]) 
-do_flow(opt=["-u", "impure_procedure.adb"]) 
+do_flow(opt=["-u", "impure_package.adb"])
+do_flow(opt=["-u", "impure_procedure.adb"])
 
 # but here the purity should be assumed
-do_flow(opt=["-u", "call.adb"]) 
+do_flow(opt=["-u", "call.adb"])
--- testsuite/gnatprove/tests/P606-024__missing_global_constant/test.py
+++ testsuite/gnatprove/tests/P606-024__missing_global_constant/test.py
@@ -1,4 +1,4 @@
- 
+
 from test_support import *
 
 prove_all()
