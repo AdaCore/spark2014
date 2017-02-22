5b17c913db234c4e634afbb6988b1512c84e8c72
Johannes Kanig <kanig@adacore.com>
2017-02-23 01:36:33 +0900

    P413-026 file test for discovery install
    Change-Id: I4ea0d2906e41011ccbeeebdbaa01d3a217ebee38

---
+++ testsuite/gnatprove/tests/P413-026__discovery_install/test.py
@@ -0,0 +1,10 @@
+from test_support import *
+
+installpath = spark_install_path()
+
+print "main bin/ dir"
+ls(os.path.join(installpath, "bin"))
+print "libexec/spark/bin/ dir"
+ls(os.path.join(installpath, "libexec", "spark", "bin"))
+print "libexec/spark/share dir"
+ls(os.path.join(installpath, "libexec", "spark", "share"))
