357959ac5e4aae96afdb713870caedeac7513ee9
Elisa Barboni <elisa.barboni@altran.com>
2017-02-27 10:56:25 +0000

    Q123-035 Modify test to include same issue with Depends
    Change-Id: I72f85f10d5c55b99b48493737ef7fb240db98468

--- testsuite/gnatprove/tests/Q123-035__flow_null_subp_with_globals/arr.adb
+++ testsuite/gnatprove/tests/Q123-035__flow_null_subp_with_globals/arr.adb
@@ -8,17 +8,35 @@ package body Arr is
    Buffer_Len : Length_T;
    Buffer     : T;
 
-   procedure Initialize (L : Length_T)
+   --  We pretend to initialize Buffer through
+   --  Fake_Initialize_Buffer_Buffer_With_Global setting Buffer as a global
+   --  output but the procedure is a null procedure.
+   procedure Initialize_With_Global (L : Length_T)
      with Global => (Output => (Buffer_Len, Buffer))
    is
-      procedure Fake_Initialize_Buffer is null
+      procedure Fake_Initialize_Buffer_With_Global is null
         with Global => (Output => Buffer);
    begin
-      Fake_Initialize_Buffer;
+      Fake_Initialize_Buffer_With_Global;
       for I in Index_T range Index_T'First .. L loop
          Buffer (I) := Integer (I);
       end loop;
       Buffer_Len := L;
-   end Initialize;
+   end Initialize_With_Global;
+
+   --  Similar to the above, but with Depends (instead of Global) on the inner
+   --  subprogram.
+   procedure Initialize_With_Depends (L : Length_T)
+     with Global => (Output => (Buffer_Len, Buffer))
+   is
+      procedure Fake_Initialize_Buffer_With_Depends is null
+        with Depends => (Buffer => null);
+   begin
+      Fake_Initialize_Buffer_With_Depends;
+      for I in Index_T range Index_T'First .. L loop
+         Buffer (I) := Integer (I);
+      end loop;
+      Buffer_Len := L;
+   end Initialize_With_Depends;
 
 end Arr;
