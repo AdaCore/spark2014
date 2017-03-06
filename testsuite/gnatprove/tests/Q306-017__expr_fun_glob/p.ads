04af7d35509097f3ad2a97b0675750715abbc57d
Piotr Trojanek <piotr.trojanek@altran.com>
2017-03-06 15:15:41 +0000

    Q306-017 make test more explicit
    * testsuite/gnatprove/tests/Q306-017__expr_fun_glob/p.ads
    Comments added; also crashes on non-expression functions.
    
    Change-Id: I0f16ed8c95f2e6094b6fe8407fd1b5f6b279b67f

--- testsuite/gnatprove/tests/Q306-017__expr_fun_glob/p.ads
+++ testsuite/gnatprove/tests/Q306-017__expr_fun_glob/p.ads
@@ -1,9 +1,13 @@
 package P is
 
    X : Integer := 0;
+   --  X is referenced by the functions below, but not included in their
+   --  explicit Global contracts.
 
    function F return Boolean is (X = 0) with Global => null;
+   --  Reference to X in expression function body
 
-   function B return Boolean is (True) with Post => X = 0, Global => null;
+   function B return Boolean with Post => X = 0, Global => null;
+   --  Reference to X in Post expression
 
 end;
