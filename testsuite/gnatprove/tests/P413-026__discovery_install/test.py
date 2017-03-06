f7bfe9eb7387bc54d8a633f9c39de08ef9119c7f
Johannes Kanig <kanig@adacore.com>
2017-03-06 12:34:54 +0900

    P413-026 fix spark discovery install test on windows
    Change-Id: Ie2b7a99c79e9f732fbab3a98b113f262a63e6971

--- testsuite/gnatprove/tests/P413-026__discovery_install/test.py
+++ testsuite/gnatprove/tests/P413-026__discovery_install/test.py
@@ -5,6 +5,6 @@ installpath = spark_install_path()
 print "main bin/ dir"
 ls(os.path.join(installpath, "bin"))
 print "libexec/spark/bin/ dir"
-ls(os.path.join(installpath, "libexec", "spark", "bin"))
+ls(os.path.join(installpath, "libexec", "spark", "bin"), filter_output=".*dll")
 print "libexec/spark/share dir"
 ls(os.path.join(installpath, "libexec", "spark", "share"))
