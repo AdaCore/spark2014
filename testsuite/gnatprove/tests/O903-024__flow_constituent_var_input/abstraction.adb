6c790b81cac8ea394cc1e4ad03c3c2efe322157d
Elisa Barboni <elisa.barboni@altran.com>
2017-03-23 16:41:17 +0000

    O903-024 Add test
    The test output shows that GNATprove does complain about C being referenced
    in the Initializes aspect but does not when referenced in the
    Refined_State (even though it should).
    
    Change-Id: I89f9e8e3a1a60e0c3401f9115728635890f4aa1c

---
+++ testsuite/gnatprove/tests/O903-024__flow_constituent_var_input/abstraction.adb
@@ -0,0 +1,13 @@
+package body Abstraction with
+   SPARK_Mode,
+   Refined_State => ((State => C))
+is
+   C : constant Integer := 0;
+
+   procedure P is
+      A : Integer := 4;
+   begin
+      null;
+   end P;
+
+end Abstraction;
