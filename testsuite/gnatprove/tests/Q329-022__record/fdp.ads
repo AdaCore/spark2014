288298f1dcf8b9d626e610a729dfc9fbd0f3b388
Piotr Trojanek <piotr.trojanek@altran.com>
2017-03-29 13:16:31 +0100

    Q329-022 add minimized reproducer
    * testsuite/gnatprove/tests/Q329-022__record/
    Add; currently crashes in proof, so disabled.
    
    Change-Id: I0babf2a5d7e8ed1b79bd5a6220704a8a5e765655

---
+++ testsuite/gnatprove/tests/Q329-022__record/fdp.ads
@@ -0,0 +1,29 @@
+with FIS;
+
+package FDP is
+   type Kind is
+      (Undefined,
+       Flight_Data_Msg);
+
+   type T is private;
+
+   Undefined_Request : constant T;
+
+private
+
+   type T (The_Kind : Kind := Undefined) is
+      record
+         case The_Kind is
+
+            when Flight_Data_Msg =>
+               Flight_Data_Msg_Data : FIS.Data;
+
+            when others =>
+               null;
+
+         end case;
+      end record;
+
+      Undefined_Request : constant T := T'(The_Kind => Undefined);
+
+end FDP;
---
+++ testsuite/gnatprove/tests/Q329-022__record/fis.ads
@@ -0,0 +1,9 @@
+package FIS
+  with SPARK_Mode => Off
+is
+
+   type Data is record
+      Dummy : Boolean;
+   end record;
+
+end;
