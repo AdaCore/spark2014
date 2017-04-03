3e7d27b00cf7631f668bc8244dd775e6eab3ed45
Johannes Kanig <kanig@adacore.com>
2017-04-03 04:23:43 -0400

    (no-tn-check) update session of red_black_tree and improve session update
    * update red-black tree session
    * modify red_black_tree test.py so taht it contains a replay() routine
    
    update-session-test should now uses the replay() routine
    
    Change-Id: Iddcb3bcd1d54cf7d1c1d41449cc4994f60712dab

--- testsuite/gnatprove/tests/red_black_trees/test.py
+++ testsuite/gnatprove/tests/red_black_trees/test.py
@@ -1,12 +1,7 @@
 from test_support import *
 
-# The example should be all proved now. To reproduce:
-#  - remove session files to avoid getting obsolete goals
-#  - launch GNATprove with --level=2 on everything (~15min with -j4)
-#  - launch GNATprove with --level=3 --timeout=30 on everything (~8min with -j4)
-#
-#  rm -r proof/sessions/
-#  gnatprove -P test.gpr --level=2 -j0
-#  gnatprove -P test.gpr --level=3 -j0 --timeout=30
+def replay():
+    prove_all(procs=0, opt=["--level=2", "--no-axiom-guard"], steps=None)
+    prove_all(procs=0, opt=["--level=3", "--no-axiom-guard"], steps=None)
 
 prove_all(opt=["--replay","--no-axiom-guard"])
