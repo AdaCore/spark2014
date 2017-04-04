8edd35a02ab985ee297dde10b15c1929c597cdb5
Johannes Kanig <kanig@adacore.com>
2017-04-04 16:34:48 +0900

    remove incorrect comment in test
    since M410-035, SPARK looks into values of deferred constants
    
    Change-Id: I7342c1bddb47aef7736f4341f95c519d548896e1

--- testsuite/gnatprove/tests/L320-006__deferred/user.adb
+++ testsuite/gnatprove/tests/L320-006__deferred/user.adb
@@ -2,8 +2,8 @@ with Pack; use Pack;
 procedure User (B : Boolean) is
 begin
    if B then
-      pragma Assert (X1.X = True);  -- must remain unprovable
-      pragma Assert (X2 = 3);  -- should be unprovable
+      pragma Assert (X1.X = True);
+      pragma Assert (X2 = 3);
    else
       pragma Assert (Query_X1 = True);
       pragma Assert (Query_X2 = 3);
