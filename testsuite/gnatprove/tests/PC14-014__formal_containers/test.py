116b7e228722c7a4e256cd4190e39443ec7023b6
Johannes Kanig <kanig@adacore.com>
2017-04-06 17:30:51 +0900

    minor update test
    one lost, one gained, likely some combination of float literal changes
    and axiom guard changes
    
    Change-Id: I09663e23057ab54cd3ea52ffea1490cffde1d4be

--- testsuite/gnatprove/tests/PC14-014__formal_containers/test.py
+++ testsuite/gnatprove/tests/PC14-014__formal_containers/test.py
@@ -1,7 +1,7 @@
 from subprocess import call
 from test_support import *
 
-prove_all(steps=300, prover=["z3"])
+prove_all(steps=300, prover=["z3"], opt=["--no-axiom-guard"])
 
 call(["gprbuild", "-q", "-P", "test.gpr"])
 call(["./run_tests"])
