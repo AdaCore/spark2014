8c165f301565aabcfcd7d2333301fe0a0492ac87
Johannes Kanig <kanig@adacore.com>
2017-04-06 05:37:30 -0400

    minor yet another session update for red black trees
    session was replayed with smaller timeout, so hopefully replay is faster now
    
    Change-Id: Ib1404c9566bdf3aee710f4d2e2a4f8692e08f2c5

--- testsuite/gnatprove/tests/red_black_trees/test.py
+++ testsuite/gnatprove/tests/red_black_trees/test.py
@@ -1,7 +1,7 @@
 from test_support import *
 
 def replay():
-    prove_all(procs=0, opt=["--level=2", "--no-axiom-guard"], steps=None)
-    prove_all(procs=0, opt=["--level=3", "--no-axiom-guard"], steps=None)
+    prove_all(procs=0, opt=["--level=2", "--no-axiom-guard"], steps=None, vc_timeout=20)
+    prove_all(procs=0, opt=["--level=3", "--no-axiom-guard"], steps=None, vc_timeout=50)
 
 prove_all(opt=["--replay","--no-axiom-guard"])
