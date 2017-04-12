e19b253d3174c5885f5904679d74e52bc36f9375
Sylvain Dailler <dailler@adacore.com>
2017-04-12 15:21:16 +0200

    Q407-003 Update lemma library
    This commits restore the generate_session.py script to allow generation
    and checking of coq proofs. It gives an option to the script to allow
    automatic generation of .ctx files from gnatprove .v outputs. Also,
    generates diff in a temporary folder between .ctx and generated .v files.
    This script is used to update all lemmas and restore their proved state.
    Adding a sed before cpp because cpp does not understand the following
    sequence of characters: slash space end_of_line.
    
    include/generate_session.py
    (copy_file): Replace a .ctx file using the corresponding generated .v file
    (diff_file): Make a diff between two files and generate a temp file
    for it.
    (diff_all): Make diff between all .ctx and .v files.
    (kill_and_regenerate_all): Force the user to move proof/sessions and
    temp/. Readding mechanism to generate and check coq proofs.
    
    include/Makefile
    (%.v): Adding a sed to remove backslash space end of line which
    are transformed to empty string by cpp.
    
    include/proof/sessions/*/*.xml
    (): Update of sessions.
    
    include/proof/Coq/*/*.ctx
    (): Automatic update of context files using why3 generated .v.
    
    include/proof/Coq/common/*.prf
    (proofs): Updating Coq proofs according to new context files. Mainly
    renaming of variables and coping with auto that is less strong than before
    because hypothesis now are of the form A and B instead of A B.
    What was required was to destruct these hypotheses with right arguments.
    
    Change-Id: I74dac1690b4c675e880501152d910a22a938d5b0

--- include/generate_session.py
+++ include/generate_session.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import difflib
+import sys
 
 
 def exec_gnatprove(file_to_prove, option=""):
@@ -27,10 +29,12 @@ def replace_spark_mode(fname, b):
         with open(fname) as f:
             for line in f:
                 if b:
-                    new.write(line.replace("SPARK_Mode => On -- TEST_ON", "SPARK\
+                    new.write(
+                        line.replace("SPARK_Mode => On -- TEST_ON", "SPARK\
 _Mode => Off -- TEST_ON"))
                 else:
-                    new.write(line.replace("SPARK_Mode => Off -- TEST_ON", "SPARK\
+                    new.write(
+                        line.replace("SPARK_Mode => Off -- TEST_ON", "SPARK\
 _Mode => On -- TEST_ON"))
     os.remove(fname)
     shutil.move(temp, fname)
@@ -45,11 +49,93 @@ def change_all_spark_mode(b):
             replace_spark_mode(files, b)
 
 
+def copy_file(f_ctx, f_v):
+    # This creates a new file which is the new .ctx file.
+    # Behavior: Before the intros tactic we append only f_v; after the intros
+    # or '#' character, we append only f_ctx.
+    temp = f_ctx + "___tmp.tmp"
+    b = False
+    with open(temp, 'w') as new_temp:
+        with open(f_v) as file_v:
+            for line in file_v:
+                if line[:6] == "intros":
+                    break
+                else:
+                    new_temp.write(line)
+        with open(f_ctx) as file_ctx:
+            for line in file_ctx:
+                if line[:6] == "intros" or line[:1] == "#":
+                    b = True
+                if b:
+                    new_temp.write(line)
+#   Replace context file with the temp file
+    os.remove(f_ctx)
+    shutil.move(temp, f_ctx)
+
+
+def diff_file(f_ctx, g_v):
+    # This function makes a diff without spaces between files f and g. If there
+    # is a line which is in the first file and not in the second one, create a
+    # temp diff file of same name in folder temp, otherwise do not create temp
+    # file.
+    # We assume f ends with .ctx and g ends with .v. Basically, don't use this
+    # function anywhere else.
+    diff_seen = False
+    with open(f_ctx, 'r') as file1:
+        with open(g_v, 'r') as file2:
+            # Removing spaces because cpp introduce extra spaces as diff.
+            lines1 = map(lambda y:
+                         filter(lambda x:
+                                x not in ' \t', y), file1.readlines())
+            lines2 = map(lambda y:
+                         filter(lambda x:
+                                x not in ' \t', y), file2.readlines())
+            diff = difflib.unified_diff(
+                lines1,
+                lines2,
+                fromfile=f_ctx,
+                tofile=g_v, n=1)
+            for line in diff:
+                if not line[0] == '-':
+                    diff_seen = True
+                    break
+    if diff_seen:
+        temp_file = os.path.basename(f_ctx)[:-4] + ".diff"
+        temp_path = os.path.join("./temp", temp_file)
+        with open(temp_path, 'a') as new:
+            for line in diff:
+                new.write(line)
+
+
+def diff_all(gen_ctx):
+    # If files have changed due to changes in spark/why3 generates files,
+    # diff will be printed in temp.
+    # For technical reasons, diff are printed without spaces
+    # Do not use this function in an other context. It is used only once
+    # with gen_ctx to False meaning files are not erased and replaced with
+    # new ones. To replace them, pass True.
+    for root, dirs, files in os.walk('./proof'):
+        for name in files:
+            if name.endswith(".v"):
+                ctx_file = os.path.join(root, name[:-2] + ".ctx")
+                v_file = os.path.join(root, name)
+                diff_file(ctx_file, v_file)
+                if gen_ctx:
+                    copy_file(ctx_file, v_file)
+
+
 def kill_and_regenerate_all():
     change_all_spark_mode(False)
-#   cleaning and regeneration of *.v
+    if os.path.isdir("./proof/sessions"):
+        print("The folder proof/sessions still exists.")
+        print("Please, move it to be able to regenerate session.")
+        exit(1)
+    if os.path.isdir("./temp"):
+        print("The folder temp will be used. Please move it.")
+        exit(1)
+    else:
+        os.makedirs("./temp")
     os.system("make clean")
-    os.system("make generate")
 #   Force regeneration of coq files where necessary.
 #   This step is used to generate the fake coq files and put the names of
 #   coq files inside the session. This cannot be done in one step because
@@ -57,6 +143,20 @@ def kill_and_regenerate_all():
 #   check the present coq files).
     with open("manual_proof.in") as f:
         for i in f:
+            exec_gnatprove(i)
+#   Make the diff between generated .v and .ctx files. If there are differences
+#   between them not in the proof, you are sure to fail
+    diff_all(False)
+#   cleaning and regeneration of *.v
+    os.system("make clean")
+    os.system("make generate")
+    os.system("gnatprove -P spark_lemmas.gpr --prover=cvc4 \
+--level=1 --no-counterexample -j0")
+#   Do *not* remove this call as it is used to check that coq proofs are
+#   correct after regeneration. And ability to generate session is *necessary*
+#   as there is no way to extend a session in gnatprove.
+    with open("manual_proof.in") as v:
+        for i in v:
             exec_gnatprove(i)
     os.system("gnatprove -P spark_lemmas.gpr --prover=cvc4 \
 --level=4 --no-counterexample -j0")
