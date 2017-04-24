642b8d8707b985ca1616373bbd438a5be0d8ade4
Piotr Trojanek <piotr.trojanek@altran.com>
2017-04-24 10:01:19 +0100

    Q420-019 dependencies for non-ghost abstract state
    A ghost procedure can modify a non-ghost abstract state, because such an
    abstract state might contain ghost constituents. (Unlike records, where
    either all or none components are ghost, abstract states might mix ghost and
    non-ghost constituents).
    
    This affects the synthesized Depends contract.
    
    * gnat2why/flow/flow-interprocedural.adb
    (Add_Simple_Procedure_Dependency): relax condition for Dependency_Allowed.
    
    * testsuite/gnatprove/tests/Q420-019__ghost_depend_0/test.out
    Remove warning from test output.
    
    Change-Id: I99aa0c4cc0c54f89c8f00d4929355229efefeadb

--- gnat2why/flow/flow-interprocedural.adb
+++ gnat2why/flow/flow-interprocedural.adb
@@ -326,11 +326,16 @@ package body Flow.Interprocedural is
 
                            Dependency_Allowed : constant Boolean :=
                              Output_Is_Proof
-                             or else (not Ghost_Subprogram
-                                      and then not Input_Is_Proof);
+                               or else
+                             Is_Abstract_State (Output)
+                               or else
+                             (not Ghost_Subprogram
+                              and then not Input_Is_Proof);
                            --  Ghost outputs can always be modified; non-ghost
-                           --  outputs can only be modified by non-ghost
-                           --  subprograms using non-ghost inputs.
+                           --  abstract states too, because they might contain
+                           --  ghost constituents; non-ghost outputs can only
+                           --  be modified by non-ghost subprograms using
+                           --  non-ghost inputs.
 
                         begin
                            if Dependency_Allowed then
