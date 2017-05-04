28f13c3486816fea909ce90e0328748a6dee5408
Sylvain Dailler <dailler@adacore.com>
2017-05-04 14:16:27 +0200

    minor Test branch.
    Change-Id: Iffcc6f2ffb0eeefc84ff96240b2d937a73c3df44

--- gnat2why/why/gnat2why-util.adb
+++ gnat2why/why/gnat2why-util.adb
@@ -208,7 +208,7 @@ package body Gnat2Why.Util is
 
                --  If there was already an entry for the name, we need to
                --  store in the undo stack the fact that this info must be
-               --  reinserted
+               --  reinserted.
 
                M.Undo_Stack.Append
                  (Action'(Kind => Insert_Name,
