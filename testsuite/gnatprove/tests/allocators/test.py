2eb453f840bdfc7d055adf9e2cfd2de77572e46b
Claire Dross <dross@adacore.com>
2017-05-05 18:10:26 +0200

    minor bump step limit on allocators
    Change-Id: I1d9d1e961dd4c07f459e9113f3f768e4e7f7687a

--- testsuite/gnatprove/tests/allocators/test.py
+++ testsuite/gnatprove/tests/allocators/test.py
@@ -1,10 +1,10 @@
 from test_support import *
 
-prove_all(level=4,
-          steps=None,
+prove_all(steps=9000,
           prover=["cvc4", "z3"],
           no_fail=True,
-          opt=["-u",
+          opt=["--no-axiom-guard",
+               "-u",
                "simple_allocator.adb",
                "list_allocator.adb",
                "list_mod_allocator.adb"])
