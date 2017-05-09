dcda2976430b0cf10e6eea2de8df71e826e44cb9
Johannes Kanig <kanig@adacore.com>
2017-05-09 17:49:00 +0900

    minor update session for tokeneer
    Change-Id: Ib49f864dd6b1fdce766420663b772c6ace478a4b

--- testsuite/gnatprove/tests/tokeneer/test.py
+++ testsuite/gnatprove/tests/tokeneer/test.py
@@ -1,5 +1,9 @@
 import glob
 from test_support import *
+
+def replay():
+    prove_all(procs=0, opt=["--level=2", "--no-axiom-guard"], steps=None, vc_timeout=20)
+
 prove_all(opt=["--replay", "--no-axiom-guard"], procs=4)
 
 # testing the proof information in .spark files. See <gnatprove/spark_report>
@@ -10,5 +14,6 @@ for fn in glob.glob("gnatprove/*.spark"):
         d = json.load(f)
         for r in d['proof']:
             count += len(r['check_tree'])
-assert (count > 1000)
+if __name__ == "__main__":
+    assert (count > 1000)
 
