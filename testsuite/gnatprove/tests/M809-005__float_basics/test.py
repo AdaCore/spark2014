121a6e02a5ff10f2ad46c83dccb3e4a9ce03713a
Johannes Kanig <kanig@adacore.com>
2017-05-24 16:51:21 +0900

    Q520-002 test output update
    Change-Id: I8c265e3054631200cedf7b55cdfb421e8013445d

--- testsuite/gnatprove/tests/M809-005__float_basics/test.py
+++ testsuite/gnatprove/tests/M809-005__float_basics/test.py
@@ -1,2 +1,6 @@
 from test_support import *
+
+def replay():
+    prove_all(procs=10, counterexample=False)
+
 prove_all(opt=["--replay"])
--- testsuite/gnatprove/tests/P512-044__exist_on_bv/test.py
+++ testsuite/gnatprove/tests/P512-044__exist_on_bv/test.py
@@ -1,3 +1,3 @@
 from test_support import *
 
-prove_all(prover=["cvc4", "altergo"],steps=222)
+prove_all(prover=["cvc4", "altergo"],steps=300)
