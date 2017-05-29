fbae56e1cca0077e47d912f29722fe4b646b8293
Yannick Moy <moy@adacore.com>
2017-05-29 23:02:08 +0200

    Q529-038 Add test
    Change-Id: Ida0475a45301a265b1824e02d82fbcd946f6628c

---
+++ testsuite/gnatprove/tests/Q529-038__predicate/bounded_stacks.adb
@@ -0,0 +1,79 @@
+package body Bounded_Stacks is
+
+   ------------
+   -- Extent --
+   ------------
+
+   function Extent (This : Stack) return Natural is
+     (This.Top);
+
+   -----------
+   -- Empty --
+   -----------
+
+   function Empty (This : Stack) return Boolean is
+     (This.Top = 0);
+
+   ----------
+   -- Full --
+   ----------
+
+   function Full (This : Stack) return Boolean is
+     (This.Top = This.Capacity);
+
+   -----------
+   -- Reset --
+   -----------
+
+   procedure Reset (This : out Stack) is
+   begin
+      This := (This.Capacity, Top => 0, others => <>);
+--        This.Top := 0;
+   end Reset;
+
+   ----------
+   -- Push --
+   ----------
+
+   procedure Push (This : in out Stack;  Item : Element) is
+   begin
+      This.Top := This.Top + 1;
+      This.Values (This.Top) := Item;
+   end Push;
+
+   ---------
+   -- Pop --
+   ---------
+
+   procedure Pop (This : in out Stack;  Item : out Element) is
+   begin
+      Item := This.Values (This.Top);
+      This.Top := This.Top - 1;
+   end Pop;
+
+   ----------
+   -- Peek --
+   ----------
+
+   function Peek (This : Stack) return Element is
+     (This.Values (This.Top));
+
+   ---------
+   -- "=" --
+   ---------
+
+   function "=" (Left, Right : Stack) return Boolean is
+   begin
+      if Left.Top /= Right.Top then
+         return False;
+      else
+         for K in 1 .. Left.Top loop
+            if Left.Values (K) /= Right.Values (K) then
+               return False;
+            end if;
+         end loop;
+         return True;
+      end if;
+   end "=";
+
+end Bounded_Stacks;
---
+++ testsuite/gnatprove/tests/Q529-038__predicate/bounded_stacks.ads
@@ -0,0 +1,57 @@
+generic
+   type Element is private;
+   Default_Element : Element;
+package Bounded_Stacks is
+
+   type Stack (Capacity : Positive) is tagged private
+      with Default_Initial_Condition => Empty (Stack);
+
+   function "=" (Left, Right : Stack) return Boolean;
+
+   function Extent (This : Stack) return Natural;
+
+   function Empty (This : Stack) return Boolean;
+
+   function Full (This : Stack) return Boolean;
+
+   procedure Reset (This : out Stack) with
+     Post'Class => Empty (This) and
+                   not Full (This),
+     Global     => null,
+     Depends    => (This =>+ null);
+
+   procedure Push (This : in out Stack;  Item : Element) with
+     Pre'Class  => not Full (This) and
+                   Extent (This) < This.Capacity,
+     Post'Class => not Empty (This) and
+                   Extent (This) = Extent (This'Old) + 1,
+     Global     => null,
+     Depends    => (This =>+ Item);
+
+   procedure Pop (This : in out Stack;  Item : out Element) with
+     Pre'Class  => not Empty (This),
+     Post'Class => not Full (This) and
+                   Extent (This) = Extent (This'Old) - 1,
+     Global     => null,
+     Depends    => (This =>+ null, Item => This);
+
+   function Peek (This : Stack) return Element with
+     Pre'Class => not Empty (This),
+     Global    => null,
+     Depends   => (Peek'Result => This);
+
+private
+
+   type Contents is array (Positive range <>) of Element;
+
+   type Stack (Capacity : Positive) is tagged record
+      Values : Contents (1 .. Capacity); -- := (others => Default_Element);
+--        Top    : Natural;
+      Top    : Natural := 0;
+   end record with Predicate => Top <= Capacity,
+     Annotate => (GNATprove,
+                  Intentional,
+                  "type ""Stack"" is not fully initialized",
+                  "Because zeroing Top is sufficient");
+
+end Bounded_Stacks;
