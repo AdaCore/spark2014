9826cbcd0f724add062126a20e1cd91d5bcf8ff1
Johannes Kanig <kanig@adacore.com>
2017-05-29 05:19:09 -0400

    minor session and testsuite updates
    Change-Id: I740056899e3dcce8a0fd257f9d6c9f60a78e2466

--- testsuite/gnatprove/tests/M809-005__float_basics/test.py
+++ testsuite/gnatprove/tests/M809-005__float_basics/test.py
@@ -1,6 +1,6 @@
 from test_support import *
 
 def replay():
-    prove_all(procs=10, counterexample=False)
+    prove_all(procs=10, counterexample=False, prover=["z3","cvc4","altergo"])
 
 prove_all(opt=["--replay"])
