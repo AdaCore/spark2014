5a23df043e5bd6d7b9a72a45409a9797e616e195
Sylvain Dailler <dailler@adacore.com>
2017-05-31 12:47:00 +0200

    Q313-019 Added traces to tests on float
    Adding traces to tests on floats.
    
    tests/Q313-019_elementary_ce_float/*:
    (test) Added traces.
    
    Change-Id: If254581cc6347dea3cdfd1017b04a9d31c6dd66e

--- testsuite/gnatprove/tests/Q313-019_elementary_ce_float/float_example.adb
+++ testsuite/gnatprove/tests/Q313-019_elementary_ce_float/float_example.adb
@@ -5,18 +5,20 @@ is
    type Float_7 is digits 7;
 
    function Minus_I (A, B: Float_7) return Float_7
-     with Post => Minus_I'Result = A + B  -- Should be '-' instead of '+'
+     with Post => Minus_I'Result = A + B --  @COUNTEREXAMPLE
+     -- Should be '-' instead of '+'
    is
    begin
-      return A - B;
+      return A - B;  --  @COUNTEREXAMPLE
    end Minus_I;
 
    function Bounded_Add (A, B : Float_7) return Float_7
      with Pre => A < 4.0 and then B < 4.0,
-     Post => Bounded_Add'Result < 7.0 -- Should be 8.0 instead of 7.0
+     Post => Bounded_Add'Result < 7.0 --  @COUNTEREXAMPLE
+     -- Should be 8.0 instead of 7.0
    is
    begin
-      return A + B;
+      return A + B;  --  @COUNTEREXAMPLE
    end Bounded_Add;
 
 end Float_Example;
