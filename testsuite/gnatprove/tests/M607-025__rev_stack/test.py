2eb2a79ac84f0eeed8651873d15db67368195f85
Piotr Trojanek <piotr.trojanek@altran.com>
2017-06-07 11:17:40 +0100

    M607-025 tune ordering of provers
    With the default ordering of provers on this test (*):
    1) alt-ergo runs for  ~90sec/8GB and discharges no VCs
    2) cvc4     runs for ~150sec/1GB and discharges most of the VCs
    3) z3       almost immediately discharges few remaining VCs
    
    Performance data from a pretty powerful desktop; on a less powerful (but
    still decent) laptop it causes swapping and makes the OS generally
    unresponsive.
    
    It seems better to run z3 first, as it discharges all the VC in a several
    seconds and needs no more than 1MB of RAM.
    
    * testsuite/gnatprove/tests/M607-025__rev_stack/test.py
    Change ordering of provers.
    
    * testsuite/gnatprove/tests/M607-025__rev_stack/test.out
    Update output: all VCs are now proved by Z3.
    
    Change-Id: I7b207021ac062734009045fac0c407298acd3869

--- testsuite/gnatprove/tests/M607-025__rev_stack/test.py
+++ testsuite/gnatprove/tests/M607-025__rev_stack/test.py
@@ -1,2 +1,3 @@
 from test_support import *
-prove_all(steps=None, level=4, no_fail=True)
+prove_all(steps=None, level=4, no_fail=True, prover=["z3", "cvc4", "alt-ergo"])
+# Alt-ergo consumes gigabytes of memory, but doesn't prove any VC here
