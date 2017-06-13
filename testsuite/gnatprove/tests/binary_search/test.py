ef97b41897085f2f39c90ae8d88c452ec4710207
Johannes Kanig <kanig@adacore.com>
2017-06-13 09:51:51 +0900

    Q524-002 more tests for discovery testing
    Change-Id: Ie8ea2b7d6fd2829410925b309aea0b220e90cdbe

--- testsuite/gnatprove/tests/binary_search/test.py
+++ testsuite/gnatprove/tests/binary_search/test.py
@@ -1,2 +1,3 @@
 from test_support import *
-prove_all(no_fail=True, opt=["-u", "binary_search.adb"])
+prove_all(no_fail=True, opt=["-u", "binary_search.adb"], prover=["altergo"],
+        steps=2000)
