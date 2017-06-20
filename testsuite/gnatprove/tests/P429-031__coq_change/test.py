73c38035709f1bb6047e54b80f90e1361d29e3c1
Johannes Kanig <kanig@adacore.com>
2017-06-20 18:18:50 +0900

    (no-tn-check) add comments to test
    Change-Id: Id3dedeee3b83eec82b49f2decad01f0ed59bfb0b

--- testsuite/gnatprove/tests/P429-031__coq_change/test.py
+++ testsuite/gnatprove/tests/P429-031__coq_change/test.py
@@ -2,6 +2,9 @@ from shutil import copyfile
 from test_support import *
 import glob
 
+# This test is supposed to test whether a Coq proof can survive minor changes
+# in the input file. See in-script comments for more details.
+
 proof = """admit.
 
 Admitted.
@@ -22,14 +25,27 @@ def edit_file():
     copyfile(new_file, ads_file)
 
 
+# first call cvc4 to get rid of "easy" VCs
 prove_all(prover=["cvc4"], counterexample=False)
 print "======================================="
+# first call to Coq, to produce a VC for the unproved postcondition
 prove_all(opt=["--prover=coq", "--limit-line=lemmas.ads:15"], steps=None, counterexample=False, filter_output=".*Grammar extension")
+# "edit" the proof, in this case the proof is simply "admit"
 edit_proof()
 print "======================================="
+# rerun gnatprove with Coq again, to check the proof; everything should be
+# proved now.
 prove_all(opt=["--prover=coq", "--limit-line=lemmas.ads:15"], steps=None, counterexample=False, filter_output=".*Grammar extension")
 print "======================================="
+# now edit the source file, we want to know if gnatprove can still associate
+# the VC with the proof. In fact here the file modification makes the VC
+# unprovable in principle, but our "admit" proof above will still prove it. As
+# this test is for tracking of VCs only, it's fine.
 edit_file()
+# on Windows, after editing a source file, need to wait because otherwise
+# gprbuild will think nothing changed
 sleep_on_windows(4)
+# run gnatprove with Coq again to check proof
 prove_all(opt=["--prover=coq", "--limit-line=lemmas.ads:15"], steps=None, counterexample=False, filter_output=".*Grammar extension")
+# run gnatprove again to see full results
 prove_all(counterexample=False)
