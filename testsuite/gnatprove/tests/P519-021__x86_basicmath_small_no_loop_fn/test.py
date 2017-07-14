d3bdd1a6d56f0b157bba3942c5a51b26b7b9d6ac
Johannes Kanig <kanig@adacore.com>
2017-07-14 18:00:18 +0900

    minor allow more time and parallelism on this test
    Change-Id: I15a3fc9e678319de1ea06fa0d3a797ad2e0c0fc3

--- testsuite/gnatprove/tests/P519-021__x86_basicmath_small_no_loop_fn/test.py
+++ testsuite/gnatprove/tests/P519-021__x86_basicmath_small_no_loop_fn/test.py
@@ -1,7 +1,9 @@
 from test_support import *
+# allow parallelism on this test which has a handful of very long-running VCs (~2 minutes each)
 prove_all(steps=500000,
           prover=["cvc4_cbqi", "cvc4"],
           opt=["--why3-conf=why3.conf"],
           counterexample=False,
+          procs=8,
           # no_fail=True,
           subdue_flow=True)
