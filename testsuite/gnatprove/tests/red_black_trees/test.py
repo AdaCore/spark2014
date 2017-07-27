b1c4505ad2057e39ea623aa8a6d9dbc6242b0c2f
Claire Dross <dross@adacore.com>
2017-07-27 14:13:19 +0200

    minor update red black trees sessions
    Change-Id: If02f85ef715723ce1c06ae6334dd6a58842a5feb

--- testsuite/gnatprove/tests/red_black_trees/test.py
+++ testsuite/gnatprove/tests/red_black_trees/test.py
@@ -2,11 +2,12 @@ from test_support import *
 
 def replay():
     prove_all(procs=0, opt=["--level=2", "--no-axiom-guard", "--no-counterexample"], prover=["cvc4","z3","altergo"], steps=None, vc_timeout=20)
-    prove_all(procs=0, opt=["--level=3", "--no-axiom-guard", "--no-counterexample"], prover=["cvc4","z3","altergo"], steps=None, vc_timeout=120)
+    prove_all(procs=0, opt=["--level=3", "--no-axiom-guard", "--no-counterexample"], prover=["cvc4","z3","altergo"], steps=None, vc_timeout=50)
+    prove_all(procs=0, opt=["--level=3", "--no-axiom-guard", "--no-counterexample"], prover=["cvc4","z3","altergo"], steps=None, vc_timeout=360)
 
 prove_all(opt=["--replay",
                "--no-axiom-guard",
                "--no-counterexample"],
           steps=None,
-          vc_timeout=300,
+          vc_timeout=360,
           prover=["cvc4", "z3", "altergo"])
