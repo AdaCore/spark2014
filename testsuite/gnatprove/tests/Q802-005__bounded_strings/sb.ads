38edfae99c5eb688cc33342c16c642cc42cb1479
Yannick Moy <moy@adacore.com>
2017-08-02 09:11:18 +0200

    Q802-005 Add test
    Change-Id: I05c77a830bc81a8b3921c980d1975325146d5f34

---
+++ testsuite/gnatprove/tests/Q802-005__bounded_strings/sb.ads
@@ -0,0 +1,3 @@
+with Ada.Strings.Bounded;
+package SB
+is new Ada.Strings.Bounded.Generic_Bounded_Length (128);
---
+++ testsuite/gnatprove/tests/Q802-005__bounded_strings/test.ads
@@ -0,0 +1,18 @@
+with Ada.Strings.Bounded;
+with Ada.Strings.Bounded.Hash;
+with Ada.Containers.Formal_Hashed_Maps;
+with SB;
+package Test with SPARK_Mode is
+
+  function SBHash is new Ada.Strings.Bounded.Hash(SB);
+
+  type Scale is new Integer;
+  package Scales_Map is
+	new Ada.Containers.Formal_Hashed_Maps(Key_Type =>
+		SB.Bounded_String,
+		Element_Type => Scale,
+		Hash => SBHash,
+		Equivalent_Keys => SB."=" );
+
+  Repository : Scales_Map.Map := Scales_Map.Empty_Map;
+end Test;
