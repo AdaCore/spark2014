c473cff0f9127e06fc8bf16cb6fbd7eaa2ac08f0
Piotr Trojanek <piotr.trojanek@altran.com>
2017-09-26 20:09:15 +0200

    Q920-019 adapt test after frontend fix
    Frontend now correctly rejects references to objects and functions declared
    in the private part of package spec from its Initial_Condition and
    Initializes contracts.
    
    * testsuite/gnatprove/tests/TU__initializes_illegal/test.out
    Update error message; this test was meant to be illegal from the start.
    
    * testsuite/gnatprove/tests/MA29-016__flow_pre_post/
    Adapt test, as otherwise it would be now rejected by the frontend; adjust
    line numbers. (This test was added with a commit message:
    
      "Analyze the initial condition in the correct scope (and make sure the
       variables seen in the spec are projected appropriately)."
    
    so apparently it was an unintentional mistake to reference the private
    variable in Initial_Condition).
    
    Change-Id: Ie9953efd84d45842c43b27f426e483cbd5a1b86a

--- testsuite/gnatprove/tests/MA29-016__flow_pre_post/foo.ads
+++ testsuite/gnatprove/tests/MA29-016__flow_pre_post/foo.ads
@@ -1,8 +1,10 @@
 package Foo with Elaborate_Body,
    Abstract_State    => State,
    Initializes       => State,
-   Initial_Condition => F1 and F3 and F4 and Get_A = C
+   Initial_Condition => F1 and F3 and F4 and Get_A = Get_C
 is
+
+   function F1 return Boolean with Global => State;
 
    --  function F2 return Boolean with Global => State;
 
@@ -14,11 +16,13 @@ is
 
    function Get_A return Integer with Global => State;
 
+   function Get_C return Integer with Global => State;
+
 private
 
    C : Integer with Part_Of => State;
 
-   function F1 return Boolean is (C > 0) with Global => C;
+   function F1 return Boolean is (C > 0);
 
    --  function F2 return Boolean is (C > 0) with Refined_Global => C;
 
@@ -27,5 +31,7 @@ private
    function F3_Totally_Unused return Boolean is (True);
 
    function F4 return Boolean is (C = Get_A);
+
+   function Get_C return Integer is (C);
 
 end Foo;
