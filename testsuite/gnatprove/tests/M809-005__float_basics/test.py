a382d865a9d3756b46e6ff467490ff19e74ada97
Sylvain Dailler <dailler@adacore.com>
2017-10-01 19:41:31 +0200

    Q927-025 Update tests floats
    This updates a replay test on floats. Steps is set to 10000 but should be
    ok because steps are faster with floats and the test is on replay.
    
    * testsuite/gnatprove/tests/M809-005__float_basic/*.xml
    Update sessions
    
    * testsuite/gnatprove/tests/M809-005__float_basic/test.py
    Steps set to 10 000
    
    * testsuite/gnatprove/tests/M809-005__float_basic/test.out
    Update test
    
    Change-Id: I01d875b9fc24e5f2926bec23a67222156da7e66b

--- testsuite/gnatprove/tests/M809-005__float_basics/test.py
+++ testsuite/gnatprove/tests/M809-005__float_basics/test.py
@@ -1,6 +1,6 @@
 from test_support import *
 
 def replay():
-    prove_all(procs=10, counterexample=False, prover=["z3","cvc4","altergo"])
+    prove_all(procs=10, counterexample=False, prover=["z3","cvc4","altergo"], steps=10000)
 
 prove_all(opt=["--replay"])
