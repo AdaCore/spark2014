cb14d0128ddeb01288aa1b9ffbc5ca16ff31333e
Elisa Barboni <elisa.barboni@altran.com>
2017-10-03 15:24:04 +0100

    Q929-028 Add test
    Current output shows a spurious error on the internal variable R2s.
    
    Change-Id: If5784a57637db65b64daadd52ff70488643e68dd

---
+++ testsuite/gnatprove/tests/Q929-028__flow_spurious_error/p.adb
@@ -0,0 +1,7 @@
+package body P
+is
+   package body Inst with
+     Refined_State => (State => Tmp)
+   is
+   end;
+end;
---
+++ testsuite/gnatprove/tests/Q929-028__flow_spurious_error/p.ads
@@ -0,0 +1,24 @@
+package P with Elaborate_Body
+is
+   type Typ (Int : Integer) is record
+      null;
+   end record;
+
+   package Inst with
+      Abstract_State => State,
+      Initializes => State
+   is
+      Max : Integer := 30;
+
+      type T is private;
+
+      Null_T : constant T;
+
+   private
+      Tmp : constant Integer := Max with Part_Of => State;
+
+      type T is new Typ (Tmp);
+
+      Null_T : constant T := (Int => Max);
+   end;
+end;
