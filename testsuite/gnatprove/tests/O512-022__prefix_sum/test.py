7218e7ad9594638f67de034dc4adca8babb98a5d
Johannes Kanig <kanig@adacore.com>
2017-10-19 10:16:03 +0900

    (no-tn-check) disable CE because of minor diffs
    Change-Id: I03965df2df4defc5601d50d4d8235c1859935d2c

--- testsuite/gnatprove/tests/O512-022__prefix_sum/test.py
+++ testsuite/gnatprove/tests/O512-022__prefix_sum/test.py
@@ -2,4 +2,5 @@ from test_support import *
 
 # Do not use Z3, as the step limit fluctuates too much between platforms, thus
 # making it impossible to share a common expected output when Z3 is used.
-prove_all(prover=["cvc4","altergo"],opt=["-u", "prefixsum.adb", "prefixsum_expanded.adb", "prefixsum_general.adb"])
+prove_all(prover=["cvc4","altergo"],opt=["-u", "prefixsum.adb",
+    "prefixsum_expanded.adb", "prefixsum_general.adb"], counterexample=False)
