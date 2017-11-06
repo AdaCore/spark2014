33f8cf41380e5a13deb7cc89733607b19a61f824
Yannick Moy <moy@adacore.com>
2017-11-06 09:10:34 +0100

    QA31-008 Remove counterexample for same output across platforms
    Change-Id: I14fa5f5ab92e1a3d95c29d1e6f470a4b992b02d7

--- testsuite/gnatprove/tests/QA31-008__von_neumann_sqrt/test.py
+++ testsuite/gnatprove/tests/QA31-008__von_neumann_sqrt/test.py
@@ -1,3 +1,5 @@
 from test_support import *
 
-prove_all()
+# Do not display counterexamples, as they differ between platforms, due to
+# spurious counterexamples being issued on Linux for non-linear VCs.
+prove_all(counterexample=False)
