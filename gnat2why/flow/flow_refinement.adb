961dbb9df2a702b1b4f44cbd598f24435c485eef
Piotr Trojanek <piotr.trojanek@altran.com>
2017-11-15 14:28:03 +0100

    simplify down-projection of abstract states
    (no-tn-check)
    
    * gnat2why/flow/flow_refinement.adb
    (Expand): semantics unchanged; remove useless iteration over
     Part_Of_Constituents (which are a subset of Refinement_Constituents);
     refactor test for null refinement out of the loop. Semantics unaffected.
    
    Change-Id: Ie8f7c9b92d3d843d17743237b77391474c0ba2f9

--- gnat2why/flow/flow_refinement.adb
+++ gnat2why/flow/flow_refinement.adb
@@ -956,31 +956,22 @@ package body Flow_Refinement is
             declare
                Pkg : constant Entity_Id := Scope (E);
 
-               procedure Expand (L : Elist_Id);
-               --  Expand each constituent of L if there are no hidden
-               --  constituents, otherwise include the abstract state E into P.
+            begin
+               if Entity_Body_In_SPARK (Pkg)
+                 and then State_Refinement_Is_Visible (E, S)
+               then
+                  --  ??? do the same as in Expand_Abstract_State
 
-               procedure Expand (L : Elist_Id) is
-               begin
-                  for C of Iter (L) loop
-                     if Nkind (C) /= N_Null then
+                  if not Has_Null_Refinement (E) then
+                     for C of Iter (Refinement_Constituents (E)) loop
                         if Is_Visible (C, S) then
                            Expand (C);
                         else
                            P.Include (E);
                            return;
                         end if;
-                     end if;
-                  end loop;
-               end Expand;
-
-            begin
-               if Entity_Body_In_SPARK (Pkg)
-                 and then State_Refinement_Is_Visible (E, S)
-               then
-                  Expand (Refinement_Constituents (E));
-                  Expand (Part_Of_Constituents (E));
-                  --  ??? do the same as in Expand_Abstract_State
+                     end loop;
+                  end if;
                else
                   P.Include (E);
                end if;
