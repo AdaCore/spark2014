2536ee4fb0e8e242dc5ccc1e9befc323ed048aa6
Sylvain Dailler <dailler@adacore.com>
2017-11-28 17:37:06 +0100

    QB21-001 Rewrite test
    Rewriting itp__server test so that it is more robust. We parse the result
    instead of just printing it. And we remove some notifications that are
    spurious like Task_monitor.
    
    * testsuite/gnatprove/tests/QA11-003__itp_server_test/*
    Now parses the output as a Json file inside the test. Tentatively readding
    this for Windows.
    
    Change-Id: I91c44d66c39e8efe7f2263cb7b71cab5432a7c13

--- testsuite/gnatprove/tests/QA11-003__itp_server_test/test.py
+++ testsuite/gnatprove/tests/QA11-003__itp_server_test/test.py
@@ -1,5 +1,23 @@
 from test_support import *
 import time
+import json
+
+DEAD = "Dead"
+INITIALIZED = "Initialized"
+NEXT_UNPROVEN = "Next_Unproven_Node_Id"
+NOTIFICATION = "notification"
+NODE_CHANGE = "Node_change"
+NODE_ID = "node_ID"
+PARENT_ID = "parent_ID"
+LTASK = "task"
+UTASK = "Task"
+UMESSAGE = "Message"
+LMESSAGE = "message"
+NEW_NODE = "New_node"
+LMESSAGE = "message"
+MESS_NOTIF = "mess_notif"
+TASK_MONITOR = "Task_Monitor"
+
 
 """ This tests is a commandline test for the itp server. It launches a server as
 a background process, and then pass it request in JSON. The output to be checked
@@ -29,8 +47,50 @@ def launch_server(limit_line="", input_file=""):
             sleep(1)
     process.wait()
     s = process.out
-    s = re.sub('"pr_time" : ([0-9]*[.])?[0-9]+', '"pr_time" : Not displayed', s)
-    return s
+    s = re.sub('"pr_time" : ([0-9]*[.])?[0-9]+', '"pr_time" : "Not displayed"', s)
+    # Parse the json outputs:
+    l = s.split (">>>>")
+    nb_unparsed = 0
+    task_monitor = 0
+    next_unproven = 0
+    for i in l:
+        try:
+            pos = i.find("{")
+            j = json.loads(i[pos:])
+            notif_type = j[NOTIFICATION]
+            if notif_type == NODE_CHANGE:
+                print (NODE_CHANGE + " " + str(j[NODE_ID]))
+            elif notif_type == NEW_NODE:
+                print (NEW_NODE + " " + str(j[NODE_ID]) + " " + str(j[PARENT_ID]))
+            elif notif_type == NEXT_UNPROVEN:
+                # TODO this is ok but we print nothing
+                next_unproven = next_unproven + 1
+            elif notif_type == INITIALIZED:
+                print (INITIALIZED)
+            elif notif_type == DEAD:
+                print (DEAD)
+            elif notif_type == UTASK:
+                print (notif_type)
+                print (j[LTASK])
+            elif notif_type == UMESSAGE:
+                message = j[LMESSAGE]
+                message_type = message[MESS_NOTIF]
+                if message_type == TASK_MONITOR:
+                    task_monitor = task_monitor + 1
+                    # TODO ignore task_monitoring
+                else:
+                    print (UMESSAGE)
+            else:
+                print "TODO PROBLEM TO REPORT"
+        except:
+            if i != "\n" and i != " ":
+                nb_unparsed = nb_unparsed + 1
+                print ("UNPARSED NOTIFICATION " + i)
+    if nb_unparsed > 1:
+        print "PROBLEM WITH UNPARSED JSON NOTIFICATIONS"
+    if next_unproven != 1:
+        print "PROBLEM WITH NEXT_UNPROVEN_NODE"
+    return "DONE"
 
 prove_all(counterexample=False, prover=["cvc4"])
 sleep(5)
