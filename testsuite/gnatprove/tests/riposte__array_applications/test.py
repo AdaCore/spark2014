4175aae988c478dc4bc5996b81579016302efbff
Florian Schanda <florian.schanda@altran.com>
2017-11-30 10:23:14 +0000

    QB30-015 proof - update cvc4
    Update CVC4 to the latest version + testsuite updates. Around ~13 new
    proofs.
    
    Change-Id: I3a6160b05174812973814128299c148bcd5a594d

--- testsuite/gnatprove/tests/riposte__array_applications/test.py
+++ testsuite/gnatprove/tests/riposte__array_applications/test.py
@@ -1,6 +1,6 @@
 from test_support import *
 
 def replay():
-    prove_all(steps=1600,procs=0,opt=["--no-axiom-guard"])
+    prove_all(steps=2500,procs=0,opt=["--no-axiom-guard"])
 
 prove_all(opt=["--replay", "--no-axiom-guard"])
--- testsuite/gnatprove/tests/text_io_get_line/test.py
+++ testsuite/gnatprove/tests/text_io_get_line/test.py
@@ -2,5 +2,6 @@ from test_support import *
 
 # Do not run with Z3 (P212-018) until deterministic on all platforms
 prove_all(opt=["--proof=progressive",
+               "--no-axiom-guard",
                "--prover=cvc4,altergo"],
-          steps=1200)
+          steps=1500)
