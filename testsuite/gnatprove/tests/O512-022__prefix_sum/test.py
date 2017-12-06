b9bd34d0a639549564569a3bc15a4e007f596149
Florian Schanda <florian.schanda@altran.com>
2017-12-06 08:58:38 +0000

    QB30-015 fix tests after cvc4/z3 updates
    Made this into a test with replay to fix local and test server diffs.
    
    Change-Id: I25d47820e003e97788e1050b2d44010cc857a133

--- testsuite/gnatprove/tests/O512-022__prefix_sum/test.py
+++ testsuite/gnatprove/tests/O512-022__prefix_sum/test.py
@@ -1,6 +1,19 @@
 from test_support import *
 
-# Do not use Z3, as the step limit fluctuates too much between platforms, thus
-# making it impossible to share a common expected output when Z3 is used.
-prove_all(prover=["cvc4","altergo"],opt=["-u", "prefixsum.adb",
-    "prefixsum_expanded.adb", "prefixsum_general.adb"], counterexample=False)
+def replay():
+    prove_all(level=4,
+              procs=0,
+              counterexample=False,
+              opt=["--no-axiom-guard",
+                   "-u",
+                   "prefixsum.adb",
+                   "prefixsum_expanded.adb",
+                   "prefixsum_general.adb"])
+
+prove_all(opt=["--replay",
+               "--no-axiom-guard",
+               "-u",
+               "prefixsum.adb",
+               "prefixsum_expanded.adb",
+               "prefixsum_general.adb"],
+          counterexample=False)
