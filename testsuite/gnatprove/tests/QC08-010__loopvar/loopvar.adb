e2d95af05dfb13eb2b86b36bb2349124976f1d62
Yannick Moy <moy@adacore.com>
2017-12-08 12:53:42 +0100

    QC08-010 Add test
    Change-Id: I3be5ae911cd0a5aaff5898ac1f24f4bce8da6292

---
+++ testsuite/gnatprove/tests/QC08-010__loopvar/loopvar.adb
@@ -0,0 +1,9 @@
+procedure Loopvar with SPARK_Mode is
+   X : Integer := 1;
+begin
+   while X < 10 loop
+      pragma Loop_Variant (Increases => X);
+      X := X + 1;
+      pragma Assert (X > 0);
+   end loop;
+end Loopvar;   
