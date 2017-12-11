a693f01abd4309f91611499b6382090fc80dd874
Elisa Barboni <elisa.barboni@altran.com>
2017-12-11 12:52:45 +0000

    Q929-022 Revert previous commit
    In the previous commit under this ticket we were considering every bounds
    to have variable inputs but the ones coming from formal parameters. This
    was wrong and we are now considering every bound to be without variable
    inputs.
    
    Note that Q929-022 remains enabled as it was xfailed for a wrong reason so
    we have now just updated its output.
    
    Change-Id: Iaef24687a8e65efd583394af7e43d3a9348a618d

--- testsuite/gnatprove/tests/Q710-004__range/essai05.adb
+++ testsuite/gnatprove/tests/Q710-004__range/essai05.adb
@@ -20,7 +20,7 @@ procedure Essai05 with
    end Conv;
 
    function Conv2 (V : Array_A) return Array_B is
-      R : Array_B (V'Range) := (others => 0);
+      R : Array_B (V'Range) := (others => 0); --@RANGE_CHECK:FAIL
    begin
       for I in V'Range loop
          R (I) := V (I);
@@ -29,8 +29,7 @@ procedure Essai05 with
    end Conv2;
 
    A : Array_A := (0, 1, 2);
-   B : Array_B := Conv (A);
-   --  We now get a spurious message here which is related to inlining.
+   B : Array_B := Conv (A); --@RANGE_CHECK:FAIL
 
 begin
    null;
