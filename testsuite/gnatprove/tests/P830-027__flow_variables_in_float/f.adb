38aabf2bd04a845bf59e25bb1c39f55d3bf58871
Elisa Barboni <elisa.barboni@altran.com>
2017-12-19 17:50:59 +0000

    P830-027 Add test
    Change-Id: I264a252069c618033ed95263cdec481ed6b36c4e

---
+++ testsuite/gnatprove/tests/P830-027__flow_variables_in_float/f.adb
@@ -0,0 +1,8 @@
+function F (X : Float; Y : Integer) return Boolean is
+
+   subtype Float_Type is Float range 0.0 .. X;
+   subtype Int_Type   is Integer range 0 .. Y;
+
+begin
+   return 1.0 in Float_Type and 1 in Int_Type;
+end;
