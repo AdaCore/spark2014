25ff17f46ae4afd8d451b6298d530669ece986bb
Johannes Kanig <kanig@adacore.com>
2018-01-03 10:20:41 +0900

    QB06-012 fix ITP server test on windows
    Simply write output to file instead of reading it ourselves, can avoid
    deadlock in that way.
    
    Change-Id: I757db92dd83bccab2f79dec6da16724f2fe6143e

--- testsuite/gnatprove/tests/QA11-003__itp_server_test/test.py
+++ testsuite/gnatprove/tests/QA11-003__itp_server_test/test.py
@@ -24,29 +24,29 @@ a background process, and then pass it request in JSON. The output to be checked
 are notifications written in JSON"""
 
 # This launches the itp_server
-def launch_server(limit_line="", input_file=""):
+def launch_server(limit_line, input_file):
 
     installdir = spark_install_path()
     bindir = os.path.join(installdir, 'libexec', 'spark', 'bin')
     Env().add_path(bindir)
 
-    cmd = ["gnat_server"]
-    cmd += ["--limit-line"]
-    cmd += [limit_line]
-    cmd += ["test.mlw"]
-    # Create a pipe to give request to the process one by one. TODO check that
-    # this works like this in python.
+    cmd = ["gnat_server", "--limit-line", limit_line, "test.mlw"]
+    # Create a pipe to give request to the process one by one.
     read, write = os.pipe()
-    # Give the gnat_server time to end by itself. Thats why we send the
-    # exit request
-    process = Run (cmd, cwd="gnatprove", input=read, bg=True, timeout=400)
-    with open ("test.in", "r") as in_file:
-        for l in in_file:
-            print(l)
-            os.write(write, l)
-            sleep(1)
-    process.wait()
-    s = process.out
+    # process writes output to file, so we can avoid deadlocking
+    with open ("proc.out", "w") as outfile:
+        process = Run (cmd, cwd="gnatprove", input=read, output=outfile, bg=True, timeout=400)
+        with open (input_file, "r") as in_file:
+            for l in in_file:
+                print(l)
+                os.write(write, l)
+                sleep(1)
+        # Give the gnat_server time to end by itself. Thats why we send the
+        # exit request
+        process.wait()
+    # read in the process output via the tempfile
+    with open ("proc.out", "r") as infile:
+        s = infile.read()
     s = re.sub('"pr_time" : ([0-9]*[.])?[0-9]+', '"pr_time" : "Not displayed"', s)
     # Parse the json outputs:
     l = s.split (">>>>")
