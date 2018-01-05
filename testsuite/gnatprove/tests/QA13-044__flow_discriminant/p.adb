fb67ec458acfacfdfd3af766241285841472225e
Elisa Barboni <elisa.barboni@altran.com>
2018-01-05 12:58:24 +0000

    QA13-044 Add test
    Change-Id: Iec5b41154b341e7be4c03fb908211fa5dc5ef384

---
+++ testsuite/gnatprove/tests/QA13-044__flow_discriminant/p.adb
@@ -0,0 +1,10 @@
+package body P is
+
+   function Foo (Input : Integer) return T
+   is
+      Result : T (Input);
+   begin
+      return Result;
+   end Foo;
+
+end P;
---
+++ testsuite/gnatprove/tests/QA13-044__flow_discriminant/p.ads
@@ -0,0 +1,8 @@
+package P is
+
+   type T (Discr : Integer) is null record;
+
+   function Foo (Input : Integer) return T
+   with Depends => (Foo'Result => (Input));
+
+end P;
