866769cd91b3bf59ebf3c68211eff665721e8f3d
Piotr Trojanek <piotr.trojanek@altran.com>
2018-01-16 13:41:07 +0100

    R116-018 add test for marking of binary operators
    Call to binary operator with SPARK_Mode => Off should be rejected.
    
    * testsuite/gnatprove/tests/R116-018__binary_op/
    Add; missing error.
    
    Change-Id: Ice20a2c09a5e1d9a4ba04e0674d3925f0a02c107

---
+++ testsuite/gnatprove/tests/R116-018__binary_op/import_intrinsic.ads
@@ -0,0 +1,7 @@
+package Import_Intrinsic with SPARK_Mode is
+   type New_Float is private;
+   function "<" (Left, Right : New_Float) return Boolean with SPARK_Mode;
+private
+   type New_Float is new Float;
+   pragma Import (Intrinsic, "<");
+end Import_Intrinsic;
