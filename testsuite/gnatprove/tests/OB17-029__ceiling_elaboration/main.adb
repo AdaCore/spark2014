7a8744d9f1ef57a0a74a0429eb3028cc5e3c864e
Elisa Barboni <elisa.barboni@altran.com>
2018-01-17 12:41:13 +0000

    OB17-029 Edit test
    Now the environment task doesn't call directly a protected operation but it
    does during elaboration of P. Failed check for ceiling priority protocol
    is no longer emitted.
    
    Change-Id: I931ac5bcfad97a4c0b668636da8e9e83d5c87d21

--- testsuite/gnatprove/tests/OB17-029__ceiling_elaboration/main.adb
+++ testsuite/gnatprove/tests/OB17-029__ceiling_elaboration/main.adb
@@ -3,7 +3,5 @@ with P;
 procedure Main is
    pragma Priority (2);
 begin
-   P.PO.Dummy;
-   --  Environment task priority with priority 2 calls a protected operation
-   --  with priority 1 => failed check for the ceiling priority protocol.
+   null;
 end;
