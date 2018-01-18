7b06eb8bf539d7c68c2b919dd95335d872d55f76
Piotr Trojanek <piotr.trojanek@altran.com>
2018-01-18 18:48:38 +0100

    R117-017 adjust test after a frontend fix
    The Impute_Procedure unit is now rightly rejected by the frontend. Remove it
    from the test.
    
    * testsuite/gnatprove/tests/O312-037__flow_pure_subprograms/
    Adapted; output updated (still not as expected).
    
    Change-Id: I800dbc15e050e4dc17efb72fa933691ccce82c7a

--- testsuite/gnatprove/tests/O312-037__flow_pure_subprograms/call.adb
+++ testsuite/gnatprove/tests/O312-037__flow_pure_subprograms/call.adb
@@ -5,7 +5,6 @@ with Ada.Numerics.Elementary_Functions;
 
 with Pure_Package;
 with Impure_Package;
-with Impure_Procedure;
 
 procedure Call
   with Global => null
@@ -23,14 +22,10 @@ begin
    X := Integer (Ada.Numerics.Elementary_Functions.Log (0.0));
 
    --  These calls should also be assumed to have aspect Global => null,
-   --  because of SPARK RM 6.1.4 (4), i.e.,
-
-   --  a library-level subprogram declared in a library unit declared as pure
+   --  because of SPARK RM 6.1.4 (4), i.e., a library-level subprogram declared
+   --  in a library unit declared as pure
    X := Pure_Package.Pure_Function (X);
    X := Impure_Package.Pure_Function (X);
-
-   --  a Pure_Function pragma applies to the subprogram
-   Impure_Procedure (X);
 
    --  These implicit assumptions should be verified when analyzing these
    --  subprograms.
--- testsuite/gnatprove/tests/O312-037__flow_pure_subprograms/impure_procedure.adb
+++
@@ -1,6 +0,0 @@
-with State;
-
-procedure Impure_Procedure (X : in out Integer) is
-begin
-   X := X + State.Y;
-end Impure_Procedure;
