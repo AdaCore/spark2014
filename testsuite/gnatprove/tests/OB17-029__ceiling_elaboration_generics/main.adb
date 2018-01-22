8db9abd7856a3e07c2b4f6c2b18bc48ae7a984fd
Elisa Barboni <elisa.barboni@altran.com>
2018-01-22 10:49:15 +0000

    OB17-029 Remove useless bit of comment from test
    Change-Id: I58c400886b88862e977fa924e390c87107b181b3

--- testsuite/gnatprove/tests/OB17-029__ceiling_elaboration_generics/main.adb
+++ testsuite/gnatprove/tests/OB17-029__ceiling_elaboration_generics/main.adb
@@ -1,8 +1,7 @@
 with Gen;
 -- We are "withing" a generic package whose body "withes" a package Q,
 -- the elaboration of which calls a protected operation. We should detect
--- a failure of the check related to the ceiling priority protocol
--- but we don't.
+-- a failure of the check related to the ceiling priority protocol.
 procedure Main is
    pragma Priority (3);
 begin
