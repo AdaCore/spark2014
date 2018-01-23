15cdc4579984070fa685a967a0cebb670a7a2e4c
Piotr Trojanek <piotr.trojanek@altran.com>
2018-01-23 20:09:24 +0100

    QA13-044 remove excessive parens from test
    Change-Id: I8cc757ec02125c78cd7f0666f2ddd36c8e38c537

--- testsuite/gnatprove/tests/QA13-044__flow_discriminant/p.ads
+++ testsuite/gnatprove/tests/QA13-044__flow_discriminant/p.ads
@@ -3,6 +3,6 @@ package P is
    type T (Discr : Integer) is null record;
 
    function Foo (Input : Integer) return T
-   with Depends => (Foo'Result => (Input));
+   with Depends => (Foo'Result => Input);
 
 end P;
