1a77f1e5282df111b8a9d594e4896a01c1f4440f
Johannes Kanig <kanig@adacore.com>
2018-01-23 09:34:33 +0100

    R103-025 remove platform differences on this test
    Change-Id: Id414405cb05107fc984b4115dd32da615cecedd4

--- testsuite/gnatprove/tests/R103-025__version/test.py
+++ testsuite/gnatprove/tests/R103-025__version/test.py
@@ -1,17 +1,30 @@
 from test_support import *
 from gnatpython.ex import Run
 
-# test the version output of gnatprove. We remove:
+# test the version output of gnatprove.
+# Typical output is like this:
+#   $ gnatprove --version
+#   <gnatprove version>
+#   <full-path-to-prover> : <version output for prover>
+# Z3 output looks like this:
+#   Z3 version <bla> - 64 bit
+# We remove:
 # - the gnatprove version, which changes too much
 # - the prover locations
-# We keep prover output, because it changes only rarely.
+# - platform (32bits/64bits) in Z3 output
 
 p = Run(["gnatprove", "--version"])
 l = p.out.splitlines()
 # drop first line of output
 l = l[1:]
 for line in l:
-    # remove everything before the dash
-    # there might be no dash, but the code still works
+    # remove everything before the colon, to remove path info
+    # there might be no colon, but the code still works
     elts = line.split(':')
-    print elts[-1]
+    text = elts[-1]
+
+    # remove everything after " - ", to remove mention of platform in z3
+    # output
+    elts = text.split(" - ")
+    text = elts[0]
+    print text
