09c2babbf359a0f09f3cfc89569d4a1dbc8ce6b0
Piotr Trojanek <piotr.trojanek@altran.com>
2018-01-24 11:03:18 +0100

    P118-021 add test from the discussion
    In the discussion I gave an example of a non-null record T (not initialized
    by default), for which we were getting a warning:
    
      p.ads:3:09: warning: unused variable "R"
    
    Now for a similar record we get:
    
      p.adb:6:09: warning: "T_Rec2" is not used in its predicate
    
    which seems completely fine.
    
    * testsuite/gnatprove/tests/P118-021__true_predicate_on_scalars/
    Extend test; add output.
    
    Change-Id: Ibbf67552eb217398e9a4751097795cf8628dd5d5

--- testsuite/gnatprove/tests/P118-021__true_predicate_on_scalars/p.adb
+++ testsuite/gnatprove/tests/P118-021__true_predicate_on_scalars/p.adb
@@ -2,9 +2,15 @@ procedure P is
 
    type T_Bool is new Boolean with Predicate => True;
    type T_Int  is new Integer with Predicate => True;
+   type T_Rec1 is null record with Predicate => True;
+   type T_Rec2 is record
+      C : Integer;
+   end record with Predicate => True;
 
-   B : T_Bool;
-   J : T_Int;
+   B  : T_Bool;
+   J  : T_Int;
+   R1 : T_Rec1;
+   R2 : T_Rec2;
 
 begin
    null;
