5d46cb4c042d063c85a46d6428b98800a1562d83
Johannes Kanig <kanig@adacore.com>
2018-01-29 19:01:38 +0900

    (no-tn-check) abandon full proof on red_black_trees
    Change-Id: Id750ee03a993b704b548db32b43705e13e2cad6e

--- testsuite/gnatprove/tests/red_black_trees/test.py
+++ testsuite/gnatprove/tests/red_black_trees/test.py
@@ -1,16 +1,10 @@
 from test_support import *
 
-# Alt-Ergo does not contribute here, so to save time we do not include it
-# in the run.
-
 def replay():
-    prove_all(procs=0, opt=["--level=2", "--no-axiom-guard", "--no-counterexample"], prover=["cvc4","z3"], steps=None, vc_timeout=20)
-    prove_all(procs=0, opt=["--level=3", "--no-axiom-guard", "--no-counterexample"], prover=["cvc4","z3"], steps=None, vc_timeout=60)
+    prove_all(procs=0, opt=["--no-axiom-guard", "--no-counterexample"],
+            prover=["cvc4"], steps=100)
 
 prove_all(opt=["--replay",
                "--no-axiom-guard",
                "--no-counterexample"],
-          procs=4,
-          steps=None,
-          vc_timeout=120,
-          prover=["cvc4", "z3"])
+          prover=["cvc4"])
