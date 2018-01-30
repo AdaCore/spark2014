a2d4e157a1b92c3723d3f48c28956b4c4a0e1b96
Piotr Trojanek <piotr.trojanek@altran.com>
2018-01-30 22:10:21 +0100

    QC19-042 disable caching in why3.conf test
    Apparently this test is incompatible with the memcached.
    
    * testsuite/gnatprove/tests/QC19-042__config/test.py
    Call prove_all with cache_allowed=False.
    
    Change-Id: I266ab52e224b8da034617bc81fb5dcbde10b0c06

--- testsuite/gnatprove/tests/QC19-042__config/test.py
+++ testsuite/gnatprove/tests/QC19-042__config/test.py
@@ -1,3 +1,3 @@
 from test_support import *
-prove_all(opt=["--why3-conf=no_main_section.conf", "--prover=altergo2"])
-prove_all(opt=["--why3-conf=wrong_driver_file.conf", "--prover=altergo2"])
+prove_all(opt=["--why3-conf=no_main_section.conf", "--prover=altergo2"], cache_allowed=False)
+prove_all(opt=["--why3-conf=wrong_driver_file.conf", "--prover=altergo2"], cache_allowed=False)
