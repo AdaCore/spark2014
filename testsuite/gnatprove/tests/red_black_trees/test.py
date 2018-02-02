95ba1e4f81215f931017752f864cbf08a2af95fb
Johannes Kanig <kanig@adacore.com>
2018-02-02 18:10:19 +0900

    (no-tn-check) tentatively disable replay on this test
    Change-Id: I4400c1068a71ed2dd4f9e694a4c50d30ac7f7a4d

--- testsuite/gnatprove/tests/red_black_trees/test.py
+++ testsuite/gnatprove/tests/red_black_trees/test.py
@@ -1,10 +1,6 @@
 from test_support import *
 
-def replay():
-    prove_all(procs=0, opt=["--no-axiom-guard", "--no-counterexample"],
-            prover=["cvc4"], steps=100)
-
-prove_all(opt=["--replay",
-               "--no-axiom-guard",
+prove_all(opt=["--no-axiom-guard",
                "--no-counterexample"],
+          steps=50,
           prover=["cvc4"])
