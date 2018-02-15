3733758ff40cb325b70a49777300004dcb26f667
Johannes Kanig <kanig@adacore.com>
2018-02-15 10:06:45 +0900

    (no-tn-check) disable CE on this test
    Change-Id: I90c57a41cf93e299d6365f13f843bca907694dda

--- testsuite/gnatprove/tests/text_io_get_line/test.py
+++ testsuite/gnatprove/tests/text_io_get_line/test.py
@@ -3,5 +3,6 @@ from test_support import *
 # Do not run with Z3 (P212-018) until deterministic on all platforms
 prove_all(opt=["--proof=progressive",
                "--no-axiom-guard",
-               "--prover=cvc4,altergo"],
+               "--prover=cvc4,altergo",
+               "--no-counterexample"],
           steps=1500)
