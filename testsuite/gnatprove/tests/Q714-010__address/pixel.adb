c446bda496e79a1e2db66f60447340caf2a5a40e
Johannes Kanig <kanig@adacore.com>
2018-03-07 10:49:06 +0900

    R307-002 move test to XFAIL and add proof markers
    Change-Id: Id6bc9d3bbd66dee485992d866a4d28f7238b83e2

--- testsuite/gnatprove/tests/Q714-010__address/pixel.adb
+++ testsuite/gnatprove/tests/Q714-010__address/pixel.adb
@@ -7,18 +7,18 @@ procedure Pixel with SPARK_Mode is
    function Zero return Natural is (0);
 
    procedure Aspect with
-      Import, Address => Buffer + (Buffer mod Storage_Offset (1 / Zero));
+      Import, Address => Buffer + (Buffer mod Storage_Offset (1 / Zero)); --@DIVISION_CHECK:FAIL
 
    procedure Clause with
       Import;
-   for Clause'Address use Buffer + (Buffer mod Storage_Offset (1 / Zero));
+   for Clause'Address use Buffer + (Buffer mod Storage_Offset (1 / Zero)); --@DIVISION_CHECK:FAIL
 
    Asp : Integer with
-      Import, Address => Buffer + (Buffer mod Storage_Offset (1 / Zero));
+      Import, Address => Buffer + (Buffer mod Storage_Offset (1 / Zero)); --@DIVISION_CHECK:FAIL
 
    Cla : Integer with
       Import;
-   for Cla'Address use Buffer + (Buffer mod Storage_Offset (1 / Zero));
+   for Cla'Address use Buffer + (Buffer mod Storage_Offset (1 / Zero)); --@DIVISION_CHECK:FAIL
 
 begin
    Aspect;
