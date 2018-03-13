46ea02e95ea285b81e428e19d47e820429d49392
Yannick Moy <moy@adacore.com>
2018-03-13 14:00:51 +0100

    R309-023 Fixes for final version
    Change-Id: Id840f7b5c876f8b5f8963d46b72890d8a5234791

--- papers/AdaEurope_2018/figures/code/spark_ex1.adb
+++ papers/AdaEurope_2018/figures/code/spark_ex1.adb
@@ -15,17 +15,16 @@ begin
 end Swap_Contents;
 
 
-
 procedure Add_One(X_Param, Y_Param : in Int_Ptr) is
-  X_Param := X_Param + 1;
-  Y_Param := Y_Param + 1;
+begin
+  X_Param.all := X_Param.all + 1;
+  Y_Param.all := Y_Param.all + 1;
 end Add_One;
 
   X : Int_Ptr := new Integer;
   Y : Int_Ptr := new Integer;
-  
+
   begin
 	Swap(X, Y);
 	(...)
   end spark_ex1;
-
--- papers/AdaEurope_2018/figures/code/spark_ex1_proof.adb
+++ papers/AdaEurope_2018/figures/code/spark_ex1_proof.adb
@@ -7,9 +7,9 @@ procedure spark_ex1 with SPARK_Mode is
   type Int_Ptr is access integer;
 
 procedure Swap_Contents(X_Param, Y_Param : in Int_Ptr) with
-  post => X_Param.all = Y_Param.all'Old 
-	and Y_Param.all = X_Param.all'Old; 
-  is
+  Post => X_Param.all = Y_Param.all'Old
+	and Y_Param.all = X_Param.all'Old;
+is
   Tmp : Integer := X_Param.all;
 begin
   X_Param.all := 0;
@@ -18,24 +18,23 @@ begin
 end Swap_Contents;
 
 procedure Add_One(X_Param, Y_Param : in Int_Ptr) with
-  post => X_Param.all = X_Param.all'Old + 1
+  Post => X_Param.all = X_Param.all'Old + 1
 	and Y_Param.all = Y_Param.all'Old + 1;
-  is
-  X_Param := X_Param + 1;
-  Y_Param := Y_Param + 1;
+is
+  X_Param.all := X_Param.all + 1;
+  Y_Param.all := Y_Param.all + 1;
 end Add_One;
 
   X : Int_Ptr := new Integer'(1);
   (...)
 
   Add_One (X, X);
-  pragma Assert (Y.all = 2);  --  incorrect assertion 
+  pragma Assert (X.all = 2);  --  incorrect assertion
 
   Swap_Contents (X, X);
-  pragma Assert (X.all = 1);  --  incorrect assertion 
+  pragma Assert (X.all = 1);  --  incorrect assertion
 
 begin
   Swap(X, Y);
   (...)
 end spark_ex1;
-
