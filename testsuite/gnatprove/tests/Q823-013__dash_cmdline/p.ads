49d404a9b3d0c616c2adb57b87b85c7441ac6c7e
Johannes Kanig <kanig@adacore.com>
2018-04-10 09:14:15 +0900

    R409-038 remove pointless test
    Change-Id: Ib10d90793dc11856b52fd1bbcb198ddf83a43ee3

--- testsuite/gnatprove/tests/Q823-013__dash_cmdline/p.ads
+++
@@ -1,5 +0,0 @@
-package P is
-
-   function Foo return Boolean is (True) with Post => False;
-
-end;
--- testsuite/gnatprove/tests/Q823-013__dash_cmdline/test.py
+++
@@ -1,4 +0,0 @@
-from test_support import *
-
-gnatprove(opt=["-P", "test.gpr", "-", "-", "-"])
-# this test is about these dashes ^    ^    ^
