d733f6a65291d7198c0623cd6f839ab9aadff294
Piotr Trojanek <piotr.trojanek@altran.com>
2018-04-12 21:07:59 +0200

    minor fix indentation in pointers paper example
    Use spaces instead of tabs; fix indentation of the RETURN statement.
    
    (no-tn-check)
    
    Change-Id: I57f435129affa39a4475367230dcd3a37a72da49

--- papers/AdaEurope_2018/figures/code/maxtree.adb
+++ papers/AdaEurope_2018/figures/code/maxtree.adb
@@ -12,17 +12,17 @@ type Rec is record
   Left, Right : Tree;
 end record;
 
-function Max (T : in Tree) return Integer is 
+function Max (T : in Tree) return Integer is
   Walker : access constant Rec := T; -- Walker observes T
   Max_Value : Natural := 0;
-begin     
+begin
   while Walker /= null loop
-	if Walker.Data > Max_Value then
-	  Max_Value := Walker.Data;
-	end if;
-	Walker := Walker.Right; -- assignment to Walker 
+    if Walker.Data > Max_Value then
+      Max_Value := Walker.Data;
+    end if;
+    Walker := Walker.Right; -- assignment to Walker
   end loop;
-return Max_Value;
+  return Max_Value;
 end Max;
 
   Tree_var : Owning_Tree_Ptr := Build_Tree(...);
@@ -32,5 +32,3 @@ begin
   Y := Max (Tree_var);
   (...)
 end maxtree;
-
-
