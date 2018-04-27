6d11a106f41c15d31219af20910b5784217b72c7
Sylvain Dailler <dailler@adacore.com>
2018-04-27 11:52:42 +0200

    R426-023 Add test
    This test checks that gnatwhy3 does not fail when there are two checks
    with the exact same reason and location.
    
    * testsuite/gnatprove/tests/R426-023__duplicate_vc/main_range.mlw
    Example file: the vc with "VC_RANGE_CHECK" reason is duplicated.
    
    * testsuite/gnatprove/tests/R426-023__duplicate_vc/test.out
    Results for the different VC as given by gnatwhy3.
    
    * testsuite/gnatprove/tests/R426-023__duplicate_vc/test.py
    This execute gnatwhy3 with cvc4 prover and with or without debug mode.
    It then parses the JSON output from gnatwhy3 and give only the id with
    their corresponding result (true or false). The association ID, Result
    should never change.
    
    * testsuite/gnatprove/tests/R426-023__duplicate_vc/why3.conf
    Minimal why3.conf.
    
    Change-Id: If8a5efba3c457f2969dc5b5b795cc77e734197ee

---
+++ testsuite/gnatprove/tests/R426-023__duplicate_vc/test.py
@@ -0,0 +1,63 @@
+from test_support import *
+import time
+import json
+
+# This test is for a bug of gnatwhy3 which, in debug mode, would reuse the
+# same files for different checks with the exact same location *and* reason
+# (when run in parallel).
+# This eventually lead to inconsistency issue when one of the check is proved
+# and not the other. The file main_range.mlw reproduce this behavior by defining
+# two checks with reason VC_RANGE_CHECK: one being provable and not the other.
+
+RESULTS="results"
+RESULT="result"
+ID="id"
+
+installdir = spark_install_path()
+bindir = os.path.join(installdir, 'libexec', 'spark', 'bin')
+Env().add_path(bindir)
+
+cmd = ["gnatwhy3",
+       "--timeout", "1",
+       "--steps", "0",
+       "--prover", "cvc4",
+       "--proof", "per_check",
+       "--debug",
+       "--force",
+       "-j", "2",
+       "--counterexample", "off",
+       "main_range.mlw"]
+
+result = Run (cmd, timeout=30)
+
+result_out = result.out
+j = json.loads(result_out)
+results = j[RESULTS]
+
+print ("Result per id are the following in debug mode:")
+
+for result in results:
+    print(str(result[ID]) + ":" + str(result[RESULT]))
+
+
+cmd = ["gnatwhy3",
+       "--timeout", "1",
+       "--steps", "0",
+       "--prover", "cvc4",
+       "--proof", "per_check",
+       "--standalone",
+       "--force",
+       "-j", "2",
+       "--counterexample", "off",
+       "main_range.mlw"]
+
+result = Run (cmd, timeout=30)
+
+result_out = result.out
+j = json.loads(result_out)
+results = j[RESULTS]
+
+print ("Result per id are the following in normal mode:")
+
+for result in results:
+    print(str(result[ID]) + ":" + str(result[RESULT]))
