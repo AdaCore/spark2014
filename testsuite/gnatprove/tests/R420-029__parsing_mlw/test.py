e1c49cc605a57639c6adb038fce336c43bf17016
Sylvain Dailler <dailler@adacore.com>
2018-04-27 12:00:08 +0200

    R420-029 Test for parsing of mlw file by gnatwhy3
    This test checks that an error is returned when an .mlw file  cannot be
    parsed by gnatwhy3.
    
    Change-Id: I0ffb4cb13459c7a4c55647a196172fda9df499fe

---
+++ testsuite/gnatprove/tests/R420-029__parsing_mlw/test.py
@@ -0,0 +1,9 @@
+from test_support import *
+
+
+installdir = spark_install_path()
+bindir = os.path.join(installdir, 'libexec', 'spark', 'bin')
+Env().add_path(bindir)
+
+process = Run(["gnatwhy3", "--debug", "incorrect.mlw"])
+print process.out
