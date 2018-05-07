ea56139ed91d91c19174bc14e9437b9fbc2ab5a9
Sylvain Dailler <dailler@adacore.com>
2018-05-07 10:52:44 +0200

    R507-001 Removing example
    This remove QA10-009 example as the original error does not occur anymore
    and the new unstability of the test is just a counterexample that changes
    accross platforms. We don't need this test anymore.
    
    * testsuite/gnatprove/tests/QA10-009__counterexample_in_gps/*
    Remove test
    
    Change-Id: I81992894fa1a835c0c820dd3b03fb14982fe0bae

--- testsuite/gnatprove/tests/QA10-009__counterexamples_in_gps/test.py
+++
@@ -1,6 +0,0 @@
-from test_support import *
-
-# This test is showing the difference of output between these two successive
-# commands. Using prove_all (or removing -j0) does not show the same behavior.
-gnatprove(opt=["-P", "test.gpr", "-j0", "-f"])
-gnatprove(opt=["-P", "test.gpr", "-j0"])
