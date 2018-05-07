731effd2d9bc38b9b24cb3aec262b9cc70fd7d34
Johannes Kanig <kanig@adacore.com>
2018-05-07 17:13:32 +0900

    (no-tn-check) smaller steps to improve test speed
    accepting 4 unproved VCs
    
    Change-Id: I87a03709147f21cf65a8fa4b79a5bd4dfa0cc85f

--- testsuite/gnatprove/tests/riposte__array_applications/test.py
+++ testsuite/gnatprove/tests/riposte__array_applications/test.py
@@ -3,6 +3,6 @@ from test_support import *
 contains_manual_proof = False
 
 def replay():
-    prove_all(steps=2500,procs=0,opt=["--no-axiom-guard"])
+    prove_all(procs=0,opt=["--no-axiom-guard"])
 
 prove_all(opt=["--no-axiom-guard"],replay=True)
