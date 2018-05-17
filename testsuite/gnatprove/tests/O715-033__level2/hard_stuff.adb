d728770cd8b8fa3afc0838272469f3cdfbdd5237
Florian Schanda <florian.schanda@altran.com>
2018-05-17 10:52:45 +0100

    Q817-011 proof - use cvc4 for floats
    Testsuite updates for the --level tests:
    
    - make the final problem much harder
    - make all level tests the same
    
    Hopefully this will resolve the diffs particularly for the "1 second"
    test.
    
    Change-Id: Ifdd66cfa7078c776edb04e31278a03a6a25d0779

--- testsuite/gnatprove/tests/O715-033__level2/hard_stuff.adb
+++ testsuite/gnatprove/tests/O715-033__level2/hard_stuff.adb
@@ -25,7 +25,7 @@ begin
       Copy : constant Float := X with Ghost;
    begin
       if X in 0.0 .. 1.0 then
-         X := X - X * X;
+         X := X - X * X * X * X;
          pragma Assert (X in 0.0 .. Copy);
       end if;
    end;
