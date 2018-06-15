ebdc2775519e3e801f89624d51fcbc977b5b4dd8
Johannes Kanig <kanig@adacore.com>
2018-06-15 10:17:45 +0900

    R529-008 improve test
    - actually check that the subdir is there
    - also check with object_dir set
    
    Change-Id: I63960a43dae715f7892534e94d11153bf44fc521

--- testsuite/gnatprove/tests/R529-008__subdirs/test.py
+++ testsuite/gnatprove/tests/R529-008__subdirs/test.py
@@ -1,2 +1,13 @@
 from test_support import *
+import os.path
 prove_all(codepeer=True, opt=["--subdirs", "toto"])
+
+my_dir = os.path.join('toto', 'gnatprove')
+assert os.path.exists(my_dir)
+assert os.path.isdir(my_dir)
+
+prove_all(codepeer=True, opt=["--subdirs", "toto"], project="test2.gpr")
+
+my_dir = os.path.join('obj', 'toto', 'gnatprove')
+assert os.path.exists(my_dir)
+assert os.path.isdir(my_dir)
