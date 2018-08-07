af71004f681f8a05485297ff81f1e254678a2482
Piotr Trojanek <piotr.trojanek@altran.com>
2018-08-07 11:20:06 +0200

    R805-001 minimize reproducer
    * testsuite/gnatprove/tests/R805-001__flow_generic/
    The Dummy function is not needed to demonstrate the problem.
    
    Change-Id: I19b5ae84eba9ad41ce9d361a15043032ed31dcb2

--- testsuite/gnatprove/tests/R805-001__flow_generic/main.adb
+++ testsuite/gnatprove/tests/R805-001__flow_generic/main.adb
@@ -3,7 +3,6 @@ procedure Main (Arg : Float) with Global => null is
    generic
       X : Float;
    package G with Initializes => null is
-      function Dummy return Integer is (0);
    end;
 
    package Outer with Initializes => null is
