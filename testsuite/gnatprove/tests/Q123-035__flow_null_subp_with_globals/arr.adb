22703282ace92367291993f19ba9a166b52e5921
Piotr Trojanek <piotr.trojanek@altran.com>
2018-08-09 13:41:29 +0200

    Q123-035 strip test to the bare minimum
    Looking at the CFGs generated for this test the problem happens in a call to
    a null procedure, whose Global contract is not verified (since null
    procedures have no body). The rest is unimportant.
    
    * testsuite/gnatprove/tests/Q123-035__flow_null_subp_with_globals/
    Minimize; update output (it still info messages, which are wrong, and lacks
    some checks that we would expect).
    
    Change-Id: I6b1b67b41c1a762489bccdbc0d0f27fa9264f6bc

--- testsuite/gnatprove/tests/Q123-035__flow_null_subp_with_globals/arr.adb
+++ testsuite/gnatprove/tests/Q123-035__flow_null_subp_with_globals/arr.adb
@@ -1,39 +1,30 @@
 package body Arr is
 
-   type Length_T is range 0 .. 1000;
-   subtype Index_T is Length_T range 1 .. Length_T'Last;
-
-   type T is array (Index_T) of Integer;
+   type T is array (0 .. 1000) of Integer;
 
    Buffer : T;
 
    --  We pretend to initialize Buffer through
    --  Fake_Initialize_Buffer_Buffer_With_Global setting Buffer as a global
    --  output but the procedure is a null procedure.
-   procedure Initialize_With_Global (L : Length_T)
-     with Global => (Output => (Buffer))
+   procedure Initialize_With_Global
+     with Global => (Output => Buffer)
    is
       procedure Fake_Initialize_Buffer_With_Global is null
         with Global => (Output => Buffer);
    begin
       Fake_Initialize_Buffer_With_Global;
-      for I in Index_T range Index_T'First .. L loop
-         Buffer (I) := Integer (I);
-      end loop;
    end Initialize_With_Global;
 
    --  Similar to the above, but with Depends (instead of Global) on the inner
    --  subprogram.
-   procedure Initialize_With_Depends (L : Length_T)
-     with Global => (Output => (Buffer))
+   procedure Initialize_With_Depends
+     with Global => (Output => Buffer)
    is
       procedure Fake_Initialize_Buffer_With_Depends is null
-        with Depends => (Buffer => L);
+        with Depends => (Buffer => null);
    begin
       Fake_Initialize_Buffer_With_Depends;
-      for I in Index_T range Index_T'First .. L loop
-         Buffer (I) := Integer (I);
-      end loop;
    end Initialize_With_Depends;
 
 end Arr;
