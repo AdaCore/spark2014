74dda89875b7cc56738d1927e492109cbb63260b
Johannes Kanig <kanig@adacore.com>
2018-09-05 18:06:19 +0900

    R903-021 update sparkskein test with better flow heuristic
    * testsuite/gnatprove/tests/sparkskein/
    With the enhanced heuristic for initializing arrays with non-static bounds
    in loops, the pragma Annotate is not needed.
    
    Change-Id: I452e69fff64b2997c10ce47acefe2a8272585d65

--- testsuite/gnatprove/tests/sparkskein/skein.adb
+++ testsuite/gnatprove/tests/sparkskein/skein.adb
@@ -242,9 +242,9 @@ is
 --                   Add_In_Range (Src_Offset, Dst'Last * 8 + 7) and
 --                   Src_Offset + Dst'Last * 8 + 7 <= Src'Last
    is
-      pragma Annotate
-        (GNATprove, False_Positive, """Dst"" might not be initialized",
-         "array Dst is initialized from Dst'First = 0 to Dst'Last");
+--      pragma Annotate
+--        (GNATprove, False_Positive, """Dst"" might not be initialized",
+--         "array Dst is initialized from Dst'First = 0 to Dst'Last");
       -- so even if the loop is not entered, all elements of Dst are
       -- initialized (zero elements in that case)
    begin
