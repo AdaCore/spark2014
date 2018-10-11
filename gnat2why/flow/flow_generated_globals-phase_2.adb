de5f628908f49a252742a8f0385fbacf2b0ad6e7
Piotr Trojanek <piotr.trojanek@altran.com>
2018-10-11 19:12:44 +0200

    RA10-010 replace low-level checks with Defining_Entity
    Apparently the low-level query that meant to give us an entity of the
    main-like subprogram didn't work on N_Subprogram_Declaration of a child
    subprogram; now it is replaced with Defining_Entity, which is designed to
    work reliably on corner-case ASTs.
    
    * gnat2why/flow/flow_generated_globals-phase_2.adb
    (Detect_Main_Subprogram): rely on Defining_Entity (needed to fix the crash)
     and Unique_Defining_Entity (for consistency).
    
    * testsuite/gnatprove/tests/RA10-010__flow_main_child_spec/
    Enable; no output (as expected).
    
    Change-Id: Iab4a794ceb6fb3020c4c100101918706f4e8418a

--- gnat2why/flow/flow_generated_globals-phase_2.adb
+++ gnat2why/flow/flow_generated_globals-phase_2.adb
@@ -585,12 +585,10 @@ package body Flow_Generated_Globals.Phase_2 is
          begin
             case Nkind (U) is
             when N_Subprogram_Body =>
-               S := (if Acts_As_Spec (U)
-                     then Defining_Unit_Name (Specification (U))
-                     else Corresponding_Spec (U));
+               S := Unique_Defining_Entity (U);
 
             when N_Subprogram_Declaration =>
-               S := Defining_Unit_Name (Specification (U));
+               S := Defining_Entity (U);
 
             when N_Package_Body =>
                if Nkind (Original_Node (U)) in N_Subprogram_Instantiation then
