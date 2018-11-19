dbcee90f68df2ea8344230f6e0678b1982131773
Johannes Kanig <kanig@adacore.com>
2018-11-19 14:13:00 +0100

    RB17-010 file test
    Change-Id: I593379a563f15c7d3dd8b9f2e2e824428a23ef11

---
+++ testsuite/gnatprove/tests/RB17-010__predicate/predicate.ads
@@ -0,0 +1,13 @@
+package Predicate                                                                                                                              
+with SPARK_Mode
+is
+   subtype P_Type is String
+   with
+      Predicate => P_Type'Length > 2;
+
+   procedure P1 (P : in out P_Type);
+
+   procedure P2 (P : out P_Type)
+    with Pre => P'Length > 2;
+
+end Predicate;
