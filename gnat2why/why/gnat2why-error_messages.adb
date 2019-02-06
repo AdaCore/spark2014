ce0910b6e22057a74aa594a3792064bd97383c2e
Piotr Trojanek <piotr.trojanek@altran.com>
2019-02-06 11:06:56 +0100

    minor do not expect entries with No_Return
    Routine Is_Error_Signaling_Procedure returns True only for some procedures,
    not for entries (because entries can't be annotated with No_Return).
    
    (no-tn-check)
    
    * gnat2why/why/gnat2why-error_messages.adb
    (Not_Proved_Message): do not call Is_Error_Signaling_Procedure on entries.
    (Proved_Message): same as above.
    
    Change-Id: I62ee54c972f481203401b96bb492e14e525dcfb6

--- gnat2why/why/gnat2why-error_messages.adb
+++ gnat2why/why/gnat2why-error_messages.adb
@@ -638,8 +638,7 @@ package body Gnat2Why.Error_Messages is
          when VC_Default_Initial_Condition =>
             return "default initial condition might fail";
          when VC_Precondition              =>
-            if Nkind (Node) in N_Entry_Call_Statement
-                             | N_Procedure_Call_Statement
+            if Nkind (Node) = N_Procedure_Call_Statement
               and then Is_Error_Signaling_Procedure (Get_Called_Entity (Node))
             then
                declare
@@ -951,8 +950,7 @@ package body Gnat2Why.Error_Messages is
          when VC_Default_Initial_Condition =>
             return "default initial condition proved";
          when VC_Precondition              =>
-            if Nkind (Node) in N_Entry_Call_Statement
-                             | N_Procedure_Call_Statement
+            if Nkind (Node) = N_Procedure_Call_Statement
               and then Is_Error_Signaling_Procedure (Get_Called_Entity (Node))
             then
                declare
