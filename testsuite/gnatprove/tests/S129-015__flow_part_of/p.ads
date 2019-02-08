c6ee5b01dfa8ab18358aa56258ce2891959ab1ce
Piotr Trojanek <piotr.trojanek@altran.com>
2019-02-08 19:22:42 +0100

    S129-015 adapt test to avoid uninteresting diff
    Adapt after commit for S114-023, i.e. "fix Up & Down projections wrt to
    Part_Of constituents".
    
    * testsuite/gnatprove/tests/S129-015__flow_part_of/p.ads
    Add Initializes contract.
    
    Change-Id: I45650c72852aa8d99ec93dcc0847af5f2f3fa34c

--- testsuite/gnatprove/tests/S129-015__flow_part_of/p.ads
+++ testsuite/gnatprove/tests/S129-015__flow_part_of/p.ads
@@ -1,4 +1,4 @@
-package P with Abstract_State => State is
+package P with Abstract_State => State, Initializes => State is
    procedure Flip with Global => (In_Out => State);
 private
    X : Boolean := True with Part_Of => State;
