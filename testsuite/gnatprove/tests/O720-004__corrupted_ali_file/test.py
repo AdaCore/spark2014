5f1fa8788c363df96c6889e8d68693cbb2b47aa9
Piotr Trojanek <piotr.trojanek@altran.com>
2019-02-12 12:17:31 +0100

    R502-058 readapt tests for corrupted ALI files
    * testsuite/gnatprove/tests/P307-006__tool_bad_cleanup/
    Preserve ALI timestamp, as otherwise it is recreated when rerunning the tool
    and the entire test becomes useless. Change test discriminant to packaged,
    because the gprbuild decides when to rerun phase1 based on a tool version
    tag that only appears in wavefront & releases, not in developer builds.
    
    * testsuite/gnatprove/tests/O720-004__corrupted_ali_file/
    Preserve ALI timestamp; remove unreferenced "target_ali2" object.
    
    Change-Id: Ia06788f0fdd5acd06b67107fb5303d610cdf18fe

--- testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.py
+++ testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.py
@@ -1,19 +1,20 @@
 from test_support import *
-import os.path
+import os
 import time
 
 #this test corrupts an ALI file and checks that gnatprove detects the issue.
 
 target_ali = os.path.join("gnatprove", "phase1", "test.ali")
-target_ali2 = os.path.join("gnatprove", "test.ali")
 
 def truncate_four_lines(a):
+    statinfo = os.stat(target_ali)
     with open(a,"r") as f:
         content = f.readlines()
     content = content[:-4]
     with open(a,"w") as f:
         for line in content:
             f.write(line)
+    os.utime(target_ali, (statinfo.st_atime, statinfo.st_mtime))
 
 prove_all()
 truncate_four_lines(target_ali)
