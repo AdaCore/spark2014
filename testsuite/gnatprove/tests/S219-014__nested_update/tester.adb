06d830a008c9f979163aca0fb8fd187963d38a49
Claire Dross <dross@adacore.com>
2019-02-21 16:36:55 +0100

    S219-014 store discriminants in symbol map before translating 'Update
    When translating component associations, we may get conversions to types
    with discriminant dependent constraints. To avoid references to unknown
    entities, store identifiers for the discriminants in the symbol table.
    
    * gnat2why/why/gnat2why-expr.adb
    (Transform_Attr): Store discriminants in the symbol table before
     translating the component associations.
    
    * S219-014__nested_update
    File customer code.
    
    Change-Id: Ie43149da7f3804f78a3318ac791c84079f71f283

---
+++ testsuite/gnatprove/tests/S219-014__nested_update/tester.adb
@@ -0,0 +1,33 @@
+pragma Spark_Mode (On);
+
+with Ada.Text_Io;
+with Generic_Ring_Buffer;
+
+
+procedure Tester
+is
+
+   package Ring_Buffer is new Generic_Ring_Buffer
+     (Element_Type => Integer);
+   use Ring_Buffer;
+   
+   
+   X : Ring_Buffer_Type (4);
+   Element : Integer;
+   Counter : Integer;
+begin
+--   X.Items := (others => 0);
+   Put (X, 1);
+   Get (X, Element);
+   Put (X, 2);
+   Put (X, 3);
+   Put (X, 4);
+   Put (X, 5);
+   
+   Counter := Size (X); -- defined in Generic_Ring_Buffer
+   for I in Natural range 1 .. Counter loop
+      Get (X, Element);
+      Ada.Text_Io.Put_Line (Integer'Image (Element));
+   end loop;
+
+end Tester;
