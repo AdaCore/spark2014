16bc00dcbd01362f1df3076d1f470c76f09d7478
Piotr Trojanek <piotr.trojanek@altran.com>
2019-03-11 10:11:32 +0100

    S311-005 disable counterexamples when using --cache switch
    A temporary workaround for developers that use memcached.
    
    * testsuite/gnatprove/tests/Q105-001__hidden_comp_default_check/test.py
    Disable counterexamples when running the testsuite with --cache.
    
    Change-Id: I7caba2a9417d20a5d459b707c9927329fdfa9a28

--- testsuite/gnatprove/tests/Q105-001__hidden_comp_default_check/test.py
+++ testsuite/gnatprove/tests/Q105-001__hidden_comp_default_check/test.py
@@ -1,3 +1,11 @@
 from test_support import *
+import os
 
-prove_all()
+# With both memcache and countexample active we get a crash,
+# so when using cache switch counterexamples off.
+if "cache" in os.environ:
+    ce = False
+else:
+    ce = True
+
+prove_all(counterexample=ce)
