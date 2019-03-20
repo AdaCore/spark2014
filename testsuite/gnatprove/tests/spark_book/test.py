3ae54b6c218df6d54252a690debe3ec0ed1e6954
Johannes Kanig <kanig@adacore.com>
2019-03-20 11:26:08 +0900

    S311-019 hopefully get same output on all platforms
    Change-Id: Idc57ac8025f116aa19410dcdff61c96736fd5d89

--- testsuite/gnatprove/tests/spark_book/test.py
+++ testsuite/gnatprove/tests/spark_book/test.py
@@ -2,4 +2,4 @@ from test_support import *
 
 # Do not use Z3, as the step limit fluctuates too much between platforms, thus
 # making it impossible to share a common expected output when Z3 is used.
-prove_all(prover=["cvc4", "altergo"],opt=["-U"], procs=4, counterexample=False)
+prove_all(prover=["cvc4", "altergo"],opt=["-U"], counterexample=False, steps=400)
