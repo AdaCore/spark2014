2a6c2115f680a890a97b6b52204f04e2a8f1b0eb
Yannick Moy <moy@adacore.com>
2019-03-22 13:00:35 +0100

    S313-020 Adapt test to removal of special Z3 configuration
    Change-Id: I517e5f478cf00e2cce6e83cb00696c33a730816e

--- testsuite/gnatprove/tests/proofinuse__floating_point/test.py
+++ testsuite/gnatprove/tests/proofinuse__floating_point/test.py
@@ -1,9 +1,3 @@
 from test_support import *
 
-# The idea of this test is to exercise FP support.
-#
-# To best exercise the floating point support we use Z3 where we strip out
-# all quantifiers.
-#
-# Once CVC4 supports floats we will include it here too.
-prove_all(prover=["z3_noquant,altergo"], steps=5000, counterexample=False)
+prove_all(prover=["z3,cvc4,altergo"], counterexample=False)
