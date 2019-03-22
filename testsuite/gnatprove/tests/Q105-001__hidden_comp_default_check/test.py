6ffe3e28e6f8ae770526e39b36882abb5db29a10
Piotr Trojanek <piotr.trojanek@altran.com>
2019-03-22 12:36:33 +0100

    S311-005 reenable counterexamples when using --cache switch
    * testsuite/gnatprove/tests/Q105-001__hidden_comp_default_check/test.py
    Counterexamples on this test should work fine again.
    
    Change-Id: Iaa33cba04c03629f5e8a7d761ee7fbd0f6e352fc

--- testsuite/gnatprove/tests/Q105-001__hidden_comp_default_check/test.py
+++ testsuite/gnatprove/tests/Q105-001__hidden_comp_default_check/test.py
@@ -1,11 +1,3 @@
 from test_support import *
-import os
 
-# With both memcache and countexample active we get a crash,
-# so when using cache switch counterexamples off.
-if "cache" in os.environ:
-    ce = False
-else:
-    ce = True
-
-prove_all(counterexample=ce)
+prove_all()
