36eefe8cf356c6e4badaf29cf8a86badf8a591c6
Yannick Moy <moy@adacore.com>
2019-03-25 17:32:46 +0100

    S313-020 Update test with cvc4_axioms removed
    Removal of special prover configuration makes 8 checks not
    proved anymore. Increasing steps up to 20_000 only recovers
    one check, so not used in testing.
    
    Change-Id: I4012b21b0e35e0e972709bc3edc165405d280839

--- testsuite/gnatprove/tests/bitwalker/test.py
+++ testsuite/gnatprove/tests/bitwalker/test.py
@@ -4,10 +4,10 @@ contains_manual_proof = False
 
 def replay():
     prove_all(opt=["--no-axiom-guard"],
-              prover=["cvc4_axioms", "z3", "altergo"],
+              prover=["cvc4", "z3", "altergo"],
               procs=10,
               steps=5000)
 
 prove_all(opt=["--no-axiom-guard"],
-          prover=["cvc4_axioms", "z3", "altergo"],
+          prover=["cvc4", "z3", "altergo"],
           replay=True)
