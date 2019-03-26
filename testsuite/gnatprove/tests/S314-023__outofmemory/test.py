b10734fe3724724b39fddbf477c0f3b7920fecb9
Johannes Kanig <kanig@adacore.com>
2019-03-26 05:22:51 -0400

    S314-023 remove test (not reliable)
    Change-Id: I51c8be80cfc4950a36bf04f013abd7468292eb09

--- testsuite/gnatprove/tests/S314-023__outofmemory/test.py
+++
@@ -1,8 +0,0 @@
-import resource
-from test_support import *
-
-soft, hard = resource.getrlimit(resource.RLIMIT_DATA)
-# setting memlimit to 128MB
-resource.setrlimit(resource.RLIMIT_DATA, (2 ** 27, hard))
-
-prove_all()
