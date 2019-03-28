bf31491e3f38189b3bc012e97212494bef981d13
Claire Dross <dross@adacore.com>
2019-03-28 12:35:37 +0100

    S327-020 file test
    Change-Id: I9fe6d69b32ba6445d496cb4c6e2d0223bdc89597

---
+++ testsuite/gnatprove/tests/S327-020__loop_inv/lp.ads
@@ -0,0 +1,10 @@
+
+package Lp with
+   SPARK_Mode
+is
+
+   type U64 is mod 2 ** 64;
+
+   function Power (I : Natural) return Natural;
+
+end Lp;
---
+++ testsuite/gnatprove/tests/S327-020__reduced/main.adb
@@ -0,0 +1,9 @@
+with Interfaces; use Interfaces;
+procedure Main is
+
+   type U64 is mod 2 ** 64;
+   type N_Array is array (Integer range <>) of Natural;
+   Powers : constant N_Array := (63, 65);
+begin
+   pragma Assert (for all K of Powers => 2 ** K in U64'Range); --@ASSERT:FAIL
+end Main;
