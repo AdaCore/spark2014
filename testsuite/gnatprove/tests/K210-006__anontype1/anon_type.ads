20985bf361f8457163435bffbcf523376afb5492
Piotr Trojanek <piotr.trojanek@altran.com>
2019-04-08 22:47:05 +0200

    minor tune whitespace in test
    (no-tn-check)
    
    Change-Id: I317edac2bacc5228bec543e99e6964b5b456d8ad

--- testsuite/gnatprove/tests/K210-006__anontype1/anon_type.ads
+++ testsuite/gnatprove/tests/K210-006__anontype1/anon_type.ads
@@ -2,7 +2,7 @@
 --  Examples for fix the anonymous type in object declarations or definitions
 
 package Anon_Type is
-   type Vector is array(Integer range <>) of Float;
+   type Vector is array (Integer range <>) of Float;
    function Increment (Var_In : in Integer) return Integer
      with Post => Increment'Result = Var_In + 1;
 end Anon_Type;
