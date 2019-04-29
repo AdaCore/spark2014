184db857279738768da433e2a06de126b608f16c
Joffrey Huguet <huguet@adacore.com>
2019-04-29 11:29:31 +0200

    S206-030 Update test output after additional changes
    Change-Id: I69f7db82447b354d1e7d7db91397a73e200074a7

--- testsuite/gnatprove/tests/L126-018__image_value/p.adb
+++ testsuite/gnatprove/tests/L126-018__image_value/p.adb
@@ -4,15 +4,15 @@ procedure P (Y : in out Integer) is pragma SPARK_Mode (On);
    type T is range 1 .. 10;
    X : T := 1;
    Unb_Str : Unbounded_String := Null_Unbounded_String;
-   Buffer : String(1..10);
+   Buffer : String (1 .. 10);
    Last : Natural := 10;
 begin
    while Last = 10 loop
       Get_Line (Buffer, Last);
-      Unb_Str := Unb_Str & Buffer(1..Last);
+      Unb_Str := Unb_Str & Buffer (1 .. Last);
    end loop;
    declare
-      S : String := To_String(Unb_Str);
+      S : String := To_String (Unb_Str);
    begin
       Put_Line (T'Image (X));
       X := T'Value (S);
