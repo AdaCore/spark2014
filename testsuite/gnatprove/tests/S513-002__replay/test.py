6404693f1daaf9003862b9cd21e9356475fa4b13
Sylvain Dailler <dailler@adacore.com>
2019-05-13 14:40:19 +0200

    S416-010 Change the reproducer to avoid using limit-subp
    To rule out potential problems involving only the use of --limit-subp,
    this commit changes the reproducer of this test (which still fail).
    
    * testsuite/gnatprove/tests/S513-002__replay/red_black_trees*
    * testsuite/gnatprove/tests/S513-002__replay/search_trees*
    
    Removed files
    
    * testsuite/gnatprove/tests/S513-002__replay/binary_trees.*
    * testsuite/gnatprove/tests/S513-002__replay/tree_model.*
    
    Pruned files
    
    * testsuite/gnatprove/tests/S513-002__replay/test.py
    Removed calls to --limit-subp
    
    * testsuite/gnatprove/tests/S513-002__replay/test.out
    Update output
    
    Change-Id: I714d609a855c91939f70e6264b258248238fe6c2

--- testsuite/gnatprove/tests/S513-002__replay/test.py
+++ testsuite/gnatprove/tests/S513-002__replay/test.py
@@ -1,5 +1,5 @@
 from test_support import *
 
-prove_all(opt=["--level=4", "--limit-subp=binary_trees.adb:10", "--no-axiom-guard", "--no-counterexample"])
+prove_all(opt=["--level=4", "--no-axiom-guard", "--no-counterexample"])
 print "--------------------------------------"
-prove_all(opt=["--limit-subp=binary_trees.adb:10", "--no-axiom-guard", "--no-counterexample"], replay=True)
+prove_all(opt=["--no-axiom-guard", "--no-counterexample"], replay=True)
