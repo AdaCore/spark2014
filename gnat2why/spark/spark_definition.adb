d568f88254ff250ccb33351a2ebf84db452d9ddc
Piotr Trojanek <piotr.trojanek@altran.com>
2019-05-14 22:07:26 +0200

    minor whitespace fix
    (no-tn-check)
    
    Change-Id: I37bae7b13397e74366048f2efd821bcce6254d64

--- gnat2why/spark/spark_definition.adb
+++ gnat2why/spark/spark_definition.adb
@@ -1138,6 +1138,7 @@ package body SPARK_Definition is
      (No (Current_SPARK_Pragma)
       and not Violation_Detected
       and not Inside_Actions);
+
    ----------
    -- Mark --
    ----------
