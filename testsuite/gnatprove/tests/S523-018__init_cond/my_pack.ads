ff4271a9097d1a71937b0f6e3326826539b52504
Claire Dross <dross@adacore.com>
2019-05-24 14:55:48 +0200

    S523-018 file test
    Change-Id: I743bf108f857dbc8d227e9560452ab622b3e6031

---
+++ testsuite/gnatprove/tests/S523-018__init_cond/my_pack.ads
@@ -0,0 +1,9 @@
+package My_Pack with SPARK_Mode,
+  Initializes => X,
+  Initial_Condition => X_True
+ is
+   pragma Elaborate_Body;
+   X : Boolean;
+   function X_True return Boolean is (X);
+   procedure Change_X;
+end My_Pack;
