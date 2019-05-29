052d06cd4c31abd9811074f68ea20f5c18b12818
Piotr Trojanek <piotr.trojanek@altran.com>
2019-05-29 19:02:00 +0200

    S529-037 add test with crash in inlining for proof
    * testsuite/gnatprove/tests/S529-037__inlining/
    Add; disabled, because of crash. Derived from ACATS.
    
    Change-Id: If2c2045dfcac08961a6dbc53c03171b0c13e2fce

---
+++ testsuite/gnatprove/tests/S529-037__inlining/c37102b.adb
@@ -0,0 +1,23 @@
+procedure C37102B is
+
+   FUNCTION IDENT_INT       -- AN IDENTITY FUNCTION FOR TYPE INTEGER.
+      ( X : INTEGER         -- THE ARGUMENT.
+      ) RETURN INTEGER      -- X.
+   IS (X) WITH GLOBAL => NULL;
+
+   function F (D : Integer) return Integer is
+   begin
+      return IDENT_INT (D);
+   end F;
+
+   type ARR is array (Integer range <>) of Integer;
+
+   G : Integer := 0;
+
+   type Q is record
+      K : ARR (1 .. F (D => G));
+   end record;
+
+begin
+   null;
+end C37102B;
