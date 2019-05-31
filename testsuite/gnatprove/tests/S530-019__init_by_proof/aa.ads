cb0d514f6147cb42985422f247f3e3dff161dfb4
Claire Dross <dross@adacore.com>
2019-05-31 10:03:00 +0200

    S530-019 file test
    Change-Id: I222cb029bc54ba867d0f5769098008ecd40162e3

---
+++ testsuite/gnatprove/tests/S530-019__init_by_proof/aa.ads
@@ -0,0 +1,14 @@
+package AA
+   with SPARK_Mode
+is
+   Global_AA        : Natural ;
+
+   procedure initGlobalsAA(status : out Natural)  with
+     Global => (Output => Global_AA);
+
+
+   procedure UseAA(X : in out Natural) with
+     Global => (Input => Global_AA);
+   --  Same as: "with Global => Global_Var;"
+
+end AA;
