8981d1272d32a979337922368cb2b4353c205564
Piotr Trojanek <piotr.trojanek@altran.com>
2019-06-03 20:32:06 +0200

    minor refactor chained if-then-elsif-else condition
    (no-tn-check)
    
    * gnat2why/flow/flow-analysis.adb
    (Search_Expr): refactor condition in patter-matching style to (hopefully)
     make it easier to understand; reword comment.
    
    Change-Id: If382305a7716926e8c485d438443a5a77c9950ec

--- gnat2why/flow/flow-analysis.adb
+++ gnat2why/flow/flow-analysis.adb
@@ -347,9 +347,8 @@ package body Flow.Analysis is
                         Normal_Use);
 
       function Search_Expr (N : Node_Id) return Traverse_Result;
-      --  This function sets First_Use to the given node if it
-      --  contains the variable we're looking for. If not, we abort
-      --  the search.
+      --  Sets First_Use to the given node if it contains the variable we're
+      --  looking for. If not, we abort the search.
 
       -----------------
       -- Search_Expr --
@@ -357,28 +356,32 @@ package body Flow.Analysis is
 
       function Search_Expr (N : Node_Id) return Traverse_Result is
       begin
-         if Nkind (N) not in N_Subprogram_Call
-                           | N_Entry_Call_Statement
-                           | N_Expanded_Name
-                           | N_Identifier
-                           | N_Selected_Component
+         if Nkind (N) in N_Subprogram_Call
+                       | N_Entry_Call_Statement
+                       | N_Expanded_Name
+                       | N_Identifier
+                       | N_Selected_Component
          then
-            --  Calling Get_Variables can be very slow. Let's only do it on
-            --  nodes that actually make sense to flag up in an check/info
-            --  message from flow; i.e. nodes that describe a
-            --  variable/constant or might use a global.
-            return OK;
-         elsif Get_Variables (N,
+            if Get_Variables (N,
                               Scope                => Scope,
                               Reduced              => not Precise,
                               Assume_In_Expression => False,
                               Fold_Functions       => False,
                               Use_Computed_Globals => True).Contains (Var_Tgt)
-         then
-            First_Use := N;
-            return OK;
+            then
+               First_Use := N;
+               return OK;
+            else
+               return Skip;
+            end if;
+
+         --  Calling Get_Variables can be very slow. Let's only do it on nodes
+         --  that actually make sense to flag up in an check/info message from
+         --  flow; i.e. nodes that describe a variable/constant or might use a
+         --  global.
+
          else
-            return Skip;
+            return OK;
          end if;
       end Search_Expr;
 
