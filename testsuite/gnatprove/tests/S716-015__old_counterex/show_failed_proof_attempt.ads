ca8911e80c6b7ff787302fc78d59fffd0d65d5fd
Sylvain Dailler <dailler@adacore.com>
2019-08-01 14:22:15 +0200

    S716-015 Add test
    Second part of adding test for S716-015
    
    * testsuite/gnatprove/tests/S716-015__old_counterex/test.out
    Update output
    
    * testsuite/gnatprove/tests/S716-015__old_counterex/show_failed_*
    Add several identical functions
    
    Change-Id: Iedcbc9b9e84b158957a616e8722c15821dd376f4

--- testsuite/gnatprove/tests/S716-015__old_counterex/show_failed_proof_attempt.ads
+++ testsuite/gnatprove/tests/S716-015__old_counterex/show_failed_proof_attempt.ads
@@ -2,7 +2,22 @@ package Show_Failed_Proof_Attempt with SPARK_Mode is
 
    C : Natural := 100;
 
-   procedure Increase (X : in out Natural) with
-     Post => (if X'Old < C then X > X'Old else X = C); -- @COUNTEREXAMPLE
+   procedure Increase (X : in out Natural) with Post => (if X'Old < C then
+                X > X'Old
+              else X = C); -- @COUNTEREXAMPLE
+
+   procedure Increase2 (X : in out Natural)
+     with Post => (if X'Old < C then
+                     X > X'Old
+                   else X = C); -- @COUNTEREXAMPLE
+
+   procedure Increase3 (X : in out Natural)
+
+     with Post => (if X'Old < C then
+                     X > X'Old
+                   else X = C); -- @COUNTEREXAMPLE
+
+   procedure Increase4 (X : in out Natural)
+     with Post => (if X'Old < C then X > X'Old else X = C); -- @COUNTEREXAMPLE
 
 end Show_Failed_Proof_Attempt;
