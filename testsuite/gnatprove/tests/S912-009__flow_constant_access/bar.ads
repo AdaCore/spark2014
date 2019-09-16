554e0cdad178d5fc19c08d8e0512c8c6e0a7f82c
Piotr Trojanek <piotr.trojanek@altran.com>
2019-09-16 14:45:13 +0200

    S912-009 extend test with assignment via a subprogram call
    * testsuite/gnatprove/tests/S912-009__flow_constant_access/
    Add a similar test, but with an assignment via a subprogram call.
    
    Change-Id: Ie8169b448206c50211a2020a2978f8ec9f3d423a

---
+++ testsuite/gnatprove/tests/S912-009__flow_constant_access/bar.ads
@@ -0,0 +1,13 @@
+package Bar with SPARK_Mode is
+   type T is private;
+   C : constant T;
+   procedure Inc (Int : in out Integer);
+   function Get return Integer;
+private
+   type Int_Acc is access Integer;
+   type T is record
+      F : Int_Acc;
+   end record;
+   C_F : constant Int_Acc := new Integer'(0);
+   C : constant T := (F => C_F);
+end;
