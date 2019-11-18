62d90f43a1ef4fe11df6068ec5823629955cbff6
Johannes Kanig <kanig@adacore.com>
2019-11-18 19:00:33 +0900

    SA17-038 remove useless test
    Change-Id: I66d30d684affc60a996ed793aa4b6fee1364aee5

--- testsuite/gnatprove/tests/M326-011__trace/test.ads
+++
@@ -1,6 +0,0 @@
-package Test is
-
-  function Test (X : Integer) return Integer
-    with Post => Test'Result = 1;
-
-end Test;
--- testsuite/gnatprove/tests/M326-011__trace/test2.ads
+++
@@ -1,6 +0,0 @@
-package Test2 is
-
-  function Test (X : Integer) return Integer
-    with Post => Test'Result = 1;
-
-end Test2;
