4001cbd1e6b577684d13b5587f0123b8e8eb2349
Piotr Trojanek <piotr.trojanek@altran.com>
2019-11-22 21:15:01 +0100

    P609-046 enable test whose output is as expected
    Most likely this test has been fixed by:
    
      commit d8b22cba40c80ca5e1e3a999f9dc8b837b4a0543
      Author: Simon Buist <simon.buist@altran.com>
      Date:   Tue Oct 1 13:29:35 2019
    
          P609-046 down-project abstract states with unambiguous refinements
    
    because now we don't generate Refined_Global for Game_Reset, since its
    Global contract has only outputs, which have unambiguous refinement.
    
    * testsuite/gnatprove/tests/OA20-016__refined_global/
    Reindent and remove extra parens; enable, because the output is as expected.
    
    Change-Id: I568bf53239e2003764b10a3cb48853384db38029

--- testsuite/gnatprove/tests/OA20-016__refined_global/repro.ads
+++ testsuite/gnatprove/tests/OA20-016__refined_global/repro.ads
@@ -1,24 +1,24 @@
 package Repro with
-  Abstract_State => Game_State,
-  Initializes    => Game_State
+    Abstract_State => Game_State,
+    Initializes    => Game_State
 is
-  type Cell is (Empty, I, O, J, L, S, T, Z);
+   type Cell is (Empty, I, O, J, L, S, T, Z);
 
-  subtype X_Coord is Integer range 1 .. 10;
-  subtype Y_Coord is Integer range 1 .. 20;
+   subtype X_Coord is Integer range 1 .. 10;
+   subtype Y_Coord is Integer range 1 .. 20;
 
-  type Line is array (X_Coord) of Cell with Pack;
-  type Board is array (Y_Coord) of Line;
+   type Line is array (X_Coord) of Cell with Pack;
+   type Board is array (Y_Coord) of Line;
 
-  function Is_Empty (B : Board) return Boolean is
-      (for all Y in Y_Coord => (for all X in X_Coord => B (y)(X) = Empty))
-  with Ghost;
+   function Is_Empty (B : Board) return Boolean is
+     (for all Y in Y_Coord => (for all X in X_Coord => B (Y)(X) = Empty))
+     with Ghost;
 
-  function Get_Board return Board
-    with Global => (Input => Game_State);
+   function Get_Board return Board
+   with Global => (Input => Game_State);
 
-  procedure Game_Reset
-    with
-      Global => (Output => (Game_State)),
-      Post  => Is_Empty (Get_Board);
+   procedure Game_Reset
+   with
+     Global => (Output => Game_State),
+     Post   => Is_Empty (Get_Board);
 end Repro;
