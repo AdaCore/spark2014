df84e65df8e0dd8cd4ad95272de95f79f92b25fd
Piotr Trojanek <piotr.trojanek@altran.com>
2019-12-02 21:39:02 +0100

    minor cleanup whitespace in test
    (no-tn-check)
    
    * testsuite/gnatprove/tests/R320-025__self_in_type_bound/
    Just to make test more readable.
    
    Change-Id: I788a2d548752807854ed1dd0627fd999388d86fe

--- testsuite/gnatprove/tests/R320-025__self_in_type_bound/p.adb
+++ testsuite/gnatprove/tests/R320-025__self_in_type_bound/p.adb
@@ -1,11 +1,9 @@
 package body P is
-
    protected body Outer_Type is
       function Outer_Fun (Inner_data : Integer) return Integer is
-
-               type T is new Integer range Inner_Data .. Outer_Data;
+         type T is new Integer range Inner_data .. Outer_Data;
       begin
          return 0;
-      end;
-   end;
-end;
+      end Outer_Fun;
+   end Outer_Type;
+end P;
