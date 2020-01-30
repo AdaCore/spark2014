69a817d7c48a351cbbe0f32e9870c50ab7dfc6e9
Johannes Kanig <kanig@adacore.com>
2020-01-30 14:48:39 +0900

    (no-tn-check) remove git clean command
    This command is very aggressive and could delete user files
    
    Change-Id: I7845fa867740a38bfc788f1bafa31282cc49a9e7

--- benchmark_script/create_benchmarks.sh
+++ benchmark_script/create_benchmarks.sh
@@ -6,6 +6,5 @@ RESULTS=${PWD}/data
 mkdir -p ${RESULTS}
 
 cd ../testsuite/gnatprove
-git clean -xdf
 ./run-tests --debug --benchmarks -j${JOBS} \
             --temp-dir=${RESULTS} --disable-cleanup
