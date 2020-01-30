346e29a249c9a1cc0e83d86e5ad9f85436990cde
Johannes Kanig <kanig@adacore.com>
2020-01-30 09:15:05 +0900

    T128-016 remove papers and slides from spark2014 repo
    Change-Id: Iecabf7a4ade57c4bee4b8965bc77dcf907325c2d

--- slides/Advisory_Panel_20131126/MB19-033_ap_demo/parent.ads
+++
@@ -1,6 +0,0 @@
-package Parent
-  with SPARK_Mode
-is
-   type Switch_Pos is (On, Off, Unknown);
-   function Read_Switch return Switch_Pos;
-end Parent;
--- slides/Advisory_Panel_20131126/MB19-033_ap_demo/spark_io.ads
+++
@@ -1,9 +0,0 @@
-package SPARK_IO
-  with Abstract_State => State,
-       Initializes    => State
-is
-
-   procedure Put_Line (S : in String)
-      with Global => (In_Out => State);
-
-end SPARK_IO;
