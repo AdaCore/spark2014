0adbc75688617b77648f4b96708397548cede2e1
Johannes Kanig <kanig@adacore.com>
2020-02-05 19:10:56 +0900

    (no-tn-check) small helper script for git bisect
    Change-Id: I1e6a5fc3f1bc13af090affb2a50cda9babb98999

---
+++ scripts/bisect.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script is useful to bisect the spark2014 repo under the following assumptions:
+# - An identified test fails with DIFF output on HEAD and doesn't fail n
+#   commits earlier
+# - most commits compile (e.g. there are no commits that depend on changes in
+#   gnat repo)
+
+# Do not run the script directly. To use the script, do the following:
+# 1. go to the toplevel of the spark2014 repo, make sure you have no local changes.
+# 2. Start the bisect for e.g. the 20 last commits:
+#    $ git bisect start HEAD HEAD~20
+# 3. Run automatic bisect via git
+#    $ git bisect run scripts/bisect_single_test.sh <testname>
+# 4. clean up afterwards
+#    $ git bisect reset
+
+make || exit 125
+make install-all || exit 125
+cd testsuite/gnatprove
+! ./run-tests $1 --exact 2>&1 | grep DIFF
