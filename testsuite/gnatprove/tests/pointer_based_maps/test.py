df0c71bdbe4315b53e819420b0f02c7677d0a272
Claire Dross <dross@adacore.com>
2020-02-07 12:13:06 +0100

    SC09-016 make pointer_based_maps a session test
    Change-Id: I0b20b7f14dc45e7629f992bc76d5f29c4b44dcea

--- testsuite/gnatprove/tests/pointer_based_maps/test.py
+++ testsuite/gnatprove/tests/pointer_based_maps/test.py
@@ -1,2 +1,9 @@
 from test_support import *
-prove_all(opt=["--no-axiom-guard"], steps=10000)
+
+contains_manual_proof = False
+
+def replay():
+    prove_all(level=2, opt=["--no-axiom-guard"], procs=0)
+    
+prove_all(opt=["--no-axiom-guard"],
+          replay=True)
