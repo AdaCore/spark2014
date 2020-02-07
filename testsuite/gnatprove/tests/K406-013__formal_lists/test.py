633095002120be830232caaba47ed55f36249bf1
Claire Dross <dross@adacore.com>
2020-02-07 15:57:14 +0100

    SC09-016 add Z3 to regain proofs in K406-013__formal_lists
    Turn it into a session test to avoid rlimit.
    
    Change-Id: Iaaac7765ad297723ecee7797ce261286b32e5043

--- testsuite/gnatprove/tests/K406-013__formal_lists/test.py
+++ testsuite/gnatprove/tests/K406-013__formal_lists/test.py
@@ -1,3 +1,11 @@
 from test_support import *
 
-prove_all(steps=50000, prover=["cvc4"], counterexample=False)
+contains_manual_proof = False
+
+def replay():
+    prove_all(prover=["cvc4", "z3"],
+              procs=0,
+              level=4,
+              counterexample=False)
+
+prove_all(prover=["cvc4", "z3"], replay=True, counterexample=False)
