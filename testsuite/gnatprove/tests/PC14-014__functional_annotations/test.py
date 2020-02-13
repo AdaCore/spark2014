bc392c3e36354dd79d52dd4acbcf1d88db553537
Johannes Kanig <kanig@adacore.com>
2020-02-13 10:17:15 +0900

    T212-019 fix use of level switch in testsuite
    Change-Id: I479c9f68038ac2372e386a44e99976a4c89b15e6

--- testsuite/gnatprove/tests/PC14-014__functional_annotations/test.py
+++ testsuite/gnatprove/tests/PC14-014__functional_annotations/test.py
@@ -3,7 +3,7 @@ from test_support import *
 contains_manual_proof = False
 
 def replay():
-    prove_all(procs=0, opt=["--level=2", "--no-axiom-guard", "--no-counterexample"], steps=None)
+    prove_all(procs=0, opt=["--no-axiom-guard", "--no-counterexample"], level=2)
 
 prove_all(opt=["--no-axiom-guard",
                "--no-counterexample"],
