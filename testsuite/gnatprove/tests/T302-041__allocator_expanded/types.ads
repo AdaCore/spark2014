71b5a2ca1c0a764c1d2b3abe3f4facf471a3fae1
Piotr Trojanek <trojanek@adacore.com>
2020-03-02 23:37:02 +0100

    T302-041 add test with a qualified uninitialized allocator
    * testsuite/gnatprove/tests/T302-041__allocator_expanded/
    Add; crashes in proof, so disabled.
    
    Change-Id: I7424a6419edf072274985832b5bc399ca1d13017

---
+++ testsuite/gnatprove/tests/T302-041__allocator_expanded/types.ads
@@ -0,0 +1,9 @@
+package Types is
+   type My_Integer is new Integer with Default_Value => -12345;
+   --  A scalar type with default initialization
+   --  ??? Currently it needs to come from a withed unit because
+   --  of a frontend bug T302-026, but it doesn't really matter.
+
+   type Int_Ptr is access My_Integer;
+   --  Access type
+end;
