8b8d2b28a10a658900f42a6ff5c1cea8f263f966
Piotr Trojanek <trojanek@adacore.com>
2020-04-05 22:21:34 +0200

    T405-006 add test with renaming of an expression
    When executed, the assertion in this code fails at runtime, so there should
    be an unproved check.
    
    * testsuite/gnatprove/tests/T405-006__rename_expr/
    Add; current output is wrong (but let's have it to track the progress).
    
    Change-Id: I762d3b6cf8293da9e27c88438991cf520f99fdcc

---
+++ testsuite/gnatprove/tests/T405-006__rename_expr/p.adb
@@ -0,0 +1,11 @@
+procedure P is
+   procedure Test (X : in out Boolean) with Pre => True is
+      Y : Boolean renames Boolean'(not X);
+   begin
+      X := not X;
+      pragma Assert (Y /= X);
+   end;
+   X : Boolean := True;
+begin
+   Test (X);
+end;
