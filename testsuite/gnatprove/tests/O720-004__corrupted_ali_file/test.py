644ab0c9f4694d2ce2cc1300ee53cfebda5c8aa7
Piotr Trojanek <trojanek@adacore.com>
2020-06-02 11:03:07 +0200

    O720-004 cleanup test driver for corrupted ALI file
    * testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.py
    Cleanup references to both "a" and "target_ali" in truncate_four_lines
    routine.
    
    Change-Id: I74b2efffe905f07f07b35400dfe79fbe30969281

--- testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.py
+++ testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.py
@@ -4,17 +4,17 @@ import time
 
 #this test corrupts an ALI file and checks that gnatprove detects the issue.
 
-target_ali = os.path.join("gnatprove", "phase1", "test.ali")
-
 def truncate_four_lines(a):
-    statinfo = os.stat(target_ali)
+    statinfo = os.stat(a)
     with open(a,"r") as f:
         content = f.readlines()
     content = content[:-4]
     with open(a,"w") as f:
         for line in content:
             f.write(line)
-    os.utime(target_ali, (statinfo.st_atime, statinfo.st_mtime))
+    os.utime(a, (statinfo.st_atime, statinfo.st_mtime))
+
+target_ali = os.path.join("gnatprove", "phase1", "test.ali")
 
 prove_all()
 truncate_four_lines(target_ali)
