fe873aed6455d05ed0f771893e09c08160c5b338
Johannes Kanig <kanig@adacore.com>
2020-06-11 08:02:38 +0900

    T605-001 fix python3 syntax errors
    Change-Id: I475dd0b609f0ec4b514df9e8fdccc319cc28f82b

--- testsuite/gnatprove/tests/SB22-010__big_integers_support/test.py
+++ testsuite/gnatprove/tests/SB22-010__big_integers_support/test.py
@@ -2,6 +2,6 @@ from e3.os.process import Run
 from test_support import *
 prove_all()
 p = Run(["gnatmake", "-P", "test.gpr", "-q"])
-print p.out
+print(p.out)
 p = Run(["./big_integers_test"])
-print p.out
+print(p.out)
