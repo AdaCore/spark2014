0fb695b63822a6a5e684f3ee2267f61969887dab
Claire Dross <dross@adacore.com>
2020-09-08 11:25:46 +0200

    Update test L831-037__slice
    no-tn-check
    
    Change-Id: I1fdbc91182b5112b0f06c7c90262e1ee8e4a83f9

--- testsuite/gnatprove/tests/L831-037__slice/lcp.adb
+++ testsuite/gnatprove/tests/L831-037__slice/lcp.adb
@@ -6,11 +6,13 @@ is
    L : Natural;
 begin
    L := 0;
-   while X + L <= A'Last
-     and then Y + L <= A'Last
+   while X <= A'Last - L
+     and then Y <= A'Last - L
      and then A (X + L) = A (Y + L)
    loop
-      pragma Loop_Invariant (A (X .. X + L - 1) = A (Y .. Y + L - 1));
+      pragma Loop_Invariant (X <= A'Last - L);
+      pragma Loop_Invariant (Y <= A'Last - L);
+      pragma Loop_Invariant (A (X .. X + L) = A (Y .. Y + L));
       pragma Loop_Variant (Increases => L);
 
       L := L + 1;
