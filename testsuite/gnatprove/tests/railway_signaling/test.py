ca7631f824f9e8e7c42d967e99807783d1f33c48
Claire Dross <dross@adacore.com>
2020-09-16 10:48:36 +0200

    T511-007 make railway_signaling a session test
    Change-Id: I5a0477c1f1742c8470398acad20a8ef7a08d02c2

--- testsuite/gnatprove/tests/railway_signaling/test.py
+++ testsuite/gnatprove/tests/railway_signaling/test.py
@@ -1,7 +1,9 @@
 from test_support import *
-# Test was fully proved with 20s timeout, --proof=per_path and a combination
-# of provers. Currently, 4 out of 5 unproved assertions are still not proved
-# with 10.000 steps and 120s timeout, with --prover=cvc4,altergo,z3 and
-# --proof=per_path. This is likely related to the changes in how arrays are
-# handled in provers now, to be investigated.
-prove_all(steps=100000, prover=["cvc4","z3"], counterexample=False)
+
+contains_manual_proof = False
+
+def replay():
+    prove_all(procs=0, level=1, counterexample=False)
+    prove_all(procs=0, level=4, counterexample=False)
+
+prove_all(replay=True, level=1, counterexample=False)
