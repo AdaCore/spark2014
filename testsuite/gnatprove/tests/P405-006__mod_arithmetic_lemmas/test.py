6fb789e642a41987a4e33768e03af2712ccd8193
Yannick Moy <moy@adacore.com>
2020-09-20 21:09:25 +0200

    T904-001 Update steps for new version of CVC4
    Change-Id: I77bac7d00d28590a73918fd348c2abf3a24e38e5

--- testsuite/gnatprove/tests/P405-006__mod_arithmetic_lemmas/test.py
+++ testsuite/gnatprove/tests/P405-006__mod_arithmetic_lemmas/test.py
@@ -3,6 +3,6 @@ from subprocess import call
 import os
 
 os.environ["SPARK_LEMMAS_OBJECT_DIR"] = TESTDIR
-prove_all()
+prove_all(steps=1000)
 call(["gprbuild", "-q", "-P", "test.gpr"])
 call(["./test_arith"])
