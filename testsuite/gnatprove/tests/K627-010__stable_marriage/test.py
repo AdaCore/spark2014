47b06f3e1f64212ead3ce8f40dd930baea4024c9
Yannick Moy <moy@adacore.com>
2020-09-21 14:21:30 +0200

    T904-001 Update test after changes in CVC4
    With the new release of CVC4, one fewer check was proved with 5000 steps.
    Use that opportunity to revisit choices for this test, and reenable Z3 and
    Alt-Ergo prover, leading to two more checks proved instead.
    
    Also reenable counter-examples, which should all be spurious on this test
    and a good target for filtering in the future.
    
    Change-Id: I414827976901f0e0021f1f2f212241e8382dd4fd

--- testsuite/gnatprove/tests/K627-010__stable_marriage/test.py
+++ testsuite/gnatprove/tests/K627-010__stable_marriage/test.py
@@ -1,4 +1,3 @@
 from test_support import *
-# Z3 has problem between mac and linux arch
 # counter examples disabled to avoid DIFFs
-prove_all(steps = 5000, prover = ["cvc4"], codepeer=True, counterexample=False)
+prove_all(steps = 5000, codepeer=True)
