ac96cd28b75ce53adff787ded7de3d1e9b7d6223
Piotr Trojanek <trojanek@adacore.com>
2020-10-01 17:07:38 +0200

    T409-016 add twin tests with delta aggregates and 'Updates
    Basic tests for tracking dependencies with delta_aggregate (d1, d2) and
    semantically equivalent versions with attribute update (u1, u2). The latter
    should act as references, but actually outputs for u2 are wrong.
    
    * testsuite/gnatprove/tests/T409-016__flow_delta_on_records/
    Add; all contracts are right, so ultimately no checks should be emitted.
    
    Change-Id: I617b6c5cf28ec6fd2c9c4904a6b7ecd6f4aa65a3

---
+++ testsuite/gnatprove/tests/T409-016__flow_delta_on_records/u1.adb
@@ -0,0 +1,16 @@
+procedure U1 (X1, X2 : Integer; Y1, Y2 : out Integer)
+  with SPARK_Mode,
+       Depends => (Y1 => X1, Y2 => X2),
+       Post => Y1 = X1 and Y2 = X2
+is
+   type R is record
+      C1, C2 : Integer;
+   end record;
+
+   Before : constant R := (C1 => 0, C2 => 0);
+   After  : R;
+begin
+   After := Before'Update (C1 => X1, C2 => X2);
+   Y1 := After.C1;
+   Y2 := After.C2;
+end;
