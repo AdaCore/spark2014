182d960bf5d2e9046877977e7d618fd9b6e322ab
Yannick Moy <moy@adacore.com>
2020-10-06 16:55:06 +0200

    TA06-037 Add test
    Change-Id: I0b66956dfa074f8c1ce2bb65907a558c269e4f20

---
+++ testsuite/gnatprove/tests/TA06-037__depends_child/outer-public_child.ads
@@ -0,0 +1,9 @@
+package Outer.Public_Child is
+   --  Outer.A1 and Outer.A2 are visible but
+   --  Outer.Hidden_State is not (by the rules of Ada).
+   --  The Global and Depends Aspects are in terms
+   --  of the encapsulating state abstraction Outer.A2.
+   procedure Init_A2_With (Val : in Integer)
+     with Global  => (Output => Outer.A2),
+          Depends => (Outer.A2 => Val);
+end Outer.Public_Child;
