823db67738cc38cb0cfed4d9fd43d43564522083
Johannes Kanig <kanig@adacore.com>
2020-11-02 09:52:53 +0900

    SA24-015 update test outputs
    * tests/prime_numbers
    Turn test uinto a session test.
    
    * tests/railway_signaling
    Add assertions to regain proofs.
    
    Change-Id: Id17282a31528c3cc777ca481eebceea705cd3015

--- testsuite/gnatprove/tests/prime_numbers/test.py
+++ testsuite/gnatprove/tests/prime_numbers/test.py
@@ -1,3 +1,8 @@
 from test_support import *
 
-prove_all(steps=500, counterexample=False)
+contains_manual_proof = False
+
+def replay():
+    prove_all(level=2, procs=0, counterexample=False)
+
+prove_all(replay=True, counterexample=False)
