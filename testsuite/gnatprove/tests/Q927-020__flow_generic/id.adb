e9e8fce75fdbd5cdfe53fc10fbfcf8e2872df3b9
Piotr Trojanek <trojanek@adacore.com>
2020-11-19 00:21:26 +0100

    Q927-020 extend test to demonstrate the problem
    A generic package with Initializes contract that references a generic actual
    of mode IN. This triggers a check from flow, which better should be avoided.
    
    A similar generic subprogram with Depends contract can be instantiated
    without complains.
    
    * testsuite/gnatprove/tests/Q927-020__flow_generic/
    Add test; update output.
    
    Change-Id: I2cfc2195a829e4655d6944d941d2ca30eb409356

--- testsuite/gnatprove/tests/Q927-020__flow_generic/id.adb
+++ testsuite/gnatprove/tests/Q927-020__flow_generic/id.adb
@@ -5,13 +5,14 @@ function Id (Inx : Integer) return Integer with
 is
    generic
       F : Integer;
-   function Proxy return Integer;
+   function Proxy return Integer with Depends => (Proxy'Result => F);
 
    function Proxy return Integer is
    begin
       return F;
    end;
 
+   function Dummy    is new Proxy (123);
    function Indirect is new Proxy (Inx);
 
 begin
