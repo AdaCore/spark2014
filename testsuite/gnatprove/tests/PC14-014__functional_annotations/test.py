d5fd91ff29f42cddfd8bcc15898d95018f465cd4
Joffrey Huguet <huguet@adacore.com>
2020-12-15 14:40:35 +0100

    TC10-010 Change switch in tests scripts
    * testsuite/gnatprove/tests/PC14-014__formal_containers/test.py
    * testsuite/gnatprove/tests/PC14-014__functional_annotations/test.py
    Change switch.
    
    Change-Id: If5fbe048d410a4c82e9bfbe3c2001778e5a43dce

--- testsuite/gnatprove/tests/PC14-014__functional_annotations/test.py
+++ testsuite/gnatprove/tests/PC14-014__functional_annotations/test.py
@@ -3,9 +3,9 @@ from test_support import *
 contains_manual_proof = False
 
 def replay():
-    prove_all(procs=0, opt=["--no-axiom-guard", "--no-counterexample"], level=4)
+    prove_all(procs=0, opt=["--no-axiom-guard", "--counterexamples=off"], level=4)
 
 if __name__ == "__main__":
     prove_all(opt=["--no-axiom-guard",
-                   "--no-counterexample"],
+                   "--counterexamples=off"],
               replay=True)
