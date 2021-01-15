f9675646d467d8854adf0250eed6bdfa88b2de90
Simon Buist <simon.buist@altran.com>
2021-01-15 10:14:07 +0000

    S406-003 fix test output
    Modify test support file to produce deterministic test output
    
    * tests/S406-003__flow-show-gg/test.out
    update test output file
    
    * tests/S406-003__flow-show-gg/test.py
    enable deterministic test output
    
    Change-Id: Ieb891873afedec4074e250e2d2b8b3669ede4815

--- testsuite/gnatprove/tests/S406-003__flow-show-gg/test.py
+++ testsuite/gnatprove/tests/S406-003__flow-show-gg/test.py
@@ -1,3 +1,4 @@
 from test_support import *
+from glob import glob
 
-do_flow(opt=["--flow-show-gg", "--no-inlining"], sort_output=False)
+do_flow(opt=["--flow-show-gg", "--no-inlining"] + sorted(glob("*.ads")), sort_output=False)
