126e1f04ecaba13ad74d739fc84f31e3ea38c59c
Simon Buist <simon.buist@altran.com>
2021-01-15 15:51:54 +0000

    S406-003 --flow-show-gg supports Global => null
    GNATprove switch --flow-show-gg now supports generating the contract:
    Global => null, for subprograms that have no globals.
    
    * src/flow/flow.adb
    (GG_To_JSON): Use {} to represent Global => null
    
    * testsuite/gnatprove/tests/S406-003__flow-show-gg/test.out
    * testsuite/gnatprove/tests/S406-003__flow-show-gg/test.py
    Add JSON output to test, showing a Global => null for function
     No_Globals
    
    Change-Id: I8e03bddd488b100aa7724d716529dcbdf7d00b62

--- testsuite/gnatprove/tests/S406-003__flow-show-gg/test.py
+++ testsuite/gnatprove/tests/S406-003__flow-show-gg/test.py
@@ -1,4 +1,11 @@
 from test_support import *
 from glob import glob
+import os
 
 do_flow(opt=["--flow-show-gg", "--no-inlining"] + sorted(glob("*.ads")), sort_output=False)
+
+print("Generated global .gg contents:")
+for file in sorted(glob("gnatprove/*.gg")):
+    print("filename:", file)
+    print("contents:")
+    cat(file)
