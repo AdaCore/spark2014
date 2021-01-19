0fe2c139457be85094ea946663da0f523c89fbde
Simon Buist <simon.buist@altran.com>
2021-01-19 10:40:13 +0000

    S406-003 include .adb files in test
    This fixes a regression where we weren't including the .adb files in the
    test support script.
    
    * tests/S406-003__flow-show-gg/test.out
    update test output
    
    * tests/S406-003__flow-show-gg/test.py
    include .adb files
    
    Change-Id: I5e0c405c1b577106469ddc64bcd0bfa63dd347cf

--- testsuite/gnatprove/tests/S406-003__flow-show-gg/test.py
+++ testsuite/gnatprove/tests/S406-003__flow-show-gg/test.py
@@ -2,7 +2,8 @@ from test_support import *
 from glob import glob
 import os
 
-do_flow(opt=["--flow-show-gg", "--no-inlining"] + sorted(glob("*.ads")), sort_output=False)
+do_flow(opt=["--flow-show-gg", "--no-inlining"] + sorted(glob("*.ad[sb]")),
+        sort_output=False)
 
 print("Generated global .gg contents:")
 for file in sorted(glob("gnatprove/*.gg")):
