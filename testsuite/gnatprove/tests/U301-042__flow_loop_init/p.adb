e469ef4f8a2651ce4c66cccc694d59274e24e558
Piotr Trojanek <trojanek@adacore.com>
2021-03-02 13:13:43 +0100

    U301-042 minimize test
    Simplify test to bare minimum.
    
    * testsuite/gnatprove/tests/U301-042__flow_loop_init/
    Add; still crashes.
    
    Change-Id: Ic3d642402cda49d7c33e741751a767626deff01a

--- testsuite/gnatprove/tests/U301-042__flow_loop_init/p.adb
+++ testsuite/gnatprove/tests/U301-042__flow_loop_init/p.adb
@@ -1,33 +1,20 @@
 procedure P (Cond : Boolean) is
    procedure Set (Output : out Integer) is null;
 
-   type T1 is record
-      A : Integer;
-      B : Integer;
+   type R is record
+      C : Integer;
    end record;
 
-   type A1 is array (Boolean) of T1;
+   type A is array (Boolean) of R;
 
-   type T2 is record
-      C2 : A1;
-   end record;
-
-   type A2 is array (Boolean) of T2;
-
-   type T3 is record
-      C3 : A2;
-   end record;
-
-   C4 : T3;
+   X : A;
 
 begin
-   for J2 in Boolean'Range loop
+   for J in Boolean'Range loop
       if Cond then
-         C4.C3 (J2) := (C2 => (others => T1'(A => 0, B => 0)));
+         X (J) := (C => 0);
       else
-         for J1 in Boolean'Range loop
-            Set (C4.C3 (J2).C2 (J1).A);
-         end loop;
+         Set (X (J).C);
       end if;
    end loop;
 end;
