273eeed2eab0fbf287762333712bd7a49059e889
Johannes Kanig <kanig@adacore.com>
2021-03-02 19:41:36 +0900

    U301-040 file test
    Change-Id: I0e418482e3dea424ccd6cc261ce76bf96af4ca7b

---
+++ testsuite/gnatprove/tests/U301-040__annotate/test_annotate.adb
@@ -0,0 +1,8 @@
+procedure Test_Annotate with SPARK_Mode is
+   X : Positive;
+   Y : Float with Address => X'Address;
+   pragma Annotate (GNATprove, Intentional, "unsuitable for aliasing via address clause", "I don't care");
+   pragma Annotate (GNATprove, Intentional, "alignment of overlaying object", "I don't care");
+begin
+   null;
+end Test_Annotate;
