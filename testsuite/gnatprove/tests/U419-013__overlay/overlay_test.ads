ee7f56568380464243b11e56a0d49caa7f40623f
Piotr Trojanek <trojanek@adacore.com>
2021-04-19 21:47:13 +0200

    U419-013 add test with alignment of overlaid parameter
    * testsuite/gnatprove/tests/U419-013__overlay/
    Add; check about alignment is spurious and there is no way to avoid it.
    
    Change-Id: Id12dd560dab371f26bfc0e565e89b3aa1721cf20

---
+++ testsuite/gnatprove/tests/U419-013__overlay/overlay_test.ads
@@ -0,0 +1,13 @@
+package Overlay_Test is
+   type NvU32 is mod 2**32;
+   type NvU8 is mod 2**8;
+
+   type Nv_DW_Block64 is array (NvU32 range 0 .. 1) of NvU32 with
+      Size        => 64,
+      Object_Size => 64;
+   type Nv_B_Block64 is array (NvU32 range 0 .. 7) of NvU8 with
+      Size        => 64,
+      Object_Size => 64;
+
+   procedure Test_Program (A : Nv_DW_Block64) with Global => null;
+end Overlay_Test;
