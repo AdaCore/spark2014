52e3d978df8ae1d1ce92738e8bef3d97eed77150
Chris Martin <cmartin@adacore.com>
2021-04-22 10:58:27 +0200

    U218-042 tests for generated Initializes and nested packages
    * tests/U218-042__flow_nested_initializes/
    Dedicated test cases and results updated
    * tests/O909-044__flow_wrong_gen_initializes/test.out
    Test suite results updated
    * tests/U218-042__flow_indirectly_nested/
    Test results updated
    * tests/TC07-027__flow_child_public/
    Test results updates, test cases annotated
    
    Change-Id: Id901ba81423e29f690f8f5e94db15c75c598c8ec

---
+++ testsuite/gnatprove/tests/TC07-027__flow_child_public/q1-q2-q3.adb
@@ -0,0 +1,8 @@
+package body Q1.Q2.Q3 is
+   procedure Set is
+   begin
+      X3A := True;
+   end Set;
+begin
+   Set;
+end;
--- testsuite/gnatprove/tests/TC07-027__flow_child_public/q1-q2.ads
+++ testsuite/gnatprove/tests/TC07-027__flow_child_public/q1-q2.ads
@@ -1,3 +1,6 @@
-package Q1.Q2 with Abstract_State => S2 is
+package Q1.Q2 with Abstract_State => S2 --, Initializes => S2
+  --  S2 is *not* fully initialized.
+  --  However adding the above Initializes contract passes???
+is
    procedure Dummy with Global => null;
 end;
