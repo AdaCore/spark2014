9357d6c7ae911ced18f3668e9f583ddb85ab237c
Johannes Kanig <kanig@adacore.com>
2021-06-10 18:14:54 +0900

    (no-tn-check) allow toggling counterexamples per file
    We fix parsing of --counterexamples switch, which was not allowed in
    file-specific switches. We also improve the test driver to allow the
    user to disable the global setting of the counterexample switch.
    
    * configuration.adb
    (Parse_Switches): move declaration of --counterexamples switch to
    the right section
    * test_support.py
    (prove_all): allow setting counterexamples argument to None to not
      force any of --counterexamples=on or off
    
    Change-Id: I9df81ba34d04c1091ff508e139dc744fc429ec87

--- testsuite/gnatprove/lib/python/test_support.py
+++ testsuite/gnatprove/lib/python/test_support.py
@@ -800,10 +800,11 @@ def prove_all(opt=None, steps=None, procs=parallel_procs,
         fullopt += ["--benchmark"]
         prover_arg = build_prover_switch([benchmark_mode()])
     fullopt += prover_arg
-    if not counterexample or benchmark_mode():
-        fullopt += ["--counterexamples=off"]
-    else:
-        fullopt += ["--counterexamples=on"]
+    if counterexample is not None:
+        if not counterexample or benchmark_mode():
+            fullopt += ["--counterexamples=off"]
+        else:
+            fullopt += ["--counterexamples=on"]
     # Add opt last, so that it may include switch -cargs
     if opt is not None:
         fullopt += opt
