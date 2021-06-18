82f7fff0400ebd3bfdcff7ffe4c01478bf822c7d
Yannick Moy <moy@adacore.com>
2021-06-18 15:20:23 +0200

    U617-014 Recover provability after changes in VC
    Special case max value for factorial for prover to unfold the remaining
    cases. Use subprogram variant instead of unproved terminating
    annotation.
    
    [changelog]
    * tests/Q615-017__factorial/factorial.ads: Update code.
    * tests/Q615-017__factorial/test.out: All proved now.
    
    Change-Id: Id9f824754339633cbf552d228d208ef079b8e460

--- testsuite/gnatprove/tests/Q615-017__factorial/factorial.ads
+++ testsuite/gnatprove/tests/Q615-017__factorial/factorial.ads
@@ -5,12 +5,17 @@ with SPARK_Mode
 is
 
    function Mathematical_Factorial (X : Integer) return Integer is
-     (if X <= 1 then 1 else X * Mathematical_Factorial (X-1))
-   with Pre => X in 1 .. 12, Ghost;
-   pragma Annotate (GNATprove, Terminating, Mathematical_Factorial);
+     (if X = 1 then 1
+      elsif X = 12 then X * Mathematical_Factorial (11)
+      else X * Mathematical_Factorial (X-1))
+   with
+     Ghost,
+     Subprogram_Variant => (Decreases => X),
+     Pre => X in 1 .. 12;
 
    function Fact (X : Integer) return Integer
-     with Pre => X in 1 .. 12,
+   with
+     Pre  => X in 1 .. 12,
      Post => Fact'Result = Mathematical_Factorial (X);
 
 end Factorial;
