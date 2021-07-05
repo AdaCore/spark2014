f72249b76c0d9d4602195cd86ae4fee8c80aeacd
Yannick Moy <moy@adacore.com>
2021-07-05 13:49:27 +0200

    Remove counterexamples causing diffs on CB
    no-tn-check
    
    Change-Id: Ia96a236db3ab5c5162e6e7645d920cb35dfe257c

--- testsuite/gnatprove/tests/T610-004__float_arithmetic_lemmas/test.py
+++ testsuite/gnatprove/tests/T610-004__float_arithmetic_lemmas/test.py
@@ -3,7 +3,7 @@ from subprocess import call
 import os
 
 os.environ["SPARK_LEMMAS_OBJECT_DIR"] = TESTDIR
-prove_all(steps=2000)
+prove_all(steps=2000, counterexample=False)
 call(["gprbuild", "-q", "-P", "test.gpr"])
 call(["./test_arith"])
 call(["./test_arith64"])
