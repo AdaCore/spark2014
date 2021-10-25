f2d1f81c66b9ff63855639dfa6d2a0b701c61206
Claire Dross <dross@adacore.com>
2021-10-25 15:58:48 +0200

    UA25-034 go to partial view to skip generic formal parameters
    * src/spark/spark_util-types.adb
    (Retysp): We skip the Itype for generic actual type parameters to avoid
     implicit conversions between access types designating different types
     on generic instance. Go to the partial view when there is one to skip
     this Itype.
    
    * tests/Q802-001__stringmap/test.out
    * tests/Q802-001__stringsep/test.out
    * tests/Q802-005__bounded_strings/test.out
    * tests/S729-021__predicates/test.out
    Remove always proved checks introduced by useless conversions.
    
    * tests/UA25-034__implicit_access_conversion
    File test.
    
    Change-Id: I4f94b76dfe4d972642149b8342af3d5b713f976e

---
+++ testsuite/gnatprove/tests/UA25-034__implicit_access_conversion/main.adb
@@ -0,0 +1,26 @@
+with Ada.Containers.Formal_Indefinite_Vectors;
+with Ada.Containers; use Ada.Containers;
+with Processors; use Processors;
+
+procedure Main with SPARK_Mode is
+
+   package VCpuVectors is new Ada.Containers.Formal_Indefinite_Vectors
+     (Index_Type                   => VCpuIdType,
+      Element_Type                 => VCpuType,
+      Max_Size_In_Storage_Elements => VCpuType'Size);
+   use all type VCpuVectors.Vector;
+
+   VCpuVector : aliased VCpuVectors.Vector (10);
+
+   procedure UseCase(VCpuId : VCpuIdType)
+     with Pre => VCpuId in 1 .. VCpuVectors.Last_Index (VCpuVector)
+   is
+      VCpuVectorAcc : constant access VCpuVectors.Vector := VCpuVector'Access;
+      VCpu          : access VCpuType := VCpuVectors.Reference (VCpuVectorAcc, VCpuId);
+   begin
+      DoStuff(VCpu.all);
+   end UseCase;
+
+begin
+   null;
+end;
---
+++ testsuite/gnatprove/tests/UA25-034__implicit_access_conversion/processors.ads
@@ -0,0 +1,18 @@
+with Ada.Containers.Formal_Indefinite_Vectors;
+with Ada.Containers; use Ada.Containers;
+
+package Processors with SPARK_Mode is
+   type VCpuIdType is new Positive;
+   type VCpuType is private;
+
+   procedure DoStuff (VCpu : in out VCpuType) with Import;
+
+   procedure DoOtherStuff (VCpu1, VCpu2 : in out VCpuType) with Import;
+
+private
+
+   type VCpuType is record
+      X, Y : Integer;
+   end record with Size => 64;
+
+end Processors;
