30ed0782d772603b7e51d32409ce4a81e1381538
Piotr Trojanek <trojanek@adacore.com>
2021-12-10 23:06:20 +0100

    UB17-024 remove unnecessary WITH clauses for parent units
    Fixes warnings-as-errors coming from recent builds of the compiler.
    
    * src/common/assumptions-search.ads
    * src/why/spark_atree-entities.ads
    * src/why/why-atree-to_json.ads
    Remove extra WITH clauses.
    
    Change-Id: I91654779eb9f9490eac297c6c5e5672e6c84cf6b

--- src/why/why-atree-to_json.ads
+++ src/why/why-atree-to_json.ads
@@ -26,8 +26,6 @@
 with GNATCOLL.JSON;    use GNATCOLL.JSON;
 with Why.Atree.Tables; use Why.Atree.Tables;
 
-with Why.Atree;    use Why.Atree;
-
 package Why.Atree.To_Json is
    function Why_Node_To_Json (Node : Why_Node) return JSON_Value;
    function Why_Node_Lists_List_To_Json (L : Why_Node_Lists.List)
