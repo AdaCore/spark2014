d022f23de04bc4b91782888c5d38021975ba3624
Johannes Kanig <kanig@adacore.com>
2022-01-03 08:55:24 +0900

    (no-tn-check) Update copyright notices
    Change-Id: I10991d89306d5dc0cbd356a35390465d3975b026

--- src/why/gnat2why-tables.ads
+++ src/why/gnat2why-tables.ads
@@ -6,8 +6,8 @@
 --                                                                          --
 --                                  S p e c                                 --
 --                                                                          --
---                     Copyright (C) 2017-2021, AdaCore                     --
---                Copyright (C) 2017-2021, Altran UK Limited                --
+--                     Copyright (C) 2017-2022, AdaCore                     --
+--                Copyright (C) 2017-2022, Altran UK Limited                --
 --                                                                          --
 -- gnat2why is  free  software;  you can redistribute  it and/or  modify it --
 -- under terms of the  GNU General Public License as published  by the Free --
