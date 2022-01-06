4b4593e63679bcca13e9e831bfa1ea5144532da2
Yannick Moy <moy@adacore.com>
2022-01-06 11:06:16 +0100

    UC17-066 Missing source change from previous patch
    Change-Id: I588764bb739878005819292c6a788c07ef55c3e0

--- testsuite/gnatprove/tests/UA27-002__justification/bad.adb
+++ testsuite/gnatprove/tests/UA27-002__justification/bad.adb
@@ -1,4 +1,5 @@
 procedure Bad is
+   pragma Annotate (GNATprove, Intentional, "raise exceptions", "demo");
 begin
    raise Program_Error;
    pragma Annotate (GNATprove, Intentional, "might be raised", "demo");
