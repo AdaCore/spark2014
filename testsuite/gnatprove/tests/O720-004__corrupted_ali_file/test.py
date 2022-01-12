7fe7779cf93a8823f72db03d013d849c99f5d76c
Dmitriy Anisimkov <anisimko@adacore.com>
2022-01-12 19:45:40 +0600

    Modify tests due to gprbuild reimplement of ALI_Closure mode
    Broken .ali files detected by gprbuild and recreated at first gnatprove
    call to gprbuild.
    
    TN: UC22-003
    Change-Id: I8d9107c501f5f70a155dd6327c91494b94614550
    Depend-On: I23becd918cd74b870cc602c5ae0a6531e700db95

--- testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.py
+++ testsuite/gnatprove/tests/O720-004__corrupted_ali_file/test.py
@@ -3,11 +3,14 @@ import os
 
 # this test corrupts an ALI file and checks that gnatprove detects the issue.
 
+last_line = ''
 
 def truncate_four_lines(a):
+    global last_line
     statinfo = os.stat(a)
     with open(a, "r") as f:
         content = f.readlines()
+    last_line = content[-1]
     content = content[:-4]
     with open(a, "w") as f:
         for line in content:
@@ -18,5 +21,12 @@ def truncate_four_lines(a):
 target_ali = os.path.join("gnatprove", "phase1", "test.ali")
 
 prove_all()
+print("------------------")
 truncate_four_lines(target_ali)
 prove_all()
+
+with open(target_ali, "r") as f:
+    content = f.readlines()
+    if last_line != content[-1]:
+        print("Wrong last line")
+        print(content[-1])
