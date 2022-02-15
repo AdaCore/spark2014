87697a67f904bf50cb5b99e6f466778314796eeb
Piotr Trojanek <trojanek@adacore.com>
2022-02-15 10:33:07 +0100

    UC15-032 adapt test with warnings about junk names
    Rename "Tmp" to "XXX" variables to keep testing if pragma Warnings is
    respected.
    
    * testsuite/gnatprove/tests/N307-022__warnings_off/
    Adapt test.
    
    Change-Id: I35fd78146e8ffb06443bbbbb2625b9d56f8f3532

--- testsuite/gnatprove/tests/N307-022__warnings_off/warns3.adb
+++ testsuite/gnatprove/tests/N307-022__warnings_off/warns3.adb
@@ -1,13 +1,13 @@
 procedure Warns3 (X : in out Integer) with
   SPARK_Mode
 is
-   Tmp : Integer;
+   XXX : Integer;
 begin
-   Tmp := X + 1;
-   X := Tmp;
+   XXX := X + 1;
+   X := XXX;
    pragma Warnings (GNAT, Off, "useles assignment");
    pragma Warnings (GNATprove, Off, "unuse assignment");
-   Tmp := Tmp + 1;
+   XXX := XXX + 1;
    pragma Warnings (GNATprove, On, "unuse assignment");
    pragma Warnings (GNAT, On, "useles assignment");
 end Warns3;
