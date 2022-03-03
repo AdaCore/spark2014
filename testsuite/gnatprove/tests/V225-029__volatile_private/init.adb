c65181f5cb662d8c02cfd1b9c9e3e9fd04c56c8b
Piotr Trojanek <trojanek@adacore.com>
2022-03-03 09:10:49 +0100

    V225-029 further minimize reproducer for a derived volatile type
    Move all code into a single unit; use more intuitive type names.
    
    * testsuite/gnatprove/tests/V225-029__volatile_private/
    Minimize; spurious error is still present.
    
    Change-Id: I5bb470ce60e36254d511d87c6b271b1bd64db6fb

--- testsuite/gnatprove/tests/V225-029__volatile_private/init.adb
+++ testsuite/gnatprove/tests/V225-029__volatile_private/init.adb
@@ -1,6 +1,6 @@
 package body Init is
 
-   procedure Initialize (Shared_Context : out Shared_Context_Type) is
+   procedure Initialize (Shared_Context : out Subchild) is
    begin
       if True then  --  dummy condition to hide recursion from GNAT
          Initialize (Shared_Context);
