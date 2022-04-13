fd81b294a7f03c53c30a96a842c8a2a5b5593b2f
Viviane Garèse <garese@adacore.com>
2022-04-13 10:18:06 +0200

    UA27-029 improve array comparison for equality check
    Only check the equality of elements we know could differ, instead of
    looping through the whole array to check every element. Fix behaviour of
    equality check between two Value_Access.
    
    * src/counterexamples/ce_rac.adb
    * src/counterexamples/ce_rac.ads
    ("=" on Scalar_Value_Type): Move to ce_values
    ("=" on Float_Value): Move to ce_values
    ("=" on Value_Type): Move to ce_values
    (RAC_Stuck): Move specification to ce_rac.ads to be able to call it from
    ce_values.
    
    * src/counterexamples/ce_values.adb
    * src/counterexamples/ce_values.ads
    (Default_Equal): Rename the default equality operator between two
    Value_Access. This helps avoid infinite recursion when checking equality
    between a Value_Access and null in the overriden "=" operator.
    ("=" on Value_Access): Override default behaviour of comparing addresses
    by checking for null and comparing contents.
    ("=" on Scalar_Value_Type): Move from ce_rac
    ("=" on Float_Value): Move to ce_rac
    ("=" on Value_Type): Move from ce_rac
    When Array_K: Check the elements at the indices listed in the values map
    of the first array, then of the second. If all indices were checked
    return True, else check if the "others" values of both arrays are equal.
    When Scalar_K : Short_circuit condition evaluation.
    When Enum_K : Simplify boolean expression.
    (Get_Array_Elt): Remove now unused function
    (Check_Array_Named): Add function to iterate through the map of indices
    to values of an array and check if each value is equal to that of a
    second array at the same index.
    
    * tests/riposte__complex_arrays/test.out
    * tests/riposte__simple_arrays/test.out
    Update outputs
    
    Change-Id: I2e9dac3ad4dfd8bc7747845ce769b8975dda4858

--- src/counterexamples/ce_rac.ads
+++ src/counterexamples/ce_rac.ads
@@ -53,6 +53,10 @@ package CE_RAC is
    --  functions grows higher than this number. Raises RAC_Unexpected_Error
    --  when something unforeseen happens.
 
+   procedure RAC_Stuck (Reason : String) with No_Return;
+   --  Raise Exn_RAC_Stuck and set result, i.e. the RAC execution failed
+   --  due to a false assumption.
+
    type Result_Kind is
      (Res_Normal,
       --  RAC execution terminated normally, without encountering an invalid
