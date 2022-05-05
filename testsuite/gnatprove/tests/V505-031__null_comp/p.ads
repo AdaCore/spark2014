395725b03307ae29ca51276b14778ea67eea9bf1
Piotr Trojanek <trojanek@adacore.com>
2022-05-05 20:45:52 +0200

    V505-031 add test with components of a null-excluding access type
    I would expect all assertions in this test to be proved.
    
    * testsuite/gnatprove/tests/V505-031__null_comp/
    Add; with output as it is today.
    
    Change-Id: Iad436dff331328dbad21abadff7f1737c74d5c50

---
+++ testsuite/gnatprove/tests/V505-031__null_comp/p.ads
@@ -0,0 +1,22 @@
+package P with SPARK_Mode is
+
+   type Buffer_Acc is not null access all String;
+
+   type Buffer_Rec is record
+      Data : Buffer_Acc (1 .. 10);
+   end record;
+
+   type Buffer_Arr is array (Boolean) of Buffer_Acc (1 .. 10);
+
+   function To_Acc return Buffer_Acc with Import;
+   function To_Rec return Buffer_Rec with Import;
+   function To_Arr return Buffer_Arr with Import;
+
+   Acc : Buffer_Acc := To_Acc;
+   Rec : Buffer_Rec := To_Rec;
+   Arr : Buffer_Arr := To_Arr;
+
+   pragma Assert (Acc /= null);
+   pragma Assert (Rec.Data /= null);
+   pragma Assert (Arr (True) /= null);
+end;
