fe6e24d789ff22b25f3959cf8934e692d9891650
Johannes Kanig <kanig@adacore.com>
2022-05-23 16:59:23 +0900

    V520-017 switch to read the docs theme
    Change-Id: Idbcf5aa8510ccfc9611d7e8e828367cad3b84a1e

--- docs/ug/conf.py
+++ docs/ug/conf.py
@@ -33,7 +33,7 @@ needs_sphinx = "1.4"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.todo", "sphinx.ext.autosectionlabel"]
+extensions = ["sphinx.ext.todo", "sphinx.ext.autosectionlabel", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -92,7 +92,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinxdoc"
+html_theme = "sphinx_rtd_theme"
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
