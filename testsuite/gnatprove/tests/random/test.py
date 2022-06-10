eda2daf2886f087033bc5021049981c1d3f67b91
Johannes Kanig <kanig@adacore.com>
2022-06-10 18:05:07 +0900

    V426-008 update testsuite
    Change-Id: I29abd945c04c38f97cba0b1186965c8870a3adc8

--- testsuite/gnatprove/tests/random/test.py
+++ testsuite/gnatprove/tests/random/test.py
@@ -2,7 +2,7 @@ from test_support import prove_all
 from e3.os.process import Run
 
 # Prove RTE
-prove_all(no_fail=True, steps=6000)
+prove_all(steps=6000)
 
 # Execute test program
 Run(["gprbuild", "-P", "test.gpr"])
