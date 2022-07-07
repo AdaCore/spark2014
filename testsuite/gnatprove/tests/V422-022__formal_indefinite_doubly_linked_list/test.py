bc12f6af52a85d7f17b91b023d4dbcf2a10af23b
Yannick Moy <moy@adacore.com>
2022-07-07 15:48:26 +0200

    (no-tn-check) Update to remove diffs on Windows
    Change-Id: I3d0b3e8b88ed15855119b2ea513ade5bd81ba50a

--- testsuite/gnatprove/tests/V422-022__formal_indefinite_doubly_linked_list/test.py
+++ testsuite/gnatprove/tests/V422-022__formal_indefinite_doubly_linked_list/test.py
@@ -2,8 +2,7 @@ from subprocess import call
 from test_support import prove_all
 
 if __name__ == "__main__":
-    prove_all()
+    prove_all(prover=["cvc5", "z3"])
 
     call(["gprbuild", "-q", "-P", "test.gpr"])
     call(["./test"])
-
