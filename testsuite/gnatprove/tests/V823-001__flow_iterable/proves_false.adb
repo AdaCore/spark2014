47c2f5508bec542530a6eeff96494fb785b3e796
Piotr Trojanek <trojanek@adacore.com>
2022-08-24 11:47:52 +0200

    V823-001 add test with recursion via aspect Iterable
    * testsuite/gnatprove/tests/V823-001__flow_iterable/
    Add; the false assertion should not be proved.
    
    Change-Id: I3532d778ce8cc8f7f558a18f929cac84389d6f52

---
+++ testsuite/gnatprove/tests/V823-001__flow_iterable/proves_false.adb
@@ -0,0 +1,48 @@
+with Interfaces; use Interfaces;
+with Ada.Numerics.Big_Numbers.Big_Integers; use Ada.Numerics.Big_Numbers.Big_Integers;
+
+procedure Proves_False with SPARK_Mode is
+   generic
+      type Value_Type is private;
+   package AbstractVector2  is
+      type Vector  is private
+         with Iterable => (
+               First => Iter_First,
+               Next => Iter_Next,
+               Has_Element => Has_Index);
+
+      function Length (M : Vector) return Big_Natural
+      with Post =>
+         (for some k in M => k = Length'Result - 1) and then
+         (for all k in M => k < Length'Result);
+
+      function Iter_First (M : Vector) return Big_Natural;
+      function Iter_Next (M : Vector; I : Big_Natural) return Big_Natural;
+      function Has_Index (M : Vector; I : Big_Natural) return Boolean;
+
+      function Empty return Vector
+         with Post => Length(Empty'Result) = 0;
+
+   private
+
+      type Vector
+      is record
+         Length : Big_Natural := 0;
+      end record;
+
+      function Length (M : Vector) return Big_Natural is (M.Length);
+
+      function Iter_First (M : Vector) return Big_Natural is (0);
+      function Iter_Next (M : Vector; I : Big_Natural) return Big_Natural is (I + 1);
+      function Has_Index (M : Vector; I : Big_Natural) return Boolean is (I < Length(M));
+
+      function Empty return Vector is (Vector'( Length => 0 ));
+   end AbstractVector2;
+
+   package NaturalVector is new AbstractVector2(Unsigned_64);
+   use NaturalVector;
+
+begin
+   pragma Assert(NaturalVector.Length(NaturalVector.Empty) = 1);
+   pragma Assert(False);
+end Proves_False;
