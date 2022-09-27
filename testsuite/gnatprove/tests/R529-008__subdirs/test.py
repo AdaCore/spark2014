a745e2a61af19168dde7290f969fab8988c246ec
Johannes Kanig <kanig@adacore.com>
2022-09-27 11:20:11 +0900

    V921-001 disable Codepeer on all tests
    Tests with regressions:
    * I827-015__oo: regression seems indeed not provable
    * JC17-017__subtype: SPARK would need a loop invariant here
    * K303-015__default: SPARK shouldn't prove this, requires inlining
      of public function without contract
    * K407-022__arith: these division checks could be proved as the
      divisor is already used previously in other divisions.
    * K518-029__call_actuals: looks like these were unsoundnesses
    * K719-020__flight: unprovable for SPARK, needs contracts
    * P201-069__simulink: accept one regression
    * PB16-052__float_invariant: some proofs could be recovered with
      cvc5 and large steps (>= 70000), but test would be too slow
    * proofinuse__pi: accept one regression
    * RA12-018__init_loop: accept one regression
    
    Change-Id: If57693c0953169bd95937f6cc23bdb99e05bd7e4

--- testsuite/gnatprove/tests/R529-008__subdirs/test.py
+++ testsuite/gnatprove/tests/R529-008__subdirs/test.py
@@ -7,7 +7,7 @@ my_dir = os.path.join("toto", "gnatprove")
 assert os.path.exists(my_dir)
 assert os.path.isdir(my_dir)
 
-prove_all(codepeer=True, opt=["--subdirs", "toto"], project="test2.gpr")
+prove_all(opt=["--subdirs", "toto"], project="test2.gpr")
 
 my_dir = os.path.join("obj", "toto", "gnatprove")
 assert os.path.exists(my_dir)
