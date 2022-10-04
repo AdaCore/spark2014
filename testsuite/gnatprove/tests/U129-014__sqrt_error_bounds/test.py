3aad607d830eb46da6e07b06bec00ea8908c2ebb
Claire Dross <dross@adacore.com>
2022-10-04 08:36:20 +0200

    V916-020 avoid exponential blow-up with handling of function guards
    * src/why/why-gen-expr.adb
    (New_Function_Call): Introduce let bindings for parameters of function
     calls in the Term domain to avoid an exponential blow-up in case of
     mutiple nested function calls.
    
    * tests/V916-020__and_or_sequence
    Enable test with counterexample generation disabled.
    
    * tests/U528-009__UC_signed_modular
    Increase the step limit to maintain the PASS marker.
    
    * tests/U129-014__sqrt_error_bounds
    Add a no-axiom-guard marker to avoid reaching the timelimit.
    
    * tests/*
    Update sessions and test outputs with minor differences in provability.
    
    Change-Id: I8a679f7ad95dc5b58e40be7b2fc66ba3016e0191

--- testsuite/gnatprove/tests/U129-014__sqrt_error_bounds/test.py
+++ testsuite/gnatprove/tests/U129-014__sqrt_error_bounds/test.py
@@ -4,4 +4,6 @@ import os
 os.environ["SPARKLIB_OBJECT_DIR"] = TESTDIR
 
 if __name__ == "__main__":
-    prove_all(check_counterexamples=False, steps=1400, sparklib=True)
+    prove_all(
+        check_counterexamples=False, steps=1400, sparklib=True, opt=["--no-axiom-guard"]
+    )
