ecc87a7510256c4141b54f629f041225550df8db
Yannick Moy <moy@adacore.com>
2022-10-13 16:42:47 +0100

    VA05-041 Update test to make it more interesting
    Change-Id: I41933b2f829040e734f972d1fbb05c53b0c5ea99

--- testsuite/gnatprove/tests/VA05-041__ownership_and_relaxed_init/use_connection.ads
+++ testsuite/gnatprove/tests/VA05-041__ownership_and_relaxed_init/use_connection.ads
@@ -6,4 +6,15 @@ is
    type IO_Pulse_T is record
       Coid : Connection_Id_T;
    end record with Relaxed_Initialization;
+
+   procedure Init_Null (Pulse : in out IO_Pulse_T)
+   with
+     Post => Pulse.Coid'Initialized and then Pulse.Coid = Null_Connection,
+     Global => null,
+     Annotate => (GNATprove, Always_Return);
+
+   procedure Init (Pulse : in out IO_Pulse_T)
+   with
+     Global => null,
+     Annotate => (GNATprove, Always_Return);
 end;
