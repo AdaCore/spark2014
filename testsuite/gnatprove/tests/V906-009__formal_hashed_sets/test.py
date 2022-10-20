9fee719ffa8e3929a6002f211a14ebe8ca807d87
Johannes Kanig <kanig@adacore.com>
2022-10-20 17:41:54 +0900

    VA10-005 update testsuite
    * R312-016__steps: small steps
    
    Change-Id: I5c08c03cbd9e9e22707170a4e0e2743c9d130026

--- testsuite/gnatprove/tests/V906-009__formal_hashed_sets/test.py
+++ testsuite/gnatprove/tests/V906-009__formal_hashed_sets/test.py
@@ -5,12 +5,13 @@ import os
 contains_manual_proof = False
 os.environ["SPARKLIB_OBJECT_DIR"] = TESTDIR
 
+
 def replay():
-    prove_all (level=1, sparklib=True)
+    prove_all(level=2, procs=0, sparklib=True)
+
 
 if __name__ == "__main__":
     prove_all(replay=True, sparklib=True)
 
     call(["gprbuild", "-q", "-P", "test.gpr"])
     call(["./test"])
-
