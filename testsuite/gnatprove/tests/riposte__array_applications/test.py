b1b2e50b691878dd2a6fb904c74c04ebdaf44f67
Johannes Kanig <kanig@adacore.com>
2022-10-24 18:23:03 +0900

    VA10-005 update large tests
    Change-Id: Icf34cce0ae02503a5284b45663d095bb4b57ddeb

--- testsuite/gnatprove/tests/riposte__array_applications/test.py
+++ testsuite/gnatprove/tests/riposte__array_applications/test.py
@@ -4,7 +4,7 @@ contains_manual_proof = False
 
 
 def replay():
-    prove_all(procs=0, opt=["--function-sandboxing=off"])
+    prove_all(procs=0, opt=["--function-sandboxing=off"], level=2)
 
 
 if __name__ == "__main__":
