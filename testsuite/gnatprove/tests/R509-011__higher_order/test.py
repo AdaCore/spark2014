5363cffeac7c597eaf7a200750365910fff7fdd2
Johannes Kanig <kanig@adacore.com>
2022-11-03 18:22:20 +0900

    V927-006 update tests
    * prime_numbers: created VB03-004
    
    Change-Id: Idf4b35b487776965f0171bb897ef7cb098b231ce

--- testsuite/gnatprove/tests/R509-011__higher_order/test.py
+++ testsuite/gnatprove/tests/R509-011__higher_order/test.py
@@ -3,8 +3,9 @@ from test_support import prove_all, TESTDIR
 import os
 
 os.environ["SPARKLIB_OBJECT_DIR"] = TESTDIR
+# disable no_fail, see VB03-004
 prove_all(
-    no_fail=True,
+    no_fail=False,
     prover=["cvc5", "altergo", "z3"],
     steps=150000,
     opt=["-u", "test_higher_order.ads"],
