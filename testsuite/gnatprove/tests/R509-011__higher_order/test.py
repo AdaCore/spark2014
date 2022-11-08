63adfc97bcb481cc97f4e9709ab1484eb0105196
Johannes Kanig <kanig@adacore.com>
2022-11-08 19:02:54 +0900

    VB02-007 update testsuite for 23.1
    Change-Id: I590ba57b18cc1b342bd0bf8986f27ab96ba249d7

--- testsuite/gnatprove/tests/R509-011__higher_order/test.py
+++ testsuite/gnatprove/tests/R509-011__higher_order/test.py
@@ -5,6 +5,6 @@ os.environ["SPARKLIB_OBJECT_DIR"] = TESTDIR
 prove_all(
     no_fail=True,
     prover=["cvc5", "altergo", "z3"],
-    steps=16000,
+    steps=70000,
     opt=["-u", "test_higher_order.ads"],
 )
