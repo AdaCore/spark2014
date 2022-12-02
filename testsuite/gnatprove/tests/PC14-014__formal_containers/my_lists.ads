19d29b54f98f7c936118cd2a57de491bdd5f96f3
Claire Dross <dross@adacore.com>
2022-12-02 12:26:30 +0100

    V906-009 remove PC14-014__formal_containers
    As it is difficult to maintain and should more or less be subsumed
    by the tests submitted on this ticket.
    
    Change-Id: Ie6dbbc48ad0d724ad67a228f04e3312f17bc9be5

--- testsuite/gnatprove/tests/PC14-014__formal_containers/my_lists.ads
+++
@@ -1,9 +0,0 @@
-with SPARK.Containers.Formal.Doubly_Linked_Lists;
-
-package My_Lists with SPARK_Mode is
-   package M is new SPARK.Containers.Formal.Doubly_Linked_Lists (Positive);
-
-   package S is new M.Generic_Sorting;
-
-   procedure Test_List;
-end My_Lists;
