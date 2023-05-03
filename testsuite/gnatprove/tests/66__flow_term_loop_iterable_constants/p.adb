df7875869d23faf636ed4a08af846a9837b789a3
Joffrey Huguet <huguet@adacore.com>
2023-05-03 14:35:16 +0200

    Update tests outputs
    [changelog]
    * testsuite/gnatprove/tests/66__flow_term_loop_iterable_constants/
    Add new test.
    * testsuite/gnatprove/tests/*
    Update tests outputs.
    
    Issue: eng/spark/spark2014#66

---
+++ testsuite/gnatprove/tests/66__flow_term_loop_iterable_constants/p.adb
@@ -0,0 +1,45 @@
+with SPARK.Containers.Formal.Unbounded_Ordered_Maps;
+with SPARK.Containers.Functional.Sets;
+with SPARK.Big_Intervals; use SPARK.Big_Intervals;
+
+package body P with SPARK_Mode, Annotate => (GNATprove, Always_Return) is
+   procedure Formal_Maps is
+      package Maps is new SPARK.Containers.Formal.Unbounded_Ordered_Maps
+        (Key_Type       => Positive,
+         Element_Type => Integer);
+      type Map is new Maps.Map;
+      use all type Map;
+
+      M : Map;
+   begin
+      Insert (M, 1, 1);
+      declare
+         C : constant Map := M;
+      begin
+         for K of C loop
+            null;
+         end loop;
+      end;
+   end Formal_Maps;
+
+   procedure Functional_Sets is
+      package Sets is new SPARK.Containers.Functional.Sets
+        (Element_Type => Integer);
+      use Sets;
+
+      S : Set;
+   begin
+      S := Add (S, 1);
+
+      for Subset of Iterate (S) loop
+         null;
+      end loop;
+   end Functional_Sets;
+
+   procedure Big_Intervals is
+   begin
+      for I in Interval'(First => 1, Last => 2) loop
+         null;
+      end loop;
+   end Big_Intervals;
+end P;
---
+++ testsuite/gnatprove/tests/66__flow_term_loop_iterable_constants/soundness_tests.ads
@@ -0,0 +1,23 @@
+package Soundness_Tests with SPARK_Mode, Annotate => (GNATprove, Always_Return) is
+
+   procedure Selected_Component;
+
+   procedure Identifier_Expanded_Name;
+
+   procedure Explicit_Dereference;
+
+   procedure Indexed_Component;
+
+   procedure Slice;
+
+   procedure Type_Conversion;
+
+   procedure Qualified_Expression;
+
+   procedure Mix;
+
+   procedure Withed_Object;
+
+   procedure Callee;
+
+end Soundness_Tests;
