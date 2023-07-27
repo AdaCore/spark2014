bc0601f8028dcfc00f90b2973690bc3f3c88b6b1
Yannick Moy <moy@adacore.com>
2023-07-27 14:18:16 +0200

    Adapt tests on SPARKlib to using the same project file as users
    Removal of sparklib_internal.gpr means that tests should use sparklib.gpr
    instead, like tests on the use of SPARKlib.
    
    Issue: eng/spark/spark2014#55
    Depends-On: https://gitlab.adacore-it.com/eng/spark/sparklib/-/merge_requests/19
    Depends-On: https://gitlab.adacore-it.com/eng/spark/spark-internal-testsuite/-/merge_requests/115

--- testsuite/gnatprove/tests/V422-016__unbounded_functional_container/test.py
+++ testsuite/gnatprove/tests/V422-016__unbounded_functional_container/test.py
@@ -1,11 +1,11 @@
 from subprocess import call
-from test_support import prove_all, TESTDIR
+from test_support import prove_all
 import os
 
-os.environ["SPARKLIB_OBJECT_DIR"] = TESTDIR
+os.environ["SPARKLIB_BODY_MODE"] = "On"
 
 if __name__ == "__main__":
-    prove_all(steps=1300)
+    prove_all(steps=1300, sparklib=True)
 
     call(["gprbuild", "-q", "-P", "test.gpr"])
     call(["./obj/infinite_sequence"])
