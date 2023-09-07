e5fa963a29eac80d09ae0732b994aa64b593b4f3
Yannick Moy <moy@adacore.com>
2023-09-07 12:53:08 +0200

    Reject representation attributes on classwide values
    Use of attributes Size, Object_Size, Value_Size and Alignment on classwide
    values can lead to a dispatching call. Reject such odd uses for now as not
    yet supported, rather than crashing.
    
    Add corresponding limitation.
    Add corresponding test.

---
+++ testsuite/gnatprove/tests/309__classwide_repr/class.adb
@@ -0,0 +1,18 @@
+procedure Class with SPARK_Mode is
+   type T is tagged record
+      C : Natural;
+   end record;
+
+   procedure Test (X : T'Class; Y : out Natural) with Pre => True is
+   begin
+      Y := T'Class'Size;
+      Y := T'Class'Value_Size;
+      Y := T'Class'Object_Size;
+      Y := T'Class'Alignment;
+   end;
+
+   X : T := (C => 0);
+   Y : Natural;
+begin
+   Test (X, Y);
+end;
