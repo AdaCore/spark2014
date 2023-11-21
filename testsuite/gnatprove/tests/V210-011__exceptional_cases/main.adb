a5624d1ce467bef578e93707bd126d3114617cf7
Yannick Moy <moy@adacore.com>
2023-11-21 16:45:47 +0100

    Move check for SPARK RM 6.1.9(1) from GNAT to marking
    Legality check on presence of (in)out parameter in consequence of
    Exceptional_Cases is now performed in marking.
    
    Move test as well.

---
+++ testsuite/gnatprove/tests/V210-011__exceptional_cases/main.adb
@@ -0,0 +1,33 @@
+procedure Main with SPARK_Mode is
+
+   type Nat_Array is array (Positive range <>) of Natural;
+
+   Overflow : exception;
+   Index    : Positive;
+
+   procedure Incr_All_2 (A : in out Nat_Array) with
+     Post => (for all I in A'Range => A (I) = A'Old (I) + 1),
+     Exceptional_Cases =>
+       (Overflow => A'Old (Index) = Natural'Last
+        and then (for all I in A'Range =>
+                     A (I) = (if I < Index then A'Old (I) + 1 else A'Old (I))));
+
+   procedure Incr_All_2 (A : in out Nat_Array) is
+   begin
+      for I in A'Range loop
+         if A (I) = Natural'Last then
+            Index := I;
+            raise Overflow;
+         end if;
+
+         A (I) := A (I) + 1;
+         pragma Loop_Invariant
+           (for all J in A'First .. I => A'Loop_Entry (J) < Natural'Last);
+         pragma Loop_Invariant
+           (for all J in A'First .. I => A (J) = A'Loop_Entry (J) + 1);
+      end loop;
+   end Incr_All_2;
+
+begin
+   null;
+end Main;
