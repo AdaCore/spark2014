7e3c9139b8798674a038f87104900256c06d7190
Johannes Kanig <kanig@adacore.com>
2024-01-12 09:27:20 +0900

    Update copyright notices

--- src/why/gnat2why-expr-aggregates.adb
+++ src/why/gnat2why-expr-aggregates.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---                     Copyright (C) 2023-2023, AdaCore                     --
+--                     Copyright (C) 2023-2024, AdaCore                     --
 --                                                                          --
 -- gnat2why is  free  software;  you can redistribute  it and/or  modify it --
 -- under terms of the  GNU General Public License as published  by the Free --
