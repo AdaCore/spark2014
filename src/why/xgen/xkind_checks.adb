c5f0350d2710a1676bf44774f14541f90783baa0
Piotr Trojanek <trojanek@adacore.com>
2024-03-08 18:17:51 +0100

    Enable quantified expressions in validity checks

--- src/why/xgen/xkind_checks.adb
+++ src/why/xgen/xkind_checks.adb
@@ -94,19 +94,10 @@ package body Xkind_Checks is
                when Id_Some =>
                   PL (O, "(not Is_Empty (" & Node_Id_Param & ")");
 
-                  if False then
-                     PL (O,
-                         " and then (for all Element of Get_List ("
-                         & Node_Id_Param & ") =>");
-                     PL (O, Kind_Check (Prefix, Id_One) & " (Element)));");
-                  else
-                     PL (O, " and then True);");
-                     Relative_Indent (O, -2);
-                     PL (O, "--  ??? Partial implementation;");
-                     PL (O, "--  ??? universal quantif on containers "
-                         & "has not been implemented yet.");
-                     Relative_Indent (O, 2);
-                  end if;
+                  PL (O,
+                      " and then (for all Element of Get_List ("
+                      & Node_Id_Param & ") =>");
+                  PL (O, Kind_Check (Prefix, Id_One) & " (Element)));");
 
                when Id_Set =>
                   PL (O,
