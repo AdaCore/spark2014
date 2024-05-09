e806fe6c3daf2297532afd5d4c03d00de8bec1ca
Johannes Kanig <kanig@adacore.com>
2024-05-09 16:14:37 +0900

    Fix registry test to sort output

--- testsuite/gnatprove/tests/gnatprove_print-gpr-registry/test.py
+++ testsuite/gnatprove/tests/gnatprove_print-gpr-registry/test.py
@@ -1,3 +1,5 @@
-from subprocess import call
+from subprocess import check_output
+import json
 
-call(["gnatprove", "--print-gpr-registry"])
+data = json.loads(check_output(["gnatprove", "--print-gpr-registry"]))
+print(json.dumps(data, sort_keys=True, indent=4))
