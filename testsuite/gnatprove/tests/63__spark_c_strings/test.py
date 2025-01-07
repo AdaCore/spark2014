fda5dea864939faf23842e39db247c6a0590a6aa
Johannes Kanig <kanig@adacore.com>
2025-01-07 19:11:28 +0900

    Increase steps for c_string tests

--- testsuite/gnatprove/tests/63__spark_c_strings/test.py
+++ testsuite/gnatprove/tests/63__spark_c_strings/test.py
@@ -5,7 +5,7 @@ import os
 os.environ["SPARKLIB_BODY_MODE"] = "On"
 
 if __name__ == "__main__":
-    prove_all(sparklib=True, steps=1000)
+    prove_all(sparklib=True, steps=11000)
 
     gprbuild(["-q", "-P", "test.gpr"])
     call(["./obj/test"])
