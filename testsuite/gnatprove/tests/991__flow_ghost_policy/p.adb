3ba0ab601e8c4af3ba7b56b920df6c71b32cee8a
Piotr Trojanek <trojanek@adacore.com>
2025-07-18 11:04:58 +0200

    Extend test with checked ghost global only known by entity name
    This extension is meant to cover case, where the problematic global is only
    known by Entity_Name, which requires flow to store extra information in the
    ALI file.

--- testsuite/gnatprove/tests/991__flow_ghost_policy/p.adb
+++ testsuite/gnatprove/tests/991__flow_ghost_policy/p.adb
@@ -1,13 +1,15 @@
+with Q;
 procedure P with SPARK_Mode is
    pragma Assertion_Policy (Ghost => Check);
    X : Integer := 15 with Ghost;
-   procedure P with Ghost, Post => X = 0, Global => (Output => X);
+   procedure P with Ghost, Post => X = 0;
    procedure P is
    begin
       X := 0;
+      Q.Set_Ignored_Ghost_Object;
    end P;
    pragma Assertion_Policy (Ghost => Ignore);
-   procedure P2 with Ghost, Post => X = 0, Global => (Output => X);
+   procedure P2 with Ghost, Post => X = 0;
    procedure P2 is
    begin
       P;
