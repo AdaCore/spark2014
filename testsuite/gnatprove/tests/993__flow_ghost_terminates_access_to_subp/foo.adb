ad89e746418cc2a9da27f1f0c7fd2787d8175f56
Joffrey Huguet <huguet@adacore.com>
2025-07-18 16:49:38 +0200

    Refine condition in Check_Ghost_Terminates
    The previous condition to determine whether a called entity is
    ghost would miss the cases where the call is made through a
    ghost access-to-subprogram variable whose type is not ghost.
    
    This does not allow to support specialized functions, but
    they will be supported under #74.

--- testsuite/gnatprove/tests/993__flow_ghost_terminates_access_to_subp/foo.adb
+++ testsuite/gnatprove/tests/993__flow_ghost_terminates_access_to_subp/foo.adb
@@ -1,5 +1,13 @@
 procedure Foo is
    type Proc_Acc is access procedure (I : out Integer);
+   type Func_Acc is access function (I : Integer) return Boolean;
+
+   function F1 (I : Integer) return Boolean is (I = 0);
+   function F2 (I : Integer) return Boolean is
+      F2_Acc : Func_Acc := F2'Access;
+   begin
+      return F2_Acc.all (I);
+   end F2;
 
    procedure P (I : out Integer) is
       P_Acc : Proc_Acc := P'Access;
@@ -8,8 +16,13 @@ procedure Foo is
    end P;
 
    P_Acc : Proc_Acc := P'Access with Ghost;
+   F1_Acc : Func_Acc := F1'Access with Ghost;
+   F2_Acc : Func_Acc := F2'Access with Ghost;
 
    I : Integer with Ghost;
+   B1, B2 : Boolean with Ghost;
 begin
-   P_Acc.all (I);
+   P_Acc.all (I);          --@TERMINATION:FAIL
+   B1 := F1_Acc.all (1);   --@TERMINATION:FAIL
+   B2 := F2_Acc.all (2);   --@TERMINATION:FAIL
 end Foo;
