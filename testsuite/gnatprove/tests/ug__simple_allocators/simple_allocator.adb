7435330e7026c35ef3d1624bc0a2ce5dc49d3195
Claire Dross <dross@adacore.com>
2025-07-21 16:36:05 +0200

    Add a section for Best Practices in the UG

---
+++ testsuite/gnatprove/tests/ug__simple_allocators/simple_allocator.adb
@@ -0,0 +1,98 @@
+with Ada.Containers; use type Ada.Containers.Count_Type;
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use Ada.Numerics.Big_Numbers.Big_Integers;
+
+package body Simple_Allocator with
+  SPARK_Mode,
+  Refined_State => (State => Data)
+is
+   package Big_From_Resource is new Signed_Conversions
+     (Int => Resource);
+
+   function Big (R : Resource) return Big_Integer renames
+     Big_From_Resource.To_Big_Integer;
+
+   type Status is (Available, Allocated);
+
+   type A is array (Valid_Resource) of Status;
+
+   Data : A := (others => Available);
+
+   function Is_Available (Res : Resource) return Boolean is
+     (Res = No_Resource or else Data (Res) = Available);
+   function Is_Allocated (Res : Resource) return Boolean is
+     (Res = No_Resource or else Data (Res) = Allocated);
+   function All_Available return Boolean is
+     (for all R in Valid_Resource => Data (R) = Available);
+
+   package body M with
+     Annotate => (GNATprove, Unhide_Info, "Package_Body")
+   is
+
+      function Is_Valid (M : T) return Boolean is
+        ((for all E of M.Available => E in Valid_Resource)
+            and then
+         (for all E of M.Allocated => E in Valid_Resource)
+            and then
+         (for all R in Valid_Resource =>
+            (declare
+               In_Avail : constant Boolean := Contains (M.Available, R);
+               In_Alloc : constant Boolean := Contains (M.Allocated, R);
+             begin
+               (case Data (R) is
+                   when Available => In_Avail and not In_Alloc,
+                   when Allocated => not In_Avail and In_Alloc))));
+
+      function Model return T is
+         Avail : Set;
+         Alloc : Set;
+      begin
+         for R in Valid_Resource loop
+            case Data (R) is
+               when Available => Avail := Add (Avail, R);
+               when Allocated => Alloc := Add (Alloc, R);
+            end case;
+            pragma Loop_Invariant
+              (Length (Avail) + Length (Alloc) = Big (R)
+                  and then
+               (for all E of Avail => E in 1 .. R)
+                  and then
+               (for all E of Alloc => E in 1 .. R)
+                  and then
+               (for all RR in 1 .. R =>
+                 (declare
+                    In_Avail : constant Boolean := Contains (Avail, RR);
+                    In_Alloc : constant Boolean := Contains (Alloc, RR);
+                  begin
+                    (case Data (RR) is
+                       when Available => In_Avail and not In_Alloc,
+                       when Allocated => not In_Avail and In_Alloc))));
+         end loop;
+         return T'(Available => Avail, Allocated => Alloc);
+      end Model;
+
+   end M;
+
+   procedure Alloc (Res : out Resource) is
+   begin
+      for R in Valid_Resource loop
+         if Data (R) = Available then
+            Data (R) := Allocated;
+            Res := R;
+            return;
+         end if;
+         pragma Loop_Invariant
+           (Data = Data'Loop_Entry
+            and then (for all RR in 1 .. R => Data (RR) = Allocated));
+      end loop;
+      Res := No_Resource;
+   end Alloc;
+
+   procedure Free (Res : Resource) is
+   begin
+      if Res /= No_Resource and then Data (Res) = Allocated then
+         Data (Res) := Available;
+      end if;
+   end Free;
+
+end Simple_Allocator;
---
+++ testsuite/gnatprove/tests/ug__simple_allocators/simple_allocator.ads
@@ -0,0 +1,90 @@
+pragma Unevaluated_Use_Of_Old (Allow);
+with SPARK.Containers.Functional.Sets;
+with Ada.Containers;
+
+package Simple_Allocator with
+  SPARK_Mode,
+  Abstract_State    => State,
+  Initializes       => State,
+  Initial_Condition => All_Available
+is
+   Capacity : constant := 10_000;
+
+   type Resource is new Integer range 0 .. Capacity;
+   subtype Valid_Resource is Resource range 1 .. Capacity;
+
+   No_Resource : constant Resource := 0;
+
+   function Is_Available (Res : Resource) return Boolean;
+   function Is_Allocated (Res : Resource) return Boolean;
+
+   function All_Available return Boolean with Ghost;
+
+   package M with Ghost is
+
+      package S is new SPARK.Containers.Functional.Sets
+        (Element_Type => Resource, Equivalent_Elements => "=");
+      use S;
+
+      type T is record
+         Available : Set;
+         Allocated : Set;
+      end record;
+
+      function "=" (X, Y : T) return Boolean is
+        (X.Available = Y.Available
+           and then
+         X.Allocated = Y.Allocated);
+
+      function Is_Add
+        (S : Set; E : Resource; Result : Set) return Boolean
+      --  Returns True if Result is S plus E.
+
+      is
+        (not Contains (S, E)
+         and then Contains (Result, E)
+         and then Included_Except (Result, S, E)
+         and then S <= Result);
+
+      function Is_Valid (M : T) return Boolean;
+
+      function Model return T with Post => Is_Valid (Model'Result);
+
+   end M;
+
+   use M; use M.S;
+
+   procedure Alloc (Res : out Resource) with
+     Contract_Cases =>
+
+       --  When no resource is available, return the special value No_Resource
+       --  with the allocator unmodified.
+
+       (Is_Empty (Model.Available) =>
+          Res = No_Resource
+            and then
+          Model = Model'Old,
+
+        --  Otherwise, return an available resource which becomes allocated
+
+        others =>
+          Is_Add (Model.Available, Res, Result => Model.Available'Old)
+            and then
+          Is_Add (Model.Allocated'Old, Res, Result => Model.Allocated));
+
+   procedure Free (Res : Resource) with
+     Contract_Cases =>
+
+       --  When the resource is allocated, make it available
+
+       (Contains (Model.Allocated, Res) =>
+          Is_Add (Model.Available'Old, Res, Result => Model.Available)
+            and then
+          Is_Add (Model.Allocated, Res, Result => Model.Allocated'Old),
+
+        --  Otherwise, do nothing
+
+        others =>
+          Model = Model'Old);
+
+end Simple_Allocator;
