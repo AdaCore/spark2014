89ede444071519545e9504a536dda2b7aad76c45
Piotr Trojanek <trojanek@adacore.com>
2025-09-11 10:56:48 +0200

    Remove leading whitespace in RM examples and adjust outputs

--- testsuite/gnatprove/tests/RM_Examples/r.ads
+++ testsuite/gnatprove/tests/RM_Examples/r.ads
@@ -1,13 +1,13 @@
-   package R
-     with Abstract_State => R1
-   is
-      -- R1 may be denoted here
-      procedure Init_R1
-        with Global  => (Output => R1),
-             Depends => (R1 => null);
+package R
+  with Abstract_State => R1
+is
+   -- R1 may be denoted here
+   procedure Init_R1
+     with Global  => (Output => R1),
+          Depends => (R1 => null);
 
-      procedure Op_1 (I : in Integer)
-        with Global   => (In_Out => R1),
-	     Depends  => (R1 =>+ I),
-	     Always_Terminates;
-   end R;
+   procedure Op_1 (I : in Integer)
+     with Global  => (In_Out => R1),
+          Depends => (R1 =>+ I),
+          Always_Terminates;
+end R;
