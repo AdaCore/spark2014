cc5eea2b1395a632b034baff8a05596596584c93
Claire Dross <dross@adacore.com>
2025-10-21 09:20:44 +0200

    Avoid introducing sliding on record component update
    When updating a record component, it can happen that the expected it
    and the effective type of the component do not match when the effective
    type is a bounded Itype introduced by the frontend for a discriminant
    dependent component. In this case, sliding is never necessary, and
    trying to introduce it might cause the tool to crash as it encounters
    discriminants for which there is no value in the context.

--- testsuite/gnatprove/tests/1090__discr_crash/main.adb
+++ testsuite/gnatprove/tests/1090__discr_crash/main.adb
@@ -5,8 +5,6 @@ is
 
    type Ar is array (Index_Type range <>) of Integer;
 
-   type D_Enum is (A, B);
-
    type State_T (F, L : Index_Type) is record
       Sources : Ar (F .. L);
    end record;
@@ -15,7 +13,20 @@ is
                      State_After  : out State_T)
    is
    begin
-      State_After.Sources := State_Before.Sources;
+      State_After.Sources := State_Before.Sources; --@LENGTH_CHECK:FAIL
+   end;
+
+   procedure Update_2 (State_Before : State_T;
+                       State_After  : out State_T) with
+     Pre => State_Before.L >= State_Before.F
+       and State_Before.L - State_Before.F = State_After.L - State_After.F;
+
+   procedure Update_2 (State_Before : State_T;
+                       State_After  : out State_T)
+   is
+   begin
+      State_After.Sources := State_Before.Sources; --@LENGTH_CHECK:PASS
+      pragma Assert (State_After.Sources (State_After.F) = State_Before.Sources (State_Before.F)); --@ASSERT:PASS
    end;
 
 begin
