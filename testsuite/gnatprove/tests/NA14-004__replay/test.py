02a838946caee95bc00ee12b40aae3d962b32b6b
Johannes Kanig <kanig@adacore.com>
2025-11-03 11:45:33 +0900

    remove useless 'contains_manual_proof'

--- testsuite/gnatprove/tests/NA14-004__replay/test.py
+++ testsuite/gnatprove/tests/NA14-004__replay/test.py
@@ -1,7 +1,5 @@
 from test_support import prove_all
 
-contains_manual_proof = False
-
 prove_all()
 # replaying GNATprove should issue exactly the same messages
 prove_all()
--- testsuite/gnatprove/tests/V708-031__functional_container_choose/test.py
+++ testsuite/gnatprove/tests/V708-031__functional_container_choose/test.py
@@ -2,7 +2,6 @@ from subprocess import call
 from test_support import prove_all, gprbuild
 import os
 
-contains_manual_proof = False
 os.environ["SPARKLIB_BODY_MODE"] = "On"
 
 if __name__ == "__main__":
