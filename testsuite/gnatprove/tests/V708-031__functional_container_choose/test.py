fdcf0f0423879bbb62221cba56a23291ec9ad4c6
Johannes Kanig <kanig@adacore.com>
2025-11-06 12:05:13 +0900

    Update large test scripts for sparklib body mode

--- testsuite/gnatprove/tests/V708-031__functional_container_choose/test.py
+++ testsuite/gnatprove/tests/V708-031__functional_container_choose/test.py
@@ -1,11 +1,8 @@
 from subprocess import call
 from test_support import prove_all, gprbuild
-import os
-
-os.environ["SPARKLIB_BODY_MODE"] = "On"
 
 if __name__ == "__main__":
-    prove_all(sparklib=True)
+    prove_all(sparklib=True, sparklib_bodymode=True)
 
     gprbuild(["-q", "-P", "test.gpr"])
     call(["./obj/test"])
