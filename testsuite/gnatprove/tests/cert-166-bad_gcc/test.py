0acc5fc74fba252d96210d2ed0a521544c85c9eb
Piotr Trojanek <trojanek@adacore.com>
2025-11-10 18:27:38 +0100

    Add test to cover detection of invalid data representation files
    This test is needed both for code coverage and tool qualification.
    
    [changelog]
    * src/why/gnat2why-data_decomposition.adb
    (Read_Data_Decomposition_JSON_File): Remove coverage exemption.

---
+++ testsuite/gnatprove/tests/cert-166-bad_gcc/test.py
@@ -0,0 +1,14 @@
+from test_support import prove_all, TESTDIR
+import os
+
+# Create path to the fake broken compiler
+bad_gnat_path = TESTDIR + os.sep + "bad-gcc"
+
+with open("test.gpr", "w") as f:
+    f.write("project Test is\n")
+    f.write("   package Compiler is\n")
+    f.write('   for Driver ("Ada") use "' + bad_gnat_path + '";')
+    f.write("   end Compiler;\n")
+    f.write("end Test;")
+
+prove_all()
