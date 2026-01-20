34ccaae833b69c8d1f4413dcb06336196cc678b4
Johannes Kanig <kanig@adacore.com>
2026-01-20 18:00:04 +0900

    Disable cache on replay test
    This test often fails in CI, apparently due to cache problems.
    Deactivating cache on this test.

--- testsuite/gnatprove/tests/S513-002__replay/test.py
+++ testsuite/gnatprove/tests/S513-002__replay/test.py
@@ -6,10 +6,12 @@ os.environ["SPARKLIB_OBJECT_DIR"] = TESTDIR
 prove_all(
     opt=["--level=4", "--function-sandboxing=off", "--counterexamples=off"],
     sparklib=True,
+    cache_allowed=False,
 )
 print("--------------------------------------")
 prove_all(
     opt=["--function-sandboxing=off", "--counterexamples=off"],
     replay=True,
     sparklib=True,
+    cache_allowed=False,
 )
