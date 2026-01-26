ccd5bf30cf63bdd3b9e62dd8cf59fbf4686c1934
Piotr Trojanek <trojanek@adacore.com>
2026-01-27 00:31:36 +0100

    Adapt test for broken GCC to work on Windows
    [changelog]
    * testsuite/gnatprove/lib/python/test_support.py
    (is_windows_platform): Refactor reusable logic from sleep().
    (sleep): Use refactored routine.
    * testsuite/gnatprove/tests/cert-166-bad_gcc/
    Adapt test to Windows.

--- testsuite/gnatprove/tests/cert-166-bad_gcc/test.py
+++ testsuite/gnatprove/tests/cert-166-bad_gcc/test.py
@@ -1,14 +1,22 @@
-from test_support import prove_all, TESTDIR
+from test_support import prove_all, is_windows_platform, TESTDIR
 import os
+import sys
 
-# Create path to the fake broken compiler. GPR reguired an absolute path,
+# Create path to the fake broken compiler. GPR requires an absolute path,
 # which depends on where this test is instantiated.
-bad_gnat_path = TESTDIR + os.sep + "bad-gcc"
+
+platform = sys.platform
+if is_windows_platform():
+    bad_gnat_exec = "bad-gcc.bat"
+else:
+    bad_gnat_exec = "bad-gcc"
+
+bad_gnat_path = TESTDIR + os.sep + bad_gnat_exec
 
 with open("test.gpr", "w") as f:
     f.write("project Test is\n")
     f.write("   package Compiler is\n")
-    f.write('   for Driver ("Ada") use "' + bad_gnat_path + '";')
+    f.write('   for Driver ("Ada") use "' + bad_gnat_path + '";\n')
     f.write("   end Compiler;\n")
     f.write("end Test;")
 
