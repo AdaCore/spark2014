a6df65cad1de27a8ea68faa80d49da78467f64ed
Vincent Jicquel <jicquel@adacore.com>
2026-02-13 15:54:08 +0100

    Parse ALI files upon Global_Gen action completion to discover new dependencies
    When a Global_Gen action completes, its ALI file becomes available for parsing.
    This parsing step may reveal additional dependencies, requiring the creation of
    new Global_Gen actions to generate the corresponding ALI files.
    
    If a newly discovered dependency belongs to an externally built view and its
    ALI file already exists, the corresponding action is still added to the tree
    to ensure the ALI file is parsed.
    
    Issue: eng/gpr/gpr-issues#769
    
    Co-Authored-by: Johannes Kanig <kanig@adacore.com>

--- src/gnatprove/gpr2-build-actions-compile-ada-global_gen.adb
+++ src/gnatprove/gpr2-build-actions-compile-ada-global_gen.adb
@@ -1,7 +1,10 @@
 with Configuration;
 with GNAT.OS_Lib;
 with GNATCOLL.VFS; use GNATCOLL.VFS;
+with GPR2.Build.Actions.Compile.Ada.Analysis;
 with GPR2.Build.Artifacts.Object_File;
+with GPR2.Message;
+with GPR2.Source_Reference;
 
 package body GPR2.Build.Actions.Compile.Ada.Global_Gen is
 
@@ -68,17 +71,89 @@ package body GPR2.Build.Actions.Compile.Ada.Global_Gen is
       Stdout : Unbounded_String := Null_Unbounded_String;
       Stderr : Unbounded_String := Null_Unbounded_String) return Boolean
    is
-      use GNAT.OS_Lib;
-      Success : Boolean := True;
-      View    : constant Project.View.Object := Self.CU.Owning_View;
-   begin
-      --  ??? Do we need to call the parent Post_Command?
 
-      --  In library projects, copy the generated .ali to the library dir
-      if View.Is_Library then
+      function Add_Global_Gen_Dep (Unit : Name_Type) return Boolean;
+      --  Create a global generation action for the given unit and add its ALI
+      --  as an input to the analysis actions that depend on the current ALI
+      --  file.
+
+      ---------------------------
+      -- Create_Global_Gen_Dep --
+      ---------------------------
+
+      function Add_Global_Gen_Dep (Unit : Name_Type) return Boolean is
+
+         CU     : Compilation_Unit.Object;
+         GG_Act : GPR2.Build.Actions.Compile.Ada.Global_Gen.Object;
+      begin
+         CU := Self.Ctxt.Namespace_Roots.First_Element.Unit (Unit);
+
+         if not CU.Is_Defined then
+            return True;
+         end if;
+
+         declare
+            GG_Id : constant Global_Gen_Id := Create (CU);
+         begin
+            if not Self.Tree.Has_Action (GG_Id) then
+               GG_Act.Initialize (CU, Global_Gen_Opt_File (CU.Owning_View));
+
+               --  Externally built units allow missing ALIs, because they are
+               --  optional to analysis actions.
+               --  Non-external units must have a valid ALI file.
+
+               if CU.Owning_View.Is_Externally_Built
+                 and then not GG_Act.Lib_Ali_File.Path.Exists
+               then
+                  return True;
+               end if;
+
+               if not Self.Tree.Add_Action (GG_Act) then
+                  return False;
+               end if;
+            else
+               GG_Act :=
+                 GPR2.Build.Actions.Compile.Ada.Global_Gen.Object
+                   (Self.Tree.Action (GG_Id));
+            end if;
+         end;
+
+         --  Make sure that Analysis actions depend on the full closure of
+         --  ALIs, which means if GGen A is used by analysis A, and GGen A
+         --  depends on GGen B, then Analysis A should also depend on GGen B.
+
+         for Succ of Self.Tree.Successors (Self.Lib_Ali_File) loop
+            if Succ in Actions.Compile.Ada.Analysis.Object'Class then
+               --  Ensure that GG_Act is always executed before the analysis
+               --  action, even if the GG_Act is externally built, as we need
+               --  its ALI parsing to be done in the Post_Command.
+               Self.Tree.Add_Input (Succ.UID, GG_Act.Lib_Ali_File, True);
+
+               --  Add the ALI file to be used by the analysis action.
+               Actions.Compile.Ada.Analysis.Object
+                 (Self.Tree.Action_Id_To_Reference (Succ.UID).Element.all)
+                 .ALI_Files
+                 .Include (GG_Act.Lib_Ali_File);
+            end if;
+         end loop;
+
+         return True;
+      end Add_Global_Gen_Dep;
+
+      use GNAT.OS_Lib;
+      View : constant Project.View.Object := Self.CU.Owning_View;
+   begin
+      if Status /= Success then
+         return True;
+      end if;
+
+      if View.Is_Library and then not View.Is_Externally_Built then
+         --  In library projects, copy the generated .ali to the library dir
+
          declare
             Lib_Dir : Virtual_File renames
               View.Library_Ali_Directory.Virtual_File;
+            Success : Boolean;
          begin
             if View.Kind not in With_Object_Dir_Kind
               or else View.Object_Directory.Virtual_File /= Lib_Dir
@@ -89,11 +164,55 @@ package body GPR2.Build.Actions.Compile.Ada.Global_Gen is
                   Lib_Dir.Display_Full_Name,
                   Success,
                   Mode => Overwrite);
+
+               if not Success then
+                  Self.Tree.Reporter.Report
+                    (GPR2.Message.Create
+                       (GPR2.Message.Error,
+                        "failed to copy the ALI file to the library directory",
+                        GPR2.Source_Reference.Object
+                          (GPR2.Source_Reference.Create
+                             (Self.Dep_File.Path.Value, 0, 0))));
+                  return False;
+               end if;
             end if;
          end;
       end if;
-      pragma Assert (Success, "Failed to copy .ali file to library directory");
-      return Success;
+
+      Self.ALI_Object := GPR2.Build.ALI_Parser.Create (Self.Lib_Ali_File.Path);
+
+      if Self.ALI_Object.Path_Name.Exists then
+         Self.ALI_Object.Parse;
+
+         if not Self.ALI_Object.Is_Parsed then
+            Self.Tree.Reporter.Report
+              (GPR2.Message.Create
+                 (GPR2.Message.Error,
+                  "failed to analyze the ALI file",
+                  GPR2.Source_Reference.Object
+                    (GPR2.Source_Reference.Create
+                       (Self.ALI_Object.Path_Name.Value, 0, 0))));
+            return False;
+         end if;
+
+         for Dep of Self.Withed_Units loop
+            if not Add_Global_Gen_Dep (Dep) then
+               Self.Tree.Reporter.Report
+                 (GPR2.Message.Create
+                    (GPR2.Message.Error,
+                     "failed to create a Global_Gen action for the dependency"
+                     & " unit "
+                     & String (Dep)
+                     & " obtained after ALI parsing",
+                     GPR2.Source_Reference.Object
+                       (GPR2.Source_Reference.Create
+                          (Self.ALI_Object.Path_Name.Value, 0, 0))));
+               return False;
+            end if;
+         end loop;
+      end if;
+
+      return True;
    end Post_Command;
 
    overriding
