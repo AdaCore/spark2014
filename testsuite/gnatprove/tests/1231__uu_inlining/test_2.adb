3e995aff0045dd82c11caac3601497e165ba04ed
Claire Dross <dross@adacore.com>
2026-02-19 17:49:49 +0100

    File tests for UU and inlining

---
+++ testsuite/gnatprove/tests/1231__uu_inlining/test_2.adb
@@ -0,0 +1,29 @@
+procedure Test_2 with SPARK_Mode is
+
+   type My_Rec (Is_Int : Boolean := False) is record
+      case Is_Int is
+         when True =>
+            F_Int : Integer;
+         when False =>
+            F_Float : Float;
+      end case;
+   end record;
+
+   type My_Rec_2 is new My_Rec with
+     Unchecked_Union;
+
+   procedure Callee (X : in out My_Rec) with Post => True is
+      B : constant Boolean := X.Is_Int;
+   begin
+      null;
+   end Callee;
+
+   procedure P (X : in out My_Rec_2) is
+   begin
+     Callee (My_Rec (X)); -- @UU_RESTRICTION:FAIL
+   end P;
+
+   X : My_Rec_2 (True) := (True, 13);
+begin
+   P (X);
+end;
