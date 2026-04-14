19c0abe84b383836c1eabc052c543ef592f6b796
Johannes Kanig <kanig@adacore.com>
2026-04-14 18:41:40 +0900

    Fix way of passing gpr file in test py

--- testsuite/gnatprove/tests/O706-003__coq_steps/test.py
+++ testsuite/gnatprove/tests/O706-003__coq_steps/test.py
@@ -2,11 +2,11 @@ from test_support import prove_all
 
 prove_all(
     prover=["coq"],
-    opt=["-P", "test.gpr", "--steps=1"],
+    opt=["--steps=1"],
     filter_output=".*Grammar extension",
 )
 prove_all(
     prover=["coq"],
-    opt=["-P", "test.gpr", "--steps=1"],
+    opt=["--steps=1"],
     filter_output=".*Grammar extension",
 )
