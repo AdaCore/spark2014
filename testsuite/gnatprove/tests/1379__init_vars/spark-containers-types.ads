5f787aae877db67dee38cfab53622d1857aa1b2c
Piotr Trojanek <trojanek@adacore.com>
2026-06-27 00:40:56 +0200

    Minimize reproducer and rename it with the __flow prefix
    This test exposes a bug that only occurs with a certain order of iteration over
    a hashed container (where the hash uses Node_Ids), so it is very fragile.

--- testsuite/gnatprove/tests/1379__init_vars/spark-containers-types.ads
+++
@@ -1,9 +0,0 @@
---
---  Copyright (C) 2022-2026, AdaCore
---
---  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
---
-
-with Ada.Containers;
-
-package SPARK.Containers.Types renames Ada.Containers;
